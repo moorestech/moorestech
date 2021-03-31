7ac626d0bf6335d80d905abb1d4109724f914d79
sakastudio <sakastudio100@gmail.com>
2021-03-31 14:50:53 +0900

    Machineのユニットテストの追加

--- industrialization-tdd/Test/MacineTest.cs
+++ industrialization-tdd/Test/MacineTest.cs
@@ -1,16 +1,27 @@
-﻿using NUnit.Framework;
+﻿using System;
+using industrialization.Installation.BeltConveyor;
+using industrialization.Installation.Machine;
+using industrialization.Item;
+using NUnit.Framework;
 
 namespace industrialization.Test
 {
     public class MacineTest
     {
-        public class Tests
+        [SetUp]
+        public void Setup()
         {
-            [Test]
-            public void Test1()
-            {
-                Assert.True(true);
-            }
+        }
+
+        [Test]
+        public void Test1()
+        {
+            int ID = 10;
+            var machine = new Macine(0,Guid.Empty,new BeltConveyor(0, Guid.Empty));
+            machine.InsertItem(new ItemStack(10,1));
+            var outputItem = machine.GetInventory().ItemStacks[1].ID;
+            
+            Assert.AreEqual(ID,outputItem);
         }
     }
 }
