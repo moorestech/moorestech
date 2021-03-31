b732d6b27f3bb4efd41d87e7df1c7f9c6ceefdc7
sakastudio <sakastudio100@gmail.com>
2021-03-31 14:51:03 +0900

    ベルトコンベアの仮実装

---
+++ industrialization-tdd/Installation/BeltConveyor/BeltConveyor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using industrialization.Inventory;
+using industrialization.Item;
+
+namespace industrialization.Installation.BeltConveyor
+{
+    //TODO ベルトコンベアの仮コードを直す
+    public class BeltConveyor : InstallationBase, IInstallationInventory, IBeltConveyor
+    {
+        private InventoryData inventoryData;
+        public BeltConveyor(int installationId, Guid guid) : base(installationId, guid)
+        {
+            inventoryData = new InventoryData(10);
+            GUID = guid;
+            InstallationID = installationId;
+        }
+
+        public bool InsertItem(ItemStack itemStack)
+        {
+            return true;
+        }
+
+        public InventoryData GetInventory()
+        {
+            return inventoryData;
+        }
+
+        public BeltConveyorState GetState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FlowItem()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
