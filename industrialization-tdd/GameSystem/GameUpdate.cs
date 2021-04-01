ffe85a4b25f2014e9daf514586b864c574aeebf4
sakastudio <sakastudio100@gmail.com>
2021-04-01 09:14:41 +0900

    Update関数を追加

---
+++ industrialization-tdd/GameSystem/GameUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace industrialization_tdd.GameSystem
+{
+    public class GameUpdate
+    {
+        public delegate void Update();
+
+        public static event Update UpdateEvent;
+
+        public static void StartUpdate()
+        {
+            Task.Run(ExeUpdate);
+        }
+
+        static void ExeUpdate()
+        {
+            while (true)
+            {
+                UpdateEvent();
+            }
+        }
+    }
+}
