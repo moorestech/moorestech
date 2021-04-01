161be98e177edee0dda09b964f459d55bc917de6
sakastudio <sakastudio100@gmail.com>
2021-04-01 13:24:12 +0900

    設置物configファイル、config読み込みクラスとユニットテストの作成

---
+++ industrialization-tdd/Installation/InstallationConfig.cs
@@ -0,0 +1,43 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace industrialization.Installation
+{
+    public class InstallationConfig
+    {
+        private const string configPath = "";
+        private static InstallationData[] _machineDatas;
+
+        public static InstallationData GetMachineData(int id)
+        {
+            if (_machineDatas == null)
+            {
+                //JSONデータの読み込み
+                var json = File.ReadAllText(configPath);
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes((json)));
+                ms.Seek(0, SeekOrigin.Begin);
+                var serializer = new DataContractJsonSerializer(typeof(installationJson));
+                var data = serializer.ReadObject(ms) as installationJson;
+            }
+
+            return null;
+        }
+        
+    }
+
+    [DataContract] 
+    public class installationJson
+    {
+        [DataMember(Name = "installations")]
+        public InstallationData[] installations;
+    }
+    public class InstallationData
+    {
+        [DataMember(Name = "name")]
+        public string name;
+        [DataMember(Name = "inventorySlots")]
+        public int inventorySlot;
+    }
+}
---
+++ industrialization-tdd/Test/InstallationConfigTest.cs
@@ -0,0 +1,21 @@
+﻿using industrialization.Installation;
+using NUnit.Framework;
+
+namespace industrialization.Test
+{
+    public class InstallationConfigTest
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            string name = InstallationConfig.GetMachineData(0).name;
+            Assert.AreEqual("TestMachine1","");
+        }
+    }
+}
