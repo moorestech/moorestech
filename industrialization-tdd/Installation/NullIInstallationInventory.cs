b8540774631a1aae57a85229c61c984d75fed946
sakastudio <sakastudio100@gmail.com>
2021-04-03 22:56:16 +0900

    Nullパターン追加

---
+++ industrialization-tdd/Installation/NullIInstallationInventory.cs
@@ -0,0 +1,18 @@
+﻿using industrialization.Inventory;
+using industrialization.Item;
+
+namespace industrialization.Installation
+{
+    public class NullIInstallationInventory : IInstallationInventory
+    {
+        public bool InsertItem(ItemStack itemStack)
+        {
+            return false;
+        }
+
+        public InventoryData GetInventory()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
