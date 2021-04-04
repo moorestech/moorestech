6377dc9371ad6b822b33b6f1918d15cd560f2d42
sakastudio <sakastudio100@gmail.com>
2021-04-05 02:02:54 +0900

    ItemStackとアイテムの加減算を実装

---
+++ industrialization-tdd/Item/ItemProcessResult.cs
@@ -0,0 +1,19 @@
+﻿namespace industrialization.Item
+{
+    public class ItemProcessResult
+    {
+        private IItemStack mineItemStack;
+        private IItemStack receiveItemStack;
+
+
+        public ItemProcessResult(IItemStack mineItemStack, IItemStack receiveItemStack)
+        {
+            this.mineItemStack = mineItemStack;
+            this.receiveItemStack = receiveItemStack;
+        }
+
+        public IItemStack ReceiveItemStack => receiveItemStack;
+
+        public IItemStack MineItemStack => mineItemStack;
+    }
+}
