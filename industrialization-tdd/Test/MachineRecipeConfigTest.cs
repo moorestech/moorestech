79203716302f10bbd12209da048a71dead63d037
sakastudio <sakastudio100@gmail.com>
2021-04-04 23:36:33 +0900

    レシピコンフィグテストケース追加

--- industrialization-tdd/Test/MachineRecipeConfigTest.cs
+++ industrialization-tdd/Test/MachineRecipeConfigTest.cs
@@ -32,7 +32,9 @@ namespace industrialization.Test
         [TestCase(0,new int[1]{0},1)]
         [TestCase(0,new int[1]{2},1)]
         [TestCase(1,new int[2]{1,2},3)]
+        [TestCase(1,new int[2]{2,1},3)]
         [TestCase(3,new int[3]{1,2,0},5)]
+        [TestCase(3,new int[3]{2,1,0},5)]
         public void RecipeInputItemInstallationIDTest(int installtionID, int[] items,int output0ID)
         {
             int ans = MachineRecipeConfig.GetRecipeData(installtionID, items.ToList()).ItemOutputs[0].ItemId;
