9aac5a8beb51e1c9d476543b687fe0ea29c8d83a
sakastudio <sakastudio100@gmail.com>
2021-04-08 13:32:37 +0900

    設置物コンフィグのインプット、アウトプットスロット作成　機械がプロセスをスタートする処理を作成

---
+++ industrialization/Util/UnixTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace industrialization.Util
+{
+    public static class UnixTime
+    {  
+        private static DateTime UNIX_EPOCH =
+            new DateTime(1970, 1, 1, 0, 0, 0, 0);
+
+        public static long GetNowUnixTime()
+        {
+            DateTime targetTime = DateTime.Now;
+            // UTC時間に変換
+            targetTime = targetTime.ToUniversalTime();
+
+            // UNIXエポックからの経過時間を取得
+            TimeSpan elapsedTime = targetTime - UNIX_EPOCH;
+   
+            // 経過秒数に変換
+            return (long)elapsedTime.TotalSeconds;
+        }
+    }
+}
