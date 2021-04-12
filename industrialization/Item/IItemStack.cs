4dc6eca0ee1a7b10fe68200789c5bdcc2260f22d
sakastudio <sakastudio100@gmail.com>
2021-04-13 00:22:23 +0900

    なんかわからんけど配列の格納で詰まる

--- industrialization/Item/IItemStack.cs
+++ industrialization/Item/IItemStack.cs
@@ -6,5 +6,6 @@
         int Amount { get; }
         ItemProcessResult AddItem(IItemStack receiveItemStack);
         IItemStack SubItem(int subAmount);
+        bool CanAdd(IItemStack item);
     }
 }
