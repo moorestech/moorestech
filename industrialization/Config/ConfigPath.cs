3ab61a948961adf6d948605fccdc60a9c9841789
sakastudio <sakastudio100@gmail.com>
2021-04-13 22:52:57 +0900

    ConfigPathの追跡終了

--- industrialization/Config/ConfigPath.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace industrialization.Config
-{
-    public class ConfigPath
-    {
-        public const string InstallationConfigPath = "C:\\Users\\satou\\RiderProjects\\industrialization\\industrialization\\Config\\Json\\installation.json";
-        public const string RecipeConfigPath = "C:\\Users\\satou\\RiderProjects\\industrialization\\industrialization\\Config\\Json\\macineRecipe.json";
-    }
-}
