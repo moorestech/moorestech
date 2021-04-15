40a9e90bf0b2412f1bf2fd2155b0fcd6b5494575
sakastudio <sakastudio100@gmail.com>
2021-04-15 19:30:13 +0900

    使ってないクラスの削除

--- industrialization/Installation/BeltConveyor/BeltConveyor.cs
+++ industrialization/Installation/BeltConveyor/BeltConveyor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using industrialization.Inventory;
 using industrialization.Item;
 
 namespace industrialization.Installation.BeltConveyor
@@ -8,10 +7,8 @@ namespace industrialization.Installation.BeltConveyor
     public class BeltConveyor : InstallationBase, IInstallationInventory, IBeltConveyor
     {
         private double _beltConveyorSpeed = 0;
-        private readonly InventoryData _inventoryData;
         public BeltConveyor(int installationId, Guid guid) : base(installationId, guid)
         {
-            _inventoryData = new InventoryData(10);
             GUID = guid;
             InstallationID = installationId;
         }
@@ -19,11 +16,6 @@ namespace industrialization.Installation.BeltConveyor
         public IItemStack InsertItem(IItemStack itemStack)
         {
             throw new NotImplementedException();
-        }
-
-        public InventoryData GetInventory()
-        {
-            return _inventoryData;
         }
 
         public BeltConveyorState GetState()
