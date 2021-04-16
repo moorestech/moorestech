3fb35a0dc6eef08dc1501177909bb386c6f961f0
sakastudio <sakastudio100@gmail.com>
2021-04-16 22:35:37 +0900

    ベルトコンベア作成中

---
+++ industrialization/Test/BeltConveyorTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using industrialization.Installation;
+using industrialization.Installation.BeltConveyor;
+using industrialization.Item;
+using NUnit.Framework;
+
+namespace industrialization.Test
+{
+    public class BeltConveyorTest
+    {
+        [Test]
+        public void InsertBeltConveyorTest()
+        {
+            var random = new Random(4123);
+            for (int i = 0; i < 20; i++)
+            {
+                //必要な変数を作成
+                int id = random.Next(0, 10);
+                int amount = random.Next(0, 10);
+                var item = ItemStackFactory.NewItemStack(id, amount);
+                var beltconveyor = new BeltConveyor(0, new Guid(),new NullIInstallationInventory());
+
+                var outputItem = beltconveyor.InsertItem(item);
+                var privateObject = new Private(beltconveyor);
+
+                
+                var value = (int) privateObject.GetField("_Value");
+                Assert.AreEqual(0, value);
+                
+                Assert.True(outputItem.Equals(ItemStackFactory.NewItemStack(id,amount-1)));
+            }
+        }
+    }
+}
