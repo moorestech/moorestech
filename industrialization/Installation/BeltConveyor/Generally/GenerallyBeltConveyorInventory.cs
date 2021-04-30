fb4d71865e6ab94df050fd1e9310b7cc39ca4b1b
sakastudio <sakastudio100@gmail.com>
2021-04-30 11:18:21 +0900

    軽くロジックの作成

--- industrialization/Installation/BeltConveyor/Generally/GenerallyBeltConveyorInventory.cs
+++ industrialization/Installation/BeltConveyor/Generally/GenerallyBeltConveyorInventory.cs
@@ -1,18 +1,37 @@
-﻿using industrialization.Installation.BeltConveyor.Interface;
+﻿using industrialization.GameSystem;
+using industrialization.Installation.BeltConveyor.Interface;
 using industrialization.Item;
 
 namespace industrialization.Installation.BeltConveyor.Generally
 {
-    public class GenerallyBeltConveyorInventory : IBeltConveyorItemInventory
+    public class GenerallyBeltConveyorInventory : IBeltConveyorItemInventory,IUpdate
     {
         private readonly IBeltConveyorConnector _beltConveyorConnector;
 
         public GenerallyBeltConveyorInventory(IBeltConveyorConnector beltConveyorConnector)
         {
             _beltConveyorConnector = beltConveyorConnector;
+            GameUpdate.AddUpdate(this);
         }
 
         public bool InsertItem(IItemStack item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update()
+        {
+            if (!ItemOutputAvailable())return;
+            if (!_beltConveyorConnector.ConnectInsert(new NullItemStack()))return;
+
+            
+            
+            //TODO アイテムを移す処理を書く
+            
+            throw new System.NotImplementedException();
+        }
+
+        private static bool ItemOutputAvailable()
         {
             throw new System.NotImplementedException();
         }
