86ea0759d40fa47b6712085df48a4ca3c2597a91
sakastudio <sakastudio100@gmail.com>
2021-05-04 23:17:14 +0900

    itemjson追加

---
+++ industrialization/Config/Item/ItemJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+using industrialization.Config.Installation;
+
+namespace industrialization.Config.Item
+{
+    [DataContract] 
+    class ItemJson
+    {
+        [DataMember(Name = "installations")]
+        private ItemConfigData[] _item;
+
+        public ItemConfigData[] Items => _item;
+    }
+}
