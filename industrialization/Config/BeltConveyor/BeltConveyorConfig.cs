5cff89ac1e0b581946ae94ddba52cf3663e7e8f2
sakastudio <sakastudio100@gmail.com>
2021-05-07 20:26:45 +0900

    微修正

--- industrialization/Config/BeltConveyor/BeltConveyorConfig.cs
+++ industrialization/Config/BeltConveyor/BeltConveyorConfig.cs
@@ -4,10 +4,12 @@ namespace industrialization.Config.BeltConveyor
 {
     public class BeltConveyorConfig
     {
+        private const int DefaultBeltConveyorNum = 4;
+        private const int DefaultBeltConveyorSpeed = 200;
         //TODO テスト用仮メソッドを実装する
         public static BeltConveyorData GetBeltConveyorData(int installtionID)
         {
-            return new BeltConveyorData(4,200);
+            return new BeltConveyorData(DefaultBeltConveyorNum,DefaultBeltConveyorSpeed);
         }
     }
 
