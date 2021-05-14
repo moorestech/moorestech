67b07e678842ad8d91b21aa036b5b1fd0435d000
sakastudio <sakastudio100@gmail.com>
2021-05-14 20:28:19 +0900

    とりあえずクラス定義

---
+++ industrialization/Installation/Machine/NormalMachineInputInventory.cs
@@ -0,0 +1,12 @@
+﻿using industrialization.Item;
+
+namespace industrialization.Installation.Machine
+{
+    public class NormalMachineInputInventory : IMachineComponent
+    {
+        public IItemStack InsertItem(IItemStack itemStacks)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
