3948fcc299c95831bc61c42f03c59127cdcf5dc3
sakastudio <sakastudio100@gmail.com>
2021-05-24 14:33:09 +0900

    wip 機械が電力がないと進まないようにする

--- industrialization/Installation/Machine/NormalMachineRunProcess.cs
+++ industrialization/Installation/Machine/NormalMachineRunProcess.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using industrialization.Config.Recipe.Data;
+using industrialization.Electric;
 using industrialization.GameSystem;
 
 namespace industrialization.Installation.Machine
 {
-    public class NormalMachineRunProcess : IUpdate
+    public class NormalMachineRunProcess : IUpdate,IInstallationElectric
     {
+        //TODO コンフィグに必要電力量を追加
+        private const int requestPower = 100;
+        
         private IMachineRecipeData _machineRecipeData;
         public readonly NormalMachineOutputInventory NormalMachineOutputInventory;
         private DateTime _processEndTime;
+        private int nowPower = 0;
         public NormalMachineRunProcess(NormalMachineOutputInventory normalMachineOutputInventory)
         {
             _processEndTime = DateTime.MaxValue;
@@ -42,11 +47,49 @@ namespace industrialization.Installation.Machine
         /// <exception cref="NotImplementedException"></exception>
         public void Update()
         {
-            if (!IsProcessing) return;
+            if (!IsProcessing)
+            {
+                nowPower = 0;
+                return;
+            }
             _processEndTime = DateTime.MaxValue;
             NormalMachineOutputInventory.InsertOutputSlot(_machineRecipeData);
+            
+            nowPower = 0;
         }
 
-        private bool IsProcessing => _processEndTime < DateTime.Now;
+        //TODO ここの実装を考える
+        //電力量が需要量より少なかったらプロセス終了までの時間を遅くする
+        //処理時間/(供給量/必要量) - 処理時間 でどれくらい延長すべきかを求める
+        private bool IsProcessing
+        {
+            get
+            {
+                //終了時刻が最大でなければとりあえず何かしら作業はしている
+                if (_processEndTime != DateTime.MaxValue)
+                {
+                    //作業中でも電力が0なら作業は進まない
+                    if (nowPower <= 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        int offset = _machineRecipeData.Time / (nowPower / requestPower)-_machineRecipeData.Time;
+                        return _processEndTime.AddMilliseconds(offset) < DateTime.Now;
+                    }
+                }
+            }
+        }
+
+        public int RequestPower()
+        {
+            return requestPower;
+        }
+
+        public void SupplyPower(double power)
+        {
+            nowPower = (int)power;
+        }
     }
 }
