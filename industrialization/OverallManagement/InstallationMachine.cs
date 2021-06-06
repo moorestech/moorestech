50889c08c4aaa894cb6b5e33f6575bfa942a0319
sakastudio <sakastudio100@gmail.com>
2021-06-06 19:31:59 +0900

    ゲームの全体管理

---
+++ industrialization/OverallManagement/InstallationMachine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using industrialization.Core.Installation;
+using industrialization.Core.Installation.Machine.util;
+
+namespace industrialization.OverallManagement
+{
+    public class InstallationMachine
+    {
+        public static void Create(int id, Guid from, Guid to)
+        {
+            var fromInventory = WorldInstallationInventoryDatastore.GetInstallation(from);
+            var machineGuid = Guid.NewGuid();
+            var machine = NormalMachineFactory.Create(id,machineGuid,WorldInstallationInventoryDatastore.GetInstallation(to));
+            machine.ChangeConnector(fromInventory);
+            
+            WorldInstallationInventoryDatastore.AddInstallation(machine,machineGuid);
+            WorldIInstallationDatastore.AddInstallation(machine);
+        }
+    }
+}
---
+++ industrialization/OverallManagement/WorldIInstallationDatastore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using industrialization.Core.Installation;
+
+namespace industrialization.OverallManagement
+{
+    public class WorldIInstallationDatastore
+    {
+        private static Dictionary<Guid, InstallationBase> _installationMasterDictionary;
+
+        public static void AddInstallation(InstallationBase installation)
+        {
+            if (!_installationMasterDictionary.ContainsKey(installation.Guid))
+            {
+                _installationMasterDictionary.Add(installation.Guid,installation);
+            }
+        }
+
+        public static InstallationBase GetInstallation(Guid guid)
+        {
+            if (_installationMasterDictionary.ContainsKey(guid))
+            {
+                return _installationMasterDictionary[guid];
+            }
+
+            return null;
+        }
+
+        public static void RemoveInstallation(InstallationBase installation)
+        {
+            if (_installationMasterDictionary.ContainsKey(installation.Guid))
+            {
+                _installationMasterDictionary.Remove(installation.Guid);
+            }
+        }
+    }
+}
