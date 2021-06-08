6654fbe834487509178382919fb1aa8ade7c0015
sakastudio <sakastudio100@gmail.com>
2021-06-08 22:14:59 +0900

    接続の設定をしたmachineを返すように

--- industrialization/OverallManagement/InstallMachine.cs
+++ industrialization/OverallManagement/InstallMachine.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using industrialization.Core.Installation;
+using industrialization.Core.Installation.Machine;
 using industrialization.Core.Installation.Machine.util;
 
 namespace industrialization.OverallManagement
 {
     public class InstallMachine
     {
-        public static void Create(int id,int x,int y, Guid from, Guid to)
+        public static NormalMachine Create(int id,int x,int y, Guid from, Guid to)
         {
             //機械の生成
             var machine = NormalMachineFactory.Create(id,Guid.NewGuid(), WorldInstallationInventoryDatastore.GetInstallation(to));
@@ -15,9 +16,11 @@ namespace industrialization.OverallManagement
             machine.ChangeConnector(WorldInstallationInventoryDatastore.GetInstallation(from));
             WorldInstallationInventoryDatastore.GetInstallation(to).ChangeConnector(machine);
             
-            //ワールドデータに登録
+            //TODO ワールドデータに登録を別のところに
             WorldInstallationInventoryDatastore.AddInstallation(machine,machine.Guid);
             WorldInstallationDatastore.AddInstallation(machine,x,y);
+
+            return machine;
         }
     }
 }
