118f7db733a75c7afc1473d32a806065fc65230a
sakastudio <sakastudio100@gmail.com>
2021-06-08 22:17:16 +0900

    guid引数名変更

--- industrialization/OverallManagement/InstallMachine.cs
+++ industrialization/OverallManagement/InstallMachine.cs
@@ -7,14 +7,14 @@ namespace industrialization.OverallManagement
 {
     public class InstallMachine
     {
-        public static NormalMachine Create(int id,int x,int y, Guid from, Guid to)
+        public static NormalMachine Create(int id,int x,int y, Guid input, Guid output)
         {
             //機械の生成
-            var machine = NormalMachineFactory.Create(id,Guid.NewGuid(), WorldInstallationInventoryDatastore.GetInstallation(to));
+            var machine = NormalMachineFactory.Create(id,Guid.NewGuid(), WorldInstallationInventoryDatastore.GetInstallation(output));
             
             //機械のコネクターを変更する
-            machine.ChangeConnector(WorldInstallationInventoryDatastore.GetInstallation(from));
-            WorldInstallationInventoryDatastore.GetInstallation(to).ChangeConnector(machine);
+            machine.ChangeConnector(WorldInstallationInventoryDatastore.GetInstallation(input));
+            WorldInstallationInventoryDatastore.GetInstallation(output).ChangeConnector(machine);
             
             //TODO ワールドデータに登録を別のところに
             WorldInstallationInventoryDatastore.AddInstallation(machine,machine.Guid);
