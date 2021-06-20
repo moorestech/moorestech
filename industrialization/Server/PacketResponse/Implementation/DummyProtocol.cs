da4bdd2be8add9bd9b14a9c5fba4673fa0e63d16
sakastudio <sakastudio100@gmail.com>
2021-06-20 10:13:52 +0900

    ダミープロトコル追加

---
+++ industrialization/Server/PacketResponse/Implementation/DummyProtocol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace industrialization.Server.PacketResponse.Implementation
+{
+    public class DummyProtocol : IPacketResponse
+    {
+        public byte[] GetResponse()
+        {
+            short id = 0;
+            return BitConverter.GetBytes(id);
+        }
+
+        public static IPacketResponse NewInstance(byte[] payload)
+        {
+            return new DummyProtocol();
+        }
+    }
+}
