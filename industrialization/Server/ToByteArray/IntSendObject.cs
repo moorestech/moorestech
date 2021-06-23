ad3897a7d6e9e2bba79f43717856d0064ce00039
sakastudio <sakastudio100@gmail.com>
2021-06-23 14:57:27 +0900

    いろんな方をbyte配列に変換する仕組み

---
+++ industrialization/Server/ToByteArray/IntSendObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace industrialization.Server.ToByteArray
+{
+    public class IntSendObject : ISendObject
+    {
+        private readonly int _sendData;
+        public IntSendObject(int sendData)
+        {
+            _sendData = sendData;
+        }
+        public byte[] GetByteArray()
+        {
+            return BitConverter.GetBytes(_sendData);
+        }
+    }
+}
