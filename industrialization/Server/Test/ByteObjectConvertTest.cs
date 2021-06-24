e563558a413d916cafe31e77c1498213fb183ff2
sakastudio <sakastudio100@gmail.com>
2021-06-24 13:19:07 +0900

    テストに合格

--- industrialization/Server/Test/ByteObjectConvertTest.cs
+++ industrialization/Server/Test/ByteObjectConvertTest.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using industrialization.Server.Util;
+using industrialization.Server.Util.ByteArrayToObject;
 using NUnit.Framework;
 
 namespace industrialization.Server.Test
@@ -30,7 +32,7 @@ namespace industrialization.Server.Test
         //intを配列に変換して同じかテストする
         public void OneintObjectToByteArrayTest()
         {
-            var random = new Random();
+            var random = new Random(1000);
             for (int i = 0; i < 100; i++)
             {
 
@@ -49,7 +51,7 @@ namespace industrialization.Server.Test
         //shortを配列に変換して同じかテストする
         public void OneshortObjectToByteArrayTest()
         {
-            var random = new Random();
+            var random = new Random(1000);
             for (int i = 0; i < 100; i++)
             {
 
@@ -63,6 +65,56 @@ namespace industrialization.Server.Test
                     Assert.AreEqual(ans[j],exp[j]);
                 }
             }
+        }
+
+        [Test]
+        //GUIDを相互変化しても問題ないかテスト
+        public void OneGUIDConvertTest()
+        {
+            var guid = Guid.NewGuid();
+            var byteData = new ByteArrayToSendObject(SendObjectToByteArray.ToByteArray(guid));
+
+            Assert.AreEqual(guid.ToString(),byteData.MoveNextToGetGuid().ToString());
+        }
+        [Test]
+        //intを相互変化しても問題ないかテスト
+        public void OneIntConvertTest()
+        {
+            var Int =  new Random(1000).Next(Int32.MinValue, Int32.MaxValue);
+            var byteData = new ByteArrayToSendObject(SendObjectToByteArray.ToByteArray(Int));
+
+            Assert.AreEqual(Int,byteData.MoveNextToGetInt());
+        }
+        [Test]
+        //shortを相互変化しても問題ないかテスト
+        public void OneShortConvertTest()
+        {
+            var Short = (short)new Random(1000).Next(short.MinValue, short.MaxValue);
+            var byteData = new ByteArrayToSendObject(SendObjectToByteArray.ToByteArray(Short));
+
+            Assert.AreEqual(Short,byteData.MoveNextToGetShort());
+        }
+        
+        [Test]
+        //いろいろ相互変換をしても問題ないかテスト
+        public void MoreConvertTest()
+        {
+            var random = new Random();
+            var id = (short) random.Next(short.MinValue, short.MaxValue);
+            var guid1 = Guid.NewGuid();
+            var guid2 = Guid.NewGuid();
+            var ans = new List<byte>();
+            ans.AddRange(SendObjectToByteArray.ToByteArray(id));
+            ans.AddRange(SendObjectToByteArray.ToByteArray(guid1));
+            ans.AddRange(SendObjectToByteArray.ToByteArray(guid2));
+            ans.AddRange(SendObjectToByteArray.ToByteArray(50));
+
+            var byteData = new ByteArrayToSendObject(ans.ToArray());
+
+            Assert.AreEqual(id,byteData.MoveNextToGetShort()) ;
+            Assert.AreEqual(guid1.ToString(),byteData.MoveNextToGetGuid().ToString());
+            Assert.AreEqual(guid2.ToString(),byteData.MoveNextToGetGuid().ToString());
+            Assert.AreEqual(50,byteData.MoveNextToGetInt());
         }
     }
 }
--- industrialization/Server/Util/ByteArrayToObject/ByteArrayToSendObject.cs
+++ industrialization/Server/Util/ByteArrayToObject/ByteArrayToSendObject.cs
@@ -14,7 +14,35 @@ namespace industrialization.Server.Util.ByteArrayToObject
 
         public Guid MoveNextToGetGuid()
         {
-            return Guid.Empty;
+            var b = new List<byte>();
+            for (int i = 0; i < 16; i++)
+            {
+                _payload.MoveNext();
+                b.Add(_payload.Current);
+            }
+            return new Guid(b.ToArray());
+        }
+
+        public int MoveNextToGetInt()
+        {
+            var b = new List<byte>();
+            for (int i = 0; i < 4; i++)
+            {
+                _payload.MoveNext();
+                b.Add(_payload.Current);
+            }
+            return BitConverter.ToInt32(b.ToArray());
+        }
+
+        public short MoveNextToGetShort()
+        {
+            var b = new List<byte>();
+            for (int i = 0; i < 2; i++)
+            {
+                _payload.MoveNext();
+                b.Add(_payload.Current);
+            }
+            return BitConverter.ToInt16(b.ToArray());
         }
     }
 }
