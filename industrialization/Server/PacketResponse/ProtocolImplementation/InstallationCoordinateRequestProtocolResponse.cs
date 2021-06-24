f5496228accd59b76eb2640f6dfb0d2ac5c74555
sakastudio <sakastudio100@gmail.com>
2021-06-24 17:40:40 +0900

    なにも設定してないときのパケットレスポンス

--- industrialization/Server/PacketResponse/ProtocolImplementation/InstallationCoordinateRequestProtocolResponse.cs
+++ industrialization/Server/PacketResponse/ProtocolImplementation/InstallationCoordinateRequestProtocolResponse.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using industrialization.Core.Installation;
 using industrialization.OverallManagement.DataStore;
+using industrialization.Server.Util;
 
 namespace industrialization.Server.PacketResponse.ProtocolImplementation
 {
@@ -17,8 +18,13 @@ namespace industrialization.Server.PacketResponse.ProtocolImplementation
         /// <returns></returns>
         public static byte[] GetResponse(byte[] payload)
         {
-            int x = BitConverter.ToInt32(new byte[4] {payload[4], payload[5], payload[6], payload[7]});
-            int y = BitConverter.ToInt32(new byte[4] {payload[8], payload[9], payload[10], payload[11]});
+            var payloadData = new ByteArrayToSendObject(payload);
+            //IDの取得
+            payloadData.MoveNextToGetShort();
+            //パディング
+            payloadData.MoveNextToGetShort();
+            int x = payloadData.MoveNextToGetInt();
+            int y = payloadData.MoveNextToGetInt();
             //入力さた座標を10の倍数に変換する
             x = x / 10 * 10;
             y = y / 10 * 10;
@@ -27,7 +33,7 @@ namespace industrialization.Server.PacketResponse.ProtocolImplementation
             var responsePayload = new List<byte>();
             //パケットIDの挿入
             short id = 1;
-            BitConverter.GetBytes(id).ToList().ForEach(b => responsePayload.Add(b));
+            responsePayload.AddRange(ByteArrayConverter.ToByteArray(id));
 
             var inst = new List<InstallationBase>();
             
@@ -38,26 +44,30 @@ namespace industrialization.Server.PacketResponse.ProtocolImplementation
                 for (int j = y; j < y+10; j++)
                 {
                     var instbase = WorldInstallationDatastore.GetInstallation(x, y);
-                    if (instbase != null)
+                    if (instbase.Guid.ToString() != Guid.Empty.ToString())
                     {
                         inst.Add(instbase);   
                     }
                 }
             }
             //建物データの数
-            BitConverter.GetBytes(inst.Count).ToList().ForEach(b => responsePayload.Add(b));
+            responsePayload.AddRange(ByteArrayConverter.ToByteArray(inst.Count));
+            //要求された座標
+            responsePayload.AddRange(ByteArrayConverter.ToByteArray(x));
+            responsePayload.AddRange(ByteArrayConverter.ToByteArray(y));
             //パディング
-            responsePayload.Add(0);
             responsePayload.Add(0);
             
             //データをバイト配列に登録
             foreach (var i in inst)
             {
                 var c = WorldInstallationDatastore.GetCoordinate(i.Guid);
-                BitConverter.GetBytes(c.x).ToList().ForEach(b => responsePayload.Add(b));
-                BitConverter.GetBytes(c.y).ToList().ForEach(b => responsePayload.Add(b));
-                BitConverter.GetBytes(i.InstallationId).ToList().ForEach(b => responsePayload.Add(b));
-                i.Guid.ToByteArray().ToList().ForEach(b => responsePayload.Add(b));
+                
+                responsePayload.AddRange(ByteArrayConverter.ToByteArray(c.x));
+                responsePayload.AddRange(ByteArrayConverter.ToByteArray(c.y));
+                
+                responsePayload.AddRange(ByteArrayConverter.ToByteArray(i.InstallationId));
+                responsePayload.AddRange(ByteArrayConverter.ToByteArray(i.Guid));
             }
 
             return responsePayload.ToArray();
--- industrialization/Server/Test/PacketResponseFactoryTest.cs
+++ industrialization/Server/Test/PacketResponseFactoryTest.cs
@@ -1,18 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using industrialization.OverallManagement.DataStore;
+using industrialization.Server.PacketResponse;
+using industrialization.Server.PacketResponse.ProtocolImplementation;
+using industrialization.Server.Util;
 using NUnit.Framework;
 
 namespace industrialization.Server.Test
 {
+    //クライアントからのバイト配列から正しいレスポンスが返るかのテスト
     public class PacketResponseFactoryTest
     {
         //なにも設定してないときに空の配列を取得する
         [Test]
         public void Building1ChunkInstalledWhenCorrectlyDataAcquirableIsTest()
         {
-            var putPayLoad = new List<byte>();
-            throw new ArgumentException();
-            Assert.True(true);
+            var send = new List<byte>();
+            send.AddRange(ByteArrayConverter.ToByteArray((short)2));
+            send.AddRange(ByteArrayConverter.ToByteArray((short)0));
+            send.AddRange(ByteArrayConverter.ToByteArray(0));
+            send.AddRange(ByteArrayConverter.ToByteArray(0));
+            var response = PacketResponseFactory.GetPacketResponse(send.ToArray());
+            var ansResponse  = new List<byte>();
+            ansResponse.AddRange(ByteArrayConverter.ToByteArray((short)1));
+            ansResponse.AddRange(ByteArrayConverter.ToByteArray(0));
+            ansResponse.AddRange(ByteArrayConverter.ToByteArray((short)0));
+            ansResponse.AddRange(ByteArrayConverter.ToByteArray(0));
+            ansResponse.AddRange(ByteArrayConverter.ToByteArray(0));
+
+
+            for (int i = 0; i < response.Length; i++)
+            {
+                Console.WriteLine(i);
+                Assert.AreEqual(ansResponse[i],response[i]);
+            }
         }
     }
 }
