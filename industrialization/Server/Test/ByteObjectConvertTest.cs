af953df48a0de2b7588c469215377c43957b0519
sakastudio <sakastudio100@gmail.com>
2021-06-24 11:48:56 +0900

    テストの作成、合格

--- industrialization/Server/Test/ByteObjectConvertTest.cs
+++ industrialization/Server/Test/ByteObjectConvertTest.cs
@@ -27,15 +27,36 @@ namespace industrialization.Server.Test
             }
         }        
         [Test]
-        //GUIDを配列に変換して同じかテストする
+        //intを配列に変換して同じかテストする
         public void OneintObjectToByteArrayTest()
         {
+            var random = new Random();
             for (int i = 0; i < 100; i++)
             {
-                var guid = Guid.NewGuid();
-                var ans = guid.ToByteArray();
 
-                var exp = SendObjectToByteArray.ToByteArray(guid);
+                var data = random.Next(Int32.MinValue, Int32.MaxValue);
+                var ans = BitConverter.GetBytes(data);
+
+                var exp = SendObjectToByteArray.ToByteArray(data);
+
+                for (int j = 0; j < ans.Length; j++)
+                {
+                    Assert.AreEqual(ans[j],exp[j]);
+                }
+            }
+        }        
+        [Test]
+        //shortを配列に変換して同じかテストする
+        public void OneshortObjectToByteArrayTest()
+        {
+            var random = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+
+                var data = (short)random.Next(short.MinValue,short.MaxValue);
+                var ans = BitConverter.GetBytes(data);
+
+                var exp = SendObjectToByteArray.ToByteArray(data);
 
                 for (int j = 0; j < ans.Length; j++)
                 {
