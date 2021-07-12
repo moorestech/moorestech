9233cb1abbb55ab7ca481f7812db6814b273a80f
sakastudio <sakastudio100@gmail.com>
2021-07-12 23:41:47 +0900

    テストコード変更

--- industrialization/Server/Test/PacketResponseFactoryTest.cs
+++ industrialization/Server/Test/PacketResponseFactoryTest.cs
@@ -44,30 +44,34 @@ namespace industrialization.Server.Test
         {
             //取得範囲外に建物設置
             PutInstallation(1, -10, -5);
-            PutInstallation(5, -1, -1);
             PutInstallation(0, -5, -5);
             PutInstallation(4, -13, -12);
             PutInstallation(1, -10, -5);
             PutInstallation(9, 10, 10);
             PutInstallation(9, 15, 10);
             PutInstallation(9, 10, 20);
+            //限界地分析
+            PutInstallation(0, -1, -1);
+            PutInstallation(0, -1, 4);
+            PutInstallation(0, 4, -1);
+            PutInstallation(0, 4, 4);
             
             //取得範囲内に建物を設置
             var ansInstallations = new List<Installation>();
             PutInstallation(1, 0, 0);
             ansInstallations.Add(new Installation(0,0,1));
-            PutInstallation(2, 0, 5);
-            ansInstallations.Add(new Installation(0,5,2));
-            PutInstallation(3, 4, 6);
-            ansInstallations.Add(new Installation(4,6,3));
-            PutInstallation(6, 5, 0);
-            ansInstallations.Add(new Installation(5,0,6));
-            PutInstallation(10, 9, 9);
-            ansInstallations.Add(new Installation(9,9,10));
-            PutInstallation(15, 9, 0);
-            ansInstallations.Add(new Installation(9,0,15));
-            PutInstallation(30, 0, 9);
-            ansInstallations.Add(new Installation(0,9,30));
+            PutInstallation(2, 0, 3);
+            ansInstallations.Add(new Installation(0,3,2));
+            PutInstallation(3, 3, 0);
+            ansInstallations.Add(new Installation(3,0,3));
+            PutInstallation(6, 3, 3);
+            ansInstallations.Add(new Installation(3,3,6));
+            PutInstallation(10, 2, 1);
+            ansInstallations.Add(new Installation(2,1,10));
+            PutInstallation(15, 0, 2);
+            ansInstallations.Add(new Installation(0,2,15));
+            PutInstallation(30, 3, 1);
+            ansInstallations.Add(new Installation(3,1,30));
             
             //設置データの要求
             var send = new List<byte>();
@@ -92,7 +96,6 @@ namespace industrialization.Server.Test
                     response.MoveNextToGetInt(),
                     response.MoveNextToGetInt(),
                     response.MoveNextToGetInt()));
-                response.MoveNextToGetInt();
             }
             
             responseInstallations.Sort((a,b) => a.InstallationsId-b.InstallationsId);
