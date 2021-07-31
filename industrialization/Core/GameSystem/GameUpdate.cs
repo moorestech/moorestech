46bae98005d8399aba7ac1060ee00a51ae06ecc3
sakastudio <sakastudio100@gmail.com>
2021-07-31 23:24:17 +0900

    プロセス実行に関する修正

--- industrialization/Core/GameSystem/GameUpdate.cs
+++ industrialization/Core/GameSystem/GameUpdate.cs
@@ -1,21 +1,32 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace industrialization.Core.GameSystem
 {
     public static class GameUpdate
     {
         private static List<IUpdate> _updates = new List<IUpdate>();
+
+        public static double UpdateTime => _updateTime;
+        private static double _updateTime = 0;
+        
         public static void AddUpdateObject(IUpdate iUpdate)
         {
             _updates.Add(iUpdate);
         }
 
+        private static DateTime _prevUpdateDateTime = DateTime.Now;
         public static void Update()
         {
+            _updateTime = DateTime.Now.Subtract(_prevUpdateDateTime).TotalMilliseconds;
+            _prevUpdateDateTime = DateTime.Now;
+            //アップデートの実行
             for (int i = _updates.Count - 1; 0 <= i ; i--)
             {
                 _updates[i]?.Update();
             }
+            Thread.Sleep(10);
         }
     }
 }
--- industrialization/Core/Installation/Machine/NormalMachineRunProcess.cs
+++ industrialization/Core/Installation/Machine/NormalMachineRunProcess.cs
@@ -10,84 +10,76 @@ namespace industrialization.Core.Installation.Machine
         //TODO コンフィグに必要電力量を追加
         private const int requestPower = 100;
         
-        private IMachineRecipeData _machineRecipeData;
+        private IMachineRecipeData _machineRecipeData = new NullMachineRecipeData();
         public readonly NormalMachineOutputInventory NormalMachineOutputInventory;
-        private DateTime _processStartTime;
         private int _nowPower = 0;
+        private ProcessState _state = ProcessState.Idle;
         public NormalMachineRunProcess(NormalMachineOutputInventory normalMachineOutputInventory)
         {
-            _processStartTime = DateTime.MaxValue;
-            _machineRecipeData = new NullMachineRecipeData();
             NormalMachineOutputInventory = normalMachineOutputInventory;
             GameUpdate.AddUpdateObject(this);
         }
-        
-        /// <summary>
-        /// 実行中かどうか、アウトプットスロットがいっぱいじゃないかを見る
-        /// </summary>
-        /// <returns></returns>
-        public bool IsAllowedToStartProcess()
-        {
-            return !IsWaiting && NormalMachineOutputInventory.IsAllowedToOutputItem(_machineRecipeData);
-        }
 
-        /// <summary>
-        /// 実際にプロセスを開始する
-        /// </summary>
-        /// <param name="machineRecipeData"></param>
-        public void StartProcess(IMachineRecipeData machineRecipeData)
-        {
-            _machineRecipeData = machineRecipeData;
-            _processStartTime = DateTime.Now;
-        }
-
-        /// <summary>
-        /// アップデートをして実行できるか見る
-        /// </summary>
-        /// <exception cref="NotImplementedException"></exception>
         public void Update()
         {
-            if (!IsWaiting) return;
-            if (IsAllowedToStartProcess())
+            switch (_state)
             {
-                StartProcess(_machineRecipeData);
+                case ProcessState.Idle :
+                    Idle();
+                    break;
+                case ProcessState.Processing :
+                    Processing();
+                    break;
+                case ProcessState.ProcessingExit :
+                    ProcessingExit();
+                    break;
             }
-            _processStartTime = DateTime.MaxValue;
-            NormalMachineOutputInventory.InsertOutputSlot(_machineRecipeData);
-            _nowPower = 0;
+        }
+
+        //TODO インプットスロット的に処理が始められそうだったら処理開始処理をだす
+        public bool IsAllowedToStartProcess =>
+            _state == ProcessState.Idle && NormalMachineOutputInventory.IsAllowedToOutputItem(_machineRecipeData);
+
+        private double _milliSecondsRemaining = 0;
+        private void Idle()
+        {
+            if (IsAllowedToStartProcess)
+            {
+                _state = ProcessState.Processing;
+                _milliSecondsRemaining = _machineRecipeData.Time;
+            }
+        }
+        private void Processing()
+        {
+            _milliSecondsRemaining -= GameUpdate.UpdateTime;
+            Console.WriteLine(GameUpdate.UpdateTime);
+            if(_milliSecondsRemaining <= 0) _state = ProcessState.ProcessingExit;
+        }
+        private void ProcessingExit()
+        {
+            
         }
 
 
-        //電力量が需要量より少なかったらプロセス終了までの時間を遅くする
-        private bool IsWaiting
+        public void StartProcess(IMachineRecipeData _machineRecipeData)
         {
-            get
+            if (_state == ProcessState.Idle)
             {
-                //電力が0の時は現在時間を更新し続ける
-                if (_nowPower <= 0)
-                {
-                    _processStartTime = DateTime.Now;
-                }
-                try
-                {
-                    int tmp = _machineRecipeData.Time / (_nowPower / requestPower);
-                    return _processStartTime.AddMilliseconds(tmp) < DateTime.Now;
-                }
-                catch (Exception e)
-                {
-                    return false;
-                }
+                this._machineRecipeData = _machineRecipeData;
+                _state = ProcessState.Processing;
             }
         }
         
-        public int RequestPower()
-        {
-            return requestPower;
-        }
+        
+        
+        public int RequestPower(){return requestPower;}
+        public void SupplyPower(int power){_nowPower = power;}
+    }
 
-        public void SupplyPower(int power)
-        {
-            _nowPower = power;
-        }
+    enum ProcessState
+    {
+        Idle,
+        Processing,
+        ProcessingExit
     }
 }
