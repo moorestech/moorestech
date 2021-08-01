05ad103070de5bbdb000f2c54b9037f801fc64aa
sakastudio <sakastudio100@gmail.com>
2021-08-02 08:48:51 +0900

    Listを辞書型に

--- industrialization/Core/Electric/ElectricSegment.cs
+++ industrialization/Core/Electric/ElectricSegment.cs
@@ -6,62 +6,74 @@ namespace industrialization.Core.Electric
     //１つの電力のセグメントの処理
     public class ElectricSegment : IUpdate
     {
-        private readonly List<IInstallationElectric> _electrics;
-        private readonly List<IPowerGenerator> _generators;
+        private readonly Dictionary<int,IInstallationElectric> _electrics;
+        private readonly Dictionary<int,IPowerGenerator> _generators;
 
         public ElectricSegment()
         {
             GameUpdate.AddUpdateObject(this);
-            _generators = new List<IPowerGenerator>();
-            _electrics = new List<IInstallationElectric>();
+            _generators = new Dictionary<int,IPowerGenerator>();
+            _electrics = new Dictionary<int,IInstallationElectric>();
         }
         
         
         public void Update()
         {
             //合計電力量の算出
-            var powersum = 0;
-            foreach (var generator in _generators)
+            var powers = 0;
+            foreach (var key in _generators.Keys)
             {
-                powersum += generator.OutputPower();
+                powers += _generators[key].OutputPower();
             }
             
             //合計電力需要量の算出
-            var requestpower = 0;
-            foreach (var electric in _electrics)
+            var requester = 0;
+            foreach (var key in _electrics.Keys)
             {
-                requestpower += electric.RequestPower();
+                requester += _electrics[key].RequestPower();
             }
             
             //電力供給の割合の算出
-            var powerRate = (double)powersum / (double)requestpower;
+            var powerRate = (double)powers / (double)requester;
             if (1 < powerRate) powerRate = 1;
             
             //電力を供給
-            foreach (var electric in _electrics)
+            foreach (var key in _electrics.Keys)
             {
-                electric.SupplyPower((int)(electric.RequestPower()*powerRate));
+                _electrics[key].SupplyPower((int)(_electrics[key].RequestPower()*powerRate));
             }
         }
 
         public void AddInstallationElectric(IInstallationElectric installationElectric)
         {
-            _electrics.Add(installationElectric);
+            if (!_electrics.ContainsKey(installationElectric.GetIntId()))
+            {
+                _electrics.Add(installationElectric.GetIntId(),installationElectric);
+            }
         }
 
         public void RemoveInstallationElectric(IInstallationElectric installationElectric)
         {
-            _electrics.Remove(installationElectric);
+            if (_electrics.ContainsKey(installationElectric.GetIntId()))
+            {
+                _electrics.Remove(installationElectric.GetIntId());
+            }
         }
 
         public void AddGenerator(IPowerGenerator powerGenerator)
         {
-            _generators.Add(powerGenerator);
+            if (!_generators.ContainsKey(powerGenerator.GetIntId()))
+            {
+                _generators.Add(powerGenerator.GetIntId(),powerGenerator);
+            }
         }
 
         public void RemoveGenerator(IPowerGenerator powerGenerator)
         {
-            _generators.Remove(powerGenerator);
+            if (_generators.ContainsKey(powerGenerator.GetIntId()))
+            {
+                _generators.Remove(powerGenerator.GetIntId());
+            }
         }
 
     }
