16f650c339e41d87fd6f24490b1b3bcb48c1aadd
sakastudio <sakastudio100@gmail.com>
2021-08-05 01:15:21 +0900

    一通りのロジック

--- industrialization/Core/Installation/BeltConveyor/Generally/GenericBeltConveyorInventory.cs
+++ industrialization/Core/Installation/BeltConveyor/Generally/GenericBeltConveyorInventory.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using industrialization.Core.Config.BeltConveyor;
 using industrialization.Core.GameSystem;
-using industrialization.Core.Installation.BeltConveyor.Generally.DataClass;
-using industrialization.Core.Installation.BeltConveyor.Interface;
 using industrialization.Core.Item;
 
 namespace industrialization.Core.Installation.BeltConveyor.Generally
@@ -12,11 +10,11 @@ namespace industrialization.Core.Installation.BeltConveyor.Generally
     /// <summary>
     /// アイテムの搬出入とインベントリの管理を行う
     /// </summary>
-    public class GenericBeltConveyorInventory : IUpdate
+    public class GenericBeltConveyorInventory : IUpdate, IInstallationInventory
     {
         private readonly int _inventoryItemNum = 4;
         private readonly double _canItemInsertTime = 500;
-        
+
         private readonly List<BeltConveyorInventoryItem> _inventoryItems;
         private IInstallationInventory _connector;
 
@@ -28,7 +26,41 @@ namespace industrialization.Core.Installation.BeltConveyor.Generally
             _canItemInsertTime = conf.BeltConveyorSpeed;
             GameUpdate.AddUpdateObject(this);
         }
-        
+
+        public IItemStack InsertItem(IItemStack itemStack)
+        {
+            //新しく挿入可能か
+            if (_inventoryItems.Count < _inventoryItemNum &&
+                _inventoryItems[0].RemainingTime < _canItemInsertTime - _canItemInsertTime / _inventoryItemNum)
+            {
+                if (_inventoryItems.Count == 0)
+                {
+                    _inventoryItems.Add(
+                        new BeltConveyorInventoryItem(itemStack.Id, _canItemInsertTime, 0));
+                }
+                else
+                {
+                    //インデックスをずらす
+                    _inventoryItems.Add(_inventoryItems[0]);
+                    for (int i = _inventoryItems.Count - 1; i >= 1; i--)
+                    {
+                        _inventoryItems[i] = _inventoryItems[i - 1];
+                    }
+
+                    _inventoryItems[0] = new BeltConveyorInventoryItem(
+                        itemStack.Id,
+                        _canItemInsertTime,
+                        _inventoryItems[1].RemainingTime + (_canItemInsertTime / _inventoryItemNum));
+                }
+
+                return itemStack.SubItem(1);
+            }
+            else
+            {
+                return itemStack;
+            }
+        }
+
         public void ChangeConnector(IInstallationInventory installationInventory)
         {
         }
@@ -49,44 +81,24 @@ namespace industrialization.Core.Installation.BeltConveyor.Generally
                         _inventoryItems[i + 1].RemainingTime + _canItemInsertTime / _inventoryItemNum;
                 }
             }
-            //時間を減らす
-            foreach (var t in _inventoryItems) { t.RemainingTime -= GameUpdate.UpdateTime; }
 
-            
-            //最後のアイテムが0だったら次に渡す
+            //時間を減らす
+            foreach (var t in _inventoryItems)
+            {
+                t.RemainingTime -= GameUpdate.UpdateTime;
+            }
+
+
+            //最後のアイテムが0だったら接続先に渡す
             if (_inventoryItems[^1].RemainingTime <= 0)
             {
                 var output = _connector.InsertItem(ItemStackFactory.NewItemStack(_inventoryItems[^1].ItemId, 1));
                 //渡した結果がnullItemだったらそのアイテムを消す
                 if (output.Id == NullItemStack.NullItemId)
                 {
-                    _inventoryItems.RemoveAt(_inventoryItems.Count-1);
+                    _inventoryItems.RemoveAt(_inventoryItems.Count - 1);
                 }
             }
-        }
-    }
-
-    class BeltConveyorInventoryItem
-    {
-        public readonly int ItemId;
-        public double LimitTime;
-        public double RemainingTime
-        {
-            get => _remainingTime;
-            set
-            {
-                if (LimitTime < value)
-                {
-                    _remainingTime = value;
-                }
-            }
-        }
-        private double _remainingTime;
-        public BeltConveyorInventoryItem(int itemId, int remainingTime, double limitTime)
-        {
-            ItemId = itemId;
-            RemainingTime = remainingTime;
-            LimitTime = limitTime;
         }
     }
 }
