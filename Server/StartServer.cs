573968e47e80e5e433a2835d84937e2694ae4fb1
sakastudio <sakastudio100@gmail.com>
2021-08-19 23:46:22 +0900

    テストコメント削除

--- Server/StartServer.cs
+++ Server/StartServer.cs
@@ -4,7 +4,6 @@ namespace industrialization.Server
 {
     public static class StartServer
     {
-        //Unityとの同期テスト
         public static void Main(string[] args)
         {
             PacketHandler.StartServer();
