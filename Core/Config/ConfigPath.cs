edd7a87979da40f22e0a8cf9ba021373d214a78d
sakastudio <sakastudio100@gmail.com>
2021-08-19 21:44:47 +0900

    ソースコードだけに変更

---
+++ Core/Config/ConfigPath.cs
@@ -0,0 +1,9 @@
+﻿namespace industrialization.Core.Config
+{
+    public static class ConfigPath
+    {
+        public const string RecipeConfigPath = "F:\\UnityPF\\industrialization-client\\Assets\\Scripts\\Server\\industrialization\\Core\\Config\\Json\\macineRecipe.json";
+        public const string BlockConfigPath = "F:\\UnityPF\\industrialization-client\\Assets\\Scripts\\Server\\industrialization\\Core\\Config\\Json\\block.json";
+        public const string ItemConfigPath = "F:\\UnityPF\\industrialization-client\\Assets\\Scripts\\Server\\industrialization\\Core\\Config\\Json\\item.json";
+    }
+}
