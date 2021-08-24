d3248ffc05083a1bf39c20077fe24e627faa0688
sakastudio <sakastudio100@gmail.com>
2021-08-25 00:43:45 +0900

    bit配列を扱う列挙子

---
+++ Server/Util/BitArrayEnumerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace industrialization.Server.Util
+{
+    public class BitArrayEnumerator
+    {
+        private BitArray _bitArray;
+        private int index = 0;
+
+        public BitArrayEnumerator(byte[] bytes)
+        {
+            _bitArray = new BitArray(bytes);
+        }
+
+        public bool MoveNextToBit()
+        {
+            return false;
+        }
+        public byte MoveNextToByte()
+        {
+            return 0;
+        }
+        public byte MoveNextToShort()
+        {
+            return 0;
+        }
+
+        
+        public int MoveNextToGetInt()
+        {
+            var tmpBitArray = new BitArray(32);
+            for (int i = 0; i < 32; i++)
+            {
+                tmpBitArray[i] = _bitArray[i + index];
+            }    
+            var tmp = new int[1];
+            tmpBitArray.CopyTo(tmp, 0);
+            return tmp[0];
+        }
+        
+    }
+}
