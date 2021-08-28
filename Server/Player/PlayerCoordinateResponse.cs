f8f65aaf15a8fd46bd0c0adc6c3d154f53c29d56
sakastudio <sakastudio100@gmail.com>
2021-08-28 18:39:36 +0900

    ネットワークのプレイヤー関係のクラス定義

---
+++ Server/Player/PlayerCoordinateResponse.cs
@@ -0,0 +1,15 @@
+﻿using industrialization.OverallManagement.DataStore;
+
+namespace industrialization.Server.Player
+{
+    public class PlayerCoordinateResponse
+    {
+        public Coordinate originPosition { get; }
+        public int[,] blocks { get; }
+        public PlayerCoordinateResponse(Coordinate originPosition)
+        {
+            this.originPosition = originPosition;
+            //TODO ここに座標取得処理を作成
+        }
+    }
+}
---
+++ Server/Player/PlayerCoordinateToDoResponse.cs
@@ -0,0 +1,13 @@
+﻿using industrialization.OverallManagement.DataStore;
+
+namespace industrialization.Server.Player
+{
+    public class PlayerCoordinateToDoResponse
+    {
+        public bool DoResponse(Coordinate coordinate)
+        {
+            //TODO ここに座標取得処理を作成
+            return false;
+        }
+    }
+}
