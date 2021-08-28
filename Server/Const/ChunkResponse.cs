4d9197f2c9c42aa545d84bd0c5d14094fcc9e9bc
sakastudio <sakastudio100@gmail.com>
2021-08-28 20:23:31 +0900

    ChunkResponseの定数追加

---
+++ Server/Const/ChunkResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace industrialization.Server.Const
+{
+    public static class ChunkResponse
+    {
+        public const int ChunkSize = 20;
+        public const int PlayerVisibleRangeChunk = 5;
+    }
+}
