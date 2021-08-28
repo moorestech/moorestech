3541db12ce5ee17aeebc38cfad06209c066b324f
sakastudio <sakastudio100@gmail.com>
2021-08-28 15:54:18 +0900

    テスト合格

--- Server/Util/BitArrayEnumerator.cs
+++ Server/Util/BitArrayEnumerator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.ComponentModel;
 
 namespace industrialization.Server.Util
 {
@@ -15,33 +16,80 @@ namespace industrialization.Server.Util
 
         public bool MoveNextToBit()
         {
-            return false;
+            var r = _bitArray.Get(index);
+            index++;
+            return r;
         }
         public byte MoveNextToByte()
         {
-            return 0;
+            var bitNum = 8;
+            
+            var tmpBitArray = new BitArray(bitNum);
+            for (int i = 0; i < bitNum; i++)
+            {
+                tmpBitArray[i] = _bitArray[i + index];
+            }
+
+            index += bitNum;
+            return ToByteArray(tmpBitArray)[0];
         }
         public short MoveNextToShort()
         {
-            return 0;
+            var bitNum = 16;
+            
+            var tmpBitArray = new BitArray(bitNum);
+            for (int i = 0; i < bitNum; i++)
+            {
+                tmpBitArray[i] = _bitArray[i + index];
+            }
+            
+            index += bitNum;
+            return BitConverter.ToInt16(ToByteArray(tmpBitArray));
         }
         public float MoveNextToFloat()
         {
-            return 0;
+            var bitNum = 32;
+            
+            var tmpBitArray = new BitArray(bitNum);
+            for (int i = 0; i < bitNum; i++)
+            {
+                tmpBitArray[i] = _bitArray[i + index];
+            }
+            
+            index += bitNum;
+            return BitConverter.ToSingle(ToByteArray(tmpBitArray));
         }
 
         
         public int MoveNextToGetInt()
         {
-            var tmpBitArray = new BitArray(32);
-            for (int i = 0; i < 32; i++)
+            var bitNum = 32;
+            
+            var tmpBitArray = new BitArray(bitNum);
+            for (int i = 0; i < bitNum; i++)
             {
                 tmpBitArray[i] = _bitArray[i + index];
-            }    
-            var tmp = new int[1];
-            tmpBitArray.CopyTo(tmp, 0);
-            return tmp[0];
+            }
+            
+            index += bitNum;
+            return BitConverter.ToInt32(ToByteArray(tmpBitArray));
         }
-        
+        byte[] ToByteArray(BitArray bits)
+        {
+            const int BYTE = 8;
+            int length = (bits.Count / BYTE) + ((bits.Count % BYTE == 0) ? 0 : 1);
+            var bytes = new byte[length];
+
+            for (int i = 0; i < bits.Length; i++)
+            {
+                int bitIndex = i % BYTE;
+                int byteIndex = i / BYTE;
+                
+                int mask = (bits[i] ? 1 : 0) << bitIndex;
+                bytes[byteIndex] |= (byte) mask;
+            }
+
+            return bytes;
+        }
     }
 }
