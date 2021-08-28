29e8032357679d7195b258d5bbe3e8b6580fc35d
sakastudio <sakastudio100@gmail.com>
2021-08-29 00:18:02 +0900

    テストが通るようにリファクタ

--- Server/Player/PlayerCoordinateResponse.cs
+++ Server/Player/PlayerCoordinateResponse.cs
@@ -1,15 +1,27 @@
-﻿using industrialization.OverallManagement.DataStore;
+﻿using industrialization.Core.Block;
+using industrialization.OverallManagement.DataStore;
+using industrialization.Server.Const;
 
 namespace industrialization.Server.Player
 {
     public class PlayerCoordinateResponse
     {
-        public Coordinate originPosition { get; }
-        public int[,] blocks { get; }
+        public Coordinate OriginPosition { get; }
+        public int[,] Blocks { get; }
         public PlayerCoordinateResponse(Coordinate originPosition)
         {
-            this.originPosition = originPosition;
-            //TODO ここに座標取得処理を作成
+            this.OriginPosition = originPosition;
+            Blocks = new int[
+                ChunkResponseConst.PlayerVisibleRangeChunk * ChunkResponseConst.ChunkSize,
+                ChunkResponseConst.PlayerVisibleRangeChunk * ChunkResponseConst.ChunkSize];
+
+            for (int i = 0; i < Blocks.GetLength(0); i++)
+            {
+                for (int j = 0; j < Blocks.GetLength(1); j++)
+                {
+                    Blocks[i, j] = BlockConst.NullIntId;
+                }                
+            }
         }
     }
 }
