86c8d7a26c2f8fbf4f593f518c25502b4607ca25
sakastudio <sakastudio100@gmail.com>
2021-08-31 19:10:20 +0900

    基本的な要素は書いた

--- Server/Player/PlayerCoordinateToResponse.cs
+++ Server/Player/PlayerCoordinateToResponse.cs
@@ -1,15 +1,55 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using industrialization.Core;
 using industrialization.OverallManagement.DataStore;
+using industrialization.OverallManagement.Util;
+using industrialization.Server.Const;
 
 namespace industrialization.Server.Player
 {
     public class PlayerCoordinateToResponse
     {
-        private Coordinate _coordinate;
+        private Coordinate _lastCoordinate = new Coordinate {x = Int32.MaxValue, y = Int32.MaxValue};
         public List<Coordinate> GetResponseCoordinate(Coordinate coordinate)
         {
             //TODO ここに座標取得処理を作成
+            var now = GetCoordinates(coordinate);
+            var last = GetCoordinates(_lastCoordinate);
+            _lastCoordinate = coordinate;
+            for (int i = now.Count - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < last.Count; j++)
+                {
+                    //もし前回の取得チャンクに今回の取得チャンクとの被りがあったら削除する
+                    if (!now.Contains(last[j])) continue;
+                    now.RemoveAt(i);
+                    last.RemoveAt(j);
+                    break;
+                }
+            }
+            
             return new List<Coordinate>();
+        }
+
+        private List<Coordinate> GetCoordinates(Coordinate coordinate)
+        {
+            var chunkHalf = ChunkResponseConst.PlayerVisibleRangeChunk / 2;
+            //その座標のチャンクの原点
+            var x = coordinate.x / ChunkResponseConst.ChunkSize * ChunkResponseConst.ChunkSize;
+            var y = coordinate.y / ChunkResponseConst.ChunkSize * ChunkResponseConst.ChunkSize;
+            
+            var result = new List<Coordinate>();
+            for (int i = -chunkHalf; i <= chunkHalf; i++)
+            {
+                for (int j = -chunkHalf; j <= chunkHalf; j++)
+                {
+                    result.Add(CoordinateCreator.New(
+                        x + i * ChunkResponseConst.ChunkSize,
+                        y + i * ChunkResponseConst.ChunkSize));
+                }
+            }
+
+            return result;
         }
     }
 }
