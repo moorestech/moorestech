c72f026239481c09940edfbb8fb3cc528dd9cf5a
sakastudio <sakastudio100@gmail.com>
2021-09-09 14:34:32 +0900

    ブロックからペイロード作る部分を分離

---
+++ Server/PacketHandle/PacketResponse/Player/BlockToPayload.cs
@@ -0,0 +1,12 @@
+using industrialization.OverallManagement.DataStore;
+
+namespace industrialization.Server.PacketHandle.PacketResponse.Player
+{
+    public class BlockToPayload
+    {
+        public static byte[] Convert(int[,] block, Coordinate chunkCoordinate)
+        {
+            
+        }
+    }
+}
