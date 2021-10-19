aadee8ad9b20989cc27a19ae6f94eca48593663b
sakastudio <sakastudio100@gmail.com>
2021-10-19 14:08:11 +0900

    何もない時何も返らないテスト

--- Test/CombinedTest/Server/PacketTest/Event/BlockPlaceEventPacketTest.cs
+++ Test/CombinedTest/Server/PacketTest/Event/BlockPlaceEventPacketTest.cs
@@ -1,3 +1,8 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using Server.PacketHandle;
+using Server.Util;
+
 namespace Test.CombinedTest.Server.PacketTest.Event
 {
     public class BlockPlaceEventPacketTest
@@ -6,7 +11,8 @@ namespace Test.CombinedTest.Server.PacketTest.Event
         [Test]
         public void DontBlockPlaceTest()
         {
-            
+            var response = PacketResponseCreator.GetPacketResponse(EventRequestData(0));
+            Assert.AreEqual(response.Count,0);
         }
         
         //ブロックを1個設置した時に1個のブロック設置イベントが返ってくるテスト
@@ -15,5 +21,15 @@ namespace Test.CombinedTest.Server.PacketTest.Event
         {
             
         }
+
+        byte[] EventRequestData(int plyaerID)
+        {
+            var payload = new List<byte>();
+            payload.AddRange(ByteArrayConverter.ToByteArray((short)4));
+            payload.AddRange(ByteArrayConverter.ToByteArray(plyaerID));
+            return payload.ToArray();
+        }
+        
+        
     }
 }
