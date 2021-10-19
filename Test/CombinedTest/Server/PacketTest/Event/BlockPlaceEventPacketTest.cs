8ce1b507db9a63614a614fa06585347f111e5c56
sakastudio <sakastudio100@gmail.com>
2021-10-19 13:38:38 +0900

    テストクラスの定義

---
+++ Test/CombinedTest/Server/PacketTest/Event/BlockPlaceEventPacketTest.cs
@@ -0,0 +1,19 @@
+namespace Test.CombinedTest.Server.PacketTest.Event
+{
+    public class BlockPlaceEventPacketTest
+    {
+        //ブロックを設置しなかった時何も返ってこないテスト
+        [Test]
+        public void DontBlockPlaceTest()
+        {
+            
+        }
+        
+        //ブロックを1個設置した時に1個のブロック設置イベントが返ってくるテスト
+        [Test]
+        public void OneBlockPlaceEvent()
+        {
+            
+        }
+    }
+}
