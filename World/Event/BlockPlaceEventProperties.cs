ddb09650046d63797d7d3a7b3ebb77460825f7e8
sakastudio <sakastudio100@gmail.com>
2021-10-22 12:38:18 +0900

    プロパティの中身定義

--- World/Event/BlockPlaceEventProperties.cs
+++ World/Event/BlockPlaceEventProperties.cs
@@ -1,7 +1,17 @@
+using Core.Block;
+using World.DataStore;
+
 namespace World.Event
 {
     public class BlockPlaceEventProperties
     {
-        
+        public readonly Coordinate Coordinate;
+        public readonly IBlock Block;
+
+        public BlockPlaceEventProperties(Coordinate coordinate, IBlock block)
+        {
+            Coordinate = coordinate;
+            Block = block;
+        }
     }
 }
