78a7469f1a2dd1495c143ecb6cca63c9ddfe1f19
sakastudio <sakastudio100@gmail.com>
2021-10-22 14:19:17 +0900

    deep copyするクラス

---
+++ Core/Util/ListDeepCopy.cs
@@ -0,0 +1,18 @@
+namespace Core.Util
+{
+    public static class ListDeepCopy
+    {
+        public static T Copy<T>(this T src)
+        {
+            using (var memoryStream = new System.IO.MemoryStream())
+            {
+                var binaryFormatter
+                    = new System.Runtime.Serialization
+                        .Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(memoryStream, src); // シリアライズ
+                memoryStream.Seek(0, System.IO.SeekOrigin.Begin);
+                return (T)binaryFormatter.Deserialize(memoryStream); // デシリアライズ
+            }
+        }
+    }
+}
