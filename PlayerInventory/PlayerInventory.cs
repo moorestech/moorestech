f695d2811d0ba8c1e72e15d9f05c2b986a1d5ffc
sakastudio <sakastudio100@gmail.com>
2021-11-20 23:41:33 +0900

    player inventoryのコンストラクタ

--- PlayerInventory/PlayerInventory.cs
+++ PlayerInventory/PlayerInventory.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using Core.Item;
+
 namespace PlayerInventory
 {
     public class PlayerInventory
     {
         public readonly int PlayerId;
-        
+        private readonly List<IItemStack> MainInventory;
+
+        public PlayerInventory(int playerId)
+        {
+            PlayerId = playerId;
+            MainInventory = new List<IItemStack>();
+            for (int i = 0; i < PlayerInventoryConst.MainInventorySize; i++)
+            {
+                MainInventory.Add(new NullItemStack());
+            }
+        }
     }
 }
