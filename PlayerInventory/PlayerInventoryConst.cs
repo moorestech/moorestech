7edc6418f748492d849c16a047c9ad0a681c5f93
sakastudio <sakastudio100@gmail.com>
2021-11-20 23:19:51 +0900

    プレイヤーインベントリの定数

---
+++ PlayerInventory/PlayerInventoryConst.cs
@@ -0,0 +1,9 @@
+namespace PlayerInventory
+{
+    public class PlayerInventoryConst
+    {
+        public const int MainInventoryColumns = 9;
+        public const int MainInventoryRows = 5;
+        public const int MainInventorySize = MainInventoryColumns * MainInventoryRows;
+    }
+}
