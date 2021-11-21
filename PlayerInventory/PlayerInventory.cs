2b90c2e9b89ad5c4b56e32eb4a37894d43cf1b92
sakastudio <sakastudio100@gmail.com>
2021-11-21 23:10:14 +0900

    todo

--- PlayerInventory/PlayerInventory.cs
+++ PlayerInventory/PlayerInventory.cs
@@ -5,8 +5,12 @@ namespace PlayerInventory
 {
     public class PlayerInventory
     {
+        //TODO インベントリに必要な機能
+        //TODO インベントリからアイテムを取る
+        //TODO インベントリにアイテムを入れる
+        
         public readonly int PlayerId;
-        private readonly List<IItemStack> MainInventory;
+        private readonly List<IItemStack> MainInventory;    
 
         public PlayerInventory(int playerId)
         {
