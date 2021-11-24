e03275769d1199f250134bd0745ffdc01623c1fe
sakastudio <sakastudio100@gmail.com>
2021-11-24 14:23:33 +0900

    テストのクラス

---
+++ Test/CombinedTest/Server/PacketTest/PlayerInventoryProtocolTest.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Test.CombinedTest.Server.PacketTest
+{
+    public class PlayerInventoryProtocolTest
+    {
+        //TODO テストの実装
+        public void GetPlayerInventoryProtocolTest()
+        {
+        }
+    }
+}
