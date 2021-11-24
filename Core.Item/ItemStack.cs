9653f71daf7424a6d4d0c56f9f68060e0f86ece0
sakastudio <sakastudio100@gmail.com>
2021-11-24 11:01:00 +0900

    0以下になるときでもテストが通る

--- Core.Item/ItemStack.cs
+++ Core.Item/ItemStack.cs
@@ -55,21 +55,13 @@ namespace Core.Item
             return new ItemProcessResult(new ItemStack(Id, newAmount), new NullItemStack());
         }
 
-        //TODO 0以下になるテストを書く
         public IItemStack SubItem(int subAmount)
         {
             if (0 < Amount - subAmount)
             {
                 return new ItemStack(Id,Amount-subAmount);
             }
-            else if(0 == Amount - subAmount)
-            {
-                return new NullItemStack();
-            }
-            else
-            {
-                return this;
-            }
+            return new NullItemStack();
         }
 
         public bool IsAllowedToAdd(IItemStack item)
