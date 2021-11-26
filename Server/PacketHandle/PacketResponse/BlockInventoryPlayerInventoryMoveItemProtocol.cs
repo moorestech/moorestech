1e5a6ed09c161797983f69e70aee257471c7867c
sakastudio <sakastudio100@gmail.com>
2021-11-26 18:03:33 +0900

    インベントリ同士アイテムを移す

--- Server/PacketHandle/PacketResponse/BlockInventoryPlayerInventoryMoveItemProtocol.cs
+++ Server/PacketHandle/PacketResponse/BlockInventoryPlayerInventoryMoveItemProtocol.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Core.Item;
 using PlayerInventory;
 using Server.Util;
 using World;
@@ -22,26 +23,48 @@ namespace Server.PacketHandle.PacketResponse
             payloadData.MoveNextToGetShort();
             var flag = payloadData.MoveNextToGetShort();
             var playerId = payloadData.MoveNextToGetInt();
-            var playerInventoryIndex = payloadData.MoveNextToGetInt();
+            var playerInventorySlot = payloadData.MoveNextToGetInt();
             var blockX = payloadData.MoveNextToGetInt();
             var blockY = payloadData.MoveNextToGetInt();
             var blockInventoryIndex = payloadData.MoveNextToGetInt();
-            var moveItemNumber = payloadData.MoveNextToGetInt();
-
+            var moveItemAmount = payloadData.MoveNextToGetInt();
+            
+            var blockInventory = _worldBlockDatastore.GetBlockInventory(blockX, blockY);
+            var playerInventory = _playerInventoryDataStore.GetInventoryData(playerId);
 
             //フラグが0の時はプレイヤーインベントリからブロックインベントリにアイテムを移す
             if (flag == 0)
             {
+                //プレイヤーインベントリからアイテムを取得
+                var originItem = playerInventory.GetItem(playerInventorySlot);
+                if (originItem.Amount < moveItemAmount)
+                {
+                    moveItemAmount = originItem.Amount;
+                }
+                //実際に移動するアイテムを取得
+                var moveItem = ItemStackFactory.Create(originItem.Id,moveItemAmount);
+                //ブロックインベントリにアイテムを移動
+                var replaceItem = blockInventory.ReplaceItem(blockInventoryIndex,moveItem);
+                
+                //プレイヤーインベントリに残るアイテムを計算
+                var remainItem = replaceItem.AddItem(ItemStackFactory.Create(originItem.Id,originItem.Amount - moveItemAmount)).ProcessResultItemStack;
+                
+                //プレイヤーインベントリに残りのアイテムを追加
+                playerInventory.SetItem(playerInventorySlot,remainItem);
                 
             }
             //1の時はブロックからプレイヤーインベントリにアイテムを移す
             else if (flag == 1)
             {
-                
-            }
-            else
-            {
-                
+                var originItem = blockInventory.GetItem(blockInventoryIndex);
+                if (originItem.Amount < moveItemAmount)
+                {
+                    moveItemAmount = originItem.Amount;
+                }
+                var moveItem = ItemStackFactory.Create(originItem.Id,moveItemAmount);
+                var replaceItem = playerInventory.ReplaceItem(playerInventorySlot,moveItem);
+                var remainItem = replaceItem.AddItem(ItemStackFactory.Create(originItem.Id,originItem.Amount - moveItemAmount)).ProcessResultItemStack;
+                blockInventory.SetItem(blockInventoryIndex,remainItem);
             }
 
             return new List<byte[]>();
