77c2b816d4a0f9fc1fdeb6060765dd45b99678af
sakastudio <sakastudio100@gmail.com>
2021-11-26 13:37:29 +0900

    インターフェース継承

--- Server/PacketHandle/PacketResponse/BlockInventoryPlayerInventoryMoveItemProtocol.cs
+++ Server/PacketHandle/PacketResponse/BlockInventoryPlayerInventoryMoveItemProtocol.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+
 namespace Server.PacketHandle.PacketResponse
 {
-    public class BlockInventoryPlayerInventoryMoveItemProtocol
+    public class BlockInventoryPlayerInventoryMoveItemProtocol : IPacketResponse
     {
-        
+        public List<byte[]> GetResponse(List<byte> payload)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
