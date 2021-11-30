2871e001b63bf642393c40280e320810defd4816
sakastudio <sakastudio100@gmail.com>
2021-11-30 12:39:53 +0900

    イベントのプロパティ実装

--- Game.PlayerInventory/Event/PlayerInventoryUpdateEventProperties.cs
+++ Game.PlayerInventory/Event/PlayerInventoryUpdateEventProperties.cs
@@ -2,6 +2,18 @@ namespace PlayerInventory.Event
 {
     public class PlayerInventoryUpdateEventProperties
     {
-        //TODO プロパティの実装
+        public readonly int playerId;
+        public readonly int inventorySlot;
+        public readonly int itemId;
+        public readonly int amount;
+
+
+        public PlayerInventoryUpdateEventProperties(int playerId, int inventorySlot, int itemId, int amount)
+        {
+            this.playerId = playerId;
+            this.inventorySlot = inventorySlot;
+            this.itemId = itemId;
+            this.amount = amount;
+        }
     }
 }
