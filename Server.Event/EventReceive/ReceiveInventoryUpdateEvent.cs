12515e8290fd885f679e7f4bd7580da398c10b94
sakastudio <sakastudio100@gmail.com>
2021-12-01 10:09:24 +0900

    プロトコル実装

--- Server.Event/EventReceive/ReceiveInventoryUpdateEvent.cs
+++ Server.Event/EventReceive/ReceiveInventoryUpdateEvent.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using PlayerInventory.Event;
+using Server.Util;
 
 namespace Server.Event.EventReceive
 {
     public class ReceiveInventoryUpdateEvent
     {
         private readonly EventProtocolProvider _eventProtocolProvider;
+        private const short EventId = 1;
 
         public ReceiveInventoryUpdateEvent(PlayerInventoryUpdateEvent playerInventoryUpdateEvent,EventProtocolProvider eventProtocolProvider)
         {
@@ -13,9 +16,19 @@ namespace Server.Event.EventReceive
         }
         
         
-        private void ReceivedEvent(PlayerInventoryUpdateEventProperties blockPlaceEventProperties)
+        private void ReceivedEvent(PlayerInventoryUpdateEventProperties playerInventoryUpdateEvent)
         {
             //TODO プロトコルの実装
+            var payload = new List<byte>();
+            
+            
+            payload.AddRange(ByteListConverter.ToByteArray(ServerEventConst.EventPacketId));
+            payload.AddRange(ByteListConverter.ToByteArray(EventId));
+            payload.AddRange(ByteListConverter.ToByteArray(playerInventoryUpdateEvent.inventorySlot));
+            payload.AddRange(ByteListConverter.ToByteArray(playerInventoryUpdateEvent.itemId));
+            payload.AddRange(ByteListConverter.ToByteArray(playerInventoryUpdateEvent.amount));
+            
+            _eventProtocolProvider.AddEvent(playerInventoryUpdateEvent.playerId,payload.ToArray());
 
         }
     }
