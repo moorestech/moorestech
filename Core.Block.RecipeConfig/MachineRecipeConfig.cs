f75adaa303ca6c66bcaf326f9490c5904ea89f6c
sakastudio <sakastudio100@gmail.com>
2021-12-07 11:24:45 +0900

    reformat

--- Core.Block.RecipeConfig/MachineRecipeConfig.cs
+++ Core.Block.RecipeConfig/MachineRecipeConfig.cs
@@ -8,69 +8,64 @@ namespace Core.Block.RecipeConfig
 {
     public class MachineRecipeConfig : IMachineRecipeConfig
     {
+        private readonly IMachineRecipeData[] _recipedatas;
 
-    private readonly IMachineRecipeData[] _recipedatas;
-
-    //IDからレシピデータを取得する
-    public MachineRecipeConfig()
-    {
-        _recipedatas = new MachineRecipeJsonLoad().LoadConfig();
-    }
-
-    public IMachineRecipeData GetRecipeData(int id)
-    {
-        return _recipedatas[id];
-    }
-
-    public IMachineRecipeData GetNullRecipeData()
-    {
-        return new NullMachineRecipeData();
-    }
-
-    private Dictionary<string, IMachineRecipeData> _recipeDataCash;
-
-    /// <summary>
-    /// 設置物IDと現在の搬入スロットからレシピを検索し、取得する
-    /// </summary>
-    /// <param name="BlockId">設置物ID</param>
-    /// <param name="inputItem">搬入スロット</param>
-    /// <returns>レシピデータ</returns>
-    public IMachineRecipeData GetRecipeData(int BlockId, List<IItemStack> inputItem)
-    {
-
-        if (_recipeDataCash == null)
+        //IDからレシピデータを取得する
+        public MachineRecipeConfig()
         {
-            _recipeDataCash = new Dictionary<string, IMachineRecipeData>();
-            _recipedatas.ToList().ForEach(recipe =>
-            {
-                _recipeDataCash.Add(
-                    GetKey(recipe.BlockId, recipe.ItemInputs.ToList()),
-                    recipe);
-            });
+            _recipedatas = new MachineRecipeJsonLoad().LoadConfig();
         }
 
-        var tmpInputItem = inputItem.Where(i => i.Id != ItemConst.NullItemId).ToList();
-        tmpInputItem.Sort((a, b) => a.Id - b.Id);
-        var key = GetKey(BlockId, tmpInputItem);
-        if (_recipeDataCash.ContainsKey(key))
+        public IMachineRecipeData GetRecipeData(int id)
         {
-            return _recipeDataCash[key];
+            return _recipedatas[id];
         }
-        else
+
+        public IMachineRecipeData GetNullRecipeData()
         {
             return new NullMachineRecipeData();
         }
-    }
 
-    private string GetKey(int blockId, List<IItemStack> itemId)
-    {
-        var items = "";
-        itemId.Sort((a, b) => a.Id - b.Id);
-        itemId.ForEach(i =>
+        private Dictionary<string, IMachineRecipeData> _recipeDataCash;
+
+        /// <summary>
+        /// 設置物IDと現在の搬入スロットからレシピを検索し、取得する
+        /// </summary>
+        /// <param name="BlockId">設置物ID</param>
+        /// <param name="inputItem">搬入スロット</param>
+        /// <returns>レシピデータ</returns>
+        public IMachineRecipeData GetRecipeData(int BlockId, List<IItemStack> inputItem)
         {
-            items = items + "_" + i.Id;
-        });
-        return blockId + items;
-    }
+            if (_recipeDataCash == null)
+            {
+                _recipeDataCash = new Dictionary<string, IMachineRecipeData>();
+                _recipedatas.ToList().ForEach(recipe =>
+                {
+                    _recipeDataCash.Add(
+                        GetKey(recipe.BlockId, recipe.ItemInputs.ToList()),
+                        recipe);
+                });
+            }
+
+            var tmpInputItem = inputItem.Where(i => i.Id != ItemConst.NullItemId).ToList();
+            tmpInputItem.Sort((a, b) => a.Id - b.Id);
+            var key = GetKey(BlockId, tmpInputItem);
+            if (_recipeDataCash.ContainsKey(key))
+            {
+                return _recipeDataCash[key];
+            }
+            else
+            {
+                return new NullMachineRecipeData();
+            }
+        }
+
+        private string GetKey(int blockId, List<IItemStack> itemId)
+        {
+            var items = "";
+            itemId.Sort((a, b) => a.Id - b.Id);
+            itemId.ForEach(i => { items = items + "_" + i.Id; });
+            return blockId + items;
+        }
     }
 }
