3d6117ee2eb7690eecad4a76142f86b05988edc6
sakastudio <sakastudio100@gmail.com>
2021-12-11 08:39:12 +0900

    クラス定義

---
+++ Game.Save/Json/AssembleSaveJsonText.cs
@@ -0,0 +1,22 @@
+using Game.PlayerInventory.Interface;
+using Game.World.Interface;
+
+namespace Game.Save.Json
+{
+    public class AssembleSaveJsonText
+    {
+        private IWorldBlockDatastore _worldBlockDatastore;
+        private IPlayerInventoryDataStore _inventoryDataStore;
+
+        public AssembleSaveJsonText(IPlayerInventoryDataStore inventoryDataStore, IWorldBlockDatastore worldBlockDatastore)
+        {
+            _inventoryDataStore = inventoryDataStore;
+            _worldBlockDatastore = worldBlockDatastore;
+        }
+        
+        public string AssembleSaveJson()
+        {
+            return "";
+        }
+    }
+}
