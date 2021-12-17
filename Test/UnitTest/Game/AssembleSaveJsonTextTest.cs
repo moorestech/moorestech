a23ab7907541e8873c4d8df58f6c39c0aae77407
sakastudio <sakastudio100@gmail.com>
2021-12-17 14:14:46 +0900

    ブロックを設置した時のテスト

--- Test/UnitTest/Game/AssembleSaveJsonTextTest.cs
+++ Test/UnitTest/Game/AssembleSaveJsonTextTest.cs
@@ -1,3 +1,5 @@
+using Core.Block;
+using Core.Block.BlockFactory;
 using Game.Save.Json;
 using Game.World.Interface;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,11 +26,12 @@ namespace Test.UnitTest.Game
             var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create();
             var assembleSaveJsonText = serviceProvider.GetService<AssembleSaveJsonText>();
             var worldBlockDatastore = serviceProvider.GetService<IWorldBlockDatastore>();
-            
-            //worldBlockDatastore.AddBlock()
+
+            worldBlockDatastore.AddBlock(new NormalBlock( 10, 10), 0, 0);
+            worldBlockDatastore.AddBlock(new NormalBlock( 15, 100), 10,-15);
             
             var json = assembleSaveJsonText.AssembleSaveJson();
-            Assert.AreEqual("{\"world\":[],\"inventory\":[]}",json);
+            Assert.AreEqual("{\"world\":[{\"id\":10,\"X\":0,\"Y\":0,\"intId\":10,\"param\":{}},{\"id\":15,\"X\":10,\"Y\":-15,\"intId\":100,\"param\":{}}],\"inventory\":[]}",json);
         }
         
     }
