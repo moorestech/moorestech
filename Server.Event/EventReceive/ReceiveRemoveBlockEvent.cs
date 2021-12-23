7e4d485179c09cf07a1664fa0be7360f141cc7a7
sakastudio <sakastudio100@gmail.com>
2021-12-23 11:02:21 +0900

    ブロック削除イベントを受信する

---
+++ Server.Event/EventReceive/ReceiveRemoveBlockEvent.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using Game.World.Interface.Event;
+using Server.Util;
+
+namespace Server.Event.EventReceive
+{
+    public class ReceiveRemoveBlockEvent
+    {
+        
+        private readonly EventProtocolProvider _eventProtocolProvider;
+        private const short EventId = 3;
+
+        public ReceiveRemoveBlockEvent(IBlockRemoveEvent blockRemoveEvent,EventProtocolProvider eventProtocolProvider)
+        {
+            blockRemoveEvent.Subscribe(ReceivedEvent);
+            _eventProtocolProvider = eventProtocolProvider;
+        }
+        
+        private void ReceivedEvent(BlockRemoveEventProperties blockPlaceEventProperties)
+        {
+            var c = blockPlaceEventProperties.Coordinate;
+            var payload = new List<byte>();
+            
+            payload.AddRange(ByteListConverter.ToByteArray(ServerEventConst.EventPacketId));
+            payload.AddRange(ByteListConverter.ToByteArray(EventId));
+            payload.AddRange(ByteListConverter.ToByteArray(c.X));
+            payload.AddRange(ByteListConverter.ToByteArray(c.Y));
+            
+            _eventProtocolProvider.AddBroadcastEvent(payload.ToArray());
+
+        }
+
+    }
+}
