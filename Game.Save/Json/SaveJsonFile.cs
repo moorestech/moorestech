bcda8e490c0a9e047e9597b6548b02b8aa024dff
sakastudio <sakastudio100@gmail.com>
2021-12-23 14:35:24 +0900

    ファイル実際にセーブする

--- Game.Save/Json/SaveJsonFile.cs
+++ Game.Save/Json/SaveJsonFile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Game.PlayerInventory.Interface;
 using Game.Save.Interface;
 using Game.World.Interface;
@@ -18,6 +19,7 @@ namespace Game.Save.Json
 
         public void Save()
         {
+            File.AppendAllText(_fileName.FullSaveFilePath, _assembleSaveJsonText.AssembleSaveJson());
         }
     }
 }
