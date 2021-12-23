8f50d7f7c25cef41d12b4f42dea3c3ffb2873745
sakastudio <sakastudio100@gmail.com>
2021-12-23 14:35:10 +0900

    OSによってファイルパスを変更する

--- Game.Save/Json/SaveJsonFileName.cs
+++ Game.Save/Json/SaveJsonFileName.cs
@@ -1,13 +1,35 @@
+using System;
+using System.IO;
+
 namespace Game.Save.Json
 {
+    /// <summary>
+    /// JSONでセーブするさいのファイル名を指定するクラス
+    /// </summary>
     public class SaveJsonFileName
     {
-        public string FilePath => _filePath;
+        public string FullSaveFilePath => _filePath;
         private string _filePath;
 
-        public SaveJsonFileName(string filePath)
+        public SaveJsonFileName(string fileName)
         {
-            _filePath = filePath;
+            ChangeFileName(fileName);
+        }
+
+        public void ChangeFileName(string fileName)
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.WinCE:
+                    _filePath = Path.Combine("C:", "Users", Environment.UserName, "AppData","Roaming",".moorestech","saves",fileName);
+                    break;
+                case PlatformID.Unix:
+                    _filePath = Path.Combine("Users", Environment.UserName, "Library","Application Support","moorestech","saves",fileName);
+                    break;
+            }
         }
     }
 }
