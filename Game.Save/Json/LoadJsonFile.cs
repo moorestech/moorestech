b5b7fed1ad0cdf4d6b8545d72aa386caee51e8f5
sakastudio <sakastudio100@gmail.com>
2021-12-24 13:42:45 +0900

    ロードのシステム

--- Game.Save/Json/LoadJsonFile.cs
+++ Game.Save/Json/LoadJsonFile.cs
@@ -1,12 +1,29 @@
+using System.IO;
+using Game.PlayerInventory.Interface;
 using Game.Save.Interface;
+using Game.World.Interface.DataStore;
+using Newtonsoft.Json;
 
 namespace Game.Save.Json
 {
     public class LoadJsonFile : ILoadRepository
     {
+        SaveJsonFileName _saveJsonFileName;
+        
+        private IWorldBlockDatastore _worldBlockDatastore;
+        private IPlayerInventoryDataStore _inventoryDataStore;
+
+        public LoadJsonFile(SaveJsonFileName saveJsonFileName, IWorldBlockDatastore worldBlockDatastore, IPlayerInventoryDataStore inventoryDataStore)
+        {
+            _saveJsonFileName = saveJsonFileName;
+            _worldBlockDatastore = worldBlockDatastore;
+            _inventoryDataStore = inventoryDataStore;
+        }
+
         public void Load()
         {
-            throw new System.NotImplementedException();
+            var load =  JsonConvert.DeserializeObject<SaveData>(File.ReadAllText(_saveJsonFileName.FullSaveFilePath));
+            _worldBlockDatastore.LoadBlockDataList(load.World);
         }
     }
 }
