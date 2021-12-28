8826c43e1f61f82de3332b7f43a2e53c7f9a6386
sakastudio <sakastudio100@gmail.com>
2021-12-28 14:02:07 +0900

    クラスリネーム

--- Game.World.Interface/DataStore/IWorldBlockElectricDatastore.cs
+++ Game.World.Interface/DataStore/IWorldBlockElectricDatastore.cs
@@ -2,7 +2,7 @@ using Core.Electric;
 
 namespace Game.World.Interface.DataStore
 {
-    public interface IWorldBlockElectric
+    public interface IWorldBlockElectricDatastore
     {
         public bool ExistsBlockElectric(int x, int y);
         public IBlockElectric GetBlock(int x, int y);
