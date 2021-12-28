c50e124d7b03618ea9276d55d30fde2d29c0c0ce
sakastudio <sakastudio100@gmail.com>
2021-12-28 14:02:16 +0900

    実装したクラス

---
+++ Game.World/DataStore/WorldBlockElectricDatastore.cs
@@ -0,0 +1,19 @@
+using Core.Electric;
+using Game.World.Interface.DataStore;
+
+namespace World.DataStore
+{
+    public class WorldBlockElectricDatastore : IWorldBlockElectricDatastore
+
+    {
+        public bool ExistsBlockElectric(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IBlockElectric GetBlock(int x, int y)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
