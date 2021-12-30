289b589dda7daa9fde3d136e7c1cd48f0e396f66
sakastudio <sakastudio100@gmail.com>
2021-12-30 15:36:24 +0900

    電柱の機械接続範囲取得

---
+++ Game.World/Service/MaxElectricPoleMachineConnectionRange.cs
@@ -0,0 +1,30 @@
+using System;
+using Core.Block.Config;
+using Core.Block.Config.LoadConfig.Param;
+
+namespace World.Service
+{
+    public class MaxElectricPoleMachineConnectionRange
+    {
+        int maxElectricPoleMachineConnectionRange = Int32.MinValue;
+
+        public MaxElectricPoleMachineConnectionRange(IBlockConfig blockConfig)
+        {
+            foreach (var id in blockConfig.GetBlockIds())
+            {
+                if (blockConfig.GetBlockConfig(id).Type != VanillaBlockType.ElectricPole) continue;
+                
+                var param = blockConfig.GetBlockConfig(id).Param as ElectricPoleConfigParam;
+                if (maxElectricPoleMachineConnectionRange < param.machineConnectionRange)
+                {
+                    maxElectricPoleMachineConnectionRange = param.machineConnectionRange;
+                }
+            }
+        }
+
+        public int Get()
+        {
+            return maxElectricPoleMachineConnectionRange;
+        }
+    }
+}
