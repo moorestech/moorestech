84e24848e4f138ec69310f1f1de3c4428837b38a
sakastudio <sakastudio100@gmail.com>
2021-12-31 09:34:46 +0900

    IBlockInventory実装

--- Core.Block/PowerGenerator/VanillaPowerGenerator.cs
+++ Core.Block/PowerGenerator/VanillaPowerGenerator.cs
@@ -1,9 +1,11 @@
+using Core.Block.BlockInventory;
 using Core.Electric;
+using Core.Item;
 
 namespace Core.Block.PowerGenerator
 {
     //TODO アイテムを挿入して発電するシステムを作る
-    public class VanillaPowerGenerator : IBlock,IPowerGenerator
+    public class VanillaPowerGenerator : IBlock,IPowerGenerator,IBlockInventory
     {
         private readonly int _blockId;
         private readonly int _intId;
@@ -32,6 +34,21 @@ namespace Core.Block.PowerGenerator
         public int GetBlockId()
         {
             return _blockId;
+        }
+
+        public IItemStack InsertItem(IItemStack itemStack)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AddConnector(IBlockInventory blockInventory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveConnector(IBlockInventory blockInventory)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
