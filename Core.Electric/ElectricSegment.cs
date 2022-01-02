9b244be452f663274a21e0b3f0455027538e801e
sakastudio <sakastudio100@gmail.com>
2022-01-02 11:08:42 +0900

    マージのロジック

--- Core.Electric/ElectricSegment.cs
+++ Core.Electric/ElectricSegment.cs
@@ -93,10 +93,5 @@ namespace Core.Electric
         {
             return _electricPoles.ContainsKey(id);
         }
-
-        public ElectricSegment Merge(ElectricSegment electricSegment)
-        {
-            throw new System.NotImplementedException();
-        }
     }
 }
