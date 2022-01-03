21eaeb44641d5742c98e8330bbb62ac55c540713
sakastudio <sakastudio100@gmail.com>
2022-01-03 18:45:41 +0900

    VanillaMiner定義

---
+++ Core.Block/Blocks/Miner/VanillaMiner.cs
@@ -0,0 +1,48 @@
+using Core.Block.BlockInventory;
+using Core.Electric;
+using Core.Item;
+
+namespace Core.Block.Blocks.Miner
+{
+    public class VanillaMiner: IBlock,IBlockElectric,IBlockInventory
+    {
+        private readonly int _blockId;
+        private readonly int _intId;
+
+        public VanillaMiner(int blockId, int intId)
+        {
+            _blockId = blockId;
+            _intId = intId;
+        }
+
+        public int GetRequestPower()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SupplyPower(int power)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetSaveState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+        public void AddConnector(IBlockInventory blockInventory)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveConnector(IBlockInventory blockInventory)
+        {
+            throw new System.NotImplementedException();
+        }
+        
+        public IItemStack InsertItem(IItemStack itemStack) { return itemStack; }
+        public int GetIntId() { return _intId; }
+        public int GetBlockId() { return _blockId; }
+    }
+}
