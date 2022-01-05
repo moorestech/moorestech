8203c8dffbbf4e92edd72e471a7a3edd7c2cf992
sakastudio <sakastudio100@gmail.com>
2022-01-05 22:21:59 +0900

    とりあえず仮の実装を行う

--- Game.WorldMap/CheckOreMining.cs
+++ Game.WorldMap/CheckOreMining.cs
@@ -2,12 +2,16 @@ using Core.Ore;
 
 namespace Game.WorldMap
 {
+    /// <summary>
+    /// そのintIdのブロックの直下にあるoreを返す
+    /// </summary>
     public class CheckOreMining : ICheckOreMining
     {
         public int Check(int intId)
         {
             //TODO ore configの実装をする
-            return 0;
+            //TODO とりあえず常にironを返す
+            return 1;
         }
     }
 }
