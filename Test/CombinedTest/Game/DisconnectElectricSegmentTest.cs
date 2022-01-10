e3d5a9775f4a60de2a87da4b63c890cdd07ad077
sakastudio <sakastudio100@gmail.com>
2022-01-10 15:15:12 +0900

    ループするときのテスト追加

--- Test/CombinedTest/Game/DisconnectElectricSegmentTest.cs
+++ Test/CombinedTest/Game/DisconnectElectricSegmentTest.cs
@@ -62,10 +62,52 @@ namespace Test.CombinedTest.Game
             worldBlockDatastore.RemoveBlock(6, 0);
             //セグメントが減っていることを確認する
             Assert.AreEqual(1, worldElectricSegment.GetElectricSegmentListCount());
+        }
 
-
-
-
+        //電柱を消してもループによって1つのセグメントになっている時のテスト
+        [Test]
+        public void LoopedElectricSegmentRemoveElectricPoleTest()
+        {
+            /*設置する電柱、機械、発電機の場所
+             * P □ □ P □ □ P
+             * G □ □ □ □ □ □ 
+             * M □ □ □ □ □ M
+             * P □ □ P □ □ P
+             * G □ □ G
+             */
+            var (_, saveServiceProvider) = new PacketResponseCreatorDiContainerGenerators().Create();
+            
+            var worldBlockDatastore = saveServiceProvider.GetService<IWorldBlockDatastore>();
+            var blockFactory = saveServiceProvider.GetService<BlockFactory>();
+            var worldElectricSegment = saveServiceProvider.GetService<IWorldElectricSegmentDatastore>();
+            
+            //電柱の設置
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 0), 0, 0, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 1), 3, 0, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 2), 6, 0, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 3), 0, 3, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 4), 3, 3, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(ElectricPoleId, 5), 6, 3, BlockDirection.North);
+            
+            //発電機と機械の設定
+            worldBlockDatastore.AddBlock(blockFactory.Create(MachineId, 103), 0, 1, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(PowerGenerateId, 104), 0, -1, BlockDirection.North);
+            
+            worldBlockDatastore.AddBlock(blockFactory.Create(PowerGenerateId, 105), 3, -1, BlockDirection.North);
+            worldBlockDatastore.AddBlock(blockFactory.Create(MachineId, 106), 6, 1, BlockDirection.North);
+            
+            
+            //セグメントの数を確認
+            Assert.AreEqual(1, worldElectricSegment.GetElectricSegmentListCount());
+            
+            //真ん中の電柱を削除
+            worldBlockDatastore.RemoveBlock(3, 0);
+            //セグメント数が変わってないかチェック
+            Assert.AreEqual(1, worldElectricSegment.GetElectricSegmentListCount());
+            
+            //真ん中の発電機がセグメントにないことを確認する
+            Assert.AreEqual(false,worldElectricSegment.GetElectricSegment(0).GetGenerators().ContainsKey(105));
+            
         }
     }
 }
