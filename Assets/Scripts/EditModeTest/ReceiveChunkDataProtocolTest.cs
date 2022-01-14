72057da8771dddf1ec31f970dd0ccacac25d6b81
sakastudio <sakastudio100@gmail.com>
2022-01-14 23:29:16 +0900

    サーバー側のutil系を移植

--- Assets/Scripts/EditModeTest/ReceiveChunkDataProtocolTest.cs
+++ Assets/Scripts/EditModeTest/ReceiveChunkDataProtocolTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using MainGame.GameLogic.Interface;
+using MainGame.Network.Receive;
+using NUnit.Framework;
+using UnityEngine;
 
 namespace EditModeTest
 {
@@ -7,7 +11,17 @@ namespace EditModeTest
         [Test]
         public void ReceiveChunkDataProtocolToRegisterDataStoreTest()
         {
+            var protocol = new ReceiveChunkDataProtocol(new TestDataStore());
             
+        }
+    }
+
+    class TestDataStore : IChunkDataStore
+    {
+        public Dictionary<Vector2,int[,]> Data { get; set; }
+        public void SetChunk(Vector2 chunkPosition, int[,] ids)
+        {
+            Data.Add(chunkPosition, ids);
         }
     }
 }
---
+++ Assets/Scripts/MainGame/Network.Util/ByteListConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MainGame.Network.Util
+{
+    public static class ByteListConverter
+    {
+        public static List<byte> ToByteArray(int sendData)
+        {
+            var result = BitConverter.GetBytes(sendData);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(result);
+            return result.ToList();
+        }
+
+        public static List<byte> ToByteArray(short sendData)
+        {
+            var result = BitConverter.GetBytes(sendData);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(result);
+            return result.ToList();
+        }
+
+        public static List<byte> ToByteArray(float sendData)
+        {
+            var result = BitConverter.GetBytes(sendData);
+            if (BitConverter.IsLittleEndian)
+                Array.Reverse(result);
+            return result.ToList();
+        }
+
+        public static List<byte> ToByteArray(string sendData)
+        {
+            return System.Text.Encoding.UTF8.GetBytes(sendData).ToList();
+        }
+    }
+}
