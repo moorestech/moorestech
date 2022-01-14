15efe427eefec86dfc02df456816c81bd4b38d2d
sakastudio <sakastudio100@gmail.com>
2022-01-14 23:17:31 +0900

    アセンブリの名前修正

--- Assets/Scripts/MainGame/Network/Receive/ReceiveChunkDataProtocol.cs
+++ Assets/Scripts/MainGame/Network/Receive/ReceiveChunkDataProtocol.cs
@@ -5,6 +5,12 @@ namespace MainGame.Network.Receive
     public class ReceiveChunkDataProtocol : IAnalysisPacket
     {
         private IChunkDataStore _chunkDataStore;
+
+        public ReceiveChunkDataProtocol(IChunkDataStore chunkDataStore)
+        {
+            _chunkDataStore = chunkDataStore;
+        }
+
         public void Analysis(byte[] data)
         {
             throw new System.NotImplementedException();
