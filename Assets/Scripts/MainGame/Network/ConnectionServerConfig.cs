9e8fb6bf77e0eab829b9efad49aff1a5dfc83222
sakastudio <sakastudio100@gmail.com>
2022-01-14 19:32:35 +0900

    サーバの接続設定

---
+++ Assets/Scripts/MainGame/Network/ConnectionServerConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace MainGame.Network
+{
+    public class ConnectionServerConfig
+    {
+        public readonly string IP;
+        public readonly int Port;
+
+        public ConnectionServerConfig(string ip, int port)
+        {
+            IP = ip;
+            Port = port;
+        }
+    }
+}
