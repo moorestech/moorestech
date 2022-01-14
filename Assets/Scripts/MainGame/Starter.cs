938545bbf9d8d65c6f28dae78744e00a63de094d
sakastudio <sakastudio100@gmail.com>
2022-01-14 19:30:37 +0900

    starterのスクリプト

---
+++ Assets/Scripts/MainGame/Starter.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MainGame
+{
+    public class Starter : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
