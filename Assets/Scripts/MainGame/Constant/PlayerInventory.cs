1a01061f593d669114536de61a831f423c090701
sakastudio <sakastudio100@gmail.com>
2022-01-15 12:59:50 +0900

    プレイヤーインベントリの定数

---
+++ Assets/Scripts/MainGame/Constant/PlayerInventory.cs
@@ -0,0 +1,9 @@
+﻿namespace MainGame.Constant
+{
+    public static class PlayerInventory
+    {
+        public const int MainInventoryColumns = 9;
+        public const int MainInventoryRows = 5;
+        public const int MainInventorySize = MainInventoryColumns * MainInventoryRows;
+    }
+}
