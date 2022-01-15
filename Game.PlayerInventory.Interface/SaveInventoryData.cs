e9b3224908333b4f635ad1cb3ac80bb8c6152d06
sakastudio <sakastudio100@gmail.com>
2022-01-15 17:32:42 +0900

    セーブする内容を指定

--- Game.PlayerInventory.Interface/SaveInventoryData.cs
+++ Game.PlayerInventory.Interface/SaveInventoryData.cs
@@ -1,6 +1,20 @@
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
 namespace Game.PlayerInventory.Interface
 {
     public class SaveInventoryData
     {
+        [JsonProperty("ItemId")] public List<int> ItemId { get; }
+        [JsonProperty("ItemCount")] public List<int> ItemCount { get; }
+        [JsonProperty("PlayerGUID")] public string PlayerGuid { get; }
+
+        public SaveInventoryData(List<int> itemId, List<int> itemCount,Guid playerGuid)
+        {
+            ItemId = itemId;
+            ItemCount = itemCount;
+            PlayerGuid = playerGuid.ToString();
+        }
     }
 }
