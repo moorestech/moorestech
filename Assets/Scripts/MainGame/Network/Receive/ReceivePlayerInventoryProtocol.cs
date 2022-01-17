1814847af9f0c139f8d94c2d9ad29cc1e4fe1cdc
sakastudio <sakastudio100@gmail.com>
2022-01-17 09:53:37 +0900

    プレイヤーインベントりのデータ解析

--- Assets/Scripts/MainGame/Network/Receive/ReceivePlayerInventoryProtocol.cs
+++ Assets/Scripts/MainGame/Network/Receive/ReceivePlayerInventoryProtocol.cs
@@ -1,12 +1,33 @@
 ﻿using System.Collections.Generic;
+using MainGame.Constant;
+using MainGame.Network.Util;
+using Maingame.Types;
 
 namespace MainGame.Network.Receive
 {
+    /// <summary>
+    /// Analysis player inventory data 
+    /// </summary>
     public class ReceivePlayerInventoryProtocol : IAnalysisPacket
     {
         public void Analysis(List<byte> data)
         {
-            throw new System.NotImplementedException();
+            var bytes = new ByteArrayEnumerator(data);
+            //packet id
+            bytes.MoveNextToGetShort();
+            //player id
+            var playerId = bytes.MoveNextToGetInt();
+            //padding
+            bytes.MoveNextToGetShort();
+            var items = new List<ItemStack>();
+            for (int i = 0; i < PlayerInventory.MainInventorySize; i++)
+            {
+                var id = bytes.MoveNextToGetInt();
+                var count = bytes.MoveNextToGetShort();
+                items.Add(new ItemStack(id, count));
+            }
+            
+            
         }
     }
 }
