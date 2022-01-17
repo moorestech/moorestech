0e88fb4faef06c61e4a648ebfd9d9bcd021795d9
sakastudio <sakastudio100@gmail.com>
2022-01-17 09:46:42 +0900

    ItemStackのstructを定義

---
+++ Assets/Scripts/MainGame/Types/ItemStack.cs
@@ -0,0 +1,14 @@
+namespace Maingame.Types
+{
+    public struct ItemStack
+    {
+        public int ID;
+        public int Count;
+
+        public ItemStack(int id = default, int count = default)
+        {
+            ID = id;
+            Count = count;
+        }
+    }
+}
