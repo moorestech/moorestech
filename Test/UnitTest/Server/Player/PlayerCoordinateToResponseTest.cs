17854a50445f04b657f1b4a777ddbefc1e458373
sakastudio <sakastudio100@gmail.com>
2022-01-19 10:42:08 +0900

    初期座標にマイナスが含まれていた時のテスト

--- Test/UnitTest/Server/Player/PlayerCoordinateToResponseTest.cs
+++ Test/UnitTest/Server/Player/PlayerCoordinateToResponseTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Game.World.Interface.DataStore;
 using NUnit.Framework;
 using Server.Protocol.PacketResponse.Const;
@@ -36,6 +37,40 @@ namespace Test.UnitTest.Server.Player
 
             //2回目は何も返さない
             cList = p.GetResponseCoordinate(new Coordinate(0, 0));
+            Assert.AreEqual(0, cList.Count);
+        }
+
+
+        //初期座標がマイナスの時のテスト
+        [Test]
+        public void OneMinusCoordinateResponseTest()
+        {
+            
+            var p = new PlayerCoordinateToResponse();
+            //1回目は全てを返す
+            var cList = p.GetResponseCoordinate(new Coordinate(-10, 0));
+
+            var ans = new List<Coordinate>();
+            if (ChunkResponseConst.ChunkSize != 20 || ChunkResponseConst.PlayerVisibleRangeChunk != 5)
+            {
+                Assert.Fail("Changed const?");
+            }
+
+            for (int i = -60; i <= 20; i += ChunkResponseConst.ChunkSize)
+            {
+                for (int j = -40; j <= 40; j += ChunkResponseConst.ChunkSize)
+                {
+                    ans.Add(new Coordinate(i, j));
+                }
+            }
+
+            foreach (var a in ans)
+            {
+                Assert.True(cList.Contains(a));
+            }
+
+            //2回目は何も返さない
+            cList = p.GetResponseCoordinate(new Coordinate(-10, 0));
             Assert.AreEqual(0, cList.Count);
         }
 
