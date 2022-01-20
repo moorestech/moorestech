7f875296561e2fd52d2b8d30cc053487380aa6ad
sakastudio <sakastudio100@gmail.com>
2022-01-20 21:04:16 +0900

    テストコードを修

--- Assets/Scripts/Test/EditModeTest/GameLogic/InventoryDataStoreTest.cs
+++ Assets/Scripts/Test/EditModeTest/GameLogic/InventoryDataStoreTest.cs
@@ -29,7 +29,7 @@ namespace Test.EditModeTest.GameLogic
                 new OnPlayerInventoryUpdateProperties(10,inventory));
 
             var inventoryItems = (List<ItemStack>)typeof(InventoryDataStore).GetField("_items",
-                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(inventory);
+                BindingFlags.NonPublic | BindingFlags.Instance).GetValue(inventoryDataStore);
             
             //アイテムの検証
             Assert.AreEqual(PlayerInventoryConstant.MainInventorySize,inventoryItems.Count);
@@ -44,24 +44,24 @@ namespace Test.EditModeTest.GameLogic
             inventoryEvent.OnOnPlayerInventorySlotUpdateEvent(new OnPlayerInventorySlotUpdateProperties(
                 0,new ItemStack(1,5)));
             //検証
-            Assert.AreEqual(1,inventoryItems[0].Count);
-            Assert.AreEqual(5,inventoryItems[0].ID);
+            Assert.AreEqual(1,inventoryItems[0].ID);
+            Assert.AreEqual(5,inventoryItems[0].Count);
             
             inventoryEvent.OnOnPlayerInventorySlotUpdateEvent(new OnPlayerInventorySlotUpdateProperties(
                 3,new ItemStack(5,2)));
-            Assert.AreEqual(2,inventoryItems[3].Count);
             Assert.AreEqual(5,inventoryItems[3].ID);
+            Assert.AreEqual(2,inventoryItems[3].Count);
             
             
             inventoryEvent.OnOnPlayerInventorySlotUpdateEvent(new OnPlayerInventorySlotUpdateProperties(
                 30,new ItemStack(12,23)));
-            Assert.AreEqual(0,inventoryItems[30].Count);
-            Assert.AreEqual(0,inventoryItems[30].ID);
+            Assert.AreEqual(12,inventoryItems[30].ID);
+            Assert.AreEqual(23,inventoryItems[30].Count);
             
             inventoryEvent.OnOnPlayerInventorySlotUpdateEvent(new OnPlayerInventorySlotUpdateProperties(
                 PlayerInventoryConstant.MainInventorySize-1,new ItemStack(30,10)));
-            Assert.AreEqual(30,inventoryItems[PlayerInventoryConstant.MainInventorySize-1].Count);
-            Assert.AreEqual(10,inventoryItems[PlayerInventoryConstant.MainInventorySize-1].ID);
+            Assert.AreEqual(30,inventoryItems[PlayerInventoryConstant.MainInventorySize-1].ID);
+            Assert.AreEqual(10,inventoryItems[PlayerInventoryConstant.MainInventorySize-1].Count);
             
         }
     }
