8253c2362ecb30ab6bd44354b68863bec9a97140
sakastudio <sakastudio100@gmail.com>
2022-01-20 21:01:15 +0900

    実際のデータストア

---
+++ Assets/Scripts/MainGame/GameLogic/Inventory/InventoryDataStore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MainGame.Constant;
+using MainGame.Network.Interface;
+using Maingame.Types;
+
+namespace MainGame.GameLogic.Inventory
+{
+    public class InventoryDataStore
+    {
+        private List<ItemStack> _items = new List<ItemStack>();
+        public InventoryDataStore(IPlayerInventoryUpdateEvent playerInventoryUpdateEvent)
+        {
+            playerInventoryUpdateEvent.Subscribe(UpdateInventory,UpdateSlotInventory);
+        }
+
+        public void UpdateInventory(OnPlayerInventoryUpdateProperties properties)
+        {
+            _items = properties.ItemStacks;
+        }
+
+        public void UpdateSlotInventory(OnPlayerInventorySlotUpdateProperties properties)
+        {
+            _items[properties.SlotId] = properties.ItemStack;
+        }
+    }
+}
