25abe6aa7f20f9afeee0c05fc729afbd66f282cc
sakastudio <sakastudio100@gmail.com>
2022-01-21 19:28:48 +0900

    ソケットを継承したテスト用のソケットモジュール

---
+++ Assets/Scripts/Test/TestModule/TestSocketModule.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+
+namespace Test.TestModule
+{
+    public class TestSocketModule : Socket
+    {
+        public byte[] SentData;
+        public TestSocketModule(AddressFamily addressFamily, SocketType socketType, ProtocolType protocolType) : base(addressFamily, socketType, protocolType)
+        {
+        }
+
+        public TestSocketModule(SocketInformation socketInformation) : base(socketInformation)
+        {
+        }
+
+        public TestSocketModule(SocketType socketType, ProtocolType protocolType) : base(socketType, protocolType)
+        {
+        }
+
+        public new int Send(byte[] array, int offset, int size, SocketFlags socketFlags)
+        {
+            SentData = array;
+            return 0;
+        }
+    }
+}
