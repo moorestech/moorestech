a2906da9d7f87a9f579ee9e80b1279b963f063be
sakastudio <sakastudio100@gmail.com>
2022-01-21 20:12:49 +0900

    プロトコルの内部実装

--- Assets/Scripts/MainGame/Network/Send/RequestPlayerInventoryProtocol.cs
+++ Assets/Scripts/MainGame/Network/Send/RequestPlayerInventoryProtocol.cs
@@ -1,12 +1,26 @@
-﻿using MainGame.Network.Interface.Send;
+﻿using System.Collections.Generic;
+using MainGame.Network.Interface;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
 
 namespace MainGame.Network.Send
 {
     public class RequestPlayerInventoryProtocol : IRequestPlayerInventoryProtocol
     {
+        private const short ProtocolId = 4;
+        private readonly ISocket _socket;
+
+        public RequestPlayerInventoryProtocol(ISocket socket)
+        {
+            _socket = socket;
+        }
+        
         public void Send(int playerId)
         {
+            var packet = new List<byte>();
             
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(playerId));
         }
     }
 }
