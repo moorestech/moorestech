1781e36a1489ec69c87b77c02e2438c9bca59edc
sakastudio <sakastudio100@gmail.com>
2022-01-21 19:40:12 +0900

    テストコードの実装

--- Assets/Scripts/Test/EditModeTest/Network/Send/SendPlaceHotBarBlockProtocolTest.cs
+++ Assets/Scripts/Test/EditModeTest/Network/Send/SendPlaceHotBarBlockProtocolTest.cs
@@ -1,5 +1,10 @@
-﻿using System.Net.Sockets;
+﻿using System.Linq;
+using System.Net.Sockets;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Send;
+using MainGame.Network.Util;
 using NUnit.Framework;
+using Test.TestModule;
 
 namespace Test.EditModeTest.Network.Send
 {
@@ -8,6 +13,23 @@ namespace Test.EditModeTest.Network.Send
         [Test]
         public void SendTest()
         {
+            var socket = new TestSocketModule(new SocketInformation());
+            ISendPlaceHotBarBlockProtocol placeHotBarBlockProtocol = new SendPlaceHotBarBlockProtocol();
+            
+            //送信する
+            int x = 3;
+            int y = 10;
+            short hotBarSlot = 2;
+            int playerId = 100;
+            placeHotBarBlockProtocol.Send(x,y,hotBarSlot,playerId);
+            
+            //データの検証
+            var bytes = new ByteArrayEnumerator(socket.SentData.ToList());
+            Assert.AreEqual(8,  bytes.MoveNextToGetShort()); 
+            Assert.AreEqual(hotBarSlot,  bytes.MoveNextToGetShort()); 
+            Assert.AreEqual(x,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(y,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(playerId,  bytes.MoveNextToGetInt()); 
         }
     }
 }
