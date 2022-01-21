ce8fb7a08f82c54d3c61162ad06709d4190a3634
sakastudio <sakastudio100@gmail.com>
2022-01-21 20:03:44 +0900

    テストコードの定義

---
+++ Assets/Scripts/Test/EditModeTest/Network/Send/RequestPlayerInventoryProtocolTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace Test.EditModeTest.Network.Send
+{
+    public class RequestPlayerInventoryProtocolTest
+    {
+        [Test]
+        public void SendTest()
+        {
+            var protocol = new requestplayer
+        }
+    }
+}
