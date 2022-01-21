7a154c0058adf6ff53279b66e824ae3932fc2c44
sakastudio <sakastudio100@gmail.com>
2022-01-21 19:42:39 +0900

    プロトコルの実装

--- Assets/Scripts/MainGame/Network/Send/SendPlaceHotBarBlockProtocol.cs
+++ Assets/Scripts/MainGame/Network/Send/SendPlaceHotBarBlockProtocol.cs
@@ -1,12 +1,21 @@
-﻿using MainGame.Network.Interface.Send;
+﻿using System.Collections.Generic;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
 
 namespace MainGame.Network.Send
 {
     public class SendPlaceHotBarBlockProtocol : ISendPlaceHotBarBlockProtocol
     {
+        private const short ProtocolId = 8;
         public void Send(int x, int y, short hotBarSlot, int playerId)
         {
+            var packet = new List<byte>();
             
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(hotBarSlot));
+            packet.AddRange(ToByteList.Convert(x));
+            packet.AddRange(ToByteList.Convert(y));
+            packet.AddRange(ToByteList.Convert(playerId));
         }
     }
 }
