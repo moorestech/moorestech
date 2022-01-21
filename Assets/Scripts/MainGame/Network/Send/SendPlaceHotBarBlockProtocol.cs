e82ebd07fc37fee4f937368b943ac33a88793e5f
sakastudio <sakastudio100@gmail.com>
2022-01-21 19:29:14 +0900

    実装のファイル定義

---
+++ Assets/Scripts/MainGame/Network/Send/SendPlaceHotBarBlockProtocol.cs
@@ -0,0 +1,12 @@
+﻿using MainGame.Network.Interface.Send;
+
+namespace MainGame.Network.Send
+{
+    public class SendPlaceHotBarBlockProtocol : ISendPlaceHotBarBlockProtocol
+    {
+        public void Send(int x, int y, int hotBarSlot, int playerId)
+        {
+            
+        }
+    }
+}
