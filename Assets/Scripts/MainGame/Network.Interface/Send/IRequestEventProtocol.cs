28d76605827e0d6f28cdfbafb71e11728b6e2ff0
sakastudio <sakastudio100@gmail.com>
2022-01-21 20:56:41 +0900

    add todo

--- Assets/Scripts/MainGame/Network.Interface/Send/IRequestEventProtocol.cs
+++ Assets/Scripts/MainGame/Network.Interface/Send/IRequestEventProtocol.cs
@@ -2,6 +2,7 @@
 {
     public interface IRequestEventProtocol
     {
+        //TODO
         
     }
 }
--- Assets/Scripts/MainGame/Network.Interface/Send/ISendPlayerPosition.cs
+++ Assets/Scripts/MainGame/Network.Interface/Send/ISendPlayerPosition.cs
@@ -2,6 +2,6 @@
 {
     public interface ISendPlayerPosition
     {
-        
+        //TODO
     }
 }
