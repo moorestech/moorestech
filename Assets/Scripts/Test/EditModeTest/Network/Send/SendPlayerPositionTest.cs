f571dd4225c2c4fe673c30c736c5532ccbc93db2
sakastudio <sakastudio100@gmail.com>
2022-01-24 11:12:03 +0900

    プレイヤー座標の送信テスト

---
+++ Assets/Scripts/Test/EditModeTest/Network/Send/SendPlayerPositionTest.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+using NUnit.Framework;
+using Test.TestModule;
+using UnityEngine;
+
+namespace Test.EditModeTest.Network.Send
+{
+    /// <summary>
+    /// プレイヤー座標の送信テスト
+    /// </summary>
+    public class SendPlayerPositionTest
+    {
+        
+        [Test]
+        public void SendTest()
+        {
+            var socket = new TestSocketModule();
+            ISendPlayerPosition protocol = null;
+            var playerId = 1;
+            var pos = new Vector2(123.4f, 567.8f);
+            
+            protocol.Send(playerId, pos);
+            
+            
+            
+            //データの検証
+            var bytes = new ByteArrayEnumerator(socket.SentData.ToList());
+            Assert.AreEqual(3,  bytes.MoveNextToGetShort()); 
+            Assert.AreEqual(pos.x,  bytes.MoveNextToGetFloat()); 
+            Assert.AreEqual(pos.y,  bytes.MoveNextToGetFloat()); 
+            Assert.AreEqual(playerId,  bytes.MoveNextToGetInt()); 
+        }
+    }
+}
