d97f02e2d57c64a419b46c05d5f8af9f0d5a2d22
sakastudio <sakastudio100@gmail.com>
2022-01-24 10:57:43 +0900

    プレイヤーインベントリとブロックインベントリのアイテムの移動のプロトコルのテスト

---
+++ Assets/Scripts/Test/EditModeTest/Network/Send/SendBlockInventoryPlayerInventoryMoveItemProtocol.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+using NUnit.Framework;
+using Test.TestModule;
+using UnityEngine;
+
+namespace Test.EditModeTest.Network.Send
+{
+    /// <summary>
+    /// プレイヤーインベントリとブロックインベントリのアイテムの移動のプロトコルのテスト
+    /// </summary>
+    public class SendBlockInventoryPlayerInventoryMoveItemProtocol
+    {
+        [Test]
+        public void SendTest()
+        {
+            var socket = new TestSocketModule();
+            ISendBlockInventoryPlayerInventoryMoveItemProtocol protocol = null;
+            bool toBlock = true;
+            int playerId = 10;
+            int playerInventorySlot = 3;
+            Vector2Int blockPos = new Vector2Int(123, -112);
+            int blockInventorySlot = 5;
+            int itemCount = 23;
+            
+            
+            protocol.Send(toBlock, playerId, playerInventorySlot, blockPos, blockInventorySlot, itemCount);
+            
+            
+            
+            //データの検証
+            var bytes = new ByteArrayEnumerator(socket.SentData.ToList());
+            Assert.AreEqual(5,  bytes.MoveNextToGetShort()); 
+            Assert.AreEqual(toBlock,  bytes.MoveNextToGetShort() == 1); 
+            Assert.AreEqual(playerId,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(playerInventorySlot,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(blockPos.x,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(blockPos.y,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(blockInventorySlot,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(itemCount,  bytes.MoveNextToGetInt()); 
+        }
+    }
+}
