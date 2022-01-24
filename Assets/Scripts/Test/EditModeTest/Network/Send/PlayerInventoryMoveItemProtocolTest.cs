bf575fea153dcef09e1c8ecda7687e5fa34bf69e
sakastudio <sakastudio100@gmail.com>
2022-01-24 10:13:52 +0900

    プレイヤーのインベントリ内でアイテムを移動させた時のテスト

---
+++ Assets/Scripts/Test/EditModeTest/Network/Send/PlayerInventoryMoveItemProtocolTest.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+using NUnit.Framework;
+using Test.TestModule;
+
+namespace Test.EditModeTest.Network.Send
+{
+    /// <summary>
+    /// プレイヤーのインベントリ内でアイテムを移動させた時のテスト
+    /// </summary>
+    public class PlayerInventoryMoveItemProtocolTest
+    {
+        [Test]
+        public void SendTest()
+        {
+            var socket = new TestSocketModule();
+            IPlayerInventoryMoveItemProtocol protocol = null;
+            var playerId = 1;
+            var fromSlot = 10;
+            var toSlot = 20;
+            var itemCount = 55;
+            
+            protocol.Send(playerId, fromSlot, toSlot, itemCount);
+            
+            
+            //データの検証
+            var bytes = new ByteArrayEnumerator(socket.SentData.ToList());
+            Assert.AreEqual(6,  bytes.MoveNextToGetShort()); 
+            Assert.AreEqual(playerId,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(fromSlot,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(toSlot,  bytes.MoveNextToGetInt()); 
+            Assert.AreEqual(itemCount,  bytes.MoveNextToGetInt()); 
+        }
+    }
+}
