ac98c15074482209112e918d50109fa0291e3db0
sakastudio <sakastudio100@gmail.com>
2022-01-24 11:17:02 +0900

    PlayerInventoryMoveItemProtocol

---
+++ Assets/Scripts/MainGame/Network/Send/PlayerInventoryMoveItemProtocol.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using MainGame.Network.Interface;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+
+namespace MainGame.Network.Send
+{
+    public class PlayerInventoryMoveItemProtocol : IPlayerInventoryMoveItemProtocol
+    {
+        private const short ProtocolId = 6;
+        private readonly ISocket _socket;
+
+        public PlayerInventoryMoveItemProtocol(ISocket socket)
+        {
+            _socket = socket;
+        }
+
+        public void Send(int playerId, int fromSlot, int toSlot, int itemCount)
+        {
+            var packet = new List<byte>();
+            
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(playerId));
+            packet.AddRange(ToByteList.Convert(fromSlot));
+            packet.AddRange(ToByteList.Convert(toSlot));
+            packet.AddRange(ToByteList.Convert(itemCount));
+
+            _socket.Send(packet.ToArray());
+        }
+    }
+}
