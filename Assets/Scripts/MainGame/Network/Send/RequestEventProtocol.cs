6d1e00621c6c0ace7fa83349b3d3605ca21e64fa
sakastudio <sakastudio100@gmail.com>
2022-01-24 11:27:27 +0900

    RequestEventProtocol作成

---
+++ Assets/Scripts/MainGame/Network/Send/RequestEventProtocol.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using MainGame.Network.Interface;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+
+namespace MainGame.Network.Send
+{
+    public class RequestEventProtocol:IRequestEventProtocol
+    {
+        private const short ProtocolId = 4;
+        private readonly ISocket _socket;
+        
+        public void Send(int playerId)
+        {
+            var packet = new List<byte>();
+            
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(playerId));
+            
+            _socket.Send(packet.ToArray());
+        }
+    }
+}
