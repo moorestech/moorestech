a61a122d4767538ab47154a08a5a18203eb06fdb
sakastudio <sakastudio100@gmail.com>
2022-01-25 12:13:56 +0900

    DIコンテナを組み立てる

--- Assets/Scripts/MainGame/Starter/Starter.cs
+++ Assets/Scripts/MainGame/Starter/Starter.cs
@@ -1,18 +1,60 @@
+using MainGame.GameLogic.Chunk;
+using MainGame.GameLogic.Inventory;
+using MainGame.Network;
+using MainGame.Network.Event;
+using MainGame.Network.Interface;
+using MainGame.Network.Interface.Receive;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Send;
+using MainGame.Network.Send.SocketUtil;
 using UnityEngine;
+using VContainer;
 
 namespace MainGame.Starter
 {
     public class Starter : MonoBehaviour
     {
-        // Start is called before the first frame update
+        private const string DefaultIp = "127.0.0.1";
+        private const int DefaultPort = 11564;
+        
+        
         void Start()
         {
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
+            var builder = new ContainerBuilder();
+            //サーバーに接続するためのインスタンス
+            builder.RegisterInstance(new ConnectionServerConfig(DefaultIp,DefaultPort));
+            builder.Register<ConnectionServer, ConnectionServer>(Lifetime.Singleton);
+            builder.Register<SocketInstanceCreate, SocketInstanceCreate>(Lifetime.Singleton);
+            builder.Register<AllReceivePacketAnalysisService, AllReceivePacketAnalysisService>(Lifetime.Singleton);
+            builder.Register<ISocket, SocketObject>(Lifetime.Singleton);
+            
+            //パケット受け取りイベント
+            builder.Register<IChunkUpdateEvent, ChunkUpdateEvent>(Lifetime.Singleton);
+            builder.Register<IPlayerInventoryUpdateEvent, PlayerInventoryUpdateEvent>(Lifetime.Singleton);
+            
+            //パケット送信インスタンス
+            builder.Register<IRequestEventProtocol, RequestEventProtocol>(Lifetime.Singleton);
+            builder.Register<IRequestPlayerInventoryProtocol, RequestPlayerInventoryProtocol>(Lifetime.Singleton);
+            builder.Register<ISendBlockInventoryMoveItemProtocol, SendBlockInventoryMoveItemProtocol>(Lifetime.Singleton);
+            builder.Register<ISendBlockInventoryPlayerInventoryMoveItemProtocol, SendBlockInventoryPlayerInventoryMoveItemProtocol>(Lifetime.Singleton);
+            builder.Register<ISendPlaceHotBarBlockProtocol, SendPlaceHotBarBlockProtocol>(Lifetime.Singleton);
+            builder.Register<ISendPlayerInventoryMoveItemProtocol, SendPlayerInventoryMoveItemProtocol>(Lifetime.Singleton);
+            builder.Register<ISendPlayerPositionProtocol, SendPlayerPositionProtocolProtocol>(Lifetime.Singleton);
+            
+            //データストア
+            builder.Register<ChunkDataStore, ChunkDataStore>(Lifetime.Singleton);
+            builder.Register<InventoryDataStore, InventoryDataStore>(Lifetime.Singleton);
+            
+            
+            
+            //依存関係を解決
+            var resolver = builder.Build();
+            
+            
+            
+            //データストアのインスタンスを生成する
+            resolver.Resolve<ChunkDataStore>();
+            resolver.Resolve<InventoryDataStore>();
         }
     }
 }
