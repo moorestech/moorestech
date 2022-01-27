118f034157834caafaab78b7e0542757497bf298
sakastudio <sakastudio100@gmail.com>
2022-01-28 01:11:36 +0900

    view側の設定

---
+++ Assets/Scripts/MainGame/UnityView/Chunk/ChunkBlockGameObjectDataStore.cs
@@ -0,0 +1,22 @@
+using MainGame.UnityView.Interface;
+using UnityEngine;
+
+namespace MainGame.UnityView.Chunk
+{
+    public class ChunkBlockGameObjectDataStore : MonoBehaviour
+    {
+        //TODO vcontainerでのインジェクトの方法を探す
+        private IPlaceBlockGameObject _placeBlockGameObject;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
