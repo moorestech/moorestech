dbb3ac19544513858965ba9d38c6467dfca37a80
sakastudio <sakastudio100@gmail.com>
2022-01-28 00:35:20 +0900

    viewから設置イベントを飛ばす

---
+++ Assets/Scripts/MainGame/UnityView.Interface/PlayerInput/IBlockPlaceEvent.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace MainGame.UnityView.Interface.PlayerInput
+{
+    public interface IBlockPlaceEvent
+    {
+        public delegate void OnBlockPlace(Vector2Int position,int hotBarIndex);
+
+        public void Subscribe(OnBlockPlace onBlockPlace);
+    }
+}
