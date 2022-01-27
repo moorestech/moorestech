3ab144c542505a21861fea75a5b7fb505213cd01
sakastudio <sakastudio100@gmail.com>
2022-01-28 00:33:34 +0900

    ブロック設置用のインターフェース

---
+++ Assets/Scripts/MainGame/UnityView.Interface/IPlaceBlockGameObject.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MainGame.UnityView.Interface
+{
+    public interface IPlaceBlockGameObject
+    {
+        public void OnChunkUpdate(Vector2Int chunkOriginPosition,int[,] blocks);
+        public void OnBlockUpdate(Vector2Int blockPosition,int blockId);
+    }
+}
