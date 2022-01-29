2d58a75eb9a91394d06e59b5f7d388573127d16d
sakastudio <sakastudio100@gmail.com>
2022-01-29 15:24:12 +0900

    interface削除

--- Assets/Scripts/MainGame/UnityView.Interface/IGameObjectInstantiate.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace MainGame.UnityView.Interface
-{
-    public interface IGameObjectInstantiate
-    {
-        public void Instantiate(GameObject prefab, Vector3 position, Quaternion rotation,Transform parent);
-    }
-}
