13d7b2c6a3bf413e1112c5738d0958981461061b
sakastudio <sakastudio100@gmail.com>
2022-01-29 20:51:42 +0900

    ブロックの取得を関数に切り分け

--- Assets/Scripts/Test/PlayModeTest/UnityView/ChunkBlockGameObjectDataStoreTest.cs
+++ Assets/Scripts/Test/PlayModeTest/UnityView/ChunkBlockGameObjectDataStoreTest.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using MainGame.Constant;
 using MainGame.GameLogic.Chunk;
 using MainGame.UnityView.Chunk;
@@ -18,6 +20,14 @@ namespace Test.PlayModeTest.UnityView
             var blocksData = AssetDatabase.LoadAssetAtPath<BlockObjects>("Assets/ScriptableObject/BlockObjects.asset");
             dataStore.Construct(blockUpdateEvent,blocksData);
             
+            
+            
+            //ブロックが置かれていないことを確認する
+            var blocks = GetBlocks(dataStore.transform);
+            Assert.AreEqual(0,blocks.Count);
+            
+            
+            
             //チャンクの情報を作成
             var ids = new int[ChunkConstant.ChunkSize, ChunkConstant.ChunkSize];
             ids[0, 0] = 1;
@@ -26,11 +36,25 @@ namespace Test.PlayModeTest.UnityView
             blockUpdateEvent.DiffChunkUpdate(chunkPosition,ids);
             
             
+            
             //ブロックが置かれているか確認する
-            var blocks = dataStore.transform.GetComponentsInChildren<Transform>();
-            Assert.AreEqual(1,blocks.Length);
+            blocks = GetBlocks(dataStore.transform);
+            Assert.AreEqual(1,blocks.Count);
             Assert.AreEqual(new Vector3(0,0,0),blocks[0].position);
 
+        }
+
+        private List<Transform> GetBlocks(Transform dataStore)
+        {
+            var blocks = dataStore.transform.GetComponentsInChildren<Transform>().ToList();
+            //データストアの一個したの階層のオブジェクトのみを取得
+            for (int i = blocks.Count - 1; i >= 0; i--)
+            {
+                if (blocks[i].parent == dataStore.transform) continue;
+                blocks.RemoveAt(i);
+            }
+
+            return blocks;
         }
     }
 }
