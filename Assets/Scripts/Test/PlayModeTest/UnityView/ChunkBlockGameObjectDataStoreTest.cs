bea731bd8c19df6721cb7c99a97ea44d906a402a
sakastudio <sakastudio100@gmail.com>
2022-01-29 19:11:40 +0900

    wip ブロック生成のテストコード

--- Assets/Scripts/Test/PlayModeTest/UnityView/ChunkBlockGameObjectDataStoreTest.cs
+++ Assets/Scripts/Test/PlayModeTest/UnityView/ChunkBlockGameObjectDataStoreTest.cs
@@ -1,7 +1,9 @@
+using MainGame.Constant;
 using MainGame.GameLogic.Chunk;
 using MainGame.UnityView.Chunk;
 using NUnit.Framework;
 using UnityEditor;
+using UnityEngine;
 
 namespace Test.PlayModeTest.UnityView
 {
@@ -10,12 +12,25 @@ namespace Test.PlayModeTest.UnityView
         [Test]
         public void PlaceAndRemoveEventTest()
         {
-            var dataStore = new ChunkBlockGameObjectDataStore();
+            //初期設定
+            var dataStore = new GameObject().AddComponent<ChunkBlockGameObjectDataStore>();
             var blockUpdateEvent = new BlockUpdateEvent();
-            var blocks = AssetDatabase.LoadAssetAtPath<BlockObjects>("Assets/ScriptableObject/BlockObjects.asset");
-            dataStore.Construct(blockUpdateEvent,blocks);
+            var blocksData = AssetDatabase.LoadAssetAtPath<BlockObjects>("Assets/ScriptableObject/BlockObjects.asset");
+            dataStore.Construct(blockUpdateEvent,blocksData);
             
-                
+            //チャンクの情報を作成
+            var ids = new int[ChunkConstant.ChunkSize, ChunkConstant.ChunkSize];
+            ids[0, 0] = 1;
+            var chunkPosition = new Vector2Int(0,0);
+            //イベントを発火
+            blockUpdateEvent.DiffChunkUpdate(chunkPosition,ids);
+            
+            
+            //ブロックが置かれているか確認する
+            var blocks = dataStore.transform.GetComponentsInChildren<Transform>();
+            Assert.AreEqual(1,blocks.Length);
+            Assert.AreEqual(new Vector3(0,0,0),blocks[0].position);
+
         }
     }
 }
