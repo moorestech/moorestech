d8f84d2dc0da924f7d5cde2469369c9337173b05
sakastudio <sakastudio100@gmail.com>
2022-02-02 22:56:19 +0900

    inventoryの設定をする

---
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/InventorySlot.cs
@@ -0,0 +1,26 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainGame.UnityView.UI.Inventory
+{
+    public class InventorySlot: MonoBehaviour
+    {
+        [SerializeField] private Image image;
+        [SerializeField] private TextMeshPro countText;
+        
+        public void SetItem(Sprite sprite, int count)
+        {
+            image.sprite = sprite;
+            
+            if (count == 0)
+            {
+                countText.text = "";
+            }
+            else
+            {
+                countText.text = count.ToString();
+            }
+        }
+    }
+}
