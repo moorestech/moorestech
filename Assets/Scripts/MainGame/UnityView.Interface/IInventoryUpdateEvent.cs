315536b1baca162b1309dfbc673ed15b77b803bd
sakastudio <sakastudio100@gmail.com>
2022-02-02 22:37:13 +0900

    IInventoryUpdateEventの作成

---
+++ Assets/Scripts/MainGame/UnityView.Interface/IInventoryUpdateEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace MainGame.UnityView.Interface
+{
+    public interface IInventoryUpdateEvent
+    {
+        public delegate void InventoryUpdate(int slot,int itemId,int count);
+        public void Subscribe(InventoryUpdate inventoryUpdate);
+    }
+}
