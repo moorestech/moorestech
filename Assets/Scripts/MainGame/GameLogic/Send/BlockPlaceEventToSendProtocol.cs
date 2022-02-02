f6085a75f6920cb7facc9f1bc9dc3ba416dc15a6
sakastudio <sakastudio100@gmail.com>
2022-02-02 19:28:33 +0900

    BlockPlaceEventToSendProtocolの作成

---
+++ Assets/Scripts/MainGame/GameLogic/Send/BlockPlaceEventToSendProtocol.cs
@@ -0,0 +1,27 @@
+﻿using MainGame.Network.Interface.Send;
+using MainGame.UnityView.Interface.PlayerInput;
+using UnityEngine;
+
+namespace MainGame.GameLogic.Send
+{
+    public class BlockPlaceEventToSendProtocol
+    {
+        private readonly ISendPlaceHotBarBlockProtocol _sendPlaceHotBarBlockProtocol;
+        public readonly int playerId;
+
+        public BlockPlaceEventToSendProtocol(
+            ISendPlaceHotBarBlockProtocol sendPlaceHotBarBlockProtocol,
+            IBlockPlaceEvent blockPlaceEvent,
+            ConnectionPlayerSetting connectionPlayerSetting)
+        {
+            _sendPlaceHotBarBlockProtocol = sendPlaceHotBarBlockProtocol;
+            blockPlaceEvent.Subscribe(OnBlockPlace);
+            playerId = connectionPlayerSetting.PlayerId;
+        }
+
+        public void OnBlockPlace(Vector2Int position, short hotBar)
+        {
+            _sendPlaceHotBarBlockProtocol.Send(position.x,position.y,hotBar,playerId);
+        }
+    }
+}
