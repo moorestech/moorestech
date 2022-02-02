60b307599fbf1689ea60c06c768ff7a3fefe4ce9
sakastudio <sakastudio100@gmail.com>
2022-02-02 22:19:13 +0900

    itemの画像を保存するScriptableObject

---
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/ItemImages.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainGame.UnityView.UI.Inventory
+{
+    [CreateAssetMenu(fileName = "ItemImages", menuName = "ItemImages", order = 0)]
+    public class ItemImages : ScriptableObject
+    {
+        [SerializeField] private List<Sprite> itemImageList;
+        [SerializeField] private Sprite nothingIndexItemImage;
+
+        public Sprite GetItemImage(int index)
+        {
+            if (itemImageList.Count <= index)
+            {
+                return nothingIndexItemImage;
+            }
+
+            return itemImageList[index];
+        }
+    }
+}
