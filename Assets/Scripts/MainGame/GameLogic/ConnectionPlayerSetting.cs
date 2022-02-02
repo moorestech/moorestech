0e6f69e2b0402d3032cec63be1cef1570be8a75e
sakastudio <sakastudio100@gmail.com>
2022-02-02 19:20:07 +0900

    ConnectionPlayerSettingの作成

---
+++ Assets/Scripts/MainGame/GameLogic/ConnectionPlayerSetting.cs
@@ -0,0 +1,12 @@
+﻿namespace MainGame.GameLogic
+{
+    public class ConnectionPlayerSetting
+    {
+        public readonly int PlayerId;
+
+        public ConnectionPlayerSetting(int playerId)
+        {
+            PlayerId = playerId;
+        }
+    }
+}
