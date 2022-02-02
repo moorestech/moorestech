c4f41b4127df8998bed460768d9bb155c8d07d58
sakastudio <sakastudio100@gmail.com>
2022-02-02 23:07:59 +0900

    Itemのviewを追加

---
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/MainInventoryView.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using MainGame.Constant;
+using MainGame.UnityView.Interface;
+using UnityEngine;
+using VContainer;
+
+namespace MainGame.UnityView.UI.Inventory
+{
+    public class MainInventoryView : MonoBehaviour
+    {
+        [SerializeField] private InventorySlot inventorySlot;
+        List<InventorySlot> _slots;
+        private ItemImages _itemImages;
+        
+        
+        [Inject]
+        public void Construct(IInventoryUpdateEvent inventoryUpdateEvent,ItemImages itemImages)
+        {
+            inventoryUpdateEvent.Subscribe(OnInventoryUpdate);
+            _itemImages = itemImages;
+            
+            for (int i = 0; i < PlayerInventoryConstant.MainInventorySize; i++)
+            {
+                _slots.Add(Instantiate(inventorySlot.gameObject,transform).GetComponent<InventorySlot>());
+            }
+        }
+
+        private void OnInventoryUpdate(int slot, int itemId, int count)
+        {
+            var sprite = _itemImages.GetItemImage(itemId);
+            _slots[slot].SetItem(sprite,count);
+        }
+    }
+}
