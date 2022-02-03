cd9f526830f2536bd7da208f689230d4e14286cf
sakastudio <sakastudio100@gmail.com>
2022-02-03 14:44:32 +0900

    テストモジュールの

---
+++ Assets/Scripts/Test/TestModule/UITestModule/InventoryViewTestModule.cs
@@ -0,0 +1,28 @@
+using System;
+using MainGame.GameLogic.Inventory;
+using MainGame.UnityView.UI.Inventory.Element;
+using MainGame.UnityView.UI.Inventory.View;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Test.TestModule.UITestModule
+{
+    public class InventoryViewTestModule : MonoBehaviour
+    {
+        [SerializeField] private MainInventoryItemView mainInventoryItemView;
+        [SerializeField] private ItemImages itemImages;
+
+        private void Start()
+        {
+            var update = new InventoryUpdateEvent();
+            mainInventoryItemView.Construct(update,itemImages);
+            
+            //アイテムの設定
+            update.OnOnInventoryUpdate(0,1,5);
+            update.OnOnInventoryUpdate(5,2,10);
+            update.OnOnInventoryUpdate(10,2,1);
+            update.OnOnInventoryUpdate(40,2,1);
+            update.OnOnInventoryUpdate(44,2,1);
+        }
+    }
+}
