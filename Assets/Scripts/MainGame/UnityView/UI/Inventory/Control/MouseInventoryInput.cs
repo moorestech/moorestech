eedd06fa48ce5a1f4924c8efea72dc0abcd15a78
sakastudio <sakastudio100@gmail.com>
2022-02-04 13:52:55 +0900

    実際にアイテムを動かすコード

--- Assets/Scripts/MainGame/UnityView/UI/Inventory/Control/MouseInventoryInput.cs
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/Control/MouseInventoryInput.cs
@@ -1,6 +1,9 @@
 ﻿using MainGame.UnityView.ControllerInput;
+using MainGame.UnityView.Interface.PlayerInput;
 using MainGame.UnityView.UI.Inventory.Element;
 using MainGame.UnityView.UI.Inventory.View;
+using UnityEngine;
+using UnityEngine.UI;
 using VContainer;
 using VContainer.Unity;
 
@@ -8,10 +11,24 @@ namespace MainGame.UnityView.UI.Inventory.Control
 {
     public class MouseInventoryInput : IControllerInput,IPostStartable
     {
-        private MainInventoryItemView _mainInventoryItemView;
+        [SerializeField] private Image _equippedItem;
         
+        private int _equippedItemIndex = -1;
+        private MainInventoryItemView _mainInventoryItemView;
+        private IPlayerInventoryItemMove _playerInventoryItemMove;
+        private ItemImages _itemImages;
+        
+        private MoorestechInputSettings _inputSettings = new();
+
         [Inject]
-        public void Construct(MainInventoryItemView mainInventoryItemView) { _mainInventoryItemView = mainInventoryItemView; }
+        public void Construct(
+            MainInventoryItemView mainInventoryItemView,
+            IPlayerInventoryItemMove playerInventoryItemMove,ItemImages itemImages)
+        {
+            _mainInventoryItemView = mainInventoryItemView;
+            _playerInventoryItemMove = playerInventoryItemMove;
+            _itemImages = itemImages;
+        }
         
         public void PostStart()
         {
@@ -23,6 +40,31 @@ namespace MainGame.UnityView.UI.Inventory.Control
 
         private void OnSlotClick(int slot)
         {
+            if (_equippedItemIndex == -1)
+            {
+                _equippedItemIndex = slot;
+                _equippedItem.sprite = _itemImages.GetItemImage(slot);
+                _equippedItem.gameObject.SetActive(true);
+                return;
+            }
+
+            //アイテムを半分だけおく
+            if (_inputSettings.Playable.InventoryItemHalve.inProgress)
+            {
+                _playerInventoryItemMove.MoveHalfItemStack(_equippedItemIndex,slot);
+                return;
+            }
+            
+            //アイテムを一個だけおく
+            if (_inputSettings.Playable.InventoryItemOnePut.inProgress)
+            {
+                _playerInventoryItemMove.MoveOneItemStack(_equippedItemIndex,slot);
+                return;
+            }
+            
+            //アイテムを全部おく
+            _playerInventoryItemMove.MoveAllItemStack(_equippedItemIndex,slot);
+            
             
         }
         
