8a314f516b385f162e7db63230aed5dabb6c68dc
sakastudio <sakastudio100@gmail.com>
2022-02-06 19:25:36 +0900

    PlayerInventoryItemMoveの作成

---
+++ Assets/Scripts/MainGame/GameLogic/Inventory/PlayerInventoryItemMove.cs
@@ -0,0 +1,33 @@
+﻿using MainGame.Network.Interface.Send;
+using MainGame.UnityView.Interface.PlayerInput;
+
+namespace MainGame.GameLogic.Inventory
+{
+    public class PlayerInventoryItemMove : IPlayerInventoryItemMove
+    {
+        private ISendPlayerInventoryMoveItemProtocol _sendPlayerInventoryMoveItemProtocol;
+        private InventoryDataStoreCache _inventoryDataStoreCache;
+
+
+        public PlayerInventoryItemMove(ISendPlayerInventoryMoveItemProtocol sendPlayerInventoryMoveItemProtocol, InventoryDataStoreCache inventoryDataStoreCache)
+        {
+            _sendPlayerInventoryMoveItemProtocol = sendPlayerInventoryMoveItemProtocol;
+            _inventoryDataStoreCache = inventoryDataStoreCache;
+        }
+
+        public void MoveAllItemStack(int fromSlot, int toSlot)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveHalfItemStack(int fromSlot, int toSlot)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void MoveOneItemStack(int fromSlot, int toSlot)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
