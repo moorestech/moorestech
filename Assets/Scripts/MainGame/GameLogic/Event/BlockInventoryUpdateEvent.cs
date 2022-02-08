48d1816c327086c82f296564a7dbe5ac8e44f702
sakastudio <sakastudio100@gmail.com>
2022-02-08 12:53:48 +0900

    BlockInventoryUpdateEventの作成

--- Assets/Scripts/MainGame/GameLogic/Event/BlockInventoryUpdateEvent.cs
+++ Assets/Scripts/MainGame/GameLogic/Event/BlockInventoryUpdateEvent.cs
@@ -5,10 +5,23 @@ namespace MainGame.GameLogic.Event
 {
     public class BlockInventoryUpdateEvent : IBlockInventoryUpdateEvent
     {
+        public event InventoryUpdate OnInventoryUpdate;
+        public event OpenInventory OnOpenInventory;
         
         public void Subscribe(InventoryUpdate inventoryUpdate, OpenInventory openInventory)
         {
-            
+            OnInventoryUpdate += inventoryUpdate;
+            OnOpenInventory += openInventory;
+        }
+
+        protected virtual void OnOnOpenInventory()
+        {
+            OnOpenInventory?.Invoke();
+        }
+
+        protected virtual void OnOnInventoryUpdate(int slot, int itemid, int count)
+        {
+            OnInventoryUpdate?.Invoke(slot, itemid, count);
         }
     }
 }
