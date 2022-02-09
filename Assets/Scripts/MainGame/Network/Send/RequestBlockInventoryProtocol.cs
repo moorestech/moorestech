15a2803b45758f62503f5d5c068724da7cd66531
sakastudio <sakastudio100@gmail.com>
2022-02-09 14:11:01 +0900

    実際に送信する実装

---
+++ Assets/Scripts/MainGame/Network/Send/RequestBlockInventoryProtocol.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using MainGame.Network.Interface;
+using MainGame.Network.Interface.Send;
+using MainGame.Network.Util;
+
+namespace MainGame.Network.Send
+{
+    public class RequestBlockInventoryProtocol : IRequestBlockInventoryProtocol
+    {
+        private const short ProtocolId = 9;
+        private readonly ISocket _socket;
+        
+        public void Send(int x, int y)
+        {
+            var packet = new List<byte>();
+            
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(x));
+            packet.AddRange(ToByteList.Convert(y));
+            
+            _socket.Send(packet.ToArray());
+        }
+    }
+}
