79771ac7855be8adb130a1e6e1e9e75eec5579f6
sakastudio <sakastudio100@gmail.com>
2022-02-14 14:07:23 +0900

    いらなくなったテストを削除

--- Assets/Scripts/Test/EditModeTest/GameLogic/ChunkDataStoreTest.cs
+++ Assets/Scripts/Test/EditModeTest/GameLogic/ChunkDataStoreTest.cs
@@ -41,69 +41,6 @@ namespace Test.EditModeTest.GameLogic
             Assert.True(chunk.ContainsKey(new Vector2Int(20,-100)));
         }
 
-
-        [Test]
-        public void SetBlockTest()
-        {
-            var chunkEvent = new ChunkUpdateEvent();
-            var chunkGameObjects = new GameObject().AddComponent<ChunkBlockGameObjectDataStore>();
-            var chunkDataStore = new ChunkDataStoreCache(chunkEvent,chunkGameObjects);
-
-            //検証するチャンク
-            var setChunks = new List<Vector2Int>()
-            {
-                new Vector2Int(40,40),
-                new Vector2Int(-40,40),
-                new Vector2Int(40,-40),
-                new Vector2Int(-40,-40)
-            };
-            //検証するブロック
-            var blocks = new Dictionary<(int, int), int>();
-            blocks.Add((0,0), 1);
-            blocks.Add((1,0), 2);
-            blocks.Add((1,1), 3);
-            blocks.Add((18,18), 4);
-            blocks.Add((18,19), 5);
-            blocks.Add((19,18), 6);
-            blocks.Add((19,19), 6);
-            
-            
-            //検証用チャンクをセットする
-            setChunks.ForEach(c => 
-                chunkEvent.InvokeChunkUpdateEvent(new OnChunkUpdateEventProperties(c, EmptyBlockArray())));
-                
-            
-            
-            //イベント経由でブロックをセットする
-            setChunks.ForEach(c =>
-            {
-                foreach (var block in blocks)
-                {
-                    var x = c.x + block.Key.Item1;
-                    var y = c.y + block.Key.Item2;
-                    chunkEvent.InvokeBlockUpdateEvent(new OnBlockUpdateEventProperties(
-                        new Vector2Int( x,y),
-                        block.Value));   
-                }
-            });
-            
-            //リフレクションでチャンクのデータを取得する
-            var chunk = (Dictionary<Vector2Int, int[,]>)chunkDataStore.GetType().GetField("_chunk", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(chunkDataStore);
-            
-            //ブロックが正しくセットできているかテストする
-            setChunks.ForEach(c =>
-            {
-                foreach (var block in blocks)
-                {
-                    var i = block.Key.Item1;
-                    var j = block.Key.Item2;
-                    var cBlock = chunk[c];
-                    Assert.AreEqual(block.Value, cBlock[i,j]);
-                }
-            });
-        }
-        
-
         private int[,] EmptyBlockArray()
         {
             return new int[ChunkConstant.ChunkSize, ChunkConstant.ChunkSize];
