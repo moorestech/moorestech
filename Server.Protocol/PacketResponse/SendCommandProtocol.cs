3df4f9d1de4eddeabe5a8048243085957d51f01b
sakastudio <sakastudio100@gmail.com>
2022-02-14 10:39:51 +0900

    とりあえずコマンドの実装

--- Server.Protocol/PacketResponse/SendCommandProtocol.cs
+++ Server.Protocol/PacketResponse/SendCommandProtocol.cs
@@ -1,11 +1,37 @@
 using System.Collections.Generic;
+using Core.Item;
+using Game.PlayerInventory.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Util;
 
 namespace Server.Protocol.PacketResponse
 {
     public class SendCommandProtocol : IPacketResponse
     {
+        private readonly IPlayerInventoryDataStore _playerInventoryDataStore;
+        private readonly ItemStackFactory _itemStackFactory;
+        public SendCommandProtocol(ServiceProvider serviceProvider)
+        {
+            _playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+            _itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+        }
+
         public List<byte[]> GetResponse(List<byte> payload)
         {
+            var enumerator = new ByteArrayEnumerator(payload);
+            enumerator.MoveNextToGetShort();//packet id
+            var length = enumerator.MoveNextToGetShort();//command length
+            var command = enumerator.MoveNextToGetString(length).Split(' ');//command text
+            
+            //他のコマンドを実装する場合、この実装方法をやめる
+            if (command[0] == "give")
+            {
+                var inventory = _playerInventoryDataStore.GetInventoryData(int.Parse(command[1]));
+                var item = _itemStackFactory.Create(int.Parse(command[2]), int.Parse(command[3]));
+                inventory.InsertItem(item);
+            }
+            
+            
             return new List<byte[]>();
         }
     }
