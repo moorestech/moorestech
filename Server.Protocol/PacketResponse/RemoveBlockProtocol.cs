3abe266cff407d4748716f598206ddf1b3053af9
twwalter <mgnalter.1332@gmail.com>
2022-02-15 23:35:18 +0900

    ブロック削除プロトコルのアイテム追加（修正途中）

--- Server.Protocol/PacketResponse/RemoveBlockProtocol.cs
+++ Server.Protocol/PacketResponse/RemoveBlockProtocol.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Core.Item;
+using Core.Item.Config;
 using Game.World.Interface.DataStore;
 using Microsoft.Extensions.DependencyInjection;
 using Server.Util;
@@ -9,14 +11,21 @@ namespace Server.Protocol.PacketResponse
     {
         
         private readonly IWorldBlockDatastore _worldBlockDatastore;
+        private ItemStackFactory _itemStackFactory = new ItemStackFactory(new TestItemConfig());
+        
         public  RemoveBlockProtocol(ServiceProvider serviceProvider)
         {
             _worldBlockDatastore = serviceProvider.GetService<IWorldBlockDatastore>();
 
         }
         
+        playerInventoryData.InsertItem(_itemStackFactory.Create(blockConfigData.ItemId,1));
+        
+        
+        
         public List<byte[]> GetResponse(List<byte> payload)
         {
+            
             var payloadData = new ByteArrayEnumerator(payload);
             payloadData.MoveNextToGetShort();
             int x = payloadData.MoveNextToGetInt();
@@ -24,6 +33,7 @@ namespace Server.Protocol.PacketResponse
             int playerId = payloadData.MoveNextToGetInt();
 
             _worldBlockDatastore.RemoveBlock(x, y);
+            
             return new List<byte[]>();
             
             
