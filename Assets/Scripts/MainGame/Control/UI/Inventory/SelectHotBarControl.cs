5af51a5c4699a569c41954acd184b5e20371c5c7
sakastudio <sakastudio100@gmail.com>
2022-02-15 14:52:35 +0900

    入力でホットバーを変更する機能

---
+++ Assets/Scripts/MainGame/Control/UI/Inventory/SelectHotBarControl.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using VContainer;
+
+namespace MainGame.Control.UI.Inventory
+{
+    public class SelectHotBarControl : MonoBehaviour
+    {
+        private const int HotBarCount = 9;
+        
+        private MoorestechInputSettings _inputSettings;
+        private int selectIndex;
+        
+        public void Awake()
+        {
+            _inputSettings = new();
+            _inputSettings.Enable();
+        }
+
+        private void Update()
+        {
+            if (_inputSettings.UI.HotBar.ReadValue<int>() != 0)
+            {
+                selectIndex = _inputSettings.UI.HotBar.ReadValue<int>();
+            }
+
+            if (_inputSettings.UI.SwitchHotBar.ReadValue<float>() < 0)
+            {
+                selectIndex--;
+                if (selectIndex < 0)selectIndex = HotBarCount - 1;
+            }else if (0 < _inputSettings.UI.SwitchHotBar.ReadValue<float>())
+            {
+                selectIndex++;
+                if (HotBarCount <= selectIndex)selectIndex = 0;
+            }
+        }
+    }
+}
