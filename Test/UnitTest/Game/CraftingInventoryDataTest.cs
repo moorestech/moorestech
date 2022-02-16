5ad5b18949e601516afb03fd80e33e56c9fac6dc
sakastudio <sakastudio100@gmail.com>
2022-02-16 13:09:00 +0900

    craft inventoryのテストを記述

--- Test/UnitTest/Game/CraftingInventoryDataTest.cs
+++ Test/UnitTest/Game/CraftingInventoryDataTest.cs
@@ -1,19 +1,96 @@
+using Core.Item;
+using Game.Crafting.Interface;
+using Game.PlayerInventory.Interface;
+using NUnit.Framework;
+using PlayerInventory;
+using PlayerInventory.Event;
+
 namespace Test.UnitTest.Game
 {
     public class CraftingInventoryDataTest
     {
+        private const int PlayerId = 0;
         public void GetResultTest()
         {
-            //TODO crafting inventoryにアイテムを入れる
-            //TODO インベントリのconfigからデータを取得
-            //TODO getResultを検証する
+            ICraftingConfig config = null;
+            ItemStackFactory itemStackFactory = null;
+            
+            var craftConfig = config.GetCraftingConfig()[0];
+            
+            //craftingInventoryにアイテムを入れる
+            var craftingInventory = new PlayerCraftingInventoryData(PlayerId,new PlayerInventoryUpdateEvent(),itemStackFactory);
+            for (int i = 0; i < craftConfig.Items.Count; i++)
+            {
+                craftingInventory.SetItem(i,craftConfig.Items[i]);
+            }
+            
+            //インベントリのconfigからデータを取得
+            //getResultを検証する
+            Assert.AreEqual(craftConfig.Result,craftingInventory.GetResult());
         }
 
         public void CraftTest()
         {
-            //TODO crafting inventoryにアイテムを入れる
-            //TODO インベントリのconfigからデータを取得
-            //TODO getResultを検証する
+            ICraftingConfig config = null;
+            ItemStackFactory itemStackFactory = null;
+
+            var craftConfig = config.GetCraftingConfig()[0];
+            
+            
+            //craftingInventoryにアイテムを入れる
+            var craftingInventory = new PlayerCraftingInventoryData(PlayerId,new PlayerInventoryUpdateEvent(),itemStackFactory);
+            for (int i = 0; i < craftConfig.Items.Count; i++)
+            {
+                craftingInventory.SetItem(i,craftConfig.Items[i]);
+            }
+            
+            //クラフト実行
+            craftingInventory.Craft();
+            
+            //ResultSlotにアイテムが入っているかチェック
+            Assert.AreEqual(craftConfig.Result,craftingInventory.GetItem(PlayerInventoryConst.CraftingInventorySize - 1 ));
+            
+            //クラフトスロットからアイテムが消えているかチェック
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                Assert.AreEqual(itemStackFactory.CreatEmpty(),craftingInventory.GetItem(i));
+            }
+        }
+
+        //クラフトを実行したときにアイテムが余るテスト
+        public void CraftRemainderItemTest()
+        {
+            
+            ICraftingConfig config = null;
+            ItemStackFactory itemStackFactory = null;
+
+            var craftConfig = config.GetCraftingConfig()[0];
+            
+            
+            //craftingInventoryに1つ余分にアイテムを入れる
+            var craftingInventory = new PlayerCraftingInventoryData(PlayerId,new PlayerInventoryUpdateEvent(),itemStackFactory);
+            for (int i = 0; i < craftConfig.Items.Count; i++)
+            {
+                var itemId = craftConfig.Items[i].Id;
+                var itemCount = craftConfig.Items[i].Count;
+                var setItem = itemStackFactory.Create(itemId,itemCount + 1);
+                craftingInventory.SetItem(i,setItem);
+            }
+            
+            //クラフト実行
+            craftingInventory.Craft();
+            
+            //ResultSlotにアイテムが入っているかチェック
+            Assert.AreEqual(craftConfig.Result,craftingInventory.GetItem(PlayerInventoryConst.CraftingInventorySize - 1 ));
+            
+            //クラフトスロットにアイテムが1つ残っているかチェック
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                var inventoryItem = craftingInventory.GetItem(i);
+                
+                Assert.AreEqual(craftConfig.Items[i].Id,inventoryItem.Id);//check id
+                Assert.AreEqual(1,inventoryItem.Count);//check count
+            }
         }
     }
 }
