5aa75e8a6201f37b2336d9fa0cf8fe5fc06aa979
sakastudio <sakastudio100@gmail.com>
2022-02-16 13:56:39 +0900

    CraftConfigJsonDataの作成

---
+++ Game.Crafting/Config/CraftConfigJsonData.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Game.Crafting.Config
+{
+    [DataContract]
+    public class CraftConfigJsonData
+    {
+        
+        [DataMember(Name = "recipes")] private CraftConfigDataElement[] _craftConfigElements;
+
+        public CraftConfigDataElement[] CraftConfigElements => _craftConfigElements;
+    }
+
+    [DataContract]
+    public class CraftConfigDataElement
+    {
+        [DataMember(Name = "items")] private List<CraftItem> _items;
+        [DataMember(Name = "result")] private CraftItem _result;
+
+        public List<CraftItem> Items => _items;
+
+        public CraftItem Result => _result;
+    }
+
+    [DataContract]
+    public class CraftItem
+    {
+        [DataMember(Name = "id")] private int _id;
+        [DataMember(Name = "count")] private string _count;
+
+        public int Id => _id;
+
+        public string Count => _count;
+    }
+}
