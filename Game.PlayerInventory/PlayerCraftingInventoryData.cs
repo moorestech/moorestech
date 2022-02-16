02e0583f55e3725bd05e8ce9bb44f4fc64ac4e65
sakastudio <sakastudio100@gmail.com>
2022-02-16 11:41:27 +0900

    ICraftingInventoryのinteface実装

--- Game.PlayerInventory/PlayerCraftingInventoryData.cs
+++ Game.PlayerInventory/PlayerCraftingInventoryData.cs
@@ -1,7 +1,53 @@
+using Core.Item;
+using Game.PlayerInventory.Interface;
+
 namespace PlayerInventory
 {
-    public class PlayerCraftingInventoryData
+    public class PlayerCraftingInventoryData : ICraftingInventory
     {
-        
+        public IItemStack GetItem(int slot)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetItem(int slot, IItemStack itemStack)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetItem(int slot, int itemId, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IItemStack ReplaceItem(int slot, IItemStack itemStack)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IItemStack ReplaceItem(int slot, int itemId, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IItemStack InsertItem(IItemStack itemStack)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IItemStack InsertItem(int itemId, int count)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int GetSlotSize()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Craft()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
