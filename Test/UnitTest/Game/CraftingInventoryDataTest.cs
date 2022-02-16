fe672258064ebe98f8b5679309e54b4c636b88e5
sakastudio <sakastudio100@gmail.com>
2022-02-16 12:24:28 +0900

    テストコードのクラスだけ定義

---
+++ Test/UnitTest/Game/CraftingInventoryDataTest.cs
@@ -0,0 +1,19 @@
+namespace Test.UnitTest.Game
+{
+    public class CraftingInventoryDataTest
+    {
+        public void GetResultTest()
+        {
+            //TODO crafting inventoryにアイテムを入れる
+            //TODO インベントリのconfigからデータを取得
+            //TODO getResultを検証する
+        }
+
+        public void CraftTest()
+        {
+            //TODO crafting inventoryにアイテムを入れる
+            //TODO インベントリのconfigからデータを取得
+            //TODO getResultを検証する
+        }
+    }
+}
