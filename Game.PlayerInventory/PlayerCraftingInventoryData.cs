b7d5abb0c4e2c48b4a4d9bdedf5d6e9e82bde6f3
sakastudio <sakastudio100@gmail.com>
2022-02-16 11:55:52 +0900

    PlayerInventoryDataへの処理の委譲

--- Game.PlayerInventory/PlayerCraftingInventoryData.cs
+++ Game.PlayerInventory/PlayerCraftingInventoryData.cs
@@ -1,53 +1,38 @@
 using Core.Item;
 using Game.PlayerInventory.Interface;
+using PlayerInventory.Event;
 
 namespace PlayerInventory
 {
     public class PlayerCraftingInventoryData : ICraftingInventory
     {
-        public IItemStack GetItem(int slot)
-        {
-            throw new System.NotImplementedException();
-        }
+        //CraftInventoryはほとんどプレイヤーのインベントリと処理が同じなので、IInventoryの処理はPlayerInventoryDataに委譲しておく
+        private readonly PlayerInventoryData _playerInventoryData;
 
-        public void SetItem(int slot, IItemStack itemStack)
+        public PlayerCraftingInventoryData(int playerId, PlayerInventoryUpdateEvent playerInventoryUpdateEvent,
+            ItemStackFactory itemStackFactory)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public void SetItem(int slot, int itemId, int count)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IItemStack ReplaceItem(int slot, IItemStack itemStack)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IItemStack ReplaceItem(int slot, int itemId, int count)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IItemStack InsertItem(IItemStack itemStack)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public IItemStack InsertItem(int itemId, int count)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public int GetSlotSize()
-        {
-            throw new System.NotImplementedException();
+            _playerInventoryData = 
+                new PlayerInventoryData(playerId, playerInventoryUpdateEvent, itemStackFactory,PlayerInventoryConst.CraftingInventorySize);
         }
 
         public void Craft()
         {
             throw new System.NotImplementedException();
         }
+
+
+
+        #region delgate to PlayerInventoryData
+        public IItemStack GetItem(int slot) { return _playerInventoryData.GetItem(slot); }
+        public void SetItem(int slot, IItemStack itemStack) { _playerInventoryData.SetItem(slot, itemStack); }
+        public void SetItem(int slot, int itemId, int count) { _playerInventoryData.SetItem(slot, itemId, count); }
+        public IItemStack ReplaceItem(int slot, IItemStack itemStack) { return _playerInventoryData.ReplaceItem(slot, itemStack); }
+        public IItemStack ReplaceItem(int slot, int itemId, int count) { return _playerInventoryData.ReplaceItem(slot, itemId, count); }
+        public IItemStack InsertItem(IItemStack itemStack) { return _playerInventoryData.InsertItem(itemStack); }
+        public IItemStack InsertItem(int itemId, int count) { return _playerInventoryData.InsertItem(itemId, count); }
+        public int GetSlotSize() { return _playerInventoryData.GetSlotSize(); }
+
+        #endregion
     }
 }
