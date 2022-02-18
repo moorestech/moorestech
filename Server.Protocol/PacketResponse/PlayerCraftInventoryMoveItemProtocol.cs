a4fcfbb556cf1298af7c1ae5c55fae76df900266
sakastudio <sakastudio100@gmail.com>
2022-02-18 11:59:52 +0900

    PlayerCraftInventoryMoveItemProtocolの作成

---
+++ Server.Protocol/PacketResponse/PlayerCraftInventoryMoveItemProtocol.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Core.Item;
+using Game.PlayerInventory.Interface;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Protocol.PacketResponse.Util;
+using Server.Util;
+
+namespace Server.Protocol.PacketResponse
+{
+    public class PlayerCraftInventoryMoveItemProtocol : IPacketResponse
+    {
+        private readonly IPlayerInventoryDataStore _playerInventoryDataStore;
+        private readonly ItemStackFactory _itemStackFactory;
+
+        public PlayerCraftInventoryMoveItemProtocol(ServiceProvider serviceProvider)
+        {
+            _playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+            _itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+        }
+
+        public List<byte[]> GetResponse(List<byte> payload)
+        {
+            var payloadData = new ByteArrayEnumerator(payload);
+            payloadData.MoveNextToGetShort(); //パケットID
+            var playerId = payloadData.MoveNextToGetInt();
+            var fromSlot = payloadData.MoveNextToGetInt();
+            var toSlot = payloadData.MoveNextToGetInt();
+            var itemCount = payloadData.MoveNextToGetInt();
+
+            var craftingInventory = _playerInventoryDataStore.GetInventoryData(playerId).CraftingInventory;
+
+            new InventoryItemMove().Move(_itemStackFactory, craftingInventory, fromSlot, craftingInventory, toSlot,
+                itemCount);
+
+            return new List<byte[]>();
+        }
+    }
+}
