abaa47d134bbcfe0e1217442ea4e6f41c4ab4268
sakastudio <sakastudio100@gmail.com>
2022-02-18 11:56:47 +0900

    CraftInventoryPlayerMainInventoryMoveItemProtocolの実装

---
+++ Server.Protocol/PacketResponse/CraftInventoryPlayerMainInventoryMoveItemProtocol.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Core.Inventory;
+using Core.Item;
+using Game.PlayerInventory.Interface;
+using Game.World.Interface.DataStore;
+using Microsoft.Extensions.DependencyInjection;
+using Server.Protocol.PacketResponse.Util;
+using Server.Util;
+
+namespace Server.Protocol.PacketResponse
+{
+    public class CraftInventoryPlayerMainInventoryMoveItemProtocol : IPacketResponse
+    {
+        private readonly IPlayerInventoryDataStore _playerInventoryDataStore;
+        private readonly ItemStackFactory _itemStackFactory;
+
+        public CraftInventoryPlayerMainInventoryMoveItemProtocol(ServiceProvider serviceProvider)
+        {
+            _playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+            _itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+        }
+
+        public List<byte[]> GetResponse(List<byte> payload)
+        {
+            var payloadData = new ByteArrayEnumerator(payload);
+            payloadData.MoveNextToGetShort();
+            var flag = payloadData.MoveNextToGetByte();
+            var playerId = payloadData.MoveNextToGetInt();
+            var mainInventorySlot = payloadData.MoveNextToGetInt();
+            var craftInventorySlot = payloadData.MoveNextToGetInt();
+            var moveItemCount = payloadData.MoveNextToGetInt();
+
+            var craftInventory = _playerInventoryDataStore.GetInventoryData(playerId).CraftingInventory;
+            var mainInventory = _playerInventoryDataStore.GetInventoryData(playerId).MainInventory;
+
+            var inventoryItemMove = new InventoryItemMove();
+            
+            //フラグが0の時はメインインベントリからクラフトインベントリにアイテムを移す
+            if (flag == 0)
+            {
+                inventoryItemMove.Move(_itemStackFactory, mainInventory, mainInventorySlot, craftInventory,
+                    craftInventorySlot, moveItemCount);
+            }
+            else if (flag == 1)
+            {
+                inventoryItemMove.Move(_itemStackFactory, craftInventory, craftInventorySlot, mainInventory,
+                    mainInventorySlot, moveItemCount);
+            }
+
+            return new List<byte[]>();
+        }
+    }
+}
