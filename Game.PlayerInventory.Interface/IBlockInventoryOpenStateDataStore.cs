d7933bfe10162645be64abf7d7e38058b9f7da30
sakastudio <sakastudio100@gmail.com>
2022-02-23 21:17:36 +0900

    IBlockInventoryOpenStateDataStoreの作成

---
+++ Game.PlayerInventory.Interface/IBlockInventoryOpenStateDataStore.cs
@@ -0,0 +1,10 @@
+﻿using Game.World.Interface.DataStore;
+
+namespace Game.PlayerInventory.Interface
+{
+    public interface IBlockInventoryOpenStateDataStore
+    {
+        public bool IsOpen(int playerId);
+        public Coordinate GetOpenCoordinates(int playerId);
+    }
+}
