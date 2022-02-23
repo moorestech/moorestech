f7c42a64586febb43bd435a9ea65398be9213f21
sakastudio <sakastudio100@gmail.com>
2022-02-23 21:34:58 +0900

    BlockInventoryOpenCloseProtocolの実装を作成

--- Game.PlayerInventory.Interface/IBlockInventoryOpenStateDataStore.cs
+++ Game.PlayerInventory.Interface/IBlockInventoryOpenStateDataStore.cs
@@ -6,5 +6,7 @@ namespace Game.PlayerInventory.Interface
     {
         public bool IsOpen(int playerId);
         public Coordinate GetOpenCoordinates(int playerId);
+        public void Open(int playerId, Coordinate coordinate);
+        public void Close(int playerId);
     }
 }
---
+++ Server.Protocol/PacketResponse/BlockInventoryOpenCloseProtocol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Game.PlayerInventory.Interface;
+using Game.World.Interface.DataStore;
+using Server.Util;
+
+namespace Server.Protocol.PacketResponse
+{
+    public class BlockInventoryOpenCloseProtocol : IPacketResponse
+    {
+        private readonly IBlockInventoryOpenStateDataStore _inventoryOpenState;
+        private const byte IsOpenFlag = 1;
+
+        public BlockInventoryOpenCloseProtocol(IBlockInventoryOpenStateDataStore inventoryOpenState)
+        {
+            _inventoryOpenState = inventoryOpenState;
+        }
+
+        public List<byte[]> GetResponse(List<byte> payload)
+        {
+            var enumerator = new ByteArrayEnumerator(payload);
+            enumerator.MoveNextToGetShort(); //packet id
+            var x = enumerator.MoveNextToGetInt();
+            var y = enumerator.MoveNextToGetInt();
+            var playerId = enumerator.MoveNextToGetInt();
+            var isOpen = enumerator.MoveNextToGetByte() == IsOpenFlag;
+
+            //開く、閉じるのセット
+            if (isOpen)
+            {
+                _inventoryOpenState.Open(playerId,new Coordinate(x,y));
+            }
+            else
+            {
+                _inventoryOpenState.Close(playerId);
+            }
+
+
+
+            return new List<byte[]>();
+        }
+    }
+}
