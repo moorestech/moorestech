1ae07dfcfdbfc576999914e5b5a321f866dec36f
sakastudio <sakastudio100@gmail.com>
2022-02-24 01:36:50 +0900

    Idを関数に移動

--- Server.Protocol/PacketResponse/Player/ChunkBlockToPayload.cs
+++ Server.Protocol/PacketResponse/Player/ChunkBlockToPayload.cs
@@ -7,54 +7,61 @@ namespace Server.Protocol.PacketResponse.Player
 {
     public static class ChunkBlockToPayload
     {
-        public static byte[] Convert(IWorldBlockDatastore _worldBlockDatastore, Coordinate chunkCoordinate)
+        public static byte[] Convert(IWorldBlockDatastore worldBlockDatastore, Coordinate chunkCoordinate)
         {
-            var blocks = CoordinateToChunkBlockIntArray.Convert(chunkCoordinate, _worldBlockDatastore);
             
             var payload = new List<bool>();
 
             payload.AddRange(ToBitList.Convert(ToByteList.Convert((short) 1)));
             payload.AddRange(ToBitList.Convert(ToByteList.Convert(chunkCoordinate.X)));
             payload.AddRange(ToBitList.Convert(ToByteList.Convert(chunkCoordinate.Y)));
+            
+            //ブロックのIDの追加
+            var blocks = CoordinateToChunkBlockIntArray.Convert(chunkCoordinate, worldBlockDatastore);
             for (int i = 0; i < blocks.GetLength(0); i++)
             {
                 for (int j = 0; j < blocks.GetLength(1); j++)
                 {
-                    var id = blocks[i, j];
-                    //空気ブロックの追加
-                    if (id == BlockConst.EmptyBlockId)
-                    {
-                        payload.Add(false);
-                        continue;
-                    }
-
-                    payload.Add(true);
-                    //byte整数
-                    if (byte.MinValue <= id && id <= byte.MaxValue)
-                    {
-                        payload.Add(false);
-                        payload.Add(false);
-                        payload.AddRange(ToBitList.Convert((byte) id));
-                        continue;
-                    }
-
-                    //short整数
-                    if (short.MinValue <= id && id <= short.MaxValue)
-                    {
-                        payload.Add(false);
-                        payload.Add(true);
-                        payload.AddRange(ToBitList.Convert(ToByteList.Convert((short) id)));
-                        continue;
-                    }
-
-                    //int整数
-                    payload.Add(true);
-                    payload.Add(false);
-                    payload.AddRange(ToBitList.Convert(ToByteList.Convert(id)));
+                    var blockId = blocks[i, j];
+                    SetBlockId(payload, blockId);
                 }
             }
 
             return BitListToByteList.Convert(payload).ToArray();
+        }
+
+        private static void SetBlockId(List<bool> payload,int id)
+        {
+            //空気ブロックの追加
+            if (id == BlockConst.EmptyBlockId)
+            {
+                payload.Add(false);
+                return;
+            }
+
+            payload.Add(true);
+            //byte整数
+            if (byte.MinValue <= id && id <= byte.MaxValue)
+            {
+                payload.Add(false);
+                payload.Add(false);
+                payload.AddRange(ToBitList.Convert((byte) id));
+                return;
+            }
+
+            //short整数
+            if (short.MinValue <= id && id <= short.MaxValue)
+            {
+                payload.Add(false);
+                payload.Add(true);
+                payload.AddRange(ToBitList.Convert(ToByteList.Convert((short) id)));
+                return;
+            }
+
+            //int整数
+            payload.Add(true);
+            payload.Add(false);
+            payload.AddRange(ToBitList.Convert(ToByteList.Convert(id)));
         }
     }
 }
