0c0c38086dbe7e16e3d065adcfd377c06116758a
sakastudio <sakastudio100@gmail.com>
2022-02-23 18:18:04 +0900

    IBlockOpenableInventoryUpdateEventの作成

---
+++ Core.Block/Event/IBlockOpenableInventoryUpdateEvent.cs
@@ -0,0 +1,26 @@
+﻿using Core.Item;
+
+namespace Core.Block.Event
+{
+    public interface IBlockOpenableInventoryUpdateEvent
+    {
+        public delegate void BlockInventoryEvent(
+            BlockOpenableInventoryUpdateEventProperties properties);
+
+        public void Subscribe(BlockInventoryEvent properties);
+    }
+
+    public class BlockOpenableInventoryUpdateEventProperties
+    {
+        public readonly int EntityId;
+        public readonly int SlotId;
+        public readonly IItemStack ItemStack;
+
+        public BlockOpenableInventoryUpdateEventProperties(IItemStack itemStack, int slotId, int entityId)
+        {
+            ItemStack = itemStack;
+            SlotId = slotId;
+            EntityId = entityId;
+        }
+    }
+}
