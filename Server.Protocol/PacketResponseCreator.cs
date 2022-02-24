0c1c17c02c43161080e5cfff5c6f953aab38a788
twwalter <mgnalter.1332@gmail.com>
2022-02-24 22:39:01 +0900

    採掘実行プロトコル完成

--- Server.Protocol/PacketResponseCreator.cs
+++ Server.Protocol/PacketResponseCreator.cs
@@ -31,7 +31,7 @@ namespace Server.Protocol
             _packetResponseList.Add(new CraftInventoryPlayerMainInventoryMoveItemProtocol(serviceProvider));
             _packetResponseList.Add(new CraftInventoryMoveItemProtocol(serviceProvider));
             _packetResponseList.Add(new CraftProtocol(serviceProvider));
-            _packetResponseList.Add(new DummyProtocol()); // 採掘実行プロトコルの予約
+            _packetResponseList.Add(new MiningOperationProtocol(serviceProvider)); // 採掘実行プロトコルの予約
             _packetResponseList.Add(new BlockInventoryOpenCloseProtocol(serviceProvider));
 
             serviceProvider.GetService<VeinGenerator>();
---
+++ Test/CombinedTest/Server/PacketTest/MiningOperationProtocolTest.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Core.Block.BlockInventory;
+using Core.Ore;
+using Core.Ore.Config;
+using Game.PlayerInventory.Interface;
+using Game.World.Interface.DataStore;
+using Game.WorldMap;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Server;
+using Server.Util;
+
+namespace Test.CombinedTest.Server.PacketTest
+{
+    public class MiningOperationProtocolTest
+    {
+
+        [Test]
+        public void MiningTest()
+        {
+
+            int i = 0;
+            int j = 0;
+            int PlayerId = 0;
+            int playerSlotIndex = 0;
+            var oreId = 0;
+            
+            var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create();
+            var oreConfig = serviceProvider.GetService<IOreConfig>();
+            var seed = serviceProvider.GetService<Seed>();
+            
+            var playerInventoryData =
+                serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId);
+            
+            //100×100マス内にある鉱石を探知
+            var veinGenerator = new VeinGenerator(new Seed(seed.SeedValue),new OreConfig());
+
+            
+            for (i = 0; i < 500; i++)
+            {
+                for (j = 0; j < 500; j++)
+                {
+                    oreId = veinGenerator.GetOreId(i, j);
+                    if (oreId != OreConst.NoneOreId)
+                    {
+                        
+                        break;
+                    }
+                }
+                if (oreId != OreConst.NoneOreId)
+                {
+                    break;
+                }
+            }
+
+            var oreItemId = oreConfig.OreIdToItemId(oreId);
+            
+            //プロトコルを使って鉱石を採掘
+            packet.GetPacketResponse(MiningOperation(i, j, PlayerId));
+            
+            //インベントリーに鉱石がはいっているか
+            Assert.AreEqual(oreItemId, playerInventoryData.MainOpenableInventory.GetItem(playerSlotIndex).Id);
+            Assert.AreEqual(1, playerInventoryData.MainOpenableInventory.GetItem(playerSlotIndex).Count);
+
+            //ストップウォッチ（一時退避中）
+            //System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            
+            
+            //パケット
+           List<byte> MiningOperation(int x, int y,int playerId)
+            {
+                var payload = new List<byte>();
+                payload.AddRange(ToByteList.Convert((short) 15));
+                payload.AddRange(ToByteList.Convert(x));
+                payload.AddRange(ToByteList.Convert(y));
+                payload.AddRange(ToByteList.Convert(playerId));
+
+                return payload;
+            }
+
+
+
+
+        }
+    }
+}
