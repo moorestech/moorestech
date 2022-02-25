04b2ab90b7f4e9e3e2ea39454db8e4b0aaaa3551
sakastudio <sakastudio100@gmail.com>
2022-02-25 10:22:11 +0900

    プロトコルを送るための仮コード

---
+++ Assets/Scripts/MainGame/Network/Send/SendCraftingInventoryMainInventoryMoveItemProtocol.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MainGame.Network.Send
+{
+    public class SendCraftingInventoryMainInventoryMoveItemProtocol
+    {
+        public void Send(bool toBlock, int fromSlot, int toSlot, int itemCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Scripts/MainGame/Network/Send/SendCraftingInventoryMoveItemProtocol.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MainGame.Network.Send
+{
+    public class SendCraftingInventoryMoveItemProtocol
+    {
+        public void Send( int fromSlot, int toSlot, int itemCount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
