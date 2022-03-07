8debb11be0d396bc39b400154bbc2e1f88c0c142
sakastudio <sakastudio100@gmail.com>
2022-03-08 04:01:44 +0900

    SendMiningProtocolの実装

--- Assets/Scripts/MainGame/Network/Send/SendMiningProtocol.cs
+++ Assets/Scripts/MainGame/Network/Send/SendMiningProtocol.cs
@@ -1,12 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using MainGame.Network.Util;
+using UnityEngine;
 
 namespace MainGame.Network.Send
 {
     public class SendMiningProtocol
     {
-        public void Send(Vector2Int vector2Int)
+        private readonly ISocket _socket;
+        private const short ProtocolId = 16;
+        private readonly int _playerId;
+        
+        public SendMiningProtocol(PlayerConnectionSetting playerConnectionSetting,ISocket socket)
         {
-            //TODO 
+            _socket = socket;
+            _playerId = playerConnectionSetting.PlayerId;
+        }
+        
+        public void Send(Vector2Int pos)
+        {
+            var packet = new List<byte>();
+            
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            packet.AddRange(ToByteList.Convert(pos.x));
+            packet.AddRange(ToByteList.Convert(pos.y));
+            packet.AddRange(ToByteList.Convert(_playerId));
+            
+            _socket.Send(packet.ToArray());
         }
     }
 }
