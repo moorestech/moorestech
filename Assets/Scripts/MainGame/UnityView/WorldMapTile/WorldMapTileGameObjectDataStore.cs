c02dbc15d3dd3a859fa0f2415bd41855370a055e
sakastudio <sakastudio100@gmail.com>
2022-03-07 18:12:47 +0900

    WorldMapTileGameObjectDataStoreの作成

---
+++ Assets/Scripts/MainGame/UnityView/WorldMapTile/WorldMapTileGameObjectDataStore.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+
+namespace MainGame.UnityView.WorldMapTile
+{
+    public class WorldMapTileGameObjectDataStore : MonoBehaviour
+    {
+        private WorldMapTileObjects _worldMapTileObjects;
+        
+        private Dictionary<Vector2Int,MapTileObject> _blockObjectsDictionary = new();
+
+        [Inject]
+        public void Construct(WorldMapTileObjects worldMapTileObjects)
+        {
+            _worldMapTileObjects = worldMapTileObjects;
+        }
+        
+        public void GameObjectBlockPlace(Vector2Int tilePosition, int tileId)
+        {
+            MainThreadExecutionQueue.Instance.Insert(() =>
+            {
+                var tile = Instantiate(
+                    _worldMapTileObjects.GetTile(tileId),
+                    new Vector3(tilePosition.x, 0, tilePosition.y),
+                    Quaternion.Euler(0, 0, 0),
+                    transform).GetComponent<MapTileObject>();
+                
+                _blockObjectsDictionary.Add(tilePosition, tile);
+            });
+        }
+    }
+}
