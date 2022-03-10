f192ab590ffae147fd1abff3a5e8bbadb6dd6a0b
sakastudio <sakastudio100@gmail.com>
2022-03-10 21:52:16 +0900

    SendSaveProtocolの作成

---
+++ Assets/Scripts/MainGame/Network/Send/SendSaveProtocol.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MainGame.Network.Util;
+
+namespace MainGame.Network.Send
+{
+    public class SendSaveProtocol
+    {
+        
+        private const short ProtocolId = 17;
+        private readonly ISocket _socket;
+
+        public SendSaveProtocol(ISocket socket)
+        {
+            _socket = socket;
+        }
+
+        public void Send()
+        {
+            var packet = new List<byte>();
+            
+            packet.AddRange(ToByteList.Convert(ProtocolId));
+            
+            _socket.Send(packet.ToArray());
+        }
+    }
+}
