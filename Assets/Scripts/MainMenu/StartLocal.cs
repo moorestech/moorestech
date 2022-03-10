3fbbcebb824dafc631f2c0be5863aa42dbe0f13e
sakastudio <sakastudio100@gmail.com>
2022-03-11 01:35:22 +0900

    StartLocalに名前変更

---
+++ Assets/Scripts/MainMenu/StartLocal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class StartLocal : MonoBehaviour
+    {
+        [SerializeField] private Button startLocalButton;
+            
+#if UNITY_EDITOR_WIN
+        private const string ServerExePath = "./Server/Server.exe";
+#endif
+
+        private void Start()
+        {
+            startLocalButton.onClick.AddListener(StartLocalServer);
+        }
+
+        private void StartLocalServer()
+        {
+            Debug.Log(System.IO.Path.GetFullPath(ServerExePath));
+        }
+        
+    }
+}
