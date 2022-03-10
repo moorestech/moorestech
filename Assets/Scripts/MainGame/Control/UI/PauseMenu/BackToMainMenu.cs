2507a0919b2dfd1e29ab5b0cfc3e4ada867fe13c
sakastudio <sakastudio100@gmail.com>
2022-03-10 22:18:06 +0900

    ソケットを閉じてメニューに戻る

---
+++ Assets/Scripts/MainGame/Control/UI/PauseMenu/BackToMainMenu.cs
@@ -0,0 +1,30 @@
+using MainGame.Network;
+using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
+
+namespace MainGame.Control.UI.PauseMenu
+{
+    public class BackToMainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button backToMainMenuButton;
+        private ISocket _socket;
+
+        [Inject]
+        public void Construct(ISocket socket)
+        {
+            _socket = socket;
+        }
+        
+        void Start()
+        {
+            backToMainMenuButton.onClick.AddListener(Back);
+        }
+
+        void Back()
+        {
+            _socket.Close();
+            
+        }
+    }
+}
