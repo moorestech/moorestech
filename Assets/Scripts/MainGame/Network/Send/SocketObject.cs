9da7f882ea61135fe790a493ca221eaf23d04cc6
sakastudio <sakastudio100@gmail.com>
2022-03-11 17:39:14 +0900

    不要なusingを削除

--- Assets/Scripts/MainGame/Network/Send/SocketObject.cs
+++ Assets/Scripts/MainGame/Network/Send/SocketObject.cs
@@ -1,11 +1,7 @@
-using System;
 using System.Collections.Generic;
 using System.Net.Sockets;
-using System.Threading;
 using System.Threading.Tasks;
 using MainGame.Network.Send.SocketUtil;
-using UnityEditor.Search;
-using UnityEngine;
 
 namespace MainGame.Network.Send
 {
