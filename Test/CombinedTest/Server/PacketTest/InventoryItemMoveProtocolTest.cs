a2d063057a0c11ace677ede48dba39b67078745d
sakastudio <sakastudio100@gmail.com>
2022-04-18 20:06:44 +0900

    メインインベントリへの移動

--- Test/CombinedTest/Server/PacketTest/InventoryItemMoveProtocolTest.cs
+++ Test/CombinedTest/Server/PacketTest/InventoryItemMoveProtocolTest.cs
@@ -1,26 +1,53 @@
+using System.Collections.Generic;
+using Core.Item;
 using Game.PlayerInventory.Interface;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Server;
+using Server.Util;
 using Test.Module.TestConfig;
 
 namespace Test.CombinedTest.Server.PacketTest
 {
     public class InventoryItemMoveProtocolTest
     {
+        private int playerId = 0;
+        
         [Test]
         public void MainInventoryMoveTest()
         {
-            //TODO 
             var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModuleConfigPath.FolderPath);
             
             var mainInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(0).MainOpenableInventory;
             var equipmentInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(0).EquipmentInventory;
+            var itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+            
+            
+            //インベントリの設定
+            mainInventory.SetItem(0,1,10);
+
+            //インベントリを持っているアイテムに移す
+            packet.GetPacketResponse(GetPacket(true, 0, 0, 7));
+            
+            //移っているかチェック
+            Assert.AreEqual(itemStackFactory.Create(1,3), mainInventory.GetItem(0));
+            Assert.AreEqual(itemStackFactory.Create(1,7), equipmentInventory.GetItem(0));
         }
 
-        private byte[] GetPacket()
+        //sourceInventoryIdは 0 メイン　1 クラフト 2 ブロック 
+        private List<byte> GetPacket(bool toEquipment,byte sourceInventoryId,int inventorySlot,int itemCount,int x = 0,int y = 0)
         {
-            
+            var payload = new List<byte>();
+            payload.AddRange(ToByteList.Convert((short) 5));
+            payload.Add(toEquipment ? (byte) 0 : (byte) 1);
+            payload.Add(sourceInventoryId);
+            payload.AddRange(ToByteList.Convert(playerId));
+            payload.AddRange(ToByteList.Convert(inventorySlot));
+            payload.AddRange(ToByteList.Convert(itemCount));
+            payload.AddRange(ToByteList.Convert(x));
+            payload.AddRange(ToByteList.Convert(y));
+
+            return payload;
         }
         
         
