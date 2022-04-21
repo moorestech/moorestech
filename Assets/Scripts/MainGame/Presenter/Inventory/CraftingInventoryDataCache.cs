b6bf2cdf0cf07b72057beecca92e27433a353e70
sakastudio <sakastudio100@gmail.com>
2022-04-21 10:39:24 +0900

    クラフトインベントリの更新

--- Assets/Scripts/MainGame/Presenter/Inventory/CraftingInventoryDataCache.cs
+++ Assets/Scripts/MainGame/Presenter/Inventory/CraftingInventoryDataCache.cs
@@ -1,48 +1,40 @@
 using System.Collections.Generic;
 using MainGame.Basic;
 using MainGame.Model.Network.Event;
+using MainGame.UnityView.UI.Inventory.Control;
 
 namespace MainGame.Presenter.Inventory
 {
     public class CraftingInventoryDataCache
     {
-        
-        private List<ItemStack> _items = new(new ItemStack[PlayerInventoryConstant.CraftingInventorySize]);
-        
-        public CraftingInventoryDataCache(CraftingInventoryUpdateEvent craftingInventoryUpdateEvent)
+        private readonly PlayerInventoryViewModelController _playerInventoryViewModel;
+
+        public CraftingInventoryDataCache(CraftingInventoryUpdateEvent craftingInventoryUpdateEvent,PlayerInventoryViewModelController playerInventoryViewModel)
         {
+            _playerInventoryViewModel = playerInventoryViewModel;
             craftingInventoryUpdateEvent.OnCraftingInventoryUpdate += UpdateInventory;
             craftingInventoryUpdateEvent.OnCraftingInventorySlotUpdate += UpdateSlotInventory;
         }
 
         public void UpdateInventory(CraftingInventoryUpdateProperties properties)
         {
-            _items = properties.ItemStacks;
             //イベントの発火
-            for (int i = 0; i < _items.Count; i++)
+            for (int i = 0; i < properties.ItemStacks.Count; i++)
             {
-                //viewのUIにインベントリが更新されたことを通知する処理をキューに入れる
-                var slot = i;
-                //todo to event MainThreadExecutionQueue.Instance.Insert(() => _craftingInventoryItemView.OnInventoryUpdate(slot,_items[slot]));
+                //viewのUIにインベントリが更新されたことを通知する
+                var id = properties.ItemStacks[i].ID;
+                var count = properties.ItemStacks[i].Count;
+                _playerInventoryViewModel.SetItem(PlayerInventoryConstant.MainInventorySize + i,id,count);
             }
-            //結果のアイテムの設定
-            //todo to event MainThreadExecutionQueue.Instance.Insert(() => _craftingInventoryItemView.SetResultItem(properties.ResultItemStack,properties.CanCraft));
         }
 
         public void UpdateSlotInventory(CraftingInventorySlotUpdateProperties properties)
         {
-            var s = properties.SlotId;
-            _items[s] = properties.ItemStack;
+            var slot = properties.SlotId;
+            var id = properties.ItemStack.ID;
+            var count = properties.ItemStack.Count;
             
-            //viewのUIにインベントリが更新されたことを通知する処理をキューに入れる
-            //todo event MainThreadExecutionQueue.Instance.Insert(() => _craftingInventoryItemView.OnInventoryUpdate(s,_items[s]));
-            //結果のアイテムの設定
-            //todo event MainThreadExecutionQueue.Instance.Insert(() => _craftingInventoryItemView.SetResultItem(properties.ResultItemStack,properties.CanCraft));
-        }
-        
-        public ItemStack GetItemStack(int slot)
-        {
-            return _items[slot];
+            _playerInventoryViewModel.SetItem(PlayerInventoryConstant.MainInventorySize + slot,id,count);
         }
 
         public void Initialize() { }
