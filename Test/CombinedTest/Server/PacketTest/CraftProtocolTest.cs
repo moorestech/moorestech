d0ad4de9db55fcad6ffbe448d3897f55e601536c
sakastudio <sakastudio100@gmail.com>
2022-04-21 14:33:58 +0900

    追加したプロトコルのテスト

--- Test/CombinedTest/Server/PacketTest/CraftProtocolTest.cs
+++ Test/CombinedTest/Server/PacketTest/CraftProtocolTest.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Core.Item;
 using Game.Crafting.Interface;
 using Game.PlayerInventory.Interface;
 using Microsoft.Extensions.DependencyInjection;
@@ -37,11 +38,69 @@ namespace Test.CombinedTest.Server.PacketTest
             var payLoad = new List<byte>();
             payLoad.AddRange(ToByteList.Convert(PacketId));
             payLoad.AddRange(ToByteList.Convert(PlayerId));
+            payLoad.Add(0);
             packet.GetPacketResponse(payLoad);
             
             
             //クラフト結果がResultSlotにアイテムが入っているかチェック
             Assert.AreEqual(craftConfig.Result,craftInventory.GetItem(PlayerInventoryConst.CraftingInventorySize - 1 ));
+        }
+
+        [Test]
+        public void AllCraftTest()
+        {
+            var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModuleConfigPath.FolderPath);
+            
+            var itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+            var craftInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId).CraftingOpenableInventory;
+            var mainInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId).MainOpenableInventory;
+            var craftConfig = serviceProvider.GetService<ICraftingConfig>().GetCraftingConfigList()[3]; //id3のレシピはこのテスト用のレシピ
+            
+            //craftingInventoryに2つ分のアイテムを入れる
+            craftInventory.SetItem(0,itemStackFactory.Create(1,2));
+            
+            
+            
+            //プロトコルでクラフト実行
+            var payLoad = new List<byte>();
+            payLoad.AddRange(ToByteList.Convert(PacketId));
+            payLoad.AddRange(ToByteList.Convert(PlayerId));
+            payLoad.Add(1);
+            packet.GetPacketResponse(payLoad);
+            
+            
+            //クラフト結果がメインインベントリにアイテムが入っているかチェック
+            Assert.AreEqual(craftConfig.Result.Id,mainInventory.GetItem(0 ).Id);
+            Assert.AreEqual(100,mainInventory.GetItem(0 ).Count);
+            Assert.AreEqual(60,mainInventory.GetItem(1 ).Count);
+        }
+
+        [Test]
+        public void OneStackCraftTest()
+        {
+            var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModuleConfigPath.FolderPath);
+            
+            var itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+            var craftInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId).CraftingOpenableInventory;
+            var mainInventory = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId).MainOpenableInventory;
+            var craftConfig = serviceProvider.GetService<ICraftingConfig>().GetCraftingConfigList()[3]; //id3のレシピはこのテスト用のレシピ
+            
+            //craftingInventoryに2つ分のアイテムを入れる
+            craftInventory.SetItem(0,itemStackFactory.Create(1,2));
+            
+            
+            
+            //プロトコルでクラフト実行
+            var payLoad = new List<byte>();
+            payLoad.AddRange(ToByteList.Convert(PacketId));
+            payLoad.AddRange(ToByteList.Convert(PlayerId));
+            payLoad.Add(2);
+            packet.GetPacketResponse(payLoad);
+            
+            
+            //クラフト結果がメインインベントリにアイテムが入っているかチェック
+            Assert.AreEqual(craftConfig.Result.Id,mainInventory.GetItem(0 ).Id);
+            Assert.AreEqual(80,mainInventory.GetItem(0 ).Count);
         }
     }
 }
