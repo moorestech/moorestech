2df3c590a2fee50f8a2846d5fd168105f832999a
sakastudio <sakastudio100@gmail.com>
2022-04-21 14:55:38 +0900

    テストのクラスだけ作成

---
+++ Test/CombinedTest/Server/PacketTest/CraftProtocolFailedTest.cs
@@ -0,0 +1,45 @@
+using NUnit.Framework;
+
+namespace Test.CombinedTest.Server.PacketTest
+{
+    public class CraftProtocolFailedTest
+    {
+        /// <summary>
+        /// 別のアイテムを持っていてクラフトできないテスト
+        /// </summary>
+        [Test]
+        public void CanNotNormalCraftTest()
+        {
+            
+        }
+        
+        /// <summary>
+        /// 全てクラフトを実行した時にインベントリがいっぱいだとインベントリが満杯になり、クラフトスロットに余るテスト
+        /// </summary>
+        [Test]
+        public void AllCraftReminderTest()
+        {
+            
+        }
+        
+        /// <summary>
+        /// インベントリがいっぱいでクラフトを実行できないテスト
+        /// </summary>
+        [Test]
+        public void AllCanNotCraftTest()
+        {
+            
+        }
+
+        
+        /// <summary>
+        /// インベントリがいっぱいで1スタックのクラフトを実行できないテスト
+        /// </summary>
+        [Test]
+        public void OneCanNotStackCraftTest()
+        {
+            
+        }
+        
+    }
+}
