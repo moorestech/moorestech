d172dd179ea1bd618895244addc18639dda796cf
sakastudio <sakastudio100@gmail.com>
2022-05-07 22:18:04 +0900

    ログを削除

--- Server.Protocol/PacketResponse/PlayerInventoryResponseProtocol.cs
+++ Server.Protocol/PacketResponse/PlayerInventoryResponseProtocol.cs
@@ -26,57 +26,26 @@ namespace Server.Protocol.PacketResponse
             response.AddRange(ToByteList.Convert((short) 4));
             response.AddRange(ToByteList.Convert(playerId));
             response.AddRange(ToByteList.Convert((short) 0));
-            
-            
-            //ログ用のstring
-            var inventory = new StringBuilder();
-            inventory.Append("Main");
-            inventory.Append("\n");
 
             //メインインベントリのアイテムを設定
             for (int i = 0; i < PlayerInventoryConst.MainInventorySize; i++)
             {
                 response.AddRange(ToByteList.Convert(playerInventory.MainOpenableInventory.GetItem(i).Id));
                 response.AddRange(ToByteList.Convert(playerInventory.MainOpenableInventory.GetItem(i).Count));
-                
-                inventory.Append(playerInventory.MainOpenableInventory.GetItem(i).Id + " " + playerInventory.MainOpenableInventory.GetItem(i).Count);
-                inventory.Append("　");
-                if ((i+1) % PlayerInventoryConst.MainInventoryColumns == 0)
-                {
-                    inventory.Append("\n");
-                }
             }
             
             
             //グラブインベントリのアイテムを設定
             response.AddRange(ToByteList.Convert(playerInventory.GrabInventory.GetItem(0).Id));
             response.AddRange(ToByteList.Convert(playerInventory.GrabInventory.GetItem(0).Count));
-            
-            
-            inventory.Append("\n");
-            inventory.Append("Grab");
-            inventory.Append("\n");
-            inventory.Append(playerInventory.GrabInventory.GetItem(0).Id + " " + playerInventory.GrabInventory.GetItem(0).Count);
-            inventory.Append("\n");
-            inventory.Append("Craft");
-            inventory.Append("\n");
-            
-            
+
+
             //クラフトインベントリのアイテムを設定
             for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
             {
                 response.AddRange(ToByteList.Convert(playerInventory.CraftingOpenableInventory.GetItem(i).Id));
                 response.AddRange(ToByteList.Convert(playerInventory.CraftingOpenableInventory.GetItem(i).Count));
-                
-                inventory.Append(playerInventory.CraftingOpenableInventory.GetItem(i).Id + " " + playerInventory.CraftingOpenableInventory.GetItem(i).Count);
-                inventory.Append("　");
-                if ((i+1) % PlayerInventoryConst.CraftingInventoryRows == 0)
-                {
-                    inventory.Append("\n");
-                }
             }
-            
-            Console.WriteLine(inventory.ToString());
             
             //クラフト結果のアイテムを設定
             response.AddRange(ToByteList.Convert(playerInventory.CraftingOpenableInventory.GetCreatableItem().Id));
