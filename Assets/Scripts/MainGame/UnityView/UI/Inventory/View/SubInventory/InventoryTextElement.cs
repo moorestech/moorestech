cf2047840e74781b83f015ab6dacd4afbb84ab62
sakastudio <sakastudio100@gmail.com>
2022-05-09 12:15:49 +0900

    テキスト要素のスクリプト

---
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/View/SubInventory/InventoryTextElement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace MainGame.UnityView.UI.Inventory.View.SubInventory
+{
+    public class InventoryTextElement : MonoBehaviour
+    {
+        public void SetText(string text,int fontSize){}
+        
+    }
+}
