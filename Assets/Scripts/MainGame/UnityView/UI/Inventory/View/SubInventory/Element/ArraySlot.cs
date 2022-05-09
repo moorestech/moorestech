df0d562b724af1da47df96722cfa0657012feb5d
sakastudio <sakastudio100@gmail.com>
2022-05-09 10:09:28 +0900

    見やすいように修正

--- Assets/Scripts/MainGame/UnityView/UI/Inventory/View/SubInventory/Element/ArraySlot.cs
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/View/SubInventory/Element/ArraySlot.cs
@@ -2,23 +2,23 @@ namespace MainGame.UnityView.UI.Inventory.View.SubInventory.Element
 {
     public class ArraySlot : ISubInventoryElement
     {
+
+        public SubInventoryElementType ElementType => SubInventoryElementType.ArraySlot;
+        public int Priority { get; }
+
+        public readonly float X;
+        public readonly float Y;
+
+        public readonly int Height;
+        public readonly int Width;
+        
         public ArraySlot(float x, float y, int priority, int height, int width)
         {
             X = x;
             Y = y;
-            this.priority = priority;
+            Priority = priority;
             Height = height;
             Width = width;
         }
-
-        public SubInventoryElementType ElementType => SubInventoryElementType.ArraySlot;
-        public int Priority => priority;
-        
-        public float X;
-        public float Y;
-        public int priority;
-        
-        public int Height;
-        public int Width;
     }
 }
--- Assets/Scripts/MainGame/UnityView/UI/Inventory/View/SubInventory/Element/OneSlot.cs
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/View/SubInventory/Element/OneSlot.cs
@@ -3,22 +3,21 @@ namespace MainGame.UnityView.UI.Inventory.View.SubInventory.Element
     public class OneSlot : ISubInventoryElement
     {
         public SubInventoryElementType ElementType => SubInventoryElementType.OneSlot;
-        public int Priority => priority;
-        
-        //表示するX座標
-        public float X;
-        //表示するY座標
-        public float Y;
-        //表示する順番
-        public int priority;
+        public int Priority { get; }
 
-        public InventorySlotElementOptions Options;
+        //表示するX座標
+        public readonly float X;
+        //表示するY座標
+        public readonly float Y;
+        //表示する順番
+
+        public readonly InventorySlotElementOptions Options;
 
         public OneSlot(float x, float y, int priority, InventorySlotElementOptions options)
         {
             X = x;
             Y = y;
-            this.priority = priority;
+            Priority = priority;
             Options = options;
         }
     }
