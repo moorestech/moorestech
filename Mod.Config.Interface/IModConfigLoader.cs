65dbab85ef6dea824e79dcc9d6d732789bfd0fd8
sakastudio <sakastudio100@gmail.com>
2022-05-12 22:17:12 +0900

    コンフィグで

--- Mod.Config.Interface/IModConfigLoader.cs
+++ Mod.Config.Interface/IModConfigLoader.cs
@@ -1,3 +1,9 @@
+using System.Collections.Generic;
+using Core.Block.Config.LoadConfig;
+using Core.Block.RecipeConfig;
+using Core.Item.Config;
+using Game.Crafting.Config;
+
 namespace Mod.Config.Interface
 {
     public interface IModConfigLoader
@@ -5,8 +11,11 @@ namespace Mod.Config.Interface
         
     }
 
-    public class LoadConfig
+    public class LoadConfigContainer
     {
-        
+        public readonly List<ItemConfigData> ItemConfigs;
+        public readonly List<BlockConfigData> BlockConfigs;
+        public readonly List<MachineRecipeConfig> MachineRecipeConfigs;
+        public readonly List<CraftConfig> CraftConfigs;
     }
 }
