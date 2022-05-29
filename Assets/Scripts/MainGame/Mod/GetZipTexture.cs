7a33f417d0bc30a9e7d3f89f20cda9ade7176555
sakastudio <sakastudio100@gmail.com>
2022-05-29 16:50:25 +0900

    zipからテクスチャ2dの取得

---
+++ Assets/Scripts/MainGame/Mod/GetZipTexture.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.IO.Compression;
+using UnityEngine;
+
+namespace MainGame.Mod
+{
+    public static class GetZipTexture
+    {
+        public static Texture2D Get(ZipArchive zipArchive, string path)
+        {
+            var zipArchiveEntry = zipArchive.GetEntry(path);
+            if (zipArchiveEntry == null) return null;
+            
+            using var textureBinary = new BinaryReader(zipArchiveEntry.Open());
+            
+            
+            var bytes = textureBinary.ReadBytes((int)textureBinary.BaseStream.Length);
+            
+            Texture2D texture = new Texture2D(1, 1);
+            texture.LoadImage(bytes);
+            
+            return texture;
+        }
+    }
+}
---
+++ Assets/Scripts/MainGame/Mod/ItemTextureLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.Item.Config;
+using Mod.Loader;
+using SinglePlay;
+using UnityEngine;
+
+namespace MainGame.Mod
+{
+    public static class ItemTextureLoader
+    {
+        private const string TextureDirectory = "Assets/Resources/Textures/";
+        public static List<Texture2D> GetItemTexture(string modDirectory,SinglePlayInterface singlePlayInterface)
+        {
+            var textureList = new List<Texture2D>();
+            
+            using var mods = new ModsResource(modDirectory);
+
+            foreach (var mod in mods.Mods)
+            {
+                var itemIds = singlePlayInterface.ItemConfig.GetItemIds(mod.Value.ModMetaJson.ModId);
+                var itemConfigs = itemIds.Select(singlePlayInterface.ItemConfig.GetItemConfig).ToList();
+
+                textureList.AddRange(GetTextures(itemConfigs,mod.Value));
+            }
+
+            return textureList;
+        }
+
+
+        private static List<Texture2D> GetTextures(List<ItemConfigData> itemConfigs,global::Mod.Loader.Mod mod)
+        {
+            var textureList = new List<Texture2D>();
+            foreach (var config in itemConfigs)
+            {
+                var texture = GetZipTexture.Get(mod.ZipArchive, TextureDirectory + config.Name);
+                if (texture == null)
+                {
+                    Debug.LogError("ItemTexture Not Found  ModId:" + mod.ModMetaJson.ModId + " ItemName:" + config.Name);
+                    continue;
+                }
+                textureList.Add(texture);
+            }
+
+            return textureList;
+        }
+    }
+}
