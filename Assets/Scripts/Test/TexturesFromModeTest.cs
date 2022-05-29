a7caf0e28d710f5c924caf098106947dfafbbc6f
sakastudio <sakastudio100@gmail.com>
2022-05-29 21:20:01 +0900

    テクスチャロードのテスト用コード

---
+++ Assets/Scripts/Test/TexturesFromModeTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using GameConst;
+using MainGame.Mod;
+using SinglePlay;
+using UnityEngine;
+
+namespace Test
+{
+    public class TexturesFromModeTest : MonoBehaviour
+    {
+        [SerializeField] private List<Texture2D> textures;
+
+        private void Start()
+        {
+            textures = ItemTextureLoader.GetItemTexture(ServerConst.ServerModsDirectory,new SinglePlayInterface(ServerConst.ServerModsDirectory));
+        }
+    }
+}
