3dea8072e64f3e7e6d8b4f260c9b6b2b4838045f
sakastudio <sakastudio100@gmail.com>
2022-05-31 00:27:08 +0900

    texture 2dをspriteに変換する処理

---
+++ Assets/Scripts/MainGame/Basic/TextureExtension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace MainGame.Basic
+{
+    public static class TextureExtension
+    {
+        public static Sprite ToSprite(this Texture2D texture2D)
+        {
+            return Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), Vector2.zero);
+        }
+    }
+}
