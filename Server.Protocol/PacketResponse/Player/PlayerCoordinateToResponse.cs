6c6c0eca5a05e45ad5f179c8affa7117ed62e4ef
sakastudio <sakastudio100@gmail.com>
2022-06-13 13:27:26 +0900

    PlayerCoordinateSendProtocolをエンティティデータストアに変更

--- Server.Protocol/PacketResponse/Player/PlayerCoordinateToResponse.cs
+++ Server.Protocol/PacketResponse/Player/PlayerCoordinateToResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Game.Entity.Interface;
 using Game.World.Interface.DataStore;
 using Server.Protocol.PacketResponse.Const;
 using Server.Protocol.PacketResponse.Util;
@@ -8,24 +9,44 @@ namespace Server.Protocol.PacketResponse.Player
 {
     public class PlayerCoordinateToResponse
     {
+        private readonly int _playerId;
+        private readonly IEntitiesDatastore _entitiesDatastore;
+        
+        
         private const int RequestPlayerIntervalMilliSeconds = 500;
         
         private Coordinate _lastCoordinate = new Coordinate {X = Int32.MaxValue, Y = Int32.MaxValue};
         private DateTime _lastGetTime = DateTime.MinValue;
 
-        public List<Coordinate> GetResponseChunkCoordinates(Coordinate coordinate)
+        public PlayerCoordinateToResponse(int playerId, IEntitiesDatastore entitiesDatastore)
         {
-            //前回のリクエストから500ミリ秒以上経過している場合は新たにリクエストを行う
+            _playerId = playerId;
+            _entitiesDatastore = entitiesDatastore;
+        }
+
+        public List<Coordinate> GetResponseChunkCoordinates()
+        {
+            //例えばユーザーが一度ログアウトして、再度ログインすると、クライアント側ではブロックの情報は消えているが、
+            //サーバー側では前回との差分しか返さないようになってしまう
+            //そのため、前回の取得から500ミリ秒以上経過している場合は、前回座標のリセットを行う
             if (_lastGetTime.AddMilliseconds(RequestPlayerIntervalMilliSeconds) < DateTime.Now)
             {
                 _lastCoordinate = new Coordinate {X = Int32.MaxValue, Y = Int32.MaxValue};
             }
-            
             _lastGetTime = DateTime.Now;
             
-            var now = GetCoordinates(coordinate);
+            
+
+            //今の座標を取得
+            var position = _entitiesDatastore.GetPosition(_playerId);
+            //三次元の座標系はY up, X right, Z frontなので、Y軸にZを入れる
+            var nowCoordinate = new Coordinate {X = (int)position.X, Y = (int)position.Z};
+
+            
+            
+            var now = GetCoordinates(nowCoordinate);
             var last = GetCoordinates(_lastCoordinate);
-            _lastCoordinate = coordinate;
+            _lastCoordinate = nowCoordinate;
             for (int i = now.Count - 1; i >= 0; i--)
             {
                 //もし前回の取得チャンクに今回の取得チャンクとの被りがあったら削除する
