f3bcb88a5984b6e541e6a6437b75b4b8c244833d
sakastudio <sakastudio100@gmail.com>
2022-06-13 12:55:48 +0900

    ハンドシェイクの中身の実装

--- Server.Protocol/PacketResponse/InitialHandshakeProtocol.cs
+++ Server.Protocol/PacketResponse/InitialHandshakeProtocol.cs
@@ -1,16 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Game.Entity.Interface;
 using MessagePack;
+using Server.Util.MessagePack;
 
 namespace Server.Protocol.PacketResponse
 {
     public class InitialHandshakeProtocol : IPacketResponse
     {
+        public const string Tag = "va:invItemMove";
+
+        private readonly IEntitiesDatastore _entitiesDatastore;
+        private readonly IEntityFactory _entityFactory;
+        
         public List<List<byte>> GetResponse(List<byte> payload)
         {
+            var data = MessagePackSerializer.Deserialize<RequestInitialHandshakeMessagePack>(payload.ToArray());
+
+
+            var response = new ResponseInitialHandshakeMessagePack(GetPlayerPosition(data.PlayerId));
             
-            //TODO
-            return new List<List<byte>>();
+            return new List<List<byte>>(){MessagePackSerializer.Serialize(response).ToList()};
+        }
+
+
+
+
+        private Vector2MessagePack GetPlayerPosition(int playerId)
+        {
+            if (_entitiesDatastore.Exists(playerId))
+            {
+                var pos = _entitiesDatastore.GetPosition(playerId);
+                return new Vector2MessagePack(pos.X, pos.Z);
+            }
+
+            var playerEntity = _entityFactory.CreateEntity(EntityType.VanillaPlayer, playerId);
+            _entitiesDatastore.Add(playerEntity);
+
+            
+            return new Vector2MessagePack(0, 0);
         }
     }
     
@@ -42,14 +71,11 @@ namespace Server.Protocol.PacketResponse
         {
         }
 
-
-        public ResponseInitialHandshakeMessagePack(float playerPosX, float playerPosY)
+        public ResponseInitialHandshakeMessagePack(Vector2MessagePack playerPos)
         {
-            PlayerPosX = playerPosX;
-            PlayerPosY = playerPosY;
+            PlayerPos = playerPos;
         }
 
-        public float PlayerPosX { get; set; }
-        public float PlayerPosY { get; set; }
+        public Vector2MessagePack PlayerPos { get; set; }
     }
 }
