9db84ebf8b6880abec75875d2fb6c9a476bf033f
sakastudio <sakastudio100@gmail.com>
2022-06-17 10:03:19 +0900

    ハンドシェイク終了後のイベントをメインスレッド実行

--- Assets/Scripts/MainGame/Network/Receive/ReceiveInitialHandshakeProtocol.cs
+++ Assets/Scripts/MainGame/Network/Receive/ReceiveInitialHandshakeProtocol.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using MessagePack;
 using Server.Protocol.PacketResponse;
 using UnityEngine;
@@ -7,17 +9,23 @@ using UnityEngine;
 namespace MainGame.Network.Receive
 {
     public class ReceiveInitialHandshakeProtocol: IAnalysisPacket
-    { 
+    {
+        private readonly SynchronizationContext _mainThread;
+        public ReceiveInitialHandshakeProtocol()
+        {
+        
+            _mainThread = SynchronizationContext.Current;
+        }
+
         public event Action<Vector2> OnFinishHandshake;
         public void Analysis(List<byte> packet)
         {
             var data = MessagePackSerializer.Deserialize<ResponseInitialHandshakeMessagePack>(packet.ToArray());
             
             var position = new Vector2(data.PlayerPos.X,data.PlayerPos.Y);
-            
-            
-            //TODO メインスレッドで実行する
-            OnFinishHandshake?.Invoke(position);
+
+            UniTask.SwitchToMainThread();
+            _mainThread.Post(_ => OnFinishHandshake?.Invoke(position),null);
         }
     }
 }
