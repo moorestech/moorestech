2390ae31d4d5c7a2caa75b562d84d5fb69e071d3
sakastudio <sakastudio100@gmail.com>
2022-06-17 10:19:19 +0900

    プレイヤー座標の送信かいしを設定

--- Assets/Scripts/MainGame/Presenter/Player/PlayerPositionSender.cs
+++ Assets/Scripts/MainGame/Presenter/Player/PlayerPositionSender.cs
@@ -1,3 +1,4 @@
+using MainGame.Network.Receive;
 using MainGame.Network.Send;
 using MainGame.UnityView.Game;
 using UnityEngine;
@@ -11,16 +12,33 @@ namespace MainGame.Presenter.Player
         private readonly IPlayerPosition _playerPosition;
         
         private const float Interval = 0.2f;
+
+        private bool _startPositionSend = false;
         
-        public PlayerPositionSender(SendPlayerPositionProtocolProtocol protocol, IPlayerPosition playerPosition)
+        public PlayerPositionSender(SendPlayerPositionProtocolProtocol protocol, IPlayerPosition playerPosition,ReceiveInitialHandshakeProtocol receiveInitialHandshakeProtocol)
         {
             _protocol = protocol;
             _playerPosition = playerPosition;
+            
+            //_startPositionSendがないとプレイヤーの座標が0,0,0の時にプレイヤー座標が送信されるため、
+            //不要なチャンクデータの受信など不都合が発生する可能性がある（チャンクのデータはプレイヤーの周りの情報が帰ってくる）
+            //そのため、ハンドシェイクが終わってからプレイヤー座標の送信を始める
+            receiveInitialHandshakeProtocol.OnFinishHandshake += _ => _startPositionSend = true;
         }
 
         private float _timer;
+        
+        /// <summary>
+        /// Updateと同じタイミングで呼ばれる
+        /// </summary>
         public void Tick()
         {
+            if (!_startPositionSend)
+            {
+                return;
+            }
+            
+            
             _timer += Time.deltaTime;
             if (_timer < Interval) return;
             _timer = 0;
