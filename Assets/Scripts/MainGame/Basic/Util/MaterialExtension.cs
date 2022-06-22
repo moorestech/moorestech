e0f26c96b8f2410e81c54706bd2398a02f38edbe
sakastudio <sakastudio100@gmail.com>
2022-06-22 11:37:40 +0900

    マテリアルのプロパティをちゃんとコピーできた

--- Assets/Scripts/MainGame/Basic/Util/MaterialExtension.cs
+++ Assets/Scripts/MainGame/Basic/Util/MaterialExtension.cs
@@ -6,7 +6,10 @@ namespace MainGame.Basic.Util
     public static class MaterialExtension
     {
         private static readonly Material urpMaterial = (Material)Resources.Load("URPLit");
-        private static List<string> standardToUrpColor = new() { };
+        private static readonly Dictionary<string,string> standardToUrpColor = new() {{"_Color","_BaseColor"},{"_EmissionColor","_EmissionColor"}};
+        private static readonly Dictionary<string,string> standardToUrpTexture = new() {{"_MainTex","_BaseMap"}, {"_BumpMap","_BumpMap"},{"_MetallicGlossMap","_MetallicGlossMap"},{"_ParallaxMap","_ParallaxMap"},{"_OcclusionMap","_OcclusionMap"} };
+        private static readonly Dictionary<string, string> standardToFloat = new() {{"_Metallic","_Metallic"},{"_Smoothness","_Smoothness"}};
+
         public static Material StandardToUrpLit(this Material material)
         {
             if (material.shader.name != "Standard")
@@ -16,9 +19,34 @@ namespace MainGame.Basic.Util
             }
             
             var newMaterial = new Material(urpMaterial);
-            
-            newMaterial
-            
+            newMaterial = CopyProperties(material, newMaterial, standardToUrpColor,standardToUrpTexture,standardToFloat);
+
+            return newMaterial;
+        }
+
+
+        private static Material CopyProperties(Material material,Material newMaterial,Dictionary<string,string> colorIndex,Dictionary<string,string> textureIndex,Dictionary<string,string> floatIndex)
+        {
+            newMaterial.name = material.name;
+            foreach (var index in colorIndex)
+            {
+                newMaterial.SetColor(index.Value,material.GetColor(index.Key));
+            }
+            foreach (var index in colorIndex)
+            {
+                newMaterial.SetColor(index.Value,material.GetColor(index.Key));
+            }
+            foreach (var index in textureIndex)
+            {
+                newMaterial.SetTexture(index.Value,material.GetTexture(index.Key));
+                newMaterial.SetTextureOffset(index.Value,material.GetTextureOffset(index.Key));
+                newMaterial.SetTextureScale(index.Value,material.GetTextureScale(index.Key));
+            }
+
+            foreach (var index in floatIndex)
+            {
+                newMaterial.SetFloat(index.Value,material.GetFloat(index.Key));
+            }
             return newMaterial;
         }
     }
