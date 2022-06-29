c9a712ad82437b6ea8e02151ac484471fe4e7206
sakastudio <sakastudio100@gmail.com>
2022-06-29 14:51:48 +0900

    AssemblyPrerequisiteQuestsの作成

--- Game.Quest/Config/QuestLoadConfig.cs
+++ Game.Quest/Config/QuestLoadConfig.cs
@@ -11,46 +11,86 @@ namespace Game.Quest.Config
 {
     public class QuestLoadConfig
     {
-        public (Dictionary<string, List<string>> ModIdToQuest, Dictionary<string, List<QuestConfigData>> QuestConfigs)
-            LoadConfig(Dictionary<string,string> blockJsons,List<string> mods)
+        private readonly ItemStackFactory _itemStackFactory;
+
+        public QuestLoadConfig(ItemStackFactory itemStackFactory)
         {
+            _itemStackFactory = itemStackFactory;
         }
 
 
-        private (Dictionary<string, List<string>> ModIdToQuest, Dictionary<string, List<QuestConfigData>> QuestConfigs) Load(Dictionary<string,string> questJsons,List<string> mods)
+        public (Dictionary<string, List<string>> ModIdToQuest, Dictionary<string, List<QuestConfigData>> QuestConfigs) LoadConfig(Dictionary<string,string> blockJsons)
         {
+            var jsonQuestConfig = LoadJsonToQuestConfigJsonData(blockJsons);
             
-            var modIdToQuest = new Dictionary<string, List<string>>();
-            var questConfigs = new Dictionary<string, List<QuestConfigData>>();
-            foreach (var questJson in questJsons)
+            var
+        }
+
+        
+        
+        /// <summary>
+        /// 再帰を使って前提クエストの探索、作成をする
+        /// </summary>
+        /// <param name="questConfigJsonData">前提クエストのリストを作りたいjsonコンフィグ</param>
+        /// <param name="detectLoopLog">ループ検知用のログ。再帰以外の呼び出し時は新しく作る。</param>
+        /// <param name="alreadyMadeConfigs">事前に作られたクエストの流用のために、すでに作られたクエストの辞書を渡す。再帰中に新しく作った場合は</param>
+        /// <param name="keyQuestIdJsonConfigs"></param>
+        /// <returns></returns>
+        private List<QuestConfigData> AssemblyPrerequisiteQuests(QuestConfigJsonData questConfigJsonData,List<string> detectLoopLog,Dictionary<string,QuestConfigData> alreadyMadeConfigs,Dictionary<string, QuestConfigJsonData> keyQuestIdJsonConfigs)
+        {
+            //ループがないかチェックする
+            if (detectLoopLog.Contains(questConfigJsonData.Id))
             {
-                var modId = questJson.Key;
-                var loadedQuests = JsonConvert.DeserializeObject<QuestConfigJsonData[]>(questJson.Value);
-                if (loadedQuests == null)
+                //TODO 例外を出力す方法を考える
+                Console.WriteLine("[ConfigLoadLog] ModId:" + questConfigJsonData.ModId + "の前提クエストにループがありました。前提クエストをチェックしてください。　クエストId:"+questConfigJsonData.Id);
+                return new List<QuestConfigData>();
+            }
+            
+            detectLoopLog.Add(questConfigJsonData.Id);
+            
+            //前提クエストを探索、作成するループ
+            var prerequisiteQuests = new List<QuestConfigData>();
+            foreach (var prerequisiteId in questConfigJsonData.Prerequisite)
+            {
+                //すでに作ったコンフィグがあるかチェック
+                if (alreadyMadeConfigs.TryGetValue(prerequisiteId,out var prerequisiteQuest))
                 {
-                    Console.WriteLine("[Log] ModId:" + modId + "のクエストコンフィグのロードに失敗しました。");
+                    prerequisiteQuests.Add(prerequisiteQuest);
                     continue;
                 }
-
-                var resultQuests = new List<QuestConfigData>();
-                foreach (var quest in loadedQuests)
+                
+                //なかったのでJsonコンフィグにあるかチェック
+                if (!keyQuestIdJsonConfigs.TryGetValue(prerequisiteId,out var prerequisiteJsonConfig))
                 {
-                    
-                    resultQuests.Add(questData);
+                    //TODO 例外を出力す方法を考える
+                    Console.WriteLine("[ConfigLoadLog] ModId:" + questConfigJsonData.ModId + "のクエスト "+questConfigJsonData.Id  +"前提クエストに存在しないクエストIDが渡されました。　存在しないクエストId:"+prerequisiteId);
+                    continue;
                 }
-                questConfigs.Add(modId,resultQuests);
+                
+                
+                
+                //再帰を使って前提クエストの前提クエストを取得
+                var newPrerequisiteQuests = AssemblyPrerequisiteQuests(prerequisiteJsonConfig, detectLoopLog, alreadyMadeConfigs, keyQuestIdJsonConfigs);
+                //前提クエストを作成
+                var newRequestQuest = prerequisiteJsonConfig.ToQuestConfigData(newPrerequisiteQuests, _itemStackFactory);
+                //前提クエストリストと辞書に登録
+                prerequisiteQuests.Add(newRequestQuest);
+                alreadyMadeConfigs.Add(newRequestQuest.QuestId,newRequestQuest);
             }
-        }
 
+            return prerequisiteQuests;
+        }
+        
+        
+        
         /// <summary>
-        /// JSONからQuestConfigJsonDataをロードする
+        /// JSONからQuestConfigJsonDataをロードし、
         /// </summary>
-        private (
-            Dictionary<string, QuestConfigJsonData> KeyQuestIdConfigs,
-            Dictionary<string, List<QuestConfigJsonData>> KeyModIdConfigs) LoadJsonToQuestConfigJsonData(Dictionary<string,string> questJsons)
+        /// <param name="questJsons">modIdとJsonの辞書</param>
+        /// <returns>クエストIDがキーの辞書</returns>
+        private Dictionary<string, QuestConfigJsonData> LoadJsonToQuestConfigJsonData(Dictionary<string,string> questJsons)
         {
             var keyQuestIdConfigs = new Dictionary<string, QuestConfigJsonData>();
-            var keyModIdConfigs = new Dictionary<string, List<QuestConfigJsonData>>();
             
             foreach (var questJson in questJsons)
             {
@@ -59,29 +99,31 @@ namespace Game.Quest.Config
                 var loadedQuests = JsonConvert.DeserializeObject<QuestConfigJsonData[]>(questJson.Value);
                 if (loadedQuests == null)
                 {
-                    Console.WriteLine("[Log] ModId:" + modId + "のクエストコンフィグのロードに失敗しました。JSONファイルを確認してください。");
+                    //TODO 例外を出力す方法を考える
+                    Console.WriteLine("[ConfigLoadLog] ModId:" + modId + "のクエストコンフィグのロードに失敗しました。JSONファイルを確認してください。");
                     continue;
                 }
 
                 //辞書に格納
-                var questList = loadedQuests.ToList();
-                keyModIdConfigs.Add(modId,questList);
-                foreach (var quest in questList)
+                foreach (var quest in loadedQuests)
                 {
+                    quest.ModId = modId;
                     keyQuestIdConfigs.Add(quest.Id,quest);
                 }
-                
             }
 
-            return (keyQuestIdConfigs, keyModIdConfigs);
+            return keyQuestIdConfigs;
         }
-        
     }
     
 
     [JsonObject("SpaceAssets")]
     internal class QuestConfigJsonData
     {
+        [JsonIgnore]
+        public string ModId = null;
+        
+        
         [JsonProperty("Id")]
         public string Id;
         [JsonProperty("Prerequisite")]
@@ -111,7 +153,7 @@ namespace Game.Quest.Config
 
     static class QuestLoadExtension
     {
-        public static QuestConfigData ToQuestConfigData(this QuestConfigJsonData questConfigJsonData,string modId,List<QuestConfigData> prerequisiteQuests,ItemStackFactory itemStackFactory)
+        public static QuestConfigData ToQuestConfigData(this QuestConfigJsonData questConfigJsonData,List<QuestConfigData> prerequisiteQuests,ItemStackFactory itemStackFactory)
         {
             var rewardItems = new List<IItemStack>();
             for (int i = 0; i < questConfigJsonData.RewardItem.GetLength(0); i++)
@@ -123,7 +165,7 @@ namespace Game.Quest.Config
             
             
             return new QuestConfigData(
-                modId,
+                questConfigJsonData.ModId,
                 questConfigJsonData.Id,
                 prerequisiteQuests,
                 questConfigJsonData.Category,
