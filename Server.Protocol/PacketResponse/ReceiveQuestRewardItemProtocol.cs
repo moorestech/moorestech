b5e5e8ad3f73a22321128ef9e63ed34f68e8056d
sakastudio <sakastudio100@gmail.com>
2022-07-07 21:44:01 +0900

    アイテム受け取り用クラスのmessagepack

--- Server.Protocol/PacketResponse/ReceiveQuestRewardItemProtocol.cs
+++ Server.Protocol/PacketResponse/ReceiveQuestRewardItemProtocol.cs
@@ -1,7 +1,29 @@
+using System;
+using MessagePack;
+
 namespace Server.Protocol.PacketResponse
 {
     public class ReceiveQuestRewardItemProtocol
     {
         
+    }
+        
+    
+    [MessagePackObject(keyAsPropertyName :true)]
+    public class ReceiveQuestRewardItemProtocolMessagePack : ProtocolMessagePackBase
+    {
+        public ReceiveQuestRewardItemProtocolMessagePack(int playerId, string questId)
+        {
+            PlayerId = playerId;
+            QuestId = questId;
+            Tag = QuestProgressRequestProtocol.Tag;
+        }
+        
+        [Obsolete("デシリアライズ用のコンストラクタです。基本的に使用しないでください。")]
+        public ReceiveQuestRewardItemProtocolMessagePack() { }
+
+        public int PlayerId { get; set; }
+        public string QuestId { get; set; }
+
     }
 }
