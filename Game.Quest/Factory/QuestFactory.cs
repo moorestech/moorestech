5bfafe21af011fb3e29424e0fd3f2eae40e52ff7
sakastudio <sakastudio100@gmail.com>
2022-07-08 10:41:02 +0900

    QuestFactoryのロジック作成

--- Game.Quest/Factory/QuestFactory.cs
+++ Game.Quest/Factory/QuestFactory.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using Game.Quest.Factory.QuestTemplate;
 using Game.Quest.Interface;
 
 namespace Game.Quest.Factory
@@ -6,10 +8,13 @@ namespace Game.Quest.Factory
     public class QuestFactory
     {
         private readonly IQuestConfig _questConfig;
-        private Dictionary<string,IQuest> QuestTemplates = 
+        private Dictionary<string,ItemCraftQuestTemplate> QuestTemplates =　new(); 
 
         public QuestFactory(IQuestConfig questConfig)
         {
+            //クエストのテンプレート一覧の作成
+            QuestTemplates.Add(VanillaQuestTypes.ItemCraftQuestType,new ItemCraftQuestTemplate());
+            
             _questConfig = questConfig;
         }
 
@@ -17,6 +22,13 @@ namespace Game.Quest.Factory
         {
             var quest = _questConfig.GetQuestConfig(questId);
             
+            if (QuestTemplates.ContainsKey(quest.QuestType))
+            {
+                return QuestTemplates[quest.QuestType].CreateQuest(quest);
+            }
+            
+            //TODO ログ取得基盤に入れるようにする
+            throw new ArgumentException("[QuestFactory]指定されたクエストタイプ:"+quest.QuestType + "は存在しません。");
         }
     }
 }
