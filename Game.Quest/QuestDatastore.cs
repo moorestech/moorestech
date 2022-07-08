e71505fbe37641fb7db299c2b124721944649f54
sakastudio <sakastudio100@gmail.com>
2022-07-08 21:09:09 +0900

    GetPlayerQuestProgressのロジック作成

--- Game.Quest/QuestDatastore.cs
+++ Game.Quest/QuestDatastore.cs
@@ -1,13 +1,38 @@
 using System.Collections.Generic;
+using System.Linq;
+using Game.Quest.Factory;
+using Game.Quest.Interface;
 
 namespace Game.Quest
 {
     public class QuestDatastore
     {
-        private readonly Dictionary<string, List<IQuest>> _quests = new();
-        
-        
-        
-        
+        private readonly IQuestConfig _questConfig;
+        private readonly QuestFactory _questFactory;
+
+        private readonly Dictionary<int, List<IQuest>> _quests = new();
+
+        public QuestDatastore(IQuestConfig questConfig, QuestFactory questFactory)
+        {
+            _questConfig = questConfig;
+            _questFactory = questFactory;
+        }
+
+
+        public IReadOnlyList<IQuest> GetPlayerQuestProgress(int playerId)
+        {
+            if (_quests.ContainsKey(playerId))
+            {
+                return _quests[playerId];
+            }
+
+            var newQuests =
+                _questConfig.GetAllQuestConfig().
+                    Select(quest => _questFactory.CreateQuest(quest.QuestId)).ToList();
+            _quests.Add(playerId,newQuests);
+            
+            return newQuests;
+        }
+
     }
 }
