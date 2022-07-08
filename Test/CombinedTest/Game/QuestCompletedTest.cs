c2f32c897de3a92095d535a77c7c3e7f9ae1ba45
sakastudio <sakastudio100@gmail.com>
2022-07-08 10:26:14 +0900

    add commnet

--- Test/CombinedTest/Game/QuestCompletedTest.cs
+++ Test/CombinedTest/Game/QuestCompletedTest.cs
@@ -2,6 +2,9 @@ using NUnit.Framework;
 
 namespace Test.CombinedTest.Game
 {
+    /// <summary>
+    /// クエストをクリアすることでクエスト一覧が更新されるテスト
+    /// </summary>
     public class QuestCompletedTest
     {
         [Test]
