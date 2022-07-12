d9a5e1a20f13bd95e893cba8f3c4d5e723e03ce5
sakastudio <sakastudio100@gmail.com>
2022-07-12 23:04:39 +0900

    取得用のテストコード修正

--- Test/CombinedTest/Server/PacketTest/QuestProgressRequestProtocolTest.cs
+++ Test/CombinedTest/Server/PacketTest/QuestProgressRequestProtocolTest.cs
@@ -12,6 +12,8 @@ namespace Test.CombinedTest.Server.PacketTest
 {
     public class QuestProgressRequestProtocolTest
     {
+
+        private const int PlayerId = 1;
         /// <summary>
         /// 現在のクエスト進捗状況を取得するテスト
         /// note クエストの内容はTestQuestConfig.jsonと同じです
@@ -19,17 +21,15 @@ namespace Test.CombinedTest.Server.PacketTest
         [Test]
         public void GetTest()
         {
-            //TODO テスト用のセーブデータを用意
-            var json = "{\"world\":[],\"playerInventory\":[],\"entities\":[]}";
+            //テスト用のセーブデータ
+            var json = "{\"world\":[],\"playerInventory\":[],\"entities\":[],\"quests\":{\"1\":[{\"id\":\"Test1\",\"co\":false,\"re\":false},{\"id\":\"Test2\",\"co\":true,\"re\":false},{\"id\":\"Test3\",\"co\":true,\"re\":true},{\"id\":\"Test4\",\"co\":false,\"re\":false}]}}";
 
-            var playerId = 1;
-            
             var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModDirectory.QuestTestModDirectory);
             (serviceProvider.GetService<ILoadRepository>() as LoadJsonFile).Load(json);
 
 
             //クエストのデータ要求クラス
-            var payload = MessagePackSerializer.Serialize(new QuestProgressRequestProtocolMessagePack(playerId)).ToList();
+            var payload = MessagePackSerializer.Serialize(new QuestProgressRequestProtocolMessagePack(PlayerId)).ToList();
             //データの検証
             var questResponse = MessagePackSerializer.Deserialize<QuestProgressResponseProtocolMessagePack>(packet.GetPacketResponse(payload)[0].ToArray()).Quests;
             
@@ -37,13 +37,13 @@ namespace Test.CombinedTest.Server.PacketTest
             Assert.AreEqual(false,questResponse[0].IsCompleted);
             Assert.AreEqual(false,questResponse[0].IsRewarded);
             
-            Assert.AreEqual("Test2",questResponse[0].Id);
-            Assert.AreEqual(true,questResponse[0].IsCompleted);
-            Assert.AreEqual(false,questResponse[0].IsRewarded);
+            Assert.AreEqual("Test2",questResponse[1].Id);
+            Assert.AreEqual(true,questResponse[1].IsCompleted);
+            Assert.AreEqual(false,questResponse[1].IsRewarded);
             
-            Assert.AreEqual("Test3",questResponse[0].Id);
-            Assert.AreEqual(true,questResponse[0].IsCompleted);
-            Assert.AreEqual(true,questResponse[0].IsRewarded);
+            Assert.AreEqual("Test3",questResponse[2].Id);
+            Assert.AreEqual(true,questResponse[2].IsCompleted);
+            Assert.AreEqual(true,questResponse[2].IsRewarded);
         }
     }
 }
