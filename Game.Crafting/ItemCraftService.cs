f91c31f6301195c69e8cad7a6073fe315a3a6aeb
sakastudio <sakastudio100@gmail.com>
2022-07-13 15:15:05 +0900

    AllCraft()の実装

--- Game.Crafting/ItemCraftService.cs
+++ Game.Crafting/ItemCraftService.cs
@@ -9,14 +9,16 @@ namespace Game.Crafting
     public class ItemCraftService : IItemCraftService
     {
         private readonly IIsCreatableJudgementService _isCreatableJudgementService;
+        private readonly IOpenableInventory _mainOpenableInventoryData;
         private readonly IOpenableInventory _grabInventoryData;
         private readonly ICraftingOpenableInventory _craftingOpenableInventory;
 
-        public ItemCraftService(IIsCreatableJudgementService isCreatableJudgementService, IOpenableInventory grabInventoryData, ICraftingOpenableInventory craftingOpenableInventory)
+        public ItemCraftService(IIsCreatableJudgementService isCreatableJudgementService, IOpenableInventory grabInventoryData, ICraftingOpenableInventory craftingOpenableInventory, IOpenableInventory mainOpenableInventoryData)
         {
             _isCreatableJudgementService = isCreatableJudgementService;
             _grabInventoryData = grabInventoryData;
             _craftingOpenableInventory = craftingOpenableInventory;
+            _mainOpenableInventoryData = mainOpenableInventoryData;
         }
 
         public void NormalCraft()
@@ -26,11 +28,11 @@ namespace Game.Crafting
             if (!IsCreatable()) return;
             
             //クラフト結果のアイテムを取得しておく
-            var result = _isCreatableJudgementService.GetResult(_craftingOpenableInventory.Items);
+            var result = _isCreatableJudgementService.GetResult(CraftingItems);
             if (!_grabInventoryData.GetItem(0).IsAllowedToAdd(result)) return;
 
             //クラフトしたアイテムを消費する
-            ConsumptionCraftItem(1, _craftingOpenableInventory.Items);
+            ConsumptionCraftItem(1, CraftingItems);
             
             //元のクラフト結果のアイテムを足したアイテムを持ちインベントリに追加
             var outputSlotItem = _grabInventoryData.GetItem(0);
@@ -38,7 +40,25 @@ namespace Game.Crafting
             _grabInventoryData.SetItem(0, addedOutputSlot);
         }
 
-        private bool IsCreatable() { return _isCreatableJudgementService.IsCreatable(_craftingOpenableInventory.Items); }
+        public void AllCraft()
+        {
+            var craftNum = _isCreatableJudgementService.CalcAllCraftItemNum(CraftingItems,_mainOpenableInventoryData.Items);
+            var result = _isCreatableJudgementService.GetResult(CraftingItems);
+            for (int i = 0; i < craftNum; i++)
+            {
+                _mainOpenableInventoryData.InsertItem(result);
+            }
+            ConsumptionCraftItem(craftNum, CraftingItems);
+        }
+
+        public void OneStackCraft()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private bool IsCreatable() { return _isCreatableJudgementService.IsCreatable(CraftingItems); }
+        private IReadOnlyList<IItemStack> CraftingItems => _craftingOpenableInventory.Items;
+        
         
         
         private void ConsumptionCraftItem(int itemCount,IReadOnlyList<IItemStack> craftingItems)
@@ -49,7 +69,7 @@ namespace Game.Crafting
                 for (int j = 0; j < PlayerInventoryConst.CraftingSlotSize; j++)
                 {
                     //クラフトしたアイテムを消費する
-                    var subItem = _craftingOpenableInventory.Items[j].SubItem(craftConfig.Items[j].Count);
+                    var subItem = CraftingItems[j].SubItem(craftConfig.Items[j].Count);
                     //インベントリにセット
                     _craftingOpenableInventory.SetItem(j, subItem);
                 }
