5847140c0ab66ebd5d065ae995d3e48263ef6178
sakastudio <sakastudio100@gmail.com>
2022-07-13 14:57:48 +0900

    メソッド定義

--- Game.Crafting.Interface/IItemCraftService.cs
+++ Game.Crafting.Interface/IItemCraftService.cs
@@ -2,5 +2,8 @@ namespace Game.Crafting.Interface
 {
     public interface IItemCraftService
     {
+        public void NormalCraft();
+        public void AllCraft();
+        public void OneStackCraft();
     }
 }
