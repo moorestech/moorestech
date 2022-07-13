a4ed01cb3b467573a2ed2a878b55c04abf9c8cf5
sakastudio <sakastudio100@gmail.com>
2022-07-13 15:18:59 +0900

    コメント追加

--- Game.Crafting.Interface/IItemCraftingService.cs
+++ Game.Crafting.Interface/IItemCraftingService.cs
@@ -1,9 +1,21 @@
 namespace Game.Crafting.Interface
 {
-    public interface IItemCraftService
+    /// <summary>
+    /// アイテムクラフトの実行を代行するサービスです。
+    /// </summary>
+    public interface IItemCraftingService
     {
+        /// <summary>
+        /// クラフトスロットを1回クリックしてクラフトする通常の方法
+        /// </summary>
         public void NormalCraft();
+        /// <summary>
+        /// シフト+クリックで作れるだけ作るクラフト
+        /// </summary>
         public void AllCraft();
+        /// <summary>
+        /// コントロール＋クリックで1スタック作るクラフト
+        /// </summary>
         public void OneStackCraft();
     }
 }
