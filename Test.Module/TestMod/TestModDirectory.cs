65334885b4248d488571e5a58690794b8457449d
sakastudio <sakastudio100@gmail.com>
2022-07-14 11:24:01 +0900

    Revert "modの配置とパスの変更"
    This reverts commit 6000050e53442355c8d84e2c971cf3dda9d571bc.

--- Test.Module/TestMod/TestModDirectory.cs
+++ Test.Module/TestMod/TestModDirectory.cs
@@ -18,5 +18,6 @@ namespace Test.Module.TestMod
         public static string ConfigOnlyDirectory => Path.Combine(FolderDirectory, "ConfigOnly");
         public static string ForUnitTestModDirectory => Path.Combine(FolderDirectory, "ForUnitTestMod");
         public static string MachineIoTestModDirectory => Path.Combine(FolderDirectory, "MachineIOTestMod");
+        public static string QuestTestModDirectory => Path.Combine(FolderDirectory, "QuestTestMod");
     }
 }
