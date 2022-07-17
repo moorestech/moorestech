b033d70e6377adab2de7ed9d954424fc4e046ebe
sakastudio <sakastudio100@gmail.com>
2022-07-17 19:17:23 +0900

    報酬受け取プロトコルテストクラス

---
+++ Test/CombinedTest/Server/PacketTest/EarnQuestRewardProtocolTest.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Test.CombinedTest.Server.PacketTest
+{
+    public class EarnQuestRewardProtocolTest
+    {
+        [Test]
+        public void NormalEarnTest()
+        {
+            
+        }
+    }
+}
