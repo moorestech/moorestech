40f38d77e538a17f77f8d43cfe33857c7d23959e
sakastudio <sakastudio100@gmail.com>
2022-07-18 23:11:51 +0900

    クエストUIのリネーム

---
+++ Assets/Scripts/MainGame/UnityView/UI/Quest/QuestUI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Game.Quest.Config;
+using Game.Quest.Interface;
+using UnityEngine;
+using VContainer;
+
+namespace MainGame.UnityView.UI.Quest
+{
+    public class QuestUI : MonoBehaviour
+    {
+        [SerializeField] private QuestTab m_QuestTabPrefab;
+        [SerializeField] private QuestCategoryButton m_QuestCategory;
+
+
+        [Inject]
+        public void Construct(IQuestConfig questConfig)
+        {
+        }
+    }
+}
