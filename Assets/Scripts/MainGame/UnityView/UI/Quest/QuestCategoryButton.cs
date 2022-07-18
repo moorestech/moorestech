f0a1b4e268ae127c272d495970660d832104242c
sakastudio <sakastudio100@gmail.com>
2022-07-18 23:34:55 +0900

    テキストのカテゴリ設定

--- Assets/Scripts/MainGame/UnityView/UI/Quest/QuestCategoryButton.cs
+++ Assets/Scripts/MainGame/UnityView/UI/Quest/QuestCategoryButton.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 namespace MainGame.UnityView.UI.Quest
 {
     public class QuestCategoryButton : MonoBehaviour
     {
+        [SerializeField] private TMP_Text CategoryName;
         public void SetCategory(string category)
         {
-            
+            CategoryName.text = category;
         }
     }
 }
