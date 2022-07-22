942141a367f39d7f49c1ba3ec515b19d8feec762
sakastudio <sakastudio100@gmail.com>
2022-07-23 00:28:04 +0900

    QuestViewerStateを作成

---
+++ Assets/Scripts/MainGame/UnityView/UI/UIState/QuestViewerState.cs
@@ -0,0 +1,45 @@
+using MainGame.UnityView.UI.UIState.UIObject;
+
+namespace MainGame.UnityView.UI.UIState
+{
+    public class QuestViewerState : IUIState
+    {
+        private readonly MoorestechInputSettings _input;
+        private readonly QuestViewerObject _questViewerObject;
+
+        public QuestViewerState(MoorestechInputSettings input, QuestViewerObject questViewerObject)
+        {
+            _input = input;
+            _questViewerObject = questViewerObject;
+        }
+
+        public bool IsNext()
+        {
+            return _input.UI.CloseUI.triggered || _input.UI.OpenInventory.triggered;
+        }
+
+        public UIStateEnum GetNext()
+        {
+            if (_input.UI.CloseUI.triggered)
+            {
+                return UIStateEnum.GameScreen;
+            }
+            if (_input.UI.OpenInventory.triggered)
+            {
+                return UIStateEnum.PlayerInventory;
+            }
+
+            return UIStateEnum.QuestViewer;
+        }
+
+        public void OnEnter(UIStateEnum lastStateEnum)
+        {
+            _questViewerObject.gameObject.SetActive(true);
+        }
+
+        public void OnExit()
+        {
+            _questViewerObject.gameObject.SetActive(false);
+        }
+    }
+}
