5e7618a4077311a6392fcfd929e569e1f6586bc0
sakastudio <sakastudio100@gmail.com>
2022-07-29 13:06:50 +0900

    OnePreRequestQuestTestの作成

---
+++ Test/UnitTest/Game/QuestTest.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection;
+using Game.PlayerInventory.Interface.Event;
+using Game.Quest.Interface;
+using Game.Quest.QuestEntity;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using PlayerInventory.Event;
+using Server.Boot;
+using Test.Module.TestMod;
+
+namespace Test.UnitTest.Game
+{
+    public class QuestTest
+    {
+        private const int PlayerId = 1;
+        /// <summary>
+        /// 前提クエストがあるとき、前のクエストが終了してから次のクエストが開始されることを確認する。
+        /// </summary>
+        [Test]
+        public void OnePreRequestQuestTest()
+        {
+            var (_, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModDirectory.ForUnitTestModDirectory);
+
+            var questDataStore = serviceProvider.GetService<IQuestDataStore>();
+            var craftEvent = serviceProvider.GetService<ICraftingEvent>();
+            
+            var test1Quest = (ItemCraftQuest)questDataStore.GetQuestData(PlayerId, "Test1");
+            var test2Quest = (ItemCraftQuest)questDataStore.GetQuestData(PlayerId, "Test2");
+
+            
+            //全てのクエストがまだ合格していないことをテスト
+            Assert.False(test1Quest.IsCompleted);
+            Assert.False(test2Quest.IsCompleted);
+            
+            
+            //前提クエストが１つしかないクエストが正しくクリア、クリアにならないことをテストする
+            
+            //クエスト2のアイテムクラフトイベントをInvoke
+            InvokeCraftEventWithReflection(craftEvent,GetQuestIdWithReflection(test2Quest));
+           
+            //この段階ではまだクエスト2がクリアしていないことをテスト
+            Assert.False(test2Quest.IsCompleted);
+            
+            //前提クエストのクエスト1のクリア
+            InvokeCraftEventWithReflection(craftEvent,GetQuestIdWithReflection(test1Quest));
+            //クエスト1がクリアしたことをテスト
+            Assert.True(test1Quest.IsCompleted);
+            //クエスト2のアイテムをクラフトしたのでクエスト2がクリアされることをテスト
+            Assert.True(test2Quest.IsCompleted);
+        }
+
+        [Test]
+        public void AndPreRequestQuest()
+        {
+            
+        }
+        
+        /// <summary>
+        /// アイテムクラフトのイベントを無理やり発火する
+        /// </summary>
+        private void InvokeCraftEventWithReflection(ICraftingEvent craftingEvent,int itemId)
+        {
+            //リフレクションでメソッドを取得、実行
+            var method = typeof(CraftingEvent).GetMethod("InvokeEvent", BindingFlags.NonPublic | BindingFlags.Instance);
+            //クラフトイベントを発火することで擬似的にクラフトを再現する
+            method.Invoke(craftingEvent,new object?[]{itemId,1});
+        }
+        private int GetQuestIdWithReflection(ItemCraftQuest itemCraftQuest)
+        {
+            return (int)itemCraftQuest.GetType().GetField("_questItemId", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(itemCraftQuest); 
+        }
+    }
+}
