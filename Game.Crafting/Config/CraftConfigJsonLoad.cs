3794dccc3e49e4ee02f05cbe46712feb525a64b9
sakastudio <sakastudio100@gmail.com>
2022-08-01 19:55:38 +0900

    クラフトレシピのstring化

--- Game.Crafting/Config/CraftConfigJsonLoad.cs
+++ Game.Crafting/Config/CraftConfigJsonLoad.cs
@@ -6,15 +6,36 @@ using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Text;
 using Core.ConfigJson;
+using Core.Item;
+using Game.Crafting.Interface;
 using Newtonsoft.Json;
 
 namespace Game.Crafting.Config
 {
     public class CraftConfigJsonLoad
     {
-        public List<CraftConfigDataElement> Load(List<string> jsons)
+        private readonly ItemStackFactory _itemStackFactory;
+
+        public CraftConfigJsonLoad(ItemStackFactory itemStackFactory)
         {
-            return jsons.SelectMany(JsonConvert.DeserializeObject<CraftConfigDataElement[]>).ToList();
+            _itemStackFactory = itemStackFactory;
+        }
+
+        public List<CraftingConfigData> Load(List<string> jsons)
+        {
+            var loadedData = jsons.SelectMany(JsonConvert.DeserializeObject<CraftConfigDataElement[]>).ToList();
+            
+            var result = new List<CraftingConfigData>();
+            
+            foreach (var config in loadedData)
+            {
+                var items = config.Items.Select(item => _itemStackFactory.Create(item.ModId,item.ItemName, item.Count)).ToList();
+                var resultItem = _itemStackFactory.Create(config.Result.ModId,config.Result.ItemName, config.Result.Count);
+                
+                result.Add(new CraftingConfigData(items,resultItem));
+            }
+            
+            return result;
         }
     }
 }
