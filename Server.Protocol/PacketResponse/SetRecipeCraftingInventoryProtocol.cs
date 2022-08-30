b2821e4b8de3f071b42437db98571a12e54cc348
sakastudio <sakastudio100@gmail.com>
2022-08-30 21:46:18 +0900

    SetRecipeCraftingInventoryProtocolMessagePackの作成
    (cherry picked from commit b9d51835025a926d60cf69fe77d2322c1ce26217)

---
+++ Server.Protocol/PacketResponse/SetRecipeCraftingInventoryProtocol.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using MessagePack;
+using Server.Util.MessagePack;
+
+namespace Server.Protocol.PacketResponse
+{
+    public class SetRecipeCraftingInventoryProtocol: IPacketResponse
+    {
+        public const string Tag = "va:setRecipeCraftingInventory";
+        public List<List<byte>> GetResponse(List<byte> payload)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+    
+    [MessagePackObject(keyAsPropertyName :true)]
+    public class SetRecipeCraftingInventoryProtocolMessagePack : ProtocolMessagePackBase
+    {
+        public SetRecipeCraftingInventoryProtocolMessagePack(ItemMessagePack[] recipe)
+        {
+            Tag = SetRecipeCraftingInventoryProtocol.Tag;
+            Recipe = recipe;
+        }
+        
+        [Obsolete("デシリアライズ用のコンストラクタです。基本的に使用しないでください。")]
+        public SetRecipeCraftingInventoryProtocolMessagePack() { }
+
+        public ItemMessagePack[] Recipe { get; set; }
+
+    }
+}
