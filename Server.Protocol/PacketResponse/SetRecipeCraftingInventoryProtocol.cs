05b4cc43b0dda004a59e462e4c62ee215cb30d29
sakastudio <sakastudio100@gmail.com>
2022-08-31 16:30:54 +0900

    wip SetRecipeCraftingInventoryProtocol

--- Server.Protocol/PacketResponse/SetRecipeCraftingInventoryProtocol.cs
+++ Server.Protocol/PacketResponse/SetRecipeCraftingInventoryProtocol.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Game.PlayerInventory.Interface;
 using MessagePack;
 using Microsoft.Extensions.DependencyInjection;
+using Server.Protocol.PacketResponse.Util;
 using Server.Util.MessagePack;
 
 namespace Server.Protocol.PacketResponse
@@ -23,6 +24,48 @@ namespace Server.Protocol.PacketResponse
             var mainInventory = _playerInventoryDataStore.GetInventoryData(data.PlayerId).MainOpenableInventory;
             var craftingInventory = _playerInventoryDataStore.GetInventoryData(data.PlayerId).CraftingOpenableInventory;
             var grabInventory = _playerInventoryDataStore.GetInventoryData(data.PlayerId).GrabInventory;
+            
+            
+            //クラフトインベントリ、グラブインベントリのアイテムを全てメインインベントリに移動
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                var itemCount = craftingInventory.GetItem(i).Count;
+                InventoryItemInsertService.Insert(craftingInventory,i,mainInventory,itemCount);
+            }
+            var grabItemCount = grabInventory.GetItem(0).Count;
+            InventoryItemInsertService.Insert(grabInventory,0,mainInventory,grabItemCount);
+            
+            
+            //必要なアイテムがMainインベントリにあるかチェック
+            var requiredItemCount = new Dictionary<int, int>();
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                var itemId = craftingInventory.GetItem(i).Id;
+                if (requiredItemCount.ContainsKey(itemId))
+                {
+                    requiredItemCount[itemId] += craftingInventory.GetItem(i).Count;
+                }
+                else
+                {
+                    requiredItemCount.Add(itemId, craftingInventory.GetItem(i).Count);
+                }
+            }
+            var mainInventoryItemCount = new Dictionary<int, int>();
+            for (int i = 0; i < PlayerInventoryConst.MainInventorySize; i++)
+            {
+                var itemId = mainInventory.GetItem(i).Id;
+                if (mainInventoryItemCount.ContainsKey(itemId))
+                {
+                    mainInventoryItemCount[itemId] += mainInventory.GetItem(i).Count;
+                }
+                else
+                {
+                    mainInventoryItemCount.Add(itemId, mainInventory.GetItem(i).Count);
+                }
+            }
+            
+            
+            
 
 
 
