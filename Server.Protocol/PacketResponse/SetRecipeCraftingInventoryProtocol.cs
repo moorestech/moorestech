f425cd96bf64c3d9b3cc5d595f5b608e5fedb332
sakastudio <sakastudio100@gmail.com>
2022-09-02 09:28:54 +0900

    実際に移動するロジック

--- Server.Protocol/PacketResponse/SetRecipeCraftingInventoryProtocol.cs
+++ Server.Protocol/PacketResponse/SetRecipeCraftingInventoryProtocol.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using Core.Const;
+using Core.Inventory;
+using Core.Item;
+using Core.Item.Config;
 using Game.PlayerInventory.Interface;
 using MessagePack;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,10 +16,14 @@ namespace Server.Protocol.PacketResponse
     {
         public const string Tag = "va:setRecipeCraftingInventory";
         private readonly IPlayerInventoryDataStore _playerInventoryDataStore;
+        private readonly ItemStackFactory _itemStackFactory;
+        private readonly IItemConfig _itemConfig;
         
         public SetRecipeCraftingInventoryProtocol(ServiceProvider serviceProvider)
         {
             _playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+            _itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
+            _itemConfig = serviceProvider.GetService<IItemConfig>();
         }
         public List<List<byte>> GetResponse(List<byte> payload)
         {
@@ -26,50 +34,161 @@ namespace Server.Protocol.PacketResponse
             var grabInventory = _playerInventoryDataStore.GetInventoryData(data.PlayerId).GrabInventory;
             
             
+            //アイテムの移動
+            MoveToMainInventory(mainInventory,craftingInventory,grabInventory);
+            
+            //移動できるかチェック
+            var (isReplaceable,mainInventoryRequiredItemCount) = IsReplaceable(mainInventory,data.Recipe);
+            if (!isReplaceable)
+            {
+                return new List<List<byte>>();
+            }
+            
+            //実際に移動するアイテム数を計算
+            var moveItem = CalcCraftInventoryPlaceItem(_itemStackFactory,_itemConfig,data.Recipe,mainInventoryRequiredItemCount);
+            
+            //実際に移動する
+            MoveRecipeToCraftInventory(_itemStackFactory, mainInventory, craftingInventory, moveItem);
+            
+            return new List<List<byte>>();
+        }
+
+        //--------- アイテム設置のロジック用メソッド　各メソッドがプロパティにアクセスしないことを明示するためにstaticにしておく ----------
+        
+        
+        private static void MoveToMainInventory(IOpenableInventory main, IOpenableInventory craft, IOpenableInventory grab)
+        {
             //クラフトインベントリ、グラブインベントリのアイテムを全てメインインベントリに移動
             for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
             {
-                var itemCount = craftingInventory.GetItem(i).Count;
-                InventoryItemInsertService.Insert(craftingInventory,i,mainInventory,itemCount);
+                var itemCount = craft.GetItem(i).Count;
+                InventoryItemInsertService.Insert(craft,i,main,itemCount);
             }
-            var grabItemCount = grabInventory.GetItem(0).Count;
-            InventoryItemInsertService.Insert(grabInventory,0,mainInventory,grabItemCount);
-            
-            
-            //必要なアイテムがMainインベントリにあるかチェック
+            var grabItemCount = grab.GetItem(0).Count;
+            InventoryItemInsertService.Insert(grab,0,main,grabItemCount);
+        }
+
+        private static (bool isReplacable,Dictionary<int,int> mainInventoryRequiredItemCount) IsReplaceable(IOpenableInventory mainInventory,ItemMessagePack[] recipeItem)
+        {
+            //必要なアイテムがMainインベントリにあるかチェックするための必要アイテム数辞書を作成
             var requiredItemCount = new Dictionary<int, int>();
-            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            requiredItemCount[0] = 1;
+            foreach (var item in recipeItem)
             {
-                var itemId = craftingInventory.GetItem(i).Id;
-                if (requiredItemCount.ContainsKey(itemId))
-                {
-                    requiredItemCount[itemId] += craftingInventory.GetItem(i).Count;
-                }
-                else
-                {
-                    requiredItemCount.Add(itemId, craftingInventory.GetItem(i).Count);
-                }
+                requiredItemCount[item.Id] += item.Count;
             }
-            var mainInventoryItemCount = new Dictionary<int, int>();
+            //必要なアイテム数があるかチェックするためにMainインベントリを走査
+            var mainInventoryRequiredItemCount = new Dictionary<int, int>();
             for (int i = 0; i < PlayerInventoryConst.MainInventorySize; i++)
             {
                 var itemId = mainInventory.GetItem(i).Id;
-                if (mainInventoryItemCount.ContainsKey(itemId))
+                if (!requiredItemCount.ContainsKey(itemId)) continue;
+
+                mainInventoryRequiredItemCount[itemId] += mainInventory.GetItem(i).Count;
+            }
+            
+            //アイテム数が足りているかチェックする
+            foreach (var item in requiredItemCount)
+            {
+                if (!mainInventoryRequiredItemCount.ContainsKey(item.Key)) return (false,null);
+                
+                if (mainInventoryRequiredItemCount[item.Key] < item.Value)
                 {
-                    mainInventoryItemCount[itemId] += mainInventory.GetItem(i).Count;
+                    return (false,null);
                 }
-                else
-                {
-                    mainInventoryItemCount.Add(itemId, mainInventory.GetItem(i).Count);
-                }
+            }
+            return (true,mainInventoryRequiredItemCount);
+        }
+
+        private static IItemStack[] CalcCraftInventoryPlaceItem(ItemStackFactory itemStackFactory,IItemConfig itemConfig,ItemMessagePack[] recipe,Dictionary<int,int> mainInventoryRequiredItemCount)
+        {
+            //そのアイテムIDが必要なスロットがいくつあるか求める
+            var requiredItemSlotCount = new Dictionary<int, int>();
+            foreach (var item in recipe)
+            {
+                requiredItemSlotCount[item.Id] += item.Count;
             }
             
             
+            //そのスロットに入るアイテム数を計算する
+            var craftInventoryPlaceItem = new IItemStack[PlayerInventoryConst.CraftingSlotSize];
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                var id = recipe[i].Id;
+                if (id == ItemConst.EmptyItemId)  continue;
+                
+                //一旦あまりを考慮しないアイテム数を計算する
+                //メインインベントリに入っているアイテム数 / 必要とするスロット数 = スロットに入るアイテム数　となる
+                var count = mainInventoryRequiredItemCount[id] / requiredItemSlotCount[id];
+                count = Math.Clamp(count,0, itemConfig.GetItemConfig(id).MaxStack);
+                
+                craftInventoryPlaceItem[i] = itemStackFactory.Create(id,count);
+            }
             
+            
+            //あまり分を足す
+            //アイテムIDのループを回し、一番最初にそのアイテムIDが入っているスロットを探す
+            //そのスロットにあまりを入れる
+            foreach (var id in requiredItemSlotCount.Keys)
+            {
+                for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+                {
+                    if (id != craftInventoryPlaceItem[i].Id) continue;
+                    
+                    var remainder = mainInventoryRequiredItemCount[id] % requiredItemSlotCount[id];
+                    var count = craftInventoryPlaceItem[i].Count + remainder;
+                    count = Math.Clamp(count,0, itemConfig.GetItemConfig(id).MaxStack);
+                    
+                    craftInventoryPlaceItem[i] = itemStackFactory.Create(id,count);
+                    break;
+                }
+            }
 
+            return craftInventoryPlaceItem;
+        }
 
-
-            return new List<List<byte>>();
+        private static void MoveRecipeToCraftInventory(ItemStackFactory itemStackFactory,IOpenableInventory main,IOpenableInventory craft,IItemStack[] moveItem)
+        {
+            for (int i = 0; i < PlayerInventoryConst.CraftingSlotSize; i++)
+            {
+                //メインインベントリから欲しいアイテムを収集してくる
+                if (moveItem[i].Id == ItemConst.EmptyItemId)  continue; 
+                
+                var collectedItem = itemStackFactory.CreatEmpty();
+                for (int j = 0; j < PlayerInventoryConst.MainInventorySize; j++)
+                {
+                    if (main.GetItem(j).Id != moveItem[i].Id) continue;
+                    //入ってるアイテムが目的のアイテムなので収集する
+                    var addedItemResult = collectedItem.AddItem(main.GetItem(i));
+                    
+                    //足した結果アイテムが足りなかったらそのまま続ける
+                    if (addedItemResult.ProcessResultItemStack.Count < moveItem[i].Count)
+                    {
+                        collectedItem = addedItemResult.ProcessResultItemStack;
+                        main.SetItem(j,addedItemResult.RemainderItemStack);
+                        continue;
+                    }
+                    //ピッタリだったらそのまま終了
+                    if (addedItemResult.ProcessResultItemStack.Count == moveItem[i].Count)
+                    {
+                        main.SetItem(j,addedItemResult.RemainderItemStack);
+                        break;
+                    }
+                    
+                    //多かったら余りをメインインベントリに戻す
+                    //本来入れるべきアイテムと、実際に入った数の差を計算
+                    var reminderCount = addedItemResult.ProcessResultItemStack.Count - moveItem[i].Count;
+                    //メインインベントリに入れる分のアイテムす　差分とあまりの数を足す
+                    var mainItemCount = reminderCount + addedItemResult.RemainderItemStack.Count;
+                    
+                    var item = itemStackFactory.Create(moveItem[i].Id, mainItemCount);
+                    main.SetItem(j,item);
+                }
+                
+                
+                //クラフトインベントリに入れる
+                craft.SetItem(i,moveItem[i]);
+            }
         }
     }
     
