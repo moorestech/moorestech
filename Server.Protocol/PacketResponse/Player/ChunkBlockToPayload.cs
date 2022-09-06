adbcd33bd2e47ee675a2fc22fd0100a285f9d803
sakastudio <sakastudio100@gmail.com>
2022-09-06 17:18:22 +0900

    謎の処理削除

--- Server.Protocol/PacketResponse/Player/ChunkBlockToPayload.cs
+++ Server.Protocol/PacketResponse/Player/ChunkBlockToPayload.cs
@@ -12,12 +12,6 @@ namespace Server.Protocol.PacketResponse.Player
         public static List<byte> Convert(Coordinate chunkCoordinate,IWorldBlockDatastore worldBlockDatastore, WorldMapTile worldMapTile)
         {
             
-            var payload = new List<bool>();
-
-            payload.AddRange(ToBitList.Convert(ToByteList.Convert((short) 1)));
-            payload.AddRange(ToBitList.Convert(ToByteList.Convert(chunkCoordinate.X)));
-            payload.AddRange(ToBitList.Convert(ToByteList.Convert(chunkCoordinate.Y)));
-            
             //ブロックのIDの取得
             var blocksIds = CoordinateToChunkBlockIntArray.GetBlockIdsInChunk(chunkCoordinate, worldBlockDatastore);
             var blockDirections = CoordinateToChunkBlockIntArray.GetBlockDirectionInChunk(chunkCoordinate, worldBlockDatastore);
