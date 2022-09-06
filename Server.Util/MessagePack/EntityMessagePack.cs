f2b094fc5614a096ec023ae1ff7b895c662be6c8
sakastudio <sakastudio100@gmail.com>
2022-09-06 20:51:37 +0900

    util系のmessagePackの追加

---
+++ Server.Util/MessagePack/EntityMessagePack.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Game.Entity.Interface;
+using MessagePack;
+
+namespace Server.Util.MessagePack
+{
+    [MessagePackObject(false)]
+    public class EntityMessagePack
+    {
+        [Obsolete("デシリアライズ用のコンストラクタです。基本的に使用しないでください。")]
+        public EntityMessagePack() { }
+
+        public EntityMessagePack(IEntity entity)
+        {
+            InstanceId = entity.InstanceId;
+            Type = entity.EntityType;
+            Position = new Vector3MessagePack(entity.Position);
+        }
+        public long InstanceId { get; set; }
+        public string Type { get; set; }
+        public Vector3MessagePack Position { get; set; }
+    }
+}
