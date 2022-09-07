3404f26843f9340c297c473862b2d9b1a8c1d68c
sakastudio <sakastudio100@gmail.com>
2022-09-07 14:42:23 +0900

    クラスを移動

---
+++ Assets/Scripts/MainGame/Basic/EntityProperties.cs
@@ -0,0 +1,23 @@
+using System.Numerics;
+using Server.Util.MessagePack;
+
+namespace MainGame.Basic
+{
+    public class EntityProperties
+    {
+        public readonly long InstanceId;
+        public readonly string Type;
+        public readonly Vector3 Position;
+        
+        public EntityProperties(EntityMessagePack entityMessagePack)
+        {
+            InstanceId = entityMessagePack.InstanceId;
+            Type = entityMessagePack.Type;
+            var x = entityMessagePack.Position.X;
+            var y = entityMessagePack.Position.Y;
+            var z = entityMessagePack.Position.Z;
+            Position = new Vector3(x,y,z);
+        }
+
+    }
+}
--- Assets/Scripts/MainGame/Network/Event/ReceiveEntitiesDataEvent.cs
+++ Assets/Scripts/MainGame/Network/Event/ReceiveEntitiesDataEvent.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Game.Entity.Interface;
+using MainGame.Basic;
 using Server.Protocol.PacketResponse.MessagePack;
 using Server.Util.MessagePack;
 using UnityEngine;
@@ -26,21 +27,4 @@ namespace MainGame.Network.Event
         }
     }
 
-    public class EntityProperties
-    {
-        public readonly long InstanceId;
-        public readonly string Type;
-        public readonly Vector3 Position;
-        
-        public EntityProperties(EntityMessagePack entityMessagePack)
-        {
-            InstanceId = entityMessagePack.InstanceId;
-            Type = entityMessagePack.Type;
-            var x = entityMessagePack.Position.X;
-            var y = entityMessagePack.Position.Y;
-            var z = entityMessagePack.Position.Z;
-            Position = new Vector3(x,y,z);
-        }
-
-    }
 }
