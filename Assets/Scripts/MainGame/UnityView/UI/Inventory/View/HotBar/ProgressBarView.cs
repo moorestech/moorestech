4e1353e8c38f7095d0fff16833af630b85130c22
sakastudio <sakastudio100@gmail.com>
2022-11-26 15:11:14 +0900

    プログレスバーの追加

---
+++ Assets/Scripts/MainGame/UnityView/UI/Inventory/View/HotBar/ProgressBarView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainGame.UnityView.UI.Inventory.View.HotBar
+{
+    public class ProgressBarView : MonoBehaviour
+    {
+        private const float FadeTime = 0.1f;
+
+        [SerializeField] private Slider slider;
+        [SerializeField] private CanvasGroup canvasGroup;
+
+        private float _lastSetTime;
+        
+        public void SetProgress(float progress)
+        {
+            _lastSetTime = Time.time;
+            slider.value = progress;
+        }
+
+        private void Update()
+        {
+            canvasGroup.alpha = _lastSetTime + FadeTime < Time.time ? 0 : 1;
+        }
+    }
+}
