24f402928fdb49239a514f5b3f98a64824382b12
sakastudio <sakastudio100@gmail.com>
2022-12-23 09:57:38 +0900

    カーソルコントローラーを削除、見下ろしカメラだけに修正

--- Assets/Scripts/MainGame/UnityView/Control/MouseKeyboard/CameraController.cs
+++ Assets/Scripts/MainGame/UnityView/Control/MouseKeyboard/CameraController.cs
@@ -6,46 +6,19 @@ namespace MainGame.UnityView.Control.MouseKeyboard
 {
     public class CameraController : MonoBehaviour
     {
-        private const float XSpeed = 10;
-        private const float YSpeed = 0.05f;
-        [SerializeField] private CinemachineFreeLook cinemachineFreeLook;
+        [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        private CinemachineFramingTransposer cinemachineFraming;
 
-        public bool IsCameraControlling { get; private set; }
-        private float currentDistance = 10f;
-
-        private void Start()
+        private void Awake()
         {
-            cinemachineFreeLook.m_YAxis.Value = 0.3f;
+            cinemachineFraming = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
         }
 
         private void Update()
         {
-            if (InputManager.Playable.ScreenRightClick.GetKey)
-            {
-                IsCameraControlling = true;
-                cinemachineFreeLook.m_XAxis.m_MaxSpeed = XSpeed;
-                cinemachineFreeLook.m_YAxis.m_MaxSpeed = YSpeed;
-            }
-            else
-            {
-                IsCameraControlling = false;
-                cinemachineFreeLook.m_XAxis.m_MaxSpeed = 0;
-                cinemachineFreeLook.m_YAxis.m_MaxSpeed = 0;
-            }
-            
-            currentDistance += InputManager.UI.SwitchHotBar.ReadValue<float>() / -100;
-            currentDistance = Mathf.Clamp(currentDistance, 6, 75);
-            
-            //Topの設定
-            cinemachineFreeLook.m_Orbits[0].m_Height = currentDistance;
+            cinemachineFraming.m_CameraDistance += InputManager.UI.SwitchHotBar.ReadValue<float>() / -100;
 
-            //Middleの設定
-            cinemachineFreeLook.m_Orbits[1].m_Height = currentDistance/2 + (currentDistance - currentDistance/2) / 2;
-            cinemachineFreeLook.m_Orbits[1].m_Radius = currentDistance/2 + (currentDistance - currentDistance/2) / 2;
-            
-            //Bottomの設定
-            cinemachineFreeLook.m_Orbits[2].m_Height = currentDistance/2;
-            cinemachineFreeLook.m_Orbits[2].m_Radius = currentDistance/2;
+            cinemachineFraming.m_CameraDistance = Mathf.Clamp(cinemachineFraming.m_CameraDistance, 3, 75);
         }
     }
 }
