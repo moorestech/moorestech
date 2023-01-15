eca1a6c4313f6719acbc25f70a514832108e9de9
sakastudio <sakastudio100@gmail.com>
2023-01-15 18:02:45 +0900

    500x500から鉱石位置をチェックし、スポーン地点を探すロジック

--- Game.World/DataStore/WorldSettings/WorldSpawnPointSearcher.cs
+++ Game.World/DataStore/WorldSettings/WorldSpawnPointSearcher.cs
@@ -1,7 +1,36 @@
+using Game.World.Interface.DataStore;
+using Game.WorldMap;
+
 namespace World.DataStore.WorldSettings
 {
-    public class WorldSpawnPointSearcher
+    public static class WorldSpawnPointSearcher
     {
+        /// <summary>
+        /// 鉄の鉱石ID
+        /// TODO このマジックナンバーを解消する
+        /// </summary>
+        private const int IronOreId = 1;
         
+        /// <summary>
+        /// ワールドの開始地点を探索する
+        /// TODO 必要になったらインジェクションするようにする
+        /// </summary>
+        public static Coordinate SearchSpawnPoint(VeinGenerator veinGenerator)
+        {
+            //現状は0,0から500,500まで、鉄鉱石が生成されている場所を探索し、そこをスポーン地点とする
+            for (int i = 0; i < 500; i++)
+            {
+                for (int j = 0; j < 500; j++)
+                {
+                    var oreId = veinGenerator.GetOreId(i, j);
+                    if (oreId != IronOreId) continue;
+                    
+                    return new Coordinate(i, j);
+                }
+            }
+
+            //なかったので0,0を返す
+            return new Coordinate(0,0);
+        }
     }
 }
