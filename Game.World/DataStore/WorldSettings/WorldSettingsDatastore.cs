81a1771dc9d6b8f1c7fea71a69f867dd09401273
sakastudio <sakastudio100@gmail.com>
2023-01-15 18:24:17 +0900

    生成した座標の適応

--- Game.World/DataStore/WorldSettings/WorldSettingsDatastore.cs
+++ Game.World/DataStore/WorldSettings/WorldSettingsDatastore.cs
@@ -1,11 +1,13 @@
 using Game.World.Interface.DataStore;
+using Game.WorldMap;
 
 namespace World.DataStore.WorldSettings
 {
     public class WorldSettingsDatastore : IWorldSettingsDatastore
     {
-        public WorldSettingsDatastore()
+        public WorldSettingsDatastore(VeinGenerator veinGenerator)
         {
+            WorldSpawnPoint = WorldSpawnPointSearcher.SearchSpawnPoint(veinGenerator);
         }
 
         public Coordinate WorldSpawnPoint { get; }
