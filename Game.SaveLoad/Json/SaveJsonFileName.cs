ae0a0803ef0d5e0886db610523b2bf7fd7ef01d4
sakastudio <sakastudio100@gmail.com>
2023-01-15 17:15:02 +0900

    path変数のリネーム

--- Game.SaveLoad/Json/SaveJsonFileName.cs
+++ Game.SaveLoad/Json/SaveJsonFileName.cs
@@ -9,12 +9,11 @@ namespace Game.Save.Json
     /// </summary>
     public class SaveJsonFileName
     {
-        private readonly string _filePath;
-        public string FullSaveFilePath => _filePath;
+        public string FullSaveFilePath { get; }
 
         public SaveJsonFileName(string fileName)
         {
-            _filePath = SystemPath.GetSaveFilePath(fileName);
+            FullSaveFilePath = SystemPath.GetSaveFilePath(fileName);
         }
     }
 }
