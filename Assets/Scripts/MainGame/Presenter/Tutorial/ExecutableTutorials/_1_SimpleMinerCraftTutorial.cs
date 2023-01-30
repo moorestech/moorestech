d9956cf361bbfc56b345dc9e1d8bc79a41bbeb8b
sakastudio <sakastudio100@gmail.com>
2023-01-30 14:56:51 +0900

    チュートリアルでできるように

--- Assets/Scripts/MainGame/Presenter/Tutorial/ExecutableTutorials/_1_SimpleMinerCraftTutorial.cs
+++ Assets/Scripts/MainGame/Presenter/Tutorial/ExecutableTutorials/_1_SimpleMinerCraftTutorial.cs
@@ -7,15 +7,18 @@ namespace MainGame.Presenter.Tutorial.ExecutableTutorials
     {
         public bool IsFinishTutorial { get; private set; }
         private readonly HighlightRecipeViewerItem _highlightRecipeViewerItem;
-        
-        public _1_SimpleMinerCraftTutorial(HighlightRecipeViewerItem highlightRecipeViewerItem)
+        private readonly GameUIHighlight _gameUIHighlight;
+
+        public _1_SimpleMinerCraftTutorial(HighlightRecipeViewerItem highlightRecipeViewerItem,GameUIHighlight gameUIHighlight)
         {
             _highlightRecipeViewerItem = highlightRecipeViewerItem;
+            _gameUIHighlight = gameUIHighlight;
         }
         
         
         public void StartTutorial()
         {
+            _gameUIHighlight.SetHighlight(HighlightType.CraftItemPutButton,true);
             //TODO ここがmodのロード前に呼び出されるとバグるので修正する そもそもここが動く時データがロードされていないのが問題であるので、設計を変更する必要がある
             _highlightRecipeViewerItem.SetHighLight(BaseMod.ModId,"iron ingot",true);
         }
