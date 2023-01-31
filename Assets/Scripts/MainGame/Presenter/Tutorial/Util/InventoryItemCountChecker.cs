140dbc3e4af7f046c5ba8c4c00b2c885a4f80854
sakastudio <sakastudio100@gmail.com>
2023-01-31 11:07:07 +0900

    チュートリアルのutil

---
+++ Assets/Scripts/MainGame/Presenter/Tutorial/Util/InventoryItemCountChecker.cs
@@ -0,0 +1,39 @@
+using Core.Item.Config;
+using MainGame.UnityView.UI.Inventory.Control;
+
+namespace MainGame.Presenter.Tutorial.Util
+{
+    /// <summary>
+    /// チュートリアルで使う「このアイテムがインベントリ内にn個あるか」みたいなのをチェックする
+    /// </summary>
+    public class InventoryItemCountChecker
+    {
+        private readonly PlayerInventoryViewModel _playerInventoryViewModel;
+        private readonly IItemConfig _itemConfig;
+
+        public InventoryItemCountChecker(PlayerInventoryViewModel playerInventoryViewModel,IItemConfig itemConfig)
+        {
+            _playerInventoryViewModel = playerInventoryViewModel;
+            _itemConfig = itemConfig;
+        }
+
+        /// <summary>
+        /// そのアイテムがインベントリ内に指定個数あるか
+        /// </summary>
+        /// <returns></returns>
+        public bool ExistItemMainInventory(string modId,string itemName,int itemCount)
+        {
+            var inventoryItemCount = 0;
+            var itemId = _itemConfig.GetItemId(modId, itemName);
+            //この関数はUpdateで呼ばれる可能性が高いのでLINQは使わない
+            foreach (var item in _playerInventoryViewModel.MainInventory)
+            {
+                if (item.Id == itemId)
+                {
+                    inventoryItemCount += item.Count;
+                }
+            }
+            return itemCount <= inventoryItemCount;
+        }
+    }
+}
