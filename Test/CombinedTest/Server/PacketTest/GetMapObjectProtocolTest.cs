50239bcc48528c42facc1c4c7799dc5d42b181e5
sakastudio <sakastudio100@gmail.com>
2023-05-06 22:49:30 +0900

    テストコードのベースを作成

---
+++ Test/CombinedTest/Server/PacketTest/GetMapObjectProtocolTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Test.CombinedTest.Server.PacketTest
+{
+    public class GetMapObjectProtocolTest
+    {
+        [Test]
+        public void GetTest()
+        {
+            
+            Assert.Fail();
+        }
+    }
+}
