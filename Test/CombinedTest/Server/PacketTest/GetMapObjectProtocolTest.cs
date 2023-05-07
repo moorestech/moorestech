b267373214b4d262b6c572df56ea94659b9d07b0
sakastudio <sakastudio100@gmail.com>
2023-05-07 17:40:52 +0900

    GetMapObjectProtocolTestのテスト

--- Test/CombinedTest/Server/PacketTest/GetMapObjectProtocolTest.cs
+++ Test/CombinedTest/Server/PacketTest/GetMapObjectProtocolTest.cs
@@ -1,14 +1,40 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using Core.Item;
+using Game.MapObject.Interface;
+using Game.PlayerInventory.Interface;
+using MessagePack;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Server.Boot;
+using Server.Protocol.PacketResponse;
+using Test.Module.TestMod;
 
 namespace Test.CombinedTest.Server.PacketTest
 {
     public class GetMapObjectProtocolTest
     {
+        private const int PlayerId = 0;
+
         [Test]
-        public void GetTest()
+        public void GetMapObjectProtocol_DestroyAndAddToInventory_Test()
         {
+            var (packet, serviceProvider) = new PacketResponseCreatorDiContainerGenerators().Create(TestModDirectory.ForUnitTestModDirectory);
+
+            var playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+            var worldMapObjectDataStore = serviceProvider.GetService<IMapObjectDatastore>();
+            var itemStackFactory = serviceProvider.GetService<ItemStackFactory>();
             
-            Assert.Fail();
+            
+            //マップオブジェクトを取得するプロトコルを送信
+            var mapObject = worldMapObjectDataStore.MapObjects[0];
+            packet.GetPacketResponse(MessagePackSerializer.Serialize(new GetMapObjectProtocolProtocolMessagePack(PlayerId,mapObject.InstanceId)).ToList());
+            
+
+            //実際マップオブジェクトが取得されているかのテスト
+            var mapObjectItemStack = itemStackFactory.Create(mapObject.ItemId, mapObject.ItemCount);
+            var playerInventory = playerInventoryDataStore.GetInventoryData(PlayerId).MainOpenableInventory;
+            Assert.AreEqual(playerInventory.GetItem(0),mapObjectItemStack);
+            
         }
     }
 }
