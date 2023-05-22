096c622de8ec10607c00eb4455891326ae6b01d8
sakastudio <sakastudio100@gmail.com>
2023-05-22 23:51:00 +0900

    とりあえずマップオブジェクトのベースを作成

---
+++ Assets/Scripts/MainGame/UnityView/MapObject/MapObjectGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace MainGame.UnityView.MapObject
+{
+    /// <summary>
+    /// MapObjectのGameObjectを表すクラス
+    /// TODO 今はUnity上に直接おいているので、今後はちゃんとサーバーからデータを受け取って生成するようにする
+    /// </summary>
+    public class MapObjectGameObject : MonoBehaviour
+    {
+        [SerializeField] private int instanceId;
+        public int InstanceId => instanceId;
+
+        [SerializeField] private string mapObjectType;
+        public string MapObjectType => mapObjectType;
+    }
+}
---
+++ Assets/Scripts/MainGame/UnityView/MapObject/MapObjectGameObjectController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace MainGame.UnityView.MapObject
+{
+    /// <summary>
+    /// TODO 静的なオブジェクトになってるので、サーバーからコンフィグを取得して動的に生成するようにしたい
+    /// </summary>
+    public class MapObjectGameObjectController : MonoBehaviour
+    {
+        [SerializeField] private List<MapObjectGameObject> stoneMapObjects;
+    }
+}
