ecd98cc014dfbeaa8c8d5f7ec5e965432a02e2f3
sakastudio <sakastudio100@gmail.com>
2023-06-12 11:27:36 +0900

    テストデータの修正

--- Game.MapObject/MapObjectFactory.cs
+++ Game.MapObject/MapObjectFactory.cs
@@ -33,8 +33,8 @@ namespace Game.MapObject
         {
             return type switch
             {
-                VanillaMapObjectType.VanillaTree => _itemConfig.GetItemId("moorestechBaseMod", "VanillaTree"),
-                VanillaMapObjectType.VanillaStone => _itemConfig.GetItemId("moorestechBaseMod", "VanillaStone"),
+                VanillaMapObjectType.VanillaTree => _itemConfig.GetItemId("moorestechBaseMod", "tree"),
+                VanillaMapObjectType.VanillaStone => _itemConfig.GetItemId("moorestechBaseMod", "stone"),
                 _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
             };
         }
