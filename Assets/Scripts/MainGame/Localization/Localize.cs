9380e3d5b0c36d3e283ef47990f325b061b2768e
sakastudio <sakastudio100@gmail.com>
2023-06-25 22:45:12 +0900

    ローカライズのベースシステム

---
+++ Assets/Scripts/MainGame/Localization/Localize.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.IO;
+using GameConst;
+using UnityEngine;
+
+namespace MainGame.Localization
+{
+    public static class Localize
+    {
+        /// <summary>
+        /// ローカライズ用のテキストが入っている
+        /// Key : 国コード
+        /// Value : キーとテキストのペア
+        /// </summary>
+        private static readonly Dictionary<string,Dictionary<string,string>> localizeDictionary = new();
+        
+        private static string CurrentLanguageCode { get; set; }
+        
+        private const string DefaultLanguageCode = "english";
+
+
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)] 
+        public static void Initialize()
+        {
+            //player prefsから言語コードを取得
+            CurrentLanguageCode = PlayerPrefs.GetString("LanguageCode", DefaultLanguageCode);
+            
+            //languageファイルをcsvから読み込む
+            var csvText = File.ReadAllText(Path.Combine(ServerConst.ServerDirectory, "config", "localization.csv"));
+            var csvLines = csvText.Split('\n');
+            
+            
+            //csvの1行目は言語コードなので、それを取得
+            var languageCodes = csvLines[0].Split(',');
+            foreach (var langCode in languageCodes)
+            {
+                localizeDictionary.Add(langCode,new Dictionary<string, string>());
+            }
+
+            //実際に言語ファイルを読み込む
+            for (int i = 1; i < csvLines.Length; i++)
+            {
+                var keyAndValues = csvLines[i].Split(',');
+                var key = keyAndValues[0];
+                for (int j = 1; j < keyAndValues.Length; j++)
+                {
+                    var value = keyAndValues[j];
+                    localizeDictionary[languageCodes[j]].Add(key,value);
+                }
+            }
+
+        }
+        
+        
+        public static string Get(string key)
+        {
+            if (localizeDictionary[CurrentLanguageCode].TryGetValue(key,out var value))
+            {
+                return value;
+            }
+            return $"[Localize Error] Key : {key} is not found";
+        }
+    }
+}
