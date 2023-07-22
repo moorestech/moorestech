4299465d3f2fc7d5c27229bef2ef05a8aa1d938d
sakastudio <sakastudio100@gmail.com>
2023-07-22 23:09:56 +0900

    UIBluePrintProgressArrow

---
+++ Assets/Scripts/MainGame/UnityView/UI/Builder/BluePrint/UIBluePrintProgressArrow.cs
@@ -0,0 +1,16 @@
+﻿namespace MainGame.UnityView.UI.Builder.BluePrint
+{
+    public class UIBluePrintProgressArrow : IUIBluePrintElement
+    {
+        public UIBluePrintType ElementType => UIBluePrintType.ProgressArrow;
+        public int Priority { get; }
+        public string IdName { get; }
+        
+        
+        public UIBluePrintProgressArrow(int priority, string idName)
+        {
+            Priority = priority;
+            IdName = idName;
+        }
+    }
+}
--- Assets/Scripts/MainGame/UnityView/UI/Builder/BluePrint/UIBluePrintType.cs
+++ Assets/Scripts/MainGame/UnityView/UI/Builder/BluePrint/UIBluePrintType.cs
@@ -3,6 +3,7 @@ namespace MainGame.UnityView.UI.Builder.BluePrint
     public enum UIBluePrintType{
         OneSlot,
         ArraySlot,
+        ProgressArrow,
         Text,
     }
 }
