2659b1c45c7f4b2cb5f9437b9b9c9d87b05e9eaa
sakastudio <sakastudio100@gmail.com>
2023-07-26 09:19:39 +0900

    イベントパケットをロック

--- Server.Event/EventProtocolProvider.cs
+++ Server.Event/EventProtocolProvider.cs
@@ -7,6 +7,7 @@ namespace Server.Event
     /// <summary>
     /// サーバー内で起こったイベントの中で、各プレイヤーに送る必要があるイベントを管理します。
     /// 送る必要のある各イベントはEventReceiveフォルダの中に入っています
+    /// TODO ここのロックは一時的なものなので今後はちゃんとゲーム全体としてセマフォをしっかりやる！！
     /// </summary>
     public class EventProtocolProvider
     {
@@ -14,46 +15,55 @@ namespace Server.Event
 
         public void AddEvent(int playerId, List<byte> eventByteArray)
         {
-            if (_events.TryGetValue(playerId, out var eventList))
+            lock (_events)
             {
-                if (eventList.Count <= 300)
+                if (_events.TryGetValue(playerId, out var eventList))
                 {
-                    Console.WriteLine($"target {eventList.Count}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
+                    if (eventList.Count <= 300)
+                    {
+                        Console.WriteLine($"target {eventList.Count}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
+                    }
+                    eventList.Add(eventByteArray);
                 }
-                eventList.Add(eventByteArray);
-            }
-            else
-            {
-                Console.WriteLine($"{0}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
-                _events.Add(playerId, new List<List<byte>>() {eventByteArray});
+                else
+                {
+                    Console.WriteLine($"{0}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
+                    _events.Add(playerId, new List<List<byte>>() {eventByteArray});
+                }
             }
         }
 
         public void AddBroadcastEvent(List<byte> eventByteArray)
         {
-            foreach (var key in _events.Keys)
+            lock (_events)
             {
-                if (_events[key].Count <= 300)
+                foreach (var key in _events.Keys)
                 {
-                    Console.WriteLine($"broad {_events[key].Count}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
+                    if (_events[key].Count <= 300)
+                    {
+                        Console.WriteLine($"broad {_events[key].Count}番目 {MessagePackSerializer.ConvertToJson(eventByteArray.ToArray())}");
+                    }
+                    _events[key].Add(eventByteArray);
                 }
-                _events[key].Add(eventByteArray);
             }
         }
 
         public List<List<byte>> GetEventBytesList(int playerId)
         {
-            if (_events.ContainsKey(playerId))
+            lock (_events)
             {
-                var data = _events[playerId].Copy();
-                _events[playerId].Clear();
-                return data;
-            }
-            else
-            {
-                //ブロードキャストイベントの時に使うので、Dictionaryにキーを追加しておく
-                _events.Add(playerId, new List<List<byte>>());
-                return _events[playerId];
+                if (_events.ContainsKey(playerId))
+                {
+                    var data = _events[playerId].Copy();
+                    _events[playerId].Clear();
+                    return data;
+                }
+                else
+                {
+                    //ブロードキャストイベントの時に使うので、Dictionaryにキーを追加しておく
+                    _events.Add(playerId, new List<List<byte>>());
+                    return _events[playerId];
+                }
             }
         }
     }
