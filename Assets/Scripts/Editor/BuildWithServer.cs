b637ec6379ecf30e042a9fd59c045cdecbe9f3c6
sakastudio <sakastudio100@gmail.com>
2023-08-22 13:07:21 +0900

    CI CDようにビルドパイプラインを集背い

--- Assets/Scripts/Editor/BuildWithServer.cs
+++ Assets/Scripts/Editor/BuildWithServer.cs
@@ -2,6 +2,7 @@ using System.IO;
 using System.Linq;
 using GameConst;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 
 
@@ -11,18 +12,26 @@ public class BuildWithServer
     [MenuItem("moorestech/WindowsBuild")]
     public static void WindowsBuild()
     {
-        Pipeline(BuildTarget.StandaloneWindows64);
+        Pipeline(BuildTarget.StandaloneWindows64,false);
     }
     [MenuItem("moorestech/LinuxBuild")]
     public static void LinuxBuild()
     {
-        Pipeline(BuildTarget.StandaloneLinux64);
+        Pipeline(BuildTarget.StandaloneLinux64,false);
     }
     
+    #region from Github Action
+    public static void WindowsBuildFromGithubAction()
+    {
+        Pipeline(BuildTarget.StandaloneWindows64,true);
+    }
+    public static void LinuxBuildFromGithubAction()
+    {
+        Pipeline(BuildTarget.StandaloneLinux64,true);
+    }
+    #endregion
 
-
-
-    private static void Pipeline(BuildTarget buildTarget)
+    private static void Pipeline(BuildTarget buildTarget,bool isErrorExit)
     {
         var playerPrefsKey = OutputPathKey + buildTarget;
         var path = EditorUtility.OpenFolderPanel("Build", PlayerPrefs.GetString(playerPrefsKey,""), 
@@ -44,8 +53,29 @@ public class BuildWithServer
         buildOptions.locationPathName = path + "/moorestech.exe";
         buildOptions.scenes = EditorBuildSettings.scenes.Select(s => s.path).ToArray();
             
-        BuildPipeline.BuildPlayer(buildOptions);
+        var report = BuildPipeline.BuildPlayer(buildOptions);
         
         EditorUtility.RevealInFinder( path );
+        
+        
+        if (isErrorExit)
+        {
+            //ビルドレポートを表示
+            foreach (var buildStep in report.steps)
+            {
+                var stepName = buildStep.name;
+                foreach (var message in buildStep.messages)
+                {
+                    Debug.LogError(stepName + " : " + message.type + " : " + message.content);
+                }
+            }
+            if (report.summary.result == BuildResult.Failed)
+            {
+                EditorApplication.Exit(1);
+            }
+        }
     }
+
+
+
 }
