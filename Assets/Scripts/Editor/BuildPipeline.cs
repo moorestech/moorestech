31952010cc33b6dda033cec7394a15a6e4fc893b
sakastudio <sakastudio100@gmail.com>
2023-08-27 02:12:55 +0900

    cicd

--- Assets/Scripts/Editor/BuildPipeline.cs
+++ Assets/Scripts/Editor/BuildPipeline.cs
@@ -64,7 +64,7 @@ public class BuildPipeline
         var buildOptions = new BuildPlayerOptions
         {
             target = buildTarget,
-            locationPathName = path,
+            locationPathName = path + (buildTarget == BuildTarget.StandaloneWindows64 ? "/moorestech.exe" : "/moorestech"),
             scenes = EditorBuildSettings.scenes.Select(s => s.path).ToArray()
         };
 
