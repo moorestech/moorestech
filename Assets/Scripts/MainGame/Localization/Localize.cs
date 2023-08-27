e39e70482687ddbecf152280c69286594650e985
sakastudio <sakastudio100@gmail.com>
2023-08-28 00:05:11 +0900

    ローカライザの修正

--- Assets/Scripts/MainGame/Localization/Localize.cs
+++ Assets/Scripts/MainGame/Localization/Localize.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using CsvHelper;
 using GameConst;
 using UniRx;
 using UnityEngine;
@@ -14,65 +16,73 @@ namespace MainGame.Localization
         /// Key : 国コード
         /// Value : キーとテキストのペア
         /// </summary>
-        private static readonly Dictionary<string,Dictionary<string,string>> localizeDictionary = new();
-        
+        private static readonly Dictionary<string, Dictionary<string, string>> localizeDictionary = new();
+
         public static IObservable<Unit> OnLanguageChanged => _onLanguageChangedSubject;
         private static Subject<Unit> _onLanguageChangedSubject = new();
 
         private static string CurrentLanguageCode { get; set; }
-        
+
         private const string DefaultLanguageCode = "english";
 
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)] 
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         public static void Initialize()
         {
             //player prefsから言語コードを取得
             CurrentLanguageCode = PlayerPrefs.GetString("LanguageCode", DefaultLanguageCode);
-            
-            //languageファイルをcsvから読み込む
-            var csvText = File.ReadAllText(Path.Combine(ServerConst.ServerDirectory, "config", "localization.csv"));
-            var csvLines = csvText.Split('\n');
-            
-            
-            //csvの1行目は言語コードなので、それを取得
-            var languageCodes = csvLines[0].Split(',');
-            foreach (var langCode in languageCodes)
-            {
-                localizeDictionary.Add(langCode,new Dictionary<string, string>());
-            }
 
-            //実際に言語ファイルを読み込む
-            for (int i = 1; i < csvLines.Length; i++)
+            // CSVファイルのパス
+            var csvFilePath = Path.Combine(ServerConst.ServerDirectory, "config", "localization.csv");
+
+            var languageCodes = new List<string>();
+            bool isFirstRow = true;
+
+            using var reader = new StreamReader(csvFilePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            while (csv.Read())
             {
-                var keyAndValues = csvLines[i].Split(',');
-                var key = keyAndValues[0];
-                for (int j = 1; j < keyAndValues.Length; j++)
+                if (isFirstRow)
                 {
-                    var value = keyAndValues[j];
-                    localizeDictionary[languageCodes[j]].Add(key,value);
+                    // csvの1行目は言語コードなので、それを取得
+                    for (var i = 0; csv.TryGetField<string>(i, out var field); i++)
+                    {
+                        languageCodes.Add(field);
+                        localizeDictionary.Add(field, new Dictionary<string, string>());
+                    }
+                    isFirstRow = false;
+                    continue;
+                }
+
+                var keyAndValues = new List<string>();
+                for (var i = 0; csv.TryGetField<string>(i, out var field); i++)
+                {
+                    keyAndValues.Add(field);
+                }
+
+                var key = keyAndValues[0];
+                for (var i = 1; i < keyAndValues.Count; i++)
+                {
+                    //外部ソースから取得したテキストには改行コードが\nとして入っているので、それを\nに変換
+                    localizeDictionary[languageCodes[i]].Add(key, keyAndValues[i].Replace("\\n", "\n"));
                 }
             }
-
         }
-        
-        
+
         public static string Get(string key)
         {
-            if (localizeDictionary[CurrentLanguageCode].TryGetValue(key,out var value))
+            if (localizeDictionary[CurrentLanguageCode].TryGetValue(key, out var value))
             {
                 return value;
             }
             return $"[Localize] Key : {key} is not found";
         }
-        
-        
+
         public static void SetLanguage(string languageCode)
         {
             if (localizeDictionary.ContainsKey(languageCode))
             {
                 CurrentLanguageCode = languageCode;
-                PlayerPrefs.SetString("LanguageCode",languageCode);
+                PlayerPrefs.SetString("LanguageCode", languageCode);
                 PlayerPrefs.Save();
                 _onLanguageChangedSubject?.OnNext(Unit.Default);
             }
