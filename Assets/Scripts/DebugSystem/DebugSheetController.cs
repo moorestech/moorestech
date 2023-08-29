767c3f1d4b8fcb7db59a439c5b3a41e20aeac54d
sakastudio <sakastudio100@gmail.com>
2023-08-30 00:45:15 +0900

    デバッグシートの作成

---
+++ Assets/Scripts/DebugSystem/DebugSheetController.cs
@@ -0,0 +1,21 @@
+﻿using IngameDebugConsole;
+using Tayx.Graphy;
+using UnityDebugSheet.Runtime.Core.Scripts;
+using UnityDebugSheet.Runtime.Extensions.Graphy;
+using UnityDebugSheet.Runtime.Extensions.IngameDebugConsole;
+using UnityEngine;
+
+namespace DebugSystem
+{
+    public sealed class DebugSheetController : MonoBehaviour
+    {
+        private void Start()
+        {
+            var rootPage = DebugSheet.Instance.GetOrCreateInitialPage();
+
+            rootPage.AddPageLinkButton<IngameDebugConsoleDebugPage>("In-Game Debug Console", onLoad: x => x.page.Setup(DebugLogManager.Instance));
+            rootPage.AddPageLinkButton<GraphyDebugPage>("Graphy", onLoad: x => x.page.Setup(GraphyManager.Instance));
+            rootPage.AddPageLinkButton<MainDebugPage>(nameof(MainDebugPage));
+        }
+    }
+}
