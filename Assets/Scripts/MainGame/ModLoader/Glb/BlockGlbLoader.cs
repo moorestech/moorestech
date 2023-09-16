c442d96520e7b5da1d3ddacde41586a718fcfed4
sakastudio <sakastudio100@gmail.com>
2023-09-16 23:38:14 +0900

    エラーにする

--- Assets/Scripts/MainGame/ModLoader/Glb/BlockGlbLoader.cs
+++ Assets/Scripts/MainGame/ModLoader/Glb/BlockGlbLoader.cs
@@ -47,7 +47,7 @@ namespace MainGame.ModLoader.Glb
                 var gameObject = await GlbLoader.Load(mod.ExtractedPath, BlockDirectory + config.Name + ".glb");
                 if (gameObject == null)
                 {
-                    Debug.LogWarning("GlbFile Not Found  ModId:" + mod.ModMetaJson.ModId + " BlockName:" + config.Name);
+                    Debug.LogError("GlbFile Not Found  ModId:" + mod.ModMetaJson.ModId + " BlockName:" + config.Name);
                     continue;
                 }
                 
