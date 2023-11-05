79c136287bc2ac46cbcd841439e6939de20e30ef
sakastudio <sakastudio100@gmail.com>
2023-11-05 20:41:31 +0900

    new camera system

--- moorestech_client/Assets/Scripts/MainGame/UnityView/Control/MouseKeyboard/CameraController.cs
+++ moorestech_client/Assets/Scripts/MainGame/UnityView/Control/MouseKeyboard/CameraController.cs
@@ -6,6 +6,7 @@ namespace MainGame.UnityView.Control.MouseKeyboard
     public class CameraController : MonoBehaviour
     {
         [SerializeField] private CinemachineVirtualCamera virtualCamera;
+        [SerializeField] private Vector2 sensitivity = Vector2.one;
         private CinemachineFramingTransposer cinemachineFraming;
 
         private void Awake()
@@ -13,11 +14,30 @@ namespace MainGame.UnityView.Control.MouseKeyboard
             cinemachineFraming = virtualCamera.GetCinemachineComponent<CinemachineFramingTransposer>();
         }
 
+        Vector2 _lastClickedPosition;
         private void Update()
         {
             cinemachineFraming.m_CameraDistance += InputManager.UI.SwitchHotBar.ReadValue<float>() / -100;
 
-            cinemachineFraming.m_CameraDistance = Mathf.Clamp(cinemachineFraming.m_CameraDistance, 3, 75);
+            cinemachineFraming.m_CameraDistance = Mathf.Clamp(cinemachineFraming.m_CameraDistance, 1, 75);
+            
+            //マウスのインプットによって向きを変える
+            if (InputManager.Playable.ScreenRightClick.GetKeyDown)
+            {
+                _lastClickedPosition = InputManager.Playable.ClickPosition.ReadValue<Vector2>();
+            }
+            
+            if (InputManager.Playable.ScreenRightClick.GetKey)
+            {
+                var currentClickedPosition = InputManager.Playable.ClickPosition.ReadValue<Vector2>();
+                var delta = currentClickedPosition - _lastClickedPosition;
+                _lastClickedPosition = currentClickedPosition;
+
+                var currentRotation = transform.rotation.eulerAngles;
+                currentRotation.x -= delta.y * sensitivity.y;
+                currentRotation.y += delta.x * sensitivity.x;
+                transform.rotation = Quaternion.Euler(currentRotation);
+            }
         }
     }
 }
