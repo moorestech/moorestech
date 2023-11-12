f18488bc0e8c10ac94837ad1b2dbf25b686d48ac
Juha <kukiyomenaisapp@gmail.com>
2023-11-12 18:21:19 +0900

    privateメソッドを取得できるように変更

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/MinerMiningTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/MinerMiningTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection;
 using Core.EnergySystem;
 using Core.Item;
 using Core.Ore;
@@ -67,7 +68,9 @@ namespace Tests.CombinedTest.Core
             mineEndTime = DateTime.Now.AddMilliseconds(miningTime * 2);
             while (mineEndTime.AddSeconds(0.02).CompareTo(DateTime.Now) == 1) GameUpdater.Update();
 
-            typeof(VanillaMinerBase).GetMethod("Update")!.Invoke(miner, Array.Empty<object>());
+            typeof(VanillaMinerBase).GetMethod("Update",
+                    BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Instance)!
+                .Invoke(miner, Array.Empty<object>());
 
             //鉱石2個が残っているかチェック
             var outputSlot = miner.Items[0];
