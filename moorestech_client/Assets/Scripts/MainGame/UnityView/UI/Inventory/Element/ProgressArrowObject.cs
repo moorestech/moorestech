73b104b392d6a38c77902b3952f164762d33572f
sakastudio <sakastudio100@gmail.com>
2023-12-26 00:53:32 +0900

    ProgressArrowObject

---
+++ moorestech_client/Assets/Scripts/MainGame/UnityView/UI/Inventory/Element/ProgressArrowObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainGame.UnityView.UI.Builder.Unity
+{
+    public class ProgressArrowObject : MonoBehaviour
+    {
+        [SerializeField] private Image arrowBlack;
+
+        public void SetFillAmount(float amount)
+        {
+            arrowBlack.fillAmount = amount;
+        }
+    }
+}
