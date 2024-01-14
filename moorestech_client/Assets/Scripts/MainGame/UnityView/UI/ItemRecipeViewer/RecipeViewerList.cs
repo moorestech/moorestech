e8149067483aad06066572bace27c9960db8b3a5
sakastudio <sakastudio100@gmail.com>
2024-01-14 22:48:56 +0900

    レシピビューワーの元

---
+++ moorestech_client/Assets/Scripts/MainGame/UnityView/UI/ItemRecipeViewer/RecipeViewerList.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using Core.Item.Config;
+using Game.Block.Interface.RecipeConfig;
+using Game.Crafting.Interface;
+using MainGame.UnityView.UI.Inventory.Element;
+using UniRx;
+using UnityEngine;
+
+namespace MainGame.UnityView.UI.ItemRecipeViewer
+{
+    public class RecipeViewerList : MonoBehaviour
+    {
+        [SerializeField] private ItemSlotObject itemSlotObjectPrefab;
+        
+        [SerializeField] private RectTransform itemListView;
+        [SerializeField] private RectTransform itemListParent;
+
+        public IObservable<List<IRecipeViewerRecipe>> OnClickItem => _onClickItem;
+        private readonly Subject<List<IRecipeViewerRecipe>> _onClickItem = new();
+
+        private readonly List<ItemSlotObject> _itemSlotObjects = new();
+        private IItemConfig _itemConfig;
+
+        public void Initialize(IItemConfig itemConfig,ICraftingConfig craftConfig,IMachineRecipeConfig machineRecipeConfig,ItemImageContainer itemImageContainer)
+        {
+            _itemConfig = itemConfig;
+            
+            foreach (var item in itemConfig.ItemConfigDataList)
+            {
+                var itemViewData = itemImageContainer.GetItemView(item.ItemId);
+                
+                var itemSlotObject = Instantiate(itemSlotObjectPrefab, itemListParent);
+                itemSlotObject.SetItem(itemViewData, 0);
+                
+                itemSlotObject.OnLeftClickUp.Subscribe(OnClickItemList);
+                _itemSlotObjects.Add(itemSlotObject);
+            }
+        }
+
+        private void OnClickItemList(ItemSlotObject slotObject)
+        {
+            
+        }
+    }
+
+    public interface IRecipeViewerRecipe
+    {
+        
+    }
+}
