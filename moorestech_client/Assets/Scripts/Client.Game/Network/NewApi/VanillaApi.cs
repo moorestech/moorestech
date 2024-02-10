7374605dd137028c3910970944205abdd41895bc
sakastudio <sakastudio100@gmail.com>
2024-02-10 14:06:46 +0900

    VanillaApiをstaticに

--- moorestech_client/Assets/Scripts/Client.Game/Network/NewApi/VanillaApi.cs
+++ moorestech_client/Assets/Scripts/Client.Game/Network/NewApi/VanillaApi.cs
@@ -12,35 +12,35 @@ namespace Client.Network.NewApi
         private readonly ServerConnector _serverConnector;
         private readonly ItemStackFactory _itemStackFactory;
         
+        private static VanillaApi _instance;
+        
         public VanillaApi(ServerConnector serverConnector, ItemStackFactory itemStackFactory)
         {
             _serverConnector = serverConnector;
             _itemStackFactory = itemStackFactory;
+            _instance = this;
         }
         
-        /// <summary>
-        /// TODO この呼び出しタイミングを考える
-        /// </summary>
-        public async UniTask<List<MapObjectsInfoMessagePack>> GetMapObjectInfo(CancellationToken ct)
+        public static async UniTask<List<MapObjectsInfoMessagePack>> GetMapObjectInfo(CancellationToken ct)
         {
             var request = new RequestMapObjectInfosMessagePack();
-            var response = await _serverConnector.GetInformationData<ResponseMapObjectInfosMessagePack>(request ,ct);
+            var response = await _instance._serverConnector.GetInformationData<ResponseMapObjectInfosMessagePack>(request ,ct);
             return response?.MapObjects;
         }
         
         
-        public async UniTask<List<IItemStack>> GetBlockInventory(Vector2Int blockPos,CancellationToken ct)
+        public static async UniTask<List<IItemStack>> GetBlockInventory(Vector2Int blockPos,CancellationToken ct)
         {
             var request = new RequestBlockInventoryRequestProtocolMessagePack(blockPos.x, blockPos.y);
             
-            var response = await _serverConnector.GetInformationData<BlockInventoryResponseProtocolMessagePack>(request ,ct);
+            var response = await _instance._serverConnector.GetInformationData<BlockInventoryResponseProtocolMessagePack>(request ,ct);
                         
             var items = new List<IItemStack>(response.ItemIds.Length);
             for (int i = 0; i < response.ItemIds.Length; i++)
             {
                 var id = response.ItemIds[i];
                 var count = response.ItemCounts[i];
-                items.Add(_itemStackFactory.Create(id, count));
+                items.Add(_instance._itemStackFactory.Create(id, count));
             }
             
             return items;
