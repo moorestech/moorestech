2e67673c10daefae23e09804b18536f39bae7c1d
sakastudio <sakastudio100@gmail.com>
2024-03-07 22:45:37 +0900

    GameInitializerSceneLoaderの変更

--- moorestech_client/Assets/Scripts/Client.Starter/GameInitializerSceneLoader.cs
+++ moorestech_client/Assets/Scripts/Client.Starter/GameInitializerSceneLoader.cs
@@ -4,12 +4,16 @@ using UnityEngine.SceneManagement;
 
 namespace Client.Starter
 {
+    /// <summary>
+    /// <see cref="GameInitializerSceneLoader"/>があるシーンの場合はゲーム初期化シーンをロードする
+    /// </summary>
     public class GameInitializerSceneLoader : MonoBehaviour
     {
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-        static void Init()
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        public static void Init()
         {
-            if (SceneManager.GetActiveScene().name == SceneConstant.MainGameSceneName)
+            var loader = FindObjectOfType<GameInitializerSceneLoader>();
+            if (loader != null)
             {
                 SceneManager.LoadScene(SceneConstant.GameInitializerSceneName);
             }
