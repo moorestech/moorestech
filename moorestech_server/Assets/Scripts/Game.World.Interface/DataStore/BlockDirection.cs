eb870623c5fba4d32de7e7955366a670ad09e6f1
sakastudio <sakastudio100@gmail.com>
2024-03-15 22:20:30 +0900

    BlockDirectionの定義を追加

--- moorestech_server/Assets/Scripts/Game.World.Interface/DataStore/BlockDirection.cs
+++ moorestech_server/Assets/Scripts/Game.World.Interface/DataStore/BlockDirection.cs
@@ -1,10 +1,25 @@
 namespace Game.World.Interface.DataStore
 {
+    /// <summary>
+    /// 3次元的な方向に配置するため、上、下、通常の設置×4方向の向きが必要になる
+    /// UpNorthは、ブロックを上方向にした後、通常の設置をした時の下の面が北向きになることを意味する
+    /// DownNorthは、ブロックを下方向にした後、通常の設置をした時の下の面が北向きになることを意味する
+    /// </summary>
     public enum BlockDirection
     {
+        UpNorth,
+        UpEast,
+        UpSouth,
+        UpWest,
+        
         North,
         East,
         South,
-        West
+        West,
+        
+        DownNorth,
+        DownEast,
+        DownSouth,
+        DownWest,
     }
 }
