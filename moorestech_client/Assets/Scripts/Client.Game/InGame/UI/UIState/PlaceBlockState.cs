c2a02b6f8be5e09b0382233af5b4a5a4e11d2ac0
Juha <kukiyomenaisapp@gmail.com>
2024-05-07 17:54:17 +0900

    PlaceBlockStateで右クリック長押しでカメラを回転できるように変更

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/UIState/PlaceBlockState.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/UIState/PlaceBlockState.cs
@@ -1,29 +1,47 @@
-﻿using Client.Input;
+﻿using Client.Game.InGame.Control;
+using Client.Input;
 using UnityEngine;
 
 namespace Client.Game.InGame.UI.UIState
 {
     public class PlaceBlockState : IUIState
     {
-        public PlaceBlockState()
+        private readonly InGameCameraController _inGameCameraController;
+        
+        public PlaceBlockState(InGameCameraController inGameCameraController)
         {
+            _inGameCameraController = inGameCameraController;
             Debug.Log("Create PlaceBlockState");
         }
         
         public void OnEnter(UIStateEnum lastStateEnum)
         {
             Debug.Log("Enter PlaceBlockState");
+            InputManager.MouseCursorVisible(true);
         }
         public UIStateEnum GetNext()
         {
             //TODO InputSystemのリファクタ対象
             if (InputManager.UI.CloseUI.GetKeyDown || UnityEngine.Input.GetKeyDown(KeyCode.B)) return UIStateEnum.GameScreen;
             
+            //TODO InputSystemのリファクタ対象
+            if (UnityEngine.Input.GetMouseButtonDown(1))
+            {
+                InputManager.MouseCursorVisible(false);
+                _inGameCameraController.updateCameraAngle = true;
+            }
+            if (UnityEngine.Input.GetMouseButtonUp(1))
+            {
+                InputManager.MouseCursorVisible(true);
+                _inGameCameraController.updateCameraAngle = false;
+            }
+            
             return UIStateEnum.Current;
         }
         public void OnExit()
         {
             Debug.Log("Exit PlaceBlockState");
+            InputManager.MouseCursorVisible(false);
         }
     }
 }
