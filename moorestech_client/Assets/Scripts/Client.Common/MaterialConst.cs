f8ffe6cccf90acec30e159e6a4c5dacc233cfdaf
Juha <kukiyomenaisapp@gmail.com>
2024-05-10 22:26:23 +0900

    PreviewNotPlaceableBlockMaterialを追加

--- moorestech_client/Assets/Scripts/Client.Common/MaterialConst.cs
+++ moorestech_client/Assets/Scripts/Client.Common/MaterialConst.cs
@@ -3,8 +3,9 @@ namespace Client.Common
     public class MaterialConst
     {
         public const string PlaceBlockAnimationMaterial = "PlaceBlockAnimation";
-
+        
         public const string PreviewPlaceBlockMaterial = "PreviewPlaceBlock";
         public const string PreviewRemoveBlockMaterial = "PreviewRemoveBlock";
+        public const string PreviewNotPlaceableBlockMaterial = "PreviewNotPlaceableBlock";
     }
 }
