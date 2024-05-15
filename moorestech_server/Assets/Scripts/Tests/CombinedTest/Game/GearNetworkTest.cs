e3bac75553557ab7d187d5a721100cc2b4aa47d2
Connor De Meyer <68373215+ConnorDeMeyer@users.noreply.github.com>
2024-05-15 19:23:30 +0900

    Test fix
    Position and Block ID fix

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Game/GearNetworkTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Game/GearNetworkTest.cs
@@ -126,7 +126,7 @@ namespace Tests.CombinedTest.Game
 
             var generatorPos = new Vector3Int(0, 0, 0); // 大歯車を使ってRPMを変化させた側の歯車
             var bigGearPos = new Vector3Int(0, 0, 1); // Gears on the side that changed RPM with large gears
-            var smallGear1Pos = new Vector3Int(0, 0, 1);
+            var smallGear1Pos = new Vector3Int(2, 0, 1);
 
             var smallGear2Pos = new Vector3Int(0, 0, -1); // RPMを変化させていない側の歯車（回転方向を変えないために2つの小歯車をつかう）
             var smallGear3Pos = new Vector3Int(1, 0, -1); // Gears on the side not changing RPM (two small gears are used to keep the direction of rotation the same)
@@ -134,7 +134,7 @@ namespace Tests.CombinedTest.Game
             AddBlock(ForUnitTestModBlockId.SimpleGearGenerator, generatorPos, BlockDirection.North);
             AddBlock(ForUnitTestModBlockId.BigGear, bigGearPos, BlockDirection.North);
 
-            AddBlock(ForUnitTestModBlockId.BigGear, smallGear2Pos, BlockDirection.North);
+            AddBlock(ForUnitTestModBlockId.SmallGear, smallGear2Pos, BlockDirection.North);
 
             var smallGear1 = AddBlock(ForUnitTestModBlockId.SmallGear, smallGear1Pos, BlockDirection.North);
             var smallGear1Connector = smallGear1.ComponentManager.GetComponent<BlockConnectorComponent<IGearEnergyTransformer>>();
