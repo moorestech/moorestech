4df7b5d482bbed5ada07f64cd1fc820b07a5b238
Juha <kukiyomenaisapp@gmail.com>
2024-05-21 18:20:54 +0900

    一旦コミット

---
+++ moorestech_server/Assets/Scripts/Game.Block/Config/LoadConfig/OptionLoader/GearConnectOption.cs
@@ -0,0 +1,21 @@
+﻿using Game.Block.Interface.BlockConfig;
+
+namespace Game.Block.Config.LoadConfig.OptionLoader
+{
+    public class GearConnectOption : IConnectOption
+    {
+        public bool IsReverse;
+        
+        public GearConnectOption(bool isReverse)
+        {
+            IsReverse = isReverse;
+        }
+        
+        public static IConnectOption Loader(dynamic connectorOption)
+        {
+            var reverse = (bool)connectorOption.isReverse;
+            
+            return new GearConnectOption(reverse);
+        }
+    }
+}
--- moorestech_server/Assets/Scripts/Game.Block/Config/LoadConfig/Param/GearConfigParam.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Config/LoadConfig/Param/GearConfigParam.cs
@@ -1,31 +1,37 @@
 using System.Collections.Generic;
 using Core.Item.Interface.Config;
+using Game.Block.Config.LoadConfig.OptionLoader;
 using Game.Block.Interface.BlockConfig;
 
 namespace Game.Block.Config.LoadConfig.Param
 {
     public class GearConfigParam : IBlockConfigParam
     {
-        public readonly int TeethCount;
         public readonly float LossPower;
-
+        public readonly int TeethCount;
+        
         public List<ConnectSettings> GearConnectSettings;
-
+        
         private GearConfigParam(int teethCount, float lossPower, List<ConnectSettings> gearConnectSettings)
         {
             TeethCount = teethCount;
             GearConnectSettings = gearConnectSettings;
             LossPower = lossPower;
         }
-
+        
         public static IBlockConfigParam Generate(dynamic blockParam, IItemConfig itemConfig)
         {
             int teethCount = blockParam.teethCount;
             float lossPower = blockParam.lossPower;
-
-            var gearConnectSettings = BlockConfigJsonLoad.GetConnectSettings(blockParam, "gearConnects");
-
+            
+            var gearConnectSettings = BlockConfigJsonLoad.GetConnectSettings(blockParam, "gearConnects", ()GearConnectOption.Loader);
+            var a = Hoge(1, GearConnectOption.Loader);
             return new GearConfigParam(teethCount, lossPower, gearConnectSettings);
+        }
+        
+        public static int Hoge(dynamic hoge, dynamic loader = null)
+        {
+            return loader.Invoke(hoge);
         }
     }
 }
