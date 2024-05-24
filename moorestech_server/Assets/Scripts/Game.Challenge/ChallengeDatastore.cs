08ae0326418392e9cfd8a6587ac01488544e1d27
sakastudio <sakastudio100@gmail.com>
2024-05-24 12:52:20 +0900

    チャレンジを開始できるように

--- moorestech_server/Assets/Scripts/Game.Challenge/ChallengeDatastore.cs
+++ moorestech_server/Assets/Scripts/Game.Challenge/ChallengeDatastore.cs
@@ -1,16 +1,41 @@
 using System.Collections.Generic;
 using Game.Challenge.Task;
+using UniRx;
 
 namespace Game.Challenge
 {
     public class ChallengeDatastore
     {
         private readonly Dictionary<int, List<CurrentChallenge>> _currentChallenges = new(); // Key: PlayerId Value: Current Challenge
-        private readonly Dictionary<int,List<int>> _completedChallengeIds = new(); // Key: PlayerId Value: Completed Challenge Ids
+        private readonly Dictionary<int, List<int>> _completedChallengeIds = new(); // Key: PlayerId Value: Completed Challenge Ids
 
-        public ChallengeDatastore(ChallengeConfig challengeConfig,Dictionary<int,List<int>> loadedCompletedChallengeIds)
+        public ChallengeDatastore(ChallengeConfig challengeConfig, Dictionary<int, List<int>> loadedCompletedChallengeIds)
         {
             _completedChallengeIds = loadedCompletedChallengeIds;
+
+            foreach (var playerId in _completedChallengeIds.Keys)
+            {
+                _currentChallenges.Add(playerId, new List<CurrentChallenge>());
+
+                foreach (var completedChallengeId in _completedChallengeIds[playerId])
+                {
+                    // 完了したチャレンジの次のチャレンジがクリア済みでなければ、CurrentChallengeに追加
+                    var info = challengeConfig.ChallengeInfos[completedChallengeId];
+                    foreach (var nextId in info.NextIds)
+                    {
+                        if (!_completedChallengeIds[playerId].Contains(nextId))
+                        {
+                            var currentChallenge = new CurrentChallenge(challengeConfig.ChallengeInfos[nextId]);
+                            currentChallenge.OnChallengeComplete.Subscribe(CompletedChallenge);
+                            _currentChallenges[playerId].Add(currentChallenge);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void CompletedChallenge(ChallengeInfo challengeInfo)
+        {
         }
     }
 }
--- moorestech_server/Assets/Scripts/Game.Challenge/Task/CurrentChallenge.cs
+++ moorestech_server/Assets/Scripts/Game.Challenge/Task/CurrentChallenge.cs
@@ -7,5 +7,14 @@ namespace Game.Challenge.Task
     {
         public IObservable<ChallengeInfo> OnChallengeComplete => _onChallengeComplete;
         private readonly Subject<ChallengeInfo> _onChallengeComplete = new();
+
+        public readonly ChallengeInfo Config;
+        public readonly int Id;
+
+        public CurrentChallenge(ChallengeInfo config, int id)
+        {
+            Config = config;
+            Id = id;
+        }
     }
 }
