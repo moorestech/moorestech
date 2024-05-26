5c08d49976067052a28e2c2ea0399d312c3325f9
sakastudio <sakastudio100@gmail.com>
2024-05-26 14:39:00 +0900

    GetCompletedChallengeTestの実装

---
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/GetChallengeInfoProtocolTest.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Linq;
+using Game.Challenge;
+using Game.Challenge.Task;
+using MessagePack;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Server.Boot;
+using Server.Protocol.PacketResponse;
+using Tests.Module.TestMod;
+using UniRx;
+
+namespace Tests.CombinedTest.Server.PacketTest
+{
+    public class GetChallengeInfoProtocolTest
+    {
+        private const int PlayerId = 1;
+
+        [Test]
+        public void GetCompletedChallengeTest()
+        {
+            var (packet, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+
+            // チャレンジを無理やりクリアする
+            // Forced to complete a challenge
+            var challengeDatastore = serviceProvider.GetService<ChallengeDatastore>();
+            var currentChallenges = challengeDatastore.GetCurrentChallenges(PlayerId);
+
+            foreach (var challenge in currentChallenges)
+            {
+                var subject = (Subject<CurrentChallenge>)challenge.OnChallengeComplete;
+                subject.OnNext(challenge); // 無理やりクリア
+            }
+
+            // 現在のチャレンジ情報をリクエスト
+            // Request current challenge information
+            var messagePack = new RequestChallengeMessagePack(PlayerId);
+            var response = packet.GetPacketResponse(MessagePackSerializer.Serialize(messagePack).ToList())[0];
+            var challengeInfo = MessagePackSerializer.Deserialize<ChallengeInfoMessagePack>(response.ToArray());
+
+            // 検証
+            // Verification
+            Assert.AreEqual(PlayerId, challengeInfo.PlayerId);
+            
+            Assert.AreEqual(2, challengeInfo.CurrentChallengeIds.Count);
+            Assert.IsTrue(challengeInfo.CompletedChallengeIds.Contains(1000));
+            Assert.IsTrue(challengeInfo.CompletedChallengeIds.Contains(1010));
+            
+            Assert.AreEqual(1, challengeInfo.CurrentChallengeIds.Count);
+            Assert.IsTrue(challengeInfo.CurrentChallengeIds.Contains(1020));
+        }
+    }
+}
