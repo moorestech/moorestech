ce38ba52b38190d6042d3438f55cfce8102e6dde
Juha <kukiyomenaisapp@gmail.com>
2024-06-05 13:51:21 +0900

    Update ChestLogicTest.cs

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/ChestLogicTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/ChestLogicTest.cs
@@ -33,10 +33,10 @@ namespace Tests.CombinedTest.Core
             var count = 1;
             var item = itemStackFactory.Create(id, count);
             
-            var chest = blockFactory.Create(7, 0, new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
+            var chest = blockFactory.Create(7, new EntityID(0), new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
             var chestComponent = chest.ComponentManager.GetComponent<VanillaChestComponent>();
             
-            var beltConveyor = blockFactory.Create(3, int.MaxValue, new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
+            var beltConveyor = blockFactory.Create(3, new EntityID(int.MaxValue), new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
             var beltConveyorComponent = beltConveyor.ComponentManager.GetComponent<VanillaBeltConveyorComponent>();
             beltConveyorComponent.InsertItem(item);
             
@@ -57,10 +57,10 @@ namespace Tests.CombinedTest.Core
             
             var blockFactory = ServerContext.BlockFactory;
             
-            var chest = blockFactory.Create(7, 0, new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
+            var chest = blockFactory.Create(7, new EntityID(0), new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
             var chestComponent = chest.ComponentManager.GetComponent<VanillaChestComponent>();
             
-            var beltconveyor = blockFactory.Create(3, 0, new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
+            var beltconveyor = blockFactory.Create(3, new EntityID(0), new BlockPositionInfo(Vector3Int.one, BlockDirection.North, Vector3Int.one));
             var beltConveyorComponent = beltconveyor.ComponentManager.GetComponent<VanillaBeltConveyorComponent>();
             
             chestComponent.SetItem(0, 1, 1);
