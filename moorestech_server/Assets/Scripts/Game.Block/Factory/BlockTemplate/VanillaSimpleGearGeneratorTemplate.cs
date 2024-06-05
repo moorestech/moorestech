8a5c722f2896cef4da7e301e094c33f002091d4b
Juha <kukiyomenaisapp@gmail.com>
2024-06-05 13:37:13 +0900

    Update VanillaSimpleGearGeneratorTemplate.cs

--- moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaSimpleGearGeneratorTemplate.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaSimpleGearGeneratorTemplate.cs
@@ -12,20 +12,19 @@ namespace Game.Block.Factory.BlockTemplate
 {
     public class VanillaSimpleGearGeneratorTemplate : IBlockTemplate
     {
-        public IBlock New(BlockConfigData config, int entityId, BlockPositionInfo blockPositionInfo)
+        public IBlock Load(string state, BlockConfigData config, EntityID entityId, BlockPositionInfo blockPositionInfo)
+        {
+            return CreateGear(config, entityId, blockPositionInfo);
+        }
+        public IBlock New(BlockConfigData config, EntityID entityId, BlockPositionInfo blockPositionInfo)
         {
             return CreateGear(config, entityId, blockPositionInfo);
         }
         
-        public IBlock Load(string state, BlockConfigData config, int entityId, BlockPositionInfo blockPositionInfo)
-        {
-            return CreateGear(config, entityId, blockPositionInfo);
-        }
-        
-        private IBlock CreateGear(BlockConfigData config, int entityId, BlockPositionInfo blockPositionInfo)
+        private IBlock CreateGear(BlockConfigData config, EntityID entityId, BlockPositionInfo blockPositionInfo)
         {
             var configParam = config.Param as SimpleGearGeneratorParam;
-            var connectSetting = configParam.GearConnectSettings;
+            List<ConnectSettings> connectSetting = configParam.GearConnectSettings;
             
             var blockComponent = new BlockConnectorComponent<IGearEnergyTransformer>(connectSetting, connectSetting, blockPositionInfo);
             var gearComponent = new SimpleGearGeneratorComponent(configParam, entityId, blockComponent);
@@ -33,7 +32,7 @@ namespace Game.Block.Factory.BlockTemplate
             var components = new List<IBlockComponent>
             {
                 gearComponent,
-                blockComponent
+                blockComponent,
             };
             
             return new BlockSystem(entityId, config.BlockId, components, blockPositionInfo);
