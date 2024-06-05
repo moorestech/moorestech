427b8d1b9694f2e430056f61d88d6bbbb4036aa7
Juha <kukiyomenaisapp@gmail.com>
2024-06-05 13:43:35 +0900

    Update IBlockInventoryOpenStateDataStore.cs

--- moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/IBlockInventoryOpenStateDataStore.cs
+++ moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/IBlockInventoryOpenStateDataStore.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Generic;
+using Game.Block.Interface;
 using UnityEngine;
 
 namespace Game.PlayerInventory.Interface
 {
     public interface IBlockInventoryOpenStateDataStore
     {
-        public List<int> GetBlockInventoryOpenPlayers(int blockEntityId);
+        public List<int> GetBlockInventoryOpenPlayers(EntityID blockEntityId);
         public void Open(int playerId, Vector3Int pos);
         public void Close(int playerId);
     }
