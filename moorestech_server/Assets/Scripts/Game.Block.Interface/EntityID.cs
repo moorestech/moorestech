f19367f23447f3f00beb69d2fa226bac29249f17
Juha <kukiyomenaisapp@gmail.com>
2024-06-05 13:48:34 +0900

    CreateBlockEntityIdをEntityId内に移動

--- moorestech_server/Assets/Scripts/Game.Block.Interface/EntityID.cs
+++ moorestech_server/Assets/Scripts/Game.Block.Interface/EntityID.cs
@@ -1,3 +1,4 @@
+using System;
 using UnitGenerator;
 
 namespace Game.Block.Interface
@@ -5,5 +6,11 @@ namespace Game.Block.Interface
     [UnitOf(typeof(int))]
     public readonly partial struct EntityID
     {
+        private static readonly Random Random = new(130851);
+        
+        public static EntityID Create()
+        {
+            return new EntityID(Random.Next(int.MinValue, int.MaxValue));
+        }
     }
 }
