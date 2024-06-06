166bdb25cc39f2d903434a1344e76b14e7e30d7a
sakastudio <sakastudio100@gmail.com>
2024-06-07 01:10:07 +0900

    テストコード

--- moorestech_server/Assets/Scripts/Tests/UnitTest/Core/Other/ItemStackMetaTest.cs
+++ moorestech_server/Assets/Scripts/Tests/UnitTest/Core/Other/ItemStackMetaTest.cs
@@ -1,11 +1,33 @@
+using Game.Context;
+using NUnit.Framework;
+using Server.Boot;
+using Tests.Module.TestMod;
+
 namespace Tests.UnitTest.Core.Other
 {
     public class ItemStackMetaTest
     {
+        [Test]
         // メタデータの同一性の評価
-        // Addできる、できないの評価
+        public void MetaDataEqualityTest()
+        {
+            var (_, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+            var itemsStackFactory = ServerContext.ItemStackFactory;
+            
+            var meta = new 
+        } 
         
+        [Test]
+        // Addできる、できないの評価
+        public void AddTest()
+        {
+        }
+        
+        [Test]
         // セーブ、ロードの評価
+        public void SaveLoadTest()
+        {
+        }
         
         //ベルトコンベアのメタ設定
     }
