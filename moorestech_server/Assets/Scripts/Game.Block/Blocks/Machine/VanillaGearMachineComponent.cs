89afe39bf9704731c6dc81893897df2bc9a7f213
sakastudio <sakastudio100@gmail.com>
2024-06-09 12:41:13 +0900

    VanillaGearMachineComponent実装

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/Machine/VanillaGearMachineComponent.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/Machine/VanillaGearMachineComponent.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using Game.Block.Blocks.Gear;
 using Game.Block.Config.LoadConfig.Param;
+using Game.Block.Interface;
 using Game.Block.Interface.Component;
 using Game.Gear.Common;
 using UnityEngine;
@@ -9,29 +12,63 @@ namespace Game.Block.Blocks.Machine
     /// <summary>
     ///     歯車機械を表すクラス
     /// </summary>
-    public class VanillaGearMachineComponent : GearComponent
+    public class VanillaGearMachineComponent : IGear
     {
-        public override float RequiredPower => _vanillaMachineProcessorComponent.CurrentState is ProcessState.Processing ? _configParam.RequiredPower : 0;
+        public int TeethCount => _configParam.TeethCount;
+        public BlockInstanceId BlockInstanceId { get; }
+        public float CurrentRpm => _simpleGearService.CurrentRpm;
+        public float CurrentTorque => _simpleGearService.CurrentTorque;
+        public bool IsCurrentClockwise => _simpleGearService.IsCurrentClockwise;
+        public bool IsRocked => _simpleGearService.IsRocked;
         
+        public IReadOnlyList<GearConnect> Connects =>
+            _connectorComponent.ConnectTargets.Select(
+                target => new GearConnect(target.Key, (GearConnectOption)target.Value.selfOption, (GearConnectOption)target.Value.targetOption)
+            ).ToArray();
+        
+        private readonly SimpleGearService _simpleGearService;
         private readonly VanillaMachineProcessorComponent _vanillaMachineProcessorComponent;
+        private readonly IBlockConnectorComponent<IGearEnergyTransformer> _connectorComponent;
         private readonly GearMachineConfigParam _configParam;
-        
-        public VanillaGearMachineComponent(GearMachineConfigParam configParam, int entityId, IBlockConnectorComponent<IGearEnergyTransformer> connectorComponent, VanillaMachineProcessorComponent processor) : 
-            base(configParam.TeethCount, configParam.RequiredPower, entityId, connectorComponent)
+        public VanillaGearMachineComponent(GearMachineConfigParam configParam, VanillaMachineProcessorComponent vanillaMachineProcessorComponent, IBlockConnectorComponent<IGearEnergyTransformer> connectorComponent, BlockInstanceId blockInstanceId)
         {
+            BlockInstanceId = blockInstanceId;
             _configParam = configParam;
-            _vanillaMachineProcessorComponent = processor;
+            _vanillaMachineProcessorComponent = vanillaMachineProcessorComponent;
+            _connectorComponent = connectorComponent;
+            _simpleGearService = new SimpleGearService();
         }
         
-        public override void SupplyPower(float rpm, float torque, bool isClockwise)
+        public float GetRequiredTorque(float rpm, bool isClockwise)
         {
-            base.SupplyPower(rpm, torque, isClockwise);
+            BlockException.CheckDestroy(this);
+            return _vanillaMachineProcessorComponent.CurrentState is ProcessState.Processing ? 
+                _configParam.RequiredTorque : 0;
+        }
+        
+        public void Rocked()
+        {
+            BlockException.CheckDestroy(this);
+            _simpleGearService.Rocked();
+            _vanillaMachineProcessorComponent.SupplyPower(0);
+        }
+        
+        public void SupplyPower(float rpm, float torque, bool isClockwise)
+        {
+            BlockException.CheckDestroy(this);
+            _simpleGearService.SupplyPower(rpm, torque, isClockwise);
             
             var rpmRate = Mathf.Min(rpm / _configParam.RequiredRpm, 1);
             var torqueRate = Mathf.Min(torque / _configParam.RequiredTorque, 1);
             var powerRate = rpmRate * torqueRate;
-            
             _vanillaMachineProcessorComponent.SupplyPower((int)(_configParam.RequiredPower * powerRate));
+        }
+        
+        public bool IsDestroy { get; private set; }
+        public void Destroy()
+        {
+            BlockException.CheckDestroy(this);
+            IsDestroy = true;
         }
     }
 }
