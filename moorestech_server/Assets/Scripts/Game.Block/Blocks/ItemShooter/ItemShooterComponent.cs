36ca341bd8a42798b76b0cb1a026af64d9e9380c
sakastudio <sakastudio100@gmail.com>
2024-06-10 22:24:09 +0900

    ItemShooterComponentの実装

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/ItemShooter/ItemShooterComponent.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/ItemShooter/ItemShooterComponent.cs
@@ -1,13 +1,108 @@
+using System.Collections.Generic;
+using System.Linq;
+using Core.Const;
+using Core.Item.Interface;
+using Core.Update;
+using Game.Block.Blocks.BeltConveyor;
+using Game.Block.Component;
+using Game.Block.Config.LoadConfig.Param;
+using Game.Block.Interface;
 using Game.Block.Interface.Component;
+using Game.Context;
+using UnityEngine;
 
 namespace Game.Block.Blocks.ItemShooter
 {
-    public class ItemShooterComponent : IBlockComponent
+    public class ItemShooterComponent : IItemCollectableBeltConveyor, IBlockInventory
     {
-        public bool IsDestroy { get; }
+        public IReadOnlyList<IOnBeltConveyorItem> BeltConveyorItems => _inventoryItems;
+        private ShooterInventoryItem[] _inventoryItems;
+        
+        private readonly BlockConnectorComponent<IBlockInventory> _blockConnectorComponent;
+        
+        private readonly BlockDirection _blockDirection;
+        private ItemShooterConfigParam _configParam;
+        
+        private void Update()
+        {
+            BlockException.CheckDestroy(this);
+            
+            var count = _inventoryItems.Length;
+            
+            for (var i = 0; i < count; i++)
+            {
+                var item = _inventoryItems[i];
+                if (item == null) continue;
+                
+                if (item.RemainingPercent <= 0)
+                {
+                    var insertItem = ServerContext.ItemStackFactory.Create(item.ItemId, 1, item.ItemInstanceId);
+                    
+                    if (_blockConnectorComponent.ConnectTargets.Count == 0) continue;
+                    
+                    var connector = _blockConnectorComponent.ConnectTargets.First();
+                    var target = connector.Key;
+                    if (target is ItemShooterComponent shooter)
+                    {
+                        _inventoryItems[i] = shooter.InsertItemFromShooter(item);
+                    }
+                    else
+                    {
+                        var output = connector.Key.InsertItem(insertItem);
+                        
+                        //渡した結果がnullItemだったらそのアイテムを消す
+                        if (output.Id == ItemConst.EmptyItemId) _inventoryItems[i] = null;
+                    }
+                    
+                    continue;
+                }
+                
+                //時間を減らす
+                var deltaTime = (float)GameUpdater.UpdateMillSecondTime; // floatとdobuleが混在しているの気持ち悪いから改善したい
+                item.RemainingPercent -= deltaTime * _configParam.ItemShootSpeed * item.CurrentSpeed;
+                item.RemainingPercent = Mathf.Clamp(item.RemainingPercent, 0, 1);
+                
+                // velocityを更新する
+                var acceleration = _configParam.GetAcceleration(_blockDirection);
+                item.CurrentSpeed += acceleration * deltaTime;
+                item.CurrentSpeed = Mathf.Clamp(item.CurrentSpeed, 0, float.MaxValue);
+            }
+        }
+        
+        private ShooterInventoryItem InsertItemFromShooter(ShooterInventoryItem inventoryItem)
+        {
+            for (var i = 0; i < _inventoryItems.Length; i++)
+            {
+                if (_inventoryItems[i] != null) continue;
+                
+                _inventoryItems[i] = inventoryItem;
+                return null;
+            }
+            
+            return inventoryItem;
+        }
+        
+        
+        public bool IsDestroy { get; private set; }
         public void Destroy()
         {
-            throw new System.NotImplementedException();
+            IsDestroy = true;
+        }
+    }
+    
+    public class ShooterInventoryItem : IOnBeltConveyorItem
+    {
+        public int ItemId { get; }
+        public ItemInstanceId ItemInstanceId { get; }
+        
+        public float RemainingPercent { get; set; }
+        
+        public float CurrentSpeed { get; set; }
+        
+        public ShooterInventoryItem(int itemId, ItemInstanceId itemInstanceId)
+        {
+            ItemId = itemId;
+            ItemInstanceId = itemInstanceId;
         }
     }
 }
---
+++ moorestech_server/Assets/Scripts/Game.Block/Config/LoadConfig/Param/ItemShooterConfigParam.cs
@@ -0,0 +1,55 @@
+﻿using Core.Item.Interface.Config;
+using Game.Block.Interface;
+using Game.Block.Interface.BlockConfig;
+
+namespace Game.Block.Config.LoadConfig.Param
+{
+    public class ItemShooterConfigParam : IBlockConfigParam
+    {
+        public readonly int InventoryItemNum;
+        public readonly float ItemShootSpeed;
+        
+        public readonly float DownAcceleration;
+        public readonly float UpDeceleration;
+        public readonly float HorizontalDeceleration;
+        
+        public ItemShooterConfigParam(dynamic blockParam)
+        {
+            InventoryItemNum = blockParam.inventoryItemNum;
+            ItemShootSpeed = blockParam.itemShootSpeed;
+            
+            DownAcceleration = blockParam.downAcceleration;
+            UpDeceleration = blockParam.upDeceleration;
+            HorizontalDeceleration = blockParam.horizontalDeceleration;
+        }
+        
+        public float GetAcceleration(BlockDirection blockDirection)
+        {
+            switch (blockDirection)
+            {
+                case BlockDirection.North:
+                case BlockDirection.East:
+                case BlockDirection.South:
+                case BlockDirection.West: // Minus because of deceleration
+                    return -HorizontalDeceleration; // 減速なのでマイナス
+                case BlockDirection.UpNorth:
+                case BlockDirection.UpEast:
+                case BlockDirection.UpSouth:
+                case BlockDirection.UpWest: // Minus because of deceleration
+                    return -UpDeceleration; // 減速なのでマイナス
+                case BlockDirection.DownNorth:
+                case BlockDirection.DownEast:
+                case BlockDirection.DownSouth:
+                case BlockDirection.DownWest:
+                    return DownAcceleration;
+                default:
+                    return HorizontalDeceleration;
+            }
+        }
+        
+        public static IBlockConfigParam Generate(dynamic blockParam, IItemConfig itemConfig)
+        {
+            return new ItemShooterConfigParam(blockParam);
+        }
+    }
+}
