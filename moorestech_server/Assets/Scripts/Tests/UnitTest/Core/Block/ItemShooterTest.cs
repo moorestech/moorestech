fb3be06c8a792caf9f55a0c0be273cef2a3a0bd9
sakastudio <sakastudio100@gmail.com>
2024-06-11 23:02:27 +0900

    テスト作成

--- moorestech_server/Assets/Scripts/Tests/UnitTest/Core/Block/ItemShooterTest.cs
+++ moorestech_server/Assets/Scripts/Tests/UnitTest/Core/Block/ItemShooterTest.cs
@@ -1,3 +1,7 @@
+using System;
+using Core.Const;
+using Core.Update;
+using Game.Block.Blocks.Chest;
 using Game.Block.Blocks.ItemShooter;
 using Game.Block.Interface;
 using Game.Block.Interface.Extension;
@@ -17,7 +21,7 @@ namespace Tests.UnitTest.Core.Block
         public void ShooterTest()
         {
             var (_, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
-            
+            var itemFactory = ServerContext.ItemStackFactory;
             
             // アイテムシューターのテストは、以下のように、一度下がり、再び上がるような構造になっている
             // ↓ チェスト　 
@@ -32,7 +36,7 @@ namespace Tests.UnitTest.Core.Block
             var horizonShooter3 = new Vector3Int(0, 0, 5);
             var horizonShooter4 = new Vector3Int(0, 0, 6);
             
-            var chest = AddBlock(ForUnitTestModBlockId.ChestId, chestPosition);
+            var chest = AddBlock(ForUnitTestModBlockId.ChestId, chestPosition).GetComponent<VanillaChestComponent>();
             var shooter1 = AddBlock(ForUnitTestModBlockId.ItemShooter, horizonShooter1).GetComponent<ItemShooterComponent>();
             var down = AddBlock(ForUnitTestModBlockId.ItemShooter, downShooter).GetComponent<ItemShooterComponent>();
             var shooter2 = AddBlock(ForUnitTestModBlockId.ItemShooter, horizonShooter2).GetComponent<ItemShooterComponent>();
@@ -40,16 +44,45 @@ namespace Tests.UnitTest.Core.Block
             var shooter3 = AddBlock(ForUnitTestModBlockId.ItemShooter, horizonShooter3).GetComponent<ItemShooterComponent>();
             var shooter4 = AddBlock(ForUnitTestModBlockId.ItemShooter, horizonShooter4).GetComponent<ItemShooterComponent>();
             
+            chest.InsertItem(itemFactory.Create(1,1));
             
+            GameUpdater.Update();
             
-            // 上方向は速度が低下し、ゼロになると止まる
+            // デフォルトでインサートされる速度の検証
+            var shooterItem1 = GetShooterItem(shooter1);
+            Assert.AreEqual(1, shooterItem1.ItemId);
+            Assert.AreEqual(1, shooterItem1.CurrentSpeed);
+            Assert.AreEqual(0, shooterItem1.RemainingPercent);
             
+            WaitInsertItem(down);
             
-            // 下方向に加速する
-            // 平行だと速度が低下する
-            
-            // デフォルトでインサートされる速度
-            
+            Assert.Fail();
+        }
+        
+        private void WaitInsertItem(ItemShooterComponent waitTarget)
+        {
+            var currentTime = DateTime.Now;
+            while (true)
+            {
+                var item = waitTarget.GetItem(0);
+                if (item.Id != ItemConst.EmptyItemId)
+                {
+                    break;
+                }
+                GameUpdater.Update();
+                
+                // 5秒経過したら失敗
+                if ((DateTime.Now - currentTime).TotalSeconds > 5)
+                {
+                    Assert.Fail();
+                }
+            }
+        }
+        
+        private ShooterInventoryItem GetShooterItem(ItemShooterComponent target)
+        {
+            var item = target.BeltConveyorItems[0];
+            return item as ShooterInventoryItem;
         }
         
         private IBlock AddBlock(int blockId, Vector3Int position)
@@ -57,7 +90,7 @@ namespace Tests.UnitTest.Core.Block
             var blockFactory = ServerContext.BlockFactory;
             var world = ServerContext.WorldBlockDatastore;
             
-            var block = blockFactory.Create(blockId, new BlockInstanceId(10), new BlockPositionInfo(position, BlockDirection.North, Vector3Int.one));
+            var block = blockFactory.Create(blockId, BlockInstanceId.Create(), new BlockPositionInfo(position, BlockDirection.North, Vector3Int.one));
             world.AddBlock(block);
             
             return block;
