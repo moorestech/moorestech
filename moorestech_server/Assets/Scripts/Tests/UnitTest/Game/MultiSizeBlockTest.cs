3447b30cd4b38f3921272b2b3ec9c9ddbaf32c2c
Juha <kukiyomenaisapp@gmail.com>
2024-06-28 11:00:36 +0900

    MultiSizeBlockTestのエラーを修正

--- moorestech_server/Assets/Scripts/Tests/UnitTest/Game/MultiSizeBlockTest.cs
+++ moorestech_server/Assets/Scripts/Tests/UnitTest/Game/MultiSizeBlockTest.cs
@@ -27,33 +27,33 @@ namespace Tests.UnitTest.Game
             worldDatastore = ServerContext.WorldBlockDatastore;
             
             //平面設置の検証
-            PlaceBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North);
+            worldDatastore.TryAddBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North, out _);
             RetrieveBlock(Block_1x4_Id, new Vector3Int(10, 0, 10));
             RetrieveBlock(Block_1x4_Id, new Vector3Int(10, 0, 13));
             RetrieveNonExistentBlock(new Vector3Int(9, 0, 10));
             RetrieveNonExistentBlock(new Vector3Int(10, 0, 15));
             
-            PlaceBlock(Block_1x4_Id, new Vector3Int(10, 0, -10), BlockDirection.East);
+            worldDatastore.TryAddBlock(Block_1x4_Id, new Vector3Int(10, 0, -10), BlockDirection.East, out _);
             RetrieveBlock(Block_1x4_Id, new Vector3Int(10, 0, -10));
             RetrieveBlock(Block_1x4_Id, new Vector3Int(13, 0, -10));
             RetrieveNonExistentBlock(new Vector3Int(9, 0, -10));
             RetrieveNonExistentBlock(new Vector3Int(15, 0, -10));
             
-            PlaceBlock(Block_3x2_Id, new Vector3Int(-10, 0, -10), BlockDirection.South);
+            worldDatastore.TryAddBlock(Block_3x2_Id, new Vector3Int(-10, 0, -10), BlockDirection.South, out _);
             RetrieveBlock(Block_3x2_Id, new Vector3Int(-10, 0, -10));
             RetrieveBlock(Block_3x2_Id, new Vector3Int(-8, 0, -9));
             RetrieveNonExistentBlock(new Vector3Int(-10, 0, -11));
             RetrieveNonExistentBlock(new Vector3Int(-7, 0, -9));
             
             //立体設置の検証
-            PlaceBlock(Block_1x2x3_Id, new Vector3Int(20, 0, 20), BlockDirection.North);
+            worldDatastore.TryAddBlock(Block_1x2x3_Id, new Vector3Int(20, 0, 20), BlockDirection.North, out _);
             RetrieveBlock(Block_1x2x3_Id, new Vector3Int(20, 0, 20));
             RetrieveBlock(Block_1x2x3_Id, new Vector3Int(20, 1, 22));
             RetrieveNonExistentBlock(new Vector3Int(21, 1, 22));
             RetrieveNonExistentBlock(new Vector3Int(20, 2, 22));
             RetrieveNonExistentBlock(new Vector3Int(20, 1, 23));
             
-            PlaceBlock(Block_1x2x3_Id, new Vector3Int(40, 0, 40), BlockDirection.UpEast);
+            worldDatastore.TryAddBlock(Block_1x2x3_Id, new Vector3Int(40, 0, 40), BlockDirection.UpEast, out _);
             RetrieveBlock(Block_1x2x3_Id, new Vector3Int(40, 0, 40));
             RetrieveBlock(Block_1x2x3_Id, new Vector3Int(41, 2, 40));
             RetrieveNonExistentBlock(new Vector3Int(42, 2, 40));
@@ -71,8 +71,8 @@ namespace Tests.UnitTest.Game
             _blockFactory = ServerContext.BlockFactory;
             worldDatastore = ServerContext.WorldBlockDatastore;
             
-            PlaceBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North);
-            PlaceBlock(Block_3x2_Id, new Vector3Int(10, 0, 12), BlockDirection.South);
+            worldDatastore.TryAddBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North, out _);
+            worldDatastore.TryAddBlock(Block_3x2_Id, new Vector3Int(10, 0, 12), BlockDirection.South, out _);
             
             //3x2が設置されてないことをチェックする
             RetrieveBlock(Block_3x2_Id, new Vector3Int(11, 0, 12));
@@ -87,23 +87,11 @@ namespace Tests.UnitTest.Game
             _blockFactory = ServerContext.BlockFactory;
             worldDatastore = ServerContext.WorldBlockDatastore;
             
-            
-            PlaceBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North);
-            PlaceBlock(Block_1x4_Id, new Vector3Int(11, 0, 11), BlockDirection.East);
+            worldDatastore.TryAddBlock(Block_1x4_Id, new Vector3Int(10, 0, 10), BlockDirection.North, out _);
+            worldDatastore.TryAddBlock(Block_1x4_Id, new Vector3Int(11, 0, 11), BlockDirection.East, out _);
             
             //1x4が設置されてないことをチェックする
             RetrieveBlock(Block_1x4_Id, new Vector3Int(11, 0, 11));
-        }
-        
-        /// <summary>
-        ///     ブロックを設置する
-        /// </summary>
-        private void PlaceBlock(int blockId, Vector3Int position, BlockDirection direction)
-        {
-            var posInfo = new BlockPositionInfo(position, direction, Vector3Int.one);
-            var block = _blockFactory.Create(blockId, BlockInstanceId.Create(), posInfo);
-            
-            worldDatastore.TryAddBlock(block);
         }
         
         /// <summary>
