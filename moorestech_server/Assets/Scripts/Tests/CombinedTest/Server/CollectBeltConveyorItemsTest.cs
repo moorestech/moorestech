db681080a9dc5c1408ab0c2a68c9365c92214f40
Juha <kukiyomenaisapp@gmail.com>
2024-07-10 14:54:08 +0900

    テストが終わるまでに17分くらいかかってしまう問題を修正

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/CollectBeltConveyorItemsTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/CollectBeltConveyorItemsTest.cs
@@ -21,7 +21,7 @@ namespace Tests.CombinedTest.Server
     public class CollectBeltConveyorItemsTest
     {
         //4秒で入って出るベルトコンベアで残り1秒で出る時の座標が正しいかどうかをテストする
-        private const int RemainingTime = 500;
+        private const double RemainingTime = 0.5;
         private const int ItemInstanceId = 100;
         
         private readonly List<Vector2Int> _plusPlayerCoordinate = new() { new Vector2Int(0, 0) };
