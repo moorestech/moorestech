4b0fa646bb7a619ef5c549dbf741346b99095efe
Juha <kukiyomenaisapp@gmail.com>
2024-07-19 10:03:02 +0900

    一旦EntryPointをAdditionalFilesのみに絞った

--- mooresmaster.Generator/SampleIncrementalSourceGenerator.cs
+++ mooresmaster.Generator/SampleIncrementalSourceGenerator.cs
@@ -9,26 +9,18 @@ public class SampleIncrementalSourceGenerator : IIncrementalGenerator
 {
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        Console.WriteLine("hoge");
-        context.RegisterPostInitializationOutput(context => { context.AddSource("postinit.g.cs", "// Generate from postinit.g.cs"); });
-        context.RegisterSourceOutput(
-            context.SyntaxProvider.ForAttributeWithMetadataName(
-                "global::mooresmaster.MoorestechJsonSchemaAttribute",
-                static (_, _) => true,
-                (productionContext, token) => productionContext
-            ),
-            (productionContext, syntaxContext) => Emit(productionContext)
-        );
-//        context.RegisterSourceOutput(context.AdditionalTextsProvider, Emit);
+        context.RegisterSourceOutput(context.AdditionalTextsProvider, Emit);
     }
     
     private void Emit(SourceProductionContext context)
     {
+        Console.WriteLine("Emit");
         context.AddSource("test.g.cs", "// Generate from test.g.cs");
     }
     
     private void Emit(SourceProductionContext context, AdditionalText text)
     {
-        context.AddSource(Path.GetFileNameWithoutExtension(text.Path), $"// Generate from {Path.GetFileName(text.Path)}");
+        Console.WriteLine("AdditionalTextEmit");
+        context.AddSource($"{Path.GetFileNameWithoutExtension(text.Path)}.g.cs", $"// Generate from {Path.GetFileName(text.Path)}");
     }
 }
