95081a49422d68f0494d51c7006fba17f12cb207
Juha <kukiyomenaisapp@gmail.com>
2024-07-24 16:03:09 +0900

    JsonSchemaParserをファイル分割

--- mooresmaster.Generator/JsonSchema/JsonSchemaParser.cs
+++ mooresmaster.Generator/JsonSchema/JsonSchemaParser.cs
@@ -5,49 +5,6 @@ using mooresmaster.Generator.Json;
 
 namespace mooresmaster.Generator.JsonSchema;
 
-public interface ISchema;
-
-public record Schema(string Id, ISchema InnerSchema)
-{
-    public string Id = Id;
-    public ISchema InnerSchema = InnerSchema;
-}
-
-public record ObjectSchema(Dictionary<string, ISchema> Properties, string[] Required) : ISchema
-{
-    public Dictionary<string, ISchema> Properties = Properties;
-    public string[] Required = Required;
-}
-
-public record ArraySchema(ISchema Items) : ISchema
-{
-    public ISchema Items = Items;
-}
-
-public record OneOfSchema(IfThenSchema[] IfThenArray) : ISchema
-{
-    public IfThenSchema[] IfThenArray = IfThenArray;
-}
-
-public record IfThenSchema(JsonObject If, ISchema Then)
-{
-    public JsonObject If = If;
-    public ISchema Then = Then;
-}
-
-public record StringSchema : ISchema;
-
-public record NumberSchema : ISchema;
-
-public record IntegerSchema : ISchema;
-
-public record BooleanSchema : ISchema;
-
-public record RefSchema(string Ref) : ISchema
-{
-    public string Ref = Ref;
-}
-
 public static class JsonSchemaParser
 {
     public static Schema ParseSchema(JsonObject root)
