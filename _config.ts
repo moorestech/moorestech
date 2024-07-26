01f431f139c3792efb2cb6b62832df44cbfae173
sakastudio <katsumi.sato@aktsk.jp>
2024-07-26 12:40:04 +0900

    block jsonを本スキーマに移動

--- _config.ts
+++ _config.ts
@@ -1,12 +1,16 @@
 import block from './block.json';
 import item from './item.json';
-import blockRefTest from './blockRefTest.json';
+import gearConnects from './gearConnects.json';
+import inputConnects from './inputConnects.json';
+import modelTransform from './modelTransform.json';
 import { RefResolver } from 'json-schema-ref-resolver'
 import Ajv from 'ajv';
 
 const refResolver = new RefResolver()
 refResolver.addSchema(block)
-refResolver.addSchema(blockRefTest)
+refResolver.addSchema(gearConnects)
+refResolver.addSchema(inputConnects)
+refResolver.addSchema(modelTransform)
 refResolver.addSchema(item)
 
 const ajv = new Ajv({ allErrors: true })
