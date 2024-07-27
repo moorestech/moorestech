2fb03f7c6cd46db53d58bc17c5f0b5fbd32bfff0
sakastudio <katsumi.sato@aktsk.jp>
2024-07-27 23:46:58 +0900

    チャレンジのスキーマ追加

--- _config.ts
+++ _config.ts
@@ -1,12 +1,14 @@
-import block from './block.json';
-import item from './item.json';
-import gearConnects from './gearConnects.json';
-import inputConnects from './inputConnects.json';
-import modelTransform from './modelTransform.json';
+import block from './blocks.json';
+import item from './items.json';
+import challenge from './challenges.json';
+import gearConnects from './ref/gearConnects.json';
+import inputConnects from './ref/inputConnects.json';
+import modelTransform from './ref/modelTransform.json';
 import { RefResolver } from 'json-schema-ref-resolver'
 import Ajv from 'ajv';
 
 const refResolver = new RefResolver()
+refResolver.addSchema(challenge)
 refResolver.addSchema(block)
 refResolver.addSchema(gearConnects)
 refResolver.addSchema(inputConnects)
@@ -16,6 +18,7 @@ refResolver.addSchema(item)
 const ajv = new Ajv({ allErrors: true })
 ajv.addSchema(refResolver.getDerefSchema('blocks'), '/block')
 ajv.addSchema(refResolver.getDerefSchema('items'), '/item')
+ajv.addSchema(refResolver.getDerefSchema('challenges'), '/challenge')
 
 export default {
   validator: ajv,
@@ -27,6 +30,10 @@ export default {
     item: {
       name: 'Item',
       schema: refResolver.getDerefSchema('items')
+    },
+    challenge: {
+      name: 'Challenge',
+      schema: refResolver.getDerefSchema('challenges')
     }
   }
 }
