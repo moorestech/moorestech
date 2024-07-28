ab909fd172772d2832724355318ae90b2881c5a1
sakastudio <katsumi.sato@aktsk.jp>
2024-07-28 11:42:26 +0900

    細々修正

--- _config.ts
+++ _config.ts
@@ -23,39 +23,39 @@ refResolver.addSchema(inputConnects)
 refResolver.addSchema(modelTransform)
 
 const ajv = new Ajv({ allErrors: true })
-ajv.addSchema(refResolver.getDerefSchema('blocks'), '/block')
-ajv.addSchema(refResolver.getDerefSchema('items'), '/item')
+ajv.addSchema(refResolver.getDerefSchema('blocks'), '/blocks')
+ajv.addSchema(refResolver.getDerefSchema('items'), '/items')
 ajv.addSchema(refResolver.getDerefSchema('craftRecipes'), '/craftRecipes')
-ajv.addSchema(refResolver.getDerefSchema('challenges'), '/challenge')
+ajv.addSchema(refResolver.getDerefSchema('challenges'), '/challenges')
 ajv.addSchema(refResolver.getDerefSchema('mapObjects'), '/mapObjects')
 ajv.addSchema(refResolver.getDerefSchema('machineRecipes'), '/machineRecipes')
 
 export default {
   validator: ajv,
   schemas: {
-    block: {
-      name: 'Block',
-      schema: refResolver.getDerefSchema('blocks')
-    },
     item: {
-      name: 'Item',
+      name: 'Items',
       schema: refResolver.getDerefSchema('items')
     },
-    craftRecipes: {
-      name: 'CraftRecipe',
-      schema: refResolver.getDerefSchema('craftRecipes')
-    },
-    machineRecipe: {
-      name: 'MachineRecipe',
-      schema: refResolver.getDerefSchema('machineRecipes')
-    },
-    challenge: {
-      name: 'Challenge',
-      schema: refResolver.getDerefSchema('challenges')
+    block: {
+      name: 'Blocks',
+      schema: refResolver.getDerefSchema('blocks')
     },
     mapObject: {
       name: 'MapObjects',
       schema: refResolver.getDerefSchema('mapObjects')
-    }
+    },
+    craftRecipes: {
+      name: 'CraftRecipes',
+      schema: refResolver.getDerefSchema('craftRecipes')
+    },
+    machineRecipe: {
+      name: 'MachineRecipes',
+      schema: refResolver.getDerefSchema('machineRecipes')
+    },
+    challenge: {
+      name: 'Challenges',
+      schema: refResolver.getDerefSchema('challenges')
+    },
   }
 }
