4cf256302da58655fb16cdfb2a5be4fc9ff74fe6
sakastudio <katsumi.sato@aktsk.jp>
2024-07-28 11:10:43 +0900

    mapobject修正

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import block from './blocks.json';
 import item from './items.json';
 import challenge from './challenges.json';
 import craftRecipes from './craftRecipes.json';
+import mapObjects from './mapObjects.json';
 import gearConnects from './ref/gearConnects.json';
 import inputConnects from './ref/inputConnects.json';
 import modelTransform from './ref/modelTransform.json';
@@ -9,19 +10,22 @@ import { RefResolver } from 'json-schema-ref-resolver'
 import Ajv from 'ajv';
 
 const refResolver = new RefResolver()
-refResolver.addSchema(challenge)
+refResolver.addSchema(item)
 refResolver.addSchema(block)
+refResolver.addSchema(challenge)
+refResolver.addSchema(craftRecipes)
+refResolver.addSchema(mapObjects)
+
 refResolver.addSchema(gearConnects)
 refResolver.addSchema(inputConnects)
 refResolver.addSchema(modelTransform)
-refResolver.addSchema(item)
-refResolver.addSchema(craftRecipes)
 
 const ajv = new Ajv({ allErrors: true })
 ajv.addSchema(refResolver.getDerefSchema('blocks'), '/block')
 ajv.addSchema(refResolver.getDerefSchema('items'), '/item')
 ajv.addSchema(refResolver.getDerefSchema('craftRecipes'), '/craftRecipes')
 ajv.addSchema(refResolver.getDerefSchema('challenges'), '/challenge')
+ajv.addSchema(refResolver.getDerefSchema('mapObjects'), '/mapObjects')
 
 export default {
   validator: ajv,
@@ -41,6 +45,10 @@ export default {
     challenge: {
       name: 'Challenge',
       schema: refResolver.getDerefSchema('challenges')
+    },
+    mapObject: {
+      name: 'MapObjects',
+      schema: refResolver.getDerefSchema('mapObjects')
     }
   }
 }
