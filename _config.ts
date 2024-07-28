ca7343f1d00de0209fe26595bb3db2baef3b5590
sakastudio <sakastudio100@gmail.com>
2024-07-28 09:32:51 +0900

    クラフトレシピ追加

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import item from './item.json';
 import gearConnects from './gearConnects.json';
 import inputConnects from './inputConnects.json';
 import modelTransform from './modelTransform.json';
+import craftRecipes from './craftRecipes.json';
 import { RefResolver } from 'json-schema-ref-resolver'
 import Ajv from 'ajv';
 
@@ -12,10 +13,12 @@ refResolver.addSchema(gearConnects)
 refResolver.addSchema(inputConnects)
 refResolver.addSchema(modelTransform)
 refResolver.addSchema(item)
+refResolver.addSchema(craftRecipes)
 
 const ajv = new Ajv({ allErrors: true })
 ajv.addSchema(refResolver.getDerefSchema('blocks'), '/block')
 ajv.addSchema(refResolver.getDerefSchema('items'), '/item')
+ajv.addSchema(refResolver.getDerefSchema('craftRecipes'), '/craftRecipes')
 
 export default {
   validator: ajv,
@@ -27,6 +30,10 @@ export default {
     item: {
       name: 'Item',
       schema: refResolver.getDerefSchema('items')
+    },
+    craftRecipes: {
+      name: 'CraftRecipe',
+      schema: refResolver.getDerefSchema('craftRecipes')
     }
   }
 }
