1be3d41cd1da644b782b25527abab24494304657
Juha <kukiyomenaisapp@gmail.com>
2024-08-04 15:41:08 +0900

    testmodのjsonファイルをすべて読み込むコードの作成

--- mooresmaster.SandBox/Program.cs
+++ mooresmaster.SandBox/Program.cs
@@ -5,5 +5,11 @@ internal static class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        var testModJsonsDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod");
+        var jsonPaths = Directory.GetFiles(testModJsonsDirectoryPath);
+        var jsons = jsonPaths.Select(File.ReadAllText);
+
+        foreach (var json in jsons) Console.WriteLine(json);
     }
 }
