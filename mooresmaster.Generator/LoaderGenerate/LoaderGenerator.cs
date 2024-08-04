cd41b4933f88cae83a56e819dfaf38b61f6149a5
Juha <kukiyomenaisapp@gmail.com>
2024-08-04 15:54:30 +0900

    LoaderGenerator

---
+++ mooresmaster.Generator/LoaderGenerate/LoaderGenerator.cs
@@ -0,0 +1,12 @@
+namespace mooresmaster.Generator.LoaderGenerate;
+
+public record LoaderFile(string FileName, string Code)
+{
+    public string Code = Code;
+    public string FileName = FileName;
+}
+
+public static class LoaderGenerator
+{
+
+}
