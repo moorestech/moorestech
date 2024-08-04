5ba08aab49485380c3284eadabc3acd82a1b785c
Juha <kukiyomenaisapp@gmail.com>
2024-08-04 16:47:32 +0900

    loaderのファイルだけ生成できるように

--- mooresmaster.Generator/LoaderGenerate/LoaderGenerator.cs
+++ mooresmaster.Generator/LoaderGenerate/LoaderGenerator.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using mooresmaster.Generator.Definitions;
+
 namespace mooresmaster.Generator.LoaderGenerate;
 
 public record LoaderFile(string FileName, string Code)
@@ -8,5 +11,34 @@ public record LoaderFile(string FileName, string Code)
 
 public static class LoaderGenerator
 {
+    public static LoaderFile[] Generate(Definition definition)
+    {
+        return definition
+            .TypeDefinitions
+            .Select(GenerateTypeLoaderCode)
+            .Select(value => new LoaderFile(value.fileName, value.code))
+            .ToArray();
+    }
 
+    private static (string fileName, string code) GenerateTypeLoaderCode(TypeDefinition typeDefinition)
+    {
+        return (
+            $"mooresmaster.loader.{typeDefinition.TypeName.ModuleName}.{typeDefinition.TypeName.Name}.g.cs",
+            $$$"""
+               namespace Mooresmaster.Loader.{{{typeDefinition.TypeName.ModuleName}}}
+               {
+                   public static class {{{typeDefinition.TypeName.Name}}}Loader
+                   {
+                       
+                   }
+               }
+               """
+        );
+    }
+
+    private static string Indent(this string code, bool firstLine = false, int level = 1)
+    {
+        var indent = new string(' ', 4 * level);
+        return firstLine ? indent : "" + code.Replace("\n", $"\n{indent}");
+    }
 }
