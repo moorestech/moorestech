f6023fc79dfa99c3e1b553ef1f2b5a06db52479e
Juha <kukiyomenaisapp@gmail.com>
2024-08-13 17:04:52 +0900

    GlobalLoader

--- mooresmaster.SandBox/Program.cs
+++ mooresmaster.SandBox/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Mooresmaster.Loader;
+using Mooresmaster.Loader.BlocksModule;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace mooresmaster.SandBox;
@@ -9,27 +11,31 @@ internal static class Program
     {
         Console.WriteLine("Hello, World!");
 
-        var testModJsonsDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod");
-        var jsonPaths = Directory.GetFiles(testModJsonsDirectoryPath);
-        var jsons = jsonPaths.Select(path => (path, File.ReadAllText(path)));
+        var blockJsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod", "blocks.json");
+        var blockJson = File.ReadAllText(blockJsonPath);
 
-        foreach (var (path, json) in jsons)
-        {
-            // Console.WriteLine(jsonFile);
-            var jsonData = (JObject)JsonConvert.DeserializeObject(json);
+        var json = (JToken)JsonConvert.DeserializeObject(blockJson);
+        var blocks = GlobalLoader.Load(json);
+        Console.WriteLine(blocks);
 
-            try
-            {
-                var value0 = jsonData["data"];
-                var value1 = value0[0].ToString();
-                Console.WriteLine(value1);
-            }
-            catch (Exception e)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"throw from {path}\n{e}");
-                Console.ResetColor();
-            }
-        }
+        // var testModJsonsDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod");
+        // var jsonPaths = Directory.GetFiles(testModJsonsDirectoryPath);
+        // var jsons = jsonPaths.Select(path => (path, File.ReadAllText(path)));
+
+        // foreach (var (path, json) in jsons)
+        // {
+        //     // Console.WriteLine(jsonFile);
+        //     var jsonData = (JObject)JsonConvert.DeserializeObject(json);
+        //
+        //     try
+        //     {
+        //     }
+        //     catch (Exception e)
+        //     {
+        //         Console.ForegroundColor = ConsoleColor.Red;
+        //         Console.WriteLine($"throw from {path}\n{e}");
+        //         Console.ResetColor();
+        //     }
+        // }
     }
 }
