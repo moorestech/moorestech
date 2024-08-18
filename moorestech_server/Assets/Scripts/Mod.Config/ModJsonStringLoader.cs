282c3cd9589b5ed2f8c138b4d9955a5bfc80d310
sakastudio <katsumi.sato@aktsk.jp>
2024-08-18 12:36:59 +0900

    jsonのloaderの修正

--- moorestech_server/Assets/Scripts/Mod.Config/ModJsonStringLoader.cs
+++ moorestech_server/Assets/Scripts/Mod.Config/ModJsonStringLoader.cs
@@ -1,48 +1,34 @@
 using System.Collections.Generic;
 using System.IO;
-using Core.ConfigJson;
+using Core.Master;
 using Mod.Loader;
 
 namespace Mod.Config
 {
     public class ModJsonStringLoader
     {
-        //TODO こういうコンフィグ類を、パスを追加するだけでいい感じに管理できるようにしたい
-        private const string ItemConfigPath = "config/item.json";
-        private const string BlockConfigPath = "config/block.json";
-        private const string MachineRecipeConfigPath = "config/machineRecipe.json";
-        private const string CraftRecipeConfigPath = "config/craftRecipe.json";
-        private const string MapObjectConfigPath = "config/mapObject.json";
-        private const string ChallengeConfigPath = "config/challenge.json";
-        
-        public static Dictionary<string, ConfigJson> GetConfigString(ModsResource modResource)
+        public static List<ConfigJson> GetConfigString(ModsResource modResource)
         {
-            var configDict = new Dictionary<string, ConfigJson>();
+            var configs = new List<ConfigJson>();
             
-            //zipファイルの中身のjsonファイルを読み込む
+            //展開済みzipファイルの中身のjsonファイルを読み込む
             foreach (var mod in modResource.Mods)
             {
-                var modId = mod.Value.ModMetaJson.ModId;
+                var modId = new ModId(mod.Value.ModMetaJson.ModId);
                 var extractedPath = mod.Value.ExtractedPath;
                 
-                var itemConfigJson = LoadConfigFile(extractedPath, ItemConfigPath);
-                var blockConfigJson = LoadConfigFile(extractedPath, BlockConfigPath);
-                var machineRecipeConfigJson = LoadConfigFile(extractedPath, MachineRecipeConfigPath);
-                var craftRecipeConfigJson = LoadConfigFile(extractedPath, CraftRecipeConfigPath);
-                var mapObjectConfigJson = LoadConfigFile(extractedPath, MapObjectConfigPath);
-                var challengeConfigJson = LoadConfigFile(extractedPath, ChallengeConfigPath);
-                
-                configDict.Add(modId, new ConfigJson(
-                    modId,
-                    itemConfigJson,
-                    blockConfigJson,
-                    machineRecipeConfigJson,
-                    craftRecipeConfigJson,
-                    mapObjectConfigJson,
-                    challengeConfigJson));
+                // master/ 以下のjsonファイルをすべて取得する
+                var masterJsonContents = new Dictionary<JsonFileName, string>();
+                foreach (var masterJsonPath in Directory.GetFiles(extractedPath, "master/*.json"))
+                {
+                    var fileName = new JsonFileName(Path.GetFileNameWithoutExtension(masterJsonPath));
+                    var jsonContents = File.ReadAllText(masterJsonPath);
+                    masterJsonContents.Add(fileName, jsonContents);
+                }
+                configs.Add(new ConfigJson(modId,masterJsonContents));
             }
             
-            return configDict;
+            return configs;
         }
         
         private static string LoadConfigFile(string extractedPath, string configPath)
