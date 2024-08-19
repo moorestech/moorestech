7521b30bb27e5ef2a267f994af27a85895af1714
sakastudio <katsumi.sato@aktsk.jp>
2024-08-19 21:00:27 +0900

    asset 削除

--- moorestech_client/Assets/Dependencies/lilToon/Shader/Includes/lil_pipeline_urp.hlsl
+++
@@ -1,4 +0,0 @@
-#define LIL_URP
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
-#include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/MetaInput.hlsl"
