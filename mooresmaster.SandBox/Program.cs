e3fc4c27051dce84eaef42ad48a8d0f929e6b39e
Juha <kukiyomenaisapp@gmail.com>
2024-08-19 17:05:11 +0900

    preprocessordirectiveで生成するか否かを判断するように変更

--- mooresmaster.SandBox/Program.cs
+++ mooresmaster.SandBox/Program.cs
@@ -1,5 +1,4 @@
-﻿using Mooresmaster;
-using Mooresmaster.Loader.BlocksModule;
+﻿using Mooresmaster.Loader.BlocksModule;
 using Mooresmaster.Loader.ChallengesModule;
 using Mooresmaster.Loader.CraftRecipesModule;
 using Mooresmaster.Loader.ItemsModule;
@@ -7,8 +6,6 @@ using Mooresmaster.Loader.MachineRecipesModule;
 using Mooresmaster.Loader.MapObjectsModule;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
-
-[assembly: GenerateMooresmaster]
 
 namespace mooresmaster.SandBox;
 
