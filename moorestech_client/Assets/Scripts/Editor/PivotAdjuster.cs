cfc2a8b489bc8ff26a3c2e657b5eeb4e1bba97af
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-09-15 13:32:40 +0900

    Update: エディタ拡張大枠デザイン作成

--- moorestech_client/Assets/Scripts/Editor/PivotAdjuster.cs
+++ moorestech_client/Assets/Scripts/Editor/PivotAdjuster.cs
@@ -1,14 +1,126 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.UIElements;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 /// <summary>
 /// ピボット簡易調整エディタ拡張
 /// </summary>
-public static class PivotAdjuster
+public sealed class PivotAdjuster : EditorWindow
 {
+    private enum Genre
+    {
+        [InspectorName("現代")]
+        Modern,
+        SF,
+    }
+    
+    private enum Route
+    {
+        [InspectorName("直進")]
+        Straight,
+        [InspectorName("L字")]
+        L,
+        [InspectorName("T字")]
+        T,
+        [InspectorName("十字")]
+        Cross,
+        [InspectorName("直進の斜め上がり")]
+        StraightUp,
+    }
+    
+    private enum Parts
+    {
+        [InspectorName("ベルトコンベア")]
+        BeltConveyor,
+        [InspectorName("アイテムシューター")]
+        ItemShooter,
+    }
+    
     private const string UndoAlignPivot = nameof(UndoAlignPivot);
+    private const float HeightOffsetDefault = 0.3f;
+    
+    [MenuItem("Tools/Pivot Adjuster")]
+    private static void ShowWindow() => GetWindow<PivotAdjuster>(nameof(PivotAdjuster));
+    
+    private void CreateGUI()
+    {
+        var root = rootVisualElement;
+        root.style.paddingTop = 10;
+ 
+        var objectsListView = new ListView
+        {
+            reorderMode = ListViewReorderMode.Animated,
+            showAddRemoveFooter = true,
+            showBorder = true,
+            showFoldoutHeader = true,
+            headerTitle = "Target Objects",
+            virtualizationMethod = CollectionVirtualizationMethod.DynamicHeight,
+            viewController =
+            {
+                
+            },
+            makeItem = () =>
+            {
+                Debug.Log("makeItem");
+                var objectField = new ObjectField()
+                {
+                    objectType = typeof(GameObject),
+                    allowSceneObjects = true,
+                };
+                objectField.RegisterValueChangedCallback(evt =>
+                {
+                    if (evt.newValue == null) return;
+                    if (PrefabUtility.GetPrefabAssetType(evt.newValue) != PrefabAssetType.NotAPrefab)
+                    {
+                        objectField.value = null;
+                        Debug.LogError("Prefab is not supported.");
+                    }
+                });
+                return objectField;
+            },
+        };
+        
+        root.Add(objectsListView);
+        
+        // header
+        root.Add(new Label("Name Parameter") { style = { unityFontStyleAndWeight = FontStyle.Bold } });
+        
+        var nameField = new TextField
+        {
+            isReadOnly = true,
+            style = { opacity = 0.5f },
+        };
+        root.Add(nameField);
+        
+        var genreField = new EnumField("ジャンル", Genre.Modern);
+        root.Add(genreField);
+        
+        var routeField = new EnumField("経路形状", Route.Straight);
+        root.Add(routeField);
+        
+        var partsField = new EnumField("パーツ種別", Parts.BeltConveyor);
+        root.Add(partsField);
+
+        OnNameParameterChanged(default);
+        genreField.RegisterValueChangedCallback(OnNameParameterChanged);
+        routeField.RegisterValueChangedCallback(OnNameParameterChanged);
+        partsField.RegisterValueChangedCallback(OnNameParameterChanged);
+        
+        root.Add(new Button() { name = "Prefab生成" } );
+        
+        return;
+        
+        void OnNameParameterChanged(ChangeEvent<Enum> _)
+        {
+            var genre = genreField.value;
+            var route = routeField.value;
+            var parts = partsField.value;
+            nameField.value = $"{genre}_{route}_{parts}";
+        }
+    }
     
     /// <summary>
     /// ゲームシーン上の選択中のゲームオブジェクトのピボットを調整します.
@@ -17,44 +129,25 @@ public static class PivotAdjuster
     [MenuItem("GameObject/Adjust Pivot")]
     private static void AdjustPivot()
     {
-        if (PrefabUtility.GetPrefabAssetType(Selection.activeTransform) != PrefabAssetType.NotAPrefab)
-        {
-            throw new Exception("Prefab is not supported.");
-        }
-        
         // 複数選択対応
         foreach (var activeTransform in Selection.transforms)
         {
             var renderers = activeTransform.GetComponentsInChildren<Renderer>(true);
-            if (renderers.Length < 1) throw new Exception("Renderer not found.");
-            
-            var parent = activeTransform.parent;
-            if (parent != null && parent.GetComponents<Component>().Length == 1)
+            if (renderers.Length < 1)
             {
-                var children = new List<(Transform, Vector3, Quaternion)>(parent.childCount);
-                foreach (Transform child in parent)
-                {
-                    children.Add((child, child.position, child.rotation));
-                }
-                parent.position = GetPivotPos(renderers);
-                parent.rotation = Quaternion.identity;
-                foreach (var (child, pos, rot) in children)
-                {
-                    child.position = pos;
-                    child.rotation = rot;
-                }
-                Undo.RegisterCompleteObjectUndo(parent.gameObject, UndoAlignPivot);
-                return;
+                throw new InvalidOperationException("Renderer not found.");
             }
             
             var root = new GameObject(activeTransform.name).transform;
-            root.position = GetPivotPos(renderers);
-            activeTransform.SetParent(root);
+            root.position = GetPivotPos(renderers, HeightOffsetDefault);
             
             Undo.RegisterCreatedObjectUndo(root.gameObject, UndoAlignPivot);
+            Undo.SetTransformParent(activeTransform, root, UndoAlignPivot);
+            
+            activeTransform.SetParent(root);
         }
         
-        static Vector3 GetPivotPos(in ReadOnlySpan<Renderer> renderers)
+        static Vector3 GetPivotPos(in ReadOnlySpan<Renderer> renderers, float heightOffset)
         {
             var fullyBounds = renderers[0].bounds;
             foreach (var renderer in renderers[1..])
@@ -62,7 +155,7 @@ public static class PivotAdjuster
                 fullyBounds.Encapsulate(renderer.bounds);
             }
             var (min, max) = (fullyBounds.min, fullyBounds.max);
-            return new Vector3(min.x, max.y, min.z);
+            return new Vector3(min.x, max.y - heightOffset, min.z);
         }
     }
 }
