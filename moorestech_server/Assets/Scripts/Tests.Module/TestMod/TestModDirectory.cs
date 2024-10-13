7e10e2b5ea6ce3a806d91a1436c630f60894dc6a
sakastudio <katsumi.sato@aktsk.jp>
2024-10-13 21:22:58 +0900

    alphamodをとりあえず追加

--- moorestech_server/Assets/Scripts/Tests.Module/TestMod/TestModDirectory.cs
+++ moorestech_server/Assets/Scripts/Tests.Module/TestMod/TestModDirectory.cs
@@ -9,5 +9,7 @@ namespace Tests.Module.TestMod
         
         public static string ConfigOnlyDirectory => Path.Combine(FolderDirectory, "ConfigOnly");
         public static string ForUnitTestModDirectory => Path.Combine(FolderDirectory, "ForUnitTest");
+        
+        public static string MoorestechAlphaModDirectory => Path.Combine(Environment.CurrentDirectory, "../", "moorestech_client", "Server");
     }
 }
