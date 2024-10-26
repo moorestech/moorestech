6b8b508d2694131df6014557ef358ab6b3d894c3
sakastudio <katsumi.sato@aktsk.jp>
2024-10-26 16:32:34 +0900

    gear belt save test

--- moorestech_server/Assets/Scripts/Tests/UnitTest/Game/SaveLoad/BeltConveyorSaveLoadTest.cs
+++ moorestech_server/Assets/Scripts/Tests/UnitTest/Game/SaveLoad/BeltConveyorSaveLoadTest.cs
@@ -18,7 +18,7 @@ namespace Tests.UnitTest.Game.SaveLoad
     public class BeltConveyorSaveLoadTest
     {
         [Test]
-        public void SaveLoadTest()
+        public void NormalSaveLoadTest()
         {
             var (packet, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
             
@@ -32,6 +32,55 @@ namespace Tests.UnitTest.Game.SaveLoad
             var inventoryItems = (BeltConveyorInventoryItem[])inventoryItemsField.GetValue(belt);
             
             var timeOfItemEnterToExit = ((BeltConveyorBlockParam)beltConveyor.BlockMasterElement.BlockParam).TimeOfItemEnterToExit;
+            //アイテムを設定
+            inventoryItems[0] = new BeltConveyorInventoryItem(new ItemId(1), new ItemInstanceId(0))
+            {
+                RemainingPercent = 0.3f,
+            };
+            inventoryItems[2] = new BeltConveyorInventoryItem(new ItemId(2), new ItemInstanceId(0))
+            {
+                RemainingPercent = 0.5f,
+            };
+            inventoryItems[3] = new BeltConveyorInventoryItem(new ItemId(5), new ItemInstanceId(0))
+            {
+                RemainingPercent = 1f,
+            };
+            
+            
+            //セーブデータ取得
+            var str = belt.GetSaveState();
+            Debug.Log(str);
+            
+            
+            //セーブデータをロード
+            var blockConnector = new BlockConnectorComponent<IBlockInventory>(null, null, beltPosInfo);
+            var newBelt = new VanillaBeltConveyorComponent(str, 4, 4000, blockConnector, "");
+            var newInventoryItems = (BeltConveyorInventoryItem[])inventoryItemsField.GetValue(newBelt);
+            
+            //アイテムが一致するかチェック
+            Assert.AreEqual(inventoryItems.Length, newInventoryItems.Length);
+            Assert.AreEqual(1, newInventoryItems[0].ItemId.AsPrimitive());
+            Assert.AreEqual(0.3f, newInventoryItems[0].RemainingPercent);
+            Assert.AreEqual(2, newInventoryItems[2].ItemId.AsPrimitive());
+            Assert.AreEqual(0.5f, newInventoryItems[2].RemainingPercent);
+            Assert.AreEqual(5, newInventoryItems[3].ItemId.AsPrimitive());
+            Assert.AreEqual(1f, newInventoryItems[3].RemainingPercent);
+        }
+        
+        [Test]
+        public void GearSaveLoadTest()
+        {
+            var (packet, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+            
+            var blockFactory = ServerContext.BlockFactory;
+            var beltPosInfo = new BlockPositionInfo(new Vector3Int(0, 0), BlockDirection.North, Vector3Int.one);
+            var beltConveyor = blockFactory.Create(ForUnitTestModBlockId.GearBeltConveyor, new BlockInstanceId(1), beltPosInfo);
+            
+            var belt = beltConveyor.GetComponent<VanillaBeltConveyorComponent>();
+            //リフレクションで_inventoryItemsを取得
+            var inventoryItemsField = typeof(VanillaBeltConveyorComponent).GetField("_inventoryItems", BindingFlags.NonPublic | BindingFlags.Instance);
+            var inventoryItems = (BeltConveyorInventoryItem[])inventoryItemsField.GetValue(belt);
+            
             //アイテムを設定
             inventoryItems[0] = new BeltConveyorInventoryItem(new ItemId(1), new ItemInstanceId(0))
             {
