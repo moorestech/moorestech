58a7fe170c1f8793ea6627886d95839b69e553b0
sakastudio <katsumi.sato@aktsk.jp>
2024-11-09 18:39:05 +0900

    接続システムの改善

--- moorestech_server/Assets/Scripts/Game.Block.Interface/Component/IBlockConnectorComponent.cs
+++ moorestech_server/Assets/Scripts/Game.Block.Interface/Component/IBlockConnectorComponent.cs
@@ -5,6 +5,21 @@ namespace Game.Block.Interface.Component
 {
     public interface IBlockConnectorComponent<TTarget> : IBlockComponent where TTarget : IBlockComponent
     {
-        public IReadOnlyDictionary<TTarget, (IConnectOption selfOption, IConnectOption targetOption)> ConnectedTargets { get; }
+        public IReadOnlyDictionary<TTarget, ConnectedInfo> ConnectedTargets { get; }
+    }
+    
+    public struct ConnectedInfo
+    {
+        public IConnectOption SelfOption { get; }
+        public IConnectOption TargetOption { get; }
+        
+        public IBlock TargetBlock { get; }
+        
+        public ConnectedInfo(IConnectOption selfOption, IConnectOption targetOption, IBlock targetBlock)
+        {
+            SelfOption = selfOption;
+            TargetOption = targetOption;
+            TargetBlock = targetBlock;
+        }
     }
 }
