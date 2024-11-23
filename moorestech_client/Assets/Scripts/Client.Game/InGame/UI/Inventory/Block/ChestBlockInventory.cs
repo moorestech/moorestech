cb2f39a0366dd147c8a92c6f47813156ee64eca2
sakastudio <sakastudio100@gmail.com>
2024-11-23 22:23:58 +0900

    add abstract

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/ChestBlockInventory.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/ChestBlockInventory.cs
@@ -1,9 +1,13 @@
+using Client.Game.InGame.Block;
 using UnityEngine;
 
 namespace Client.Game.InGame.UI.Inventory.Block
 {
     public class ChestBlockInventory : BlockInventoryBase
     {
-        
+        public override void OpenBlockInventoryType(BlockGameObject blockGameObject)
+        {
+            
+        }
     }
 }
