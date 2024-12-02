ed5480a50ff20838dfdd04cb519e0ee57cf033e0
Juha <kukiyomenaisapp@gmail.com>
2024-12-02 16:06:36 +0900

    だめ、うごかない

--- mooresmaster.SandBox/Program.cs
+++ mooresmaster.SandBox/Program.cs
@@ -13,19 +13,19 @@ internal static class Program
         // var items = ItemsLoader.Load(GetJson("items"));
         // var machineRecipes = MachineRecipesLoader.Load(GetJson("machineRecipes"));
         // var mapObjects = MapObjectsLoader.Load(GetJson("mapObjects"));
-
-
+        
+        
         // Console.WriteLine(blocks);
         // Console.WriteLine(challenges);
         // Console.WriteLine(craftRecipes);
         // Console.WriteLine(items);
         // Console.WriteLine(machineRecipes);
         // Console.WriteLine(mapObjects);
-
+        
         // var testModJsonsDirectoryPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod");
         // var jsonPaths = Directory.GetFiles(testModJsonsDirectoryPath);
         // var jsons = jsonPaths.Select(path => (path, File.ReadAllText(path)));
-
+        
         // foreach (var (path, json) in jsons)
         // {
         //     // Console.WriteLine(jsonFile);
@@ -42,7 +42,7 @@ internal static class Program
         //     }
         // }
     }
-
+    
     private static JToken GetJson(string name)
     {
         var blockJsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod", $"{name}.json");
