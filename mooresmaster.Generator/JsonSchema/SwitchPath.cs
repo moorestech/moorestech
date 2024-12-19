fffbf3525216da574da7daec25b5c9191add4e03
Juha <kukiyomenaisapp@gmail.com>
2024-12-19 19:07:17 +0900

    SwitchPathParserを作成

--- mooresmaster.Generator/JsonSchema/SwitchPath.cs
+++ mooresmaster.Generator/JsonSchema/SwitchPath.cs
@@ -1,17 +1,124 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace mooresmaster.Generator.JsonSchema;
 
 public static class SwitchPathParser
 {
     public static SwitchPath Parse(string path)
     {
-        return new SwitchPath([], SwitchPathType.Absolute);
+        List<ISwitchPathElement> elements = [];
+        var type = GetType(path);
+        path = GetTopPathExcludedPath(path, type);
+
+        var currentPosition = 0;
+        var (currentChar, nextChar) = GetCurrentAndNextChar(path, currentPosition);
+
+        while (currentChar != '\0')
+        {
+            (currentChar, nextChar) = GetCurrentAndNextChar(path, currentPosition);
+
+            switch (currentChar)
+            {
+                case '.': // ../ parentPath
+                    if (nextChar == '.')
+                    {
+                        currentPosition++;
+                        (currentChar, nextChar) = GetCurrentAndNextChar(path, currentPosition);
+
+                        if (nextChar == '/')
+                        {
+                            elements.Add(new ParentSwitchPathElement());
+                            currentPosition++;
+                        }
+                    }
+
+                    break;
+                default:
+                    if (IsChar(currentChar))
+                    {
+                        List<char> chars = [];
+
+                        while (IsChar(currentChar))
+                        {
+                            chars.Add(currentChar);
+                            currentPosition++;
+                            (currentChar, nextChar) = GetCurrentAndNextChar(path, currentPosition);
+                        }
+
+                        elements.Add(new NormalSwitchPathElement(new string(chars.ToArray())));
+                    }
+                    else
+                    {
+                        throw new Exception("Invalid switch path");
+                    }
+
+                    break;
+            }
+
+            currentPosition++;
+        }
+
+        return new SwitchPath(elements.ToArray(), type);
+    }
+
+    private static bool IsChar(char c)
+    {
+        return c != '.' && c != '/' && c != '\0';
+    }
+
+    private static (char currentChar, char nextChar) GetCurrentAndNextChar(string path, int currentPosition)
+    {
+        var currentChar = currentPosition < path.Length ? path[currentPosition] : (char)0;
+        var nextChar = currentPosition + 1 < path.Length ? path[currentPosition + 1] : (char)0;
+
+        return (currentChar, nextChar);
+    }
+
+    private static SwitchPathType GetType(string path)
+    {
+        if (path.StartsWith("/")) return SwitchPathType.Absolute;
+
+        if (path.StartsWith("./")) return SwitchPathType.Relative;
+
+        throw new Exception("Invalid switch path");
+    }
+
+    private static string GetTopPathExcludedPath(string path, SwitchPathType type)
+    {
+        var newPath = path.Substring(type == SwitchPathType.Absolute ? 1 : 2);
+        return newPath;
     }
 }
 
-public record SwitchPath(ISwitchPathElement[] Elements, SwitchPathType Type)
+public class SwitchPath(ISwitchPathElement[] Elements, SwitchPathType Type) : IEquatable<SwitchPath>
 {
     public readonly SwitchPathType Type = Type;
     public ISwitchPathElement[] Elements = Elements;
+
+    public bool Equals(SwitchPath? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Type == other.Type && Elements.SequenceEqual(other.Elements);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != GetType()) return false;
+        return Equals((SwitchPath)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return ((int)Type * 397) ^ Elements.GetHashCode();
+        }
+    }
 }
 
 public interface ISwitchPathElement;
@@ -22,9 +129,9 @@ public enum SwitchPathType
     Relative
 }
 
-public record NormalSwitchPathElement(string Path) : ISwitchPathElement
+public record struct NormalSwitchPathElement(string Path) : ISwitchPathElement
 {
     public string Path = Path;
 }
 
-public record ParentSwitchPathElement : ISwitchPathElement;
+public record struct ParentSwitchPathElement : ISwitchPathElement;
