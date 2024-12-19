c57c7e1c3d3591a1a75c2504c375068f1f7e83b8
Juha <kukiyomenaisapp@gmail.com>
2024-12-19 21:25:32 +0900

    optionalLoaderのテスト作成

--- mooresmaster.Tests/Test.cs
+++ mooresmaster.Tests/Test.cs
@@ -1,139 +1,14 @@
-using System.Collections.Generic;
+using mooresmaster.Generator.Definitions;
 using mooresmaster.Generator.Json;
+using mooresmaster.Generator.JsonSchema;
+using mooresmaster.Generator.NameResolve;
+using mooresmaster.Generator.Semantic;
 using Xunit;
 
 namespace mooresmaster.Tests;
 
 public class Test
 {
-    private const string BlockConfigJson = """
-                                           {
-                                             "type": "object",
-                                             "properties": {
-                                               "required": [
-                                                 "blocks"
-                                               ],
-                                               "blocks": {
-                                                 "type": "array",
-                                                 "items": {
-                                                   "type": "object",
-                                                   "properties": {
-                                                     "required": [
-                                                       "blockId",
-                                                       "itemId",
-                                                       "blockType"
-                                                     ],
-                                                     "blockId": {
-                                                       "type": "string",
-                                                       "pattern": "@guid"
-                                                     },
-                                                     "itemId": {
-                                                       "type": "string",
-                                                       "pattern": "@guid",
-                                                       "format": "@foreignKey:itemId"
-                                                     },
-                                                     "blockType": {
-                                                       "type": "string"
-                                                     },
-                                                     "blockParam": {
-                                                       "oneOf": [
-                                                         {
-                                                           "if": {
-                                                             "properties": {
-                                                               "blockType": {
-                                                                 "const": "TypeA"
-                                                               }
-                                                             }
-                                                           },
-                                                           "then": {
-                                                             "type": "object",
-                                                             "properties": {
-                                                               "paramA": {
-                                                                 "type": "string"
-                                                               },
-                                                               "paramB": {
-                                                                 "type": "integer"
-                                                               }
-                                                             },
-                                                             "required": [
-                                                               "paramA",
-                                                               "paramB"
-                                                             ]
-                                                           }
-                                                         },
-                                                         {
-                                                           "if": {
-                                                             "properties": {
-                                                               "blockType": {
-                                                                 "const": "TypeB"
-                                                               }
-                                                             }
-                                                           },
-                                                           "then": {
-                                                             "type": "object",
-                                                             "properties": {
-                                                               "paramA": {
-                                                                 "type": "boolean"
-                                                               },
-                                                               "paramB": {
-                                                                 "type": "number"
-                                                               }
-                                                             },
-                                                             "required": [
-                                                               "paramA",
-                                                               "paramB"
-                                                             ]
-                                                           }
-                                                         }
-                                                       ]
-                                                     },
-                                                     "arrayTest": {
-                                                       "type": "array",
-                                                       "items": {
-                                                         "type": "string"
-                                                       }
-                                                     },
-                                                     "objectTest": {
-                                                       "type": "object"
-                                                     },
-                                                     "vector2Test": {
-                                                       "type": "array",
-                                                       "pattern": "@vector2",
-                                                       "items": {
-                                                         "type": "number"
-                                                       }
-                                                     },
-                                                     "vector3IntTest": {
-                                                       "type": "array",
-                                                       "pattern": "@vector3Int",
-                                                       "items": {
-                                                         "type": "integer"
-                                                       }
-                                                     },
-                                                     "vector4Test": {
-                                                       "type": "array",
-                                                       "pattern": "@vector4",
-                                                       "items": {
-                                                         "type": "number"
-                                                       }
-                                                     },
-                                                     "boolTest": {
-                                                       "type": "boolean"
-                                                     },
-                                                     "intTest": {
-                                                       "type": "integer"
-                                                     },
-                                                     "floatTest": {
-                                                       "type": "number"
-                                                     }
-                                                   }
-                                                 }
-                                               }
-                                             }
-                                           }
-                                           
-                                           """;
-    
     [Fact]
     public void JsonTokenizerTest()
     {
@@ -146,27 +21,27 @@ public class Test
                        ]
                    }
                    """;
-        
+
         Token[] answer =
         [
-            new Token(TokenType.LBrace, "{"),
-            new Token(TokenType.String, "hoge"),
-            new Token(TokenType.Colon, ":"),
-            new Token(TokenType.String, "fuga"),
-            new Token(TokenType.Comma, ","),
-            new Token(TokenType.String, "piyo"),
-            new Token(TokenType.Colon, ":"),
-            new Token(TokenType.LSquare, "["),
-            new Token(TokenType.String, "puyo"),
-            new Token(TokenType.Comma, ","),
-            new Token(TokenType.String, "poyo"),
-            new Token(TokenType.RSquare, "]"),
-            new Token(TokenType.RBrace, "}")
+            new(TokenType.LBrace, "{"),
+            new(TokenType.String, "hoge"),
+            new(TokenType.Colon, ":"),
+            new(TokenType.String, "fuga"),
+            new(TokenType.Comma, ","),
+            new(TokenType.String, "piyo"),
+            new(TokenType.Colon, ":"),
+            new(TokenType.LSquare, "["),
+            new(TokenType.String, "puyo"),
+            new(TokenType.Comma, ","),
+            new(TokenType.String, "poyo"),
+            new(TokenType.RSquare, "]"),
+            new(TokenType.RBrace, "}")
         ];
         var tokens = JsonTokenizer.GetTokens(json);
         Assert.Equivalent(tokens, answer, true);
     }
-    
+
     [Fact]
     public void JsonParserTest()
     {
@@ -192,11 +67,48 @@ public class Test
         //
         // Assert.Equivalent(node, answer, true);
     }
-    
+
     [Fact]
-    public void BlockJsonParseTest()
+    public void OptionalLoaderTest()
     {
-        var tokens = JsonTokenizer.GetTokens(BlockConfigJson);
-        JsonParser.Parse(tokens);
+        var yamlSchema = """
+                         id: test
+                         type: object
+                         properties:
+                         - key: data0
+                           type: string
+                           optional: true
+                         - key: data1
+                           type: number
+                           optional: true
+                         - key: data2
+                           type: object
+                           optional: true
+                           properties:
+                           - key: data3
+                             type: number
+                             optional: true
+                         """;
+
+        var (schemaTable, nameTable, semantics, definition) = Generate(yamlSchema);
+
+        // 全てのプロパティがoptionalのはず
+        foreach (var propertySemantics in semantics.PropertySemanticsTable.Values)
+        {
+            Assert.True(propertySemantics.IsNullable);
+        }
+    }
+
+    private static (SchemaTable schemaTable, NameTable nameTable, Semantics semantics, Definition definition) Generate(string yaml)
+    {
+        var jsonSchema = Yaml.ToJson(yaml);
+        var schemaTable = new SchemaTable();
+        var json = JsonParser.Parse(JsonTokenizer.GetTokens(jsonSchema));
+        var schema = JsonSchemaParser.ParseSchema(json as JsonObject, schemaTable);
+        var semantics = SemanticsGenerator.Generate([schema], schemaTable);
+        var nameTable = NameResolver.Resolve(semantics, schemaTable);
+        var definition = DefinitionGenerator.Generate(semantics, nameTable, schemaTable);
+
+        return (schemaTable, nameTable, semantics, definition);
     }
 }
