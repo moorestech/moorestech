d234c6ecc3c2782f9710e9c1a0fb47c0c43e84e8
Juha <kukiyomenaisapp@gmail.com>
2025-01-08 12:30:17 +0900

    defineInterfaceに対してglobalかどうかの概念を追加

--- mooresmaster.Generator/DefineInterface.cs
+++ mooresmaster.Generator/DefineInterface.cs
@@ -3,9 +3,10 @@ using mooresmaster.Generator.JsonSchema;
 
 namespace mooresmaster.Generator;
 
-public class DefineInterface(string interfaceName, Dictionary<string, IDefineInterfacePropertySchema> properties, string[] implementationInterfaces)
+public class DefineInterface(string interfaceName, Dictionary<string, IDefineInterfacePropertySchema> properties, string[] implementationInterfaces, bool isGlobal)
 {
     public string[] ImplementationInterfaces = implementationInterfaces;
     public string InterfaceName = interfaceName;
+    public bool IsGlobal = isGlobal;
     public Dictionary<string, IDefineInterfacePropertySchema> Properties = properties;
 }
