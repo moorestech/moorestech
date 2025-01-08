b70ad2a138e103cb7ba85e83adcfc5316a4ee264
Juha <kukiyomenaisapp@gmail.com>
2025-01-08 18:05:02 +0900

    localを別スキーマから参照しようとするとエラーになることを検証

--- mooresmaster.Tests/DefineInterfaceTests/DefineInterfaceTest.cs
+++ mooresmaster.Tests/DefineInterfaceTests/DefineInterfaceTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Xunit;
 
@@ -31,5 +32,45 @@ public class DefineInterfaceTest
         Assert.True(firstGlobalInterface.IsGlobal);
         Assert.True(firstGlobalInterface.Properties.ContainsKey("test2"));
         Assert.True(firstGlobalInterface.Properties.ContainsKey("test3"));
+    }
+    
+    [Fact]
+    public void GlobalDefineInterfaceFailedTest()
+    {
+        const string failedTestText = """
+                                      id: defineInterfaceFailedTestSchema
+                                      type: object
+                                      
+                                      implementationInterface:
+                                        - ILocalInterface
+                                      
+                                      properties:
+                                        - key: test0
+                                          type: integer
+                                        - key: test1
+                                          type: integer
+                                      """;
+        
+        const string passedTestText = """
+                                      id: defineInterfaceFailedTestSchema
+                                      type: object
+                                      
+                                      implementationInterface:
+                                        - IGlobalInterface
+                                      
+                                      properties:
+                                        - key: test2
+                                          type: integer
+                                        - key: test3
+                                          type: integer
+                                      """;
+        
+        var defineInterfaceTestSchemaText = Test.GetSchema("DefineInterfaceTests/DefineInterfaceTestSchema.yml");
+        
+        // ScopeがLocalのInterfaceを別ファイルから参照している場合エラーになる
+        Assert.ThrowsAny<Exception>(() => { _ = Test.Generate(defineInterfaceTestSchemaText, failedTestText); });
+
+        // globalであれば問題ない
+        Test.Generate(defineInterfaceTestSchemaText, passedTestText);
     }
 }
