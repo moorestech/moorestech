45d119f8fed142842a70741e4903149385f94e16
Juha <kukiyomenaisapp@gmail.com>
2025-01-08 12:00:08 +0900

    object型以外のすべてのプロパティ名がnullになっていた問題を修正

--- mooresmaster.SandBox/Program.cs
+++ mooresmaster.SandBox/Program.cs
@@ -1,4 +1,5 @@
-﻿using Mooresmaster.Loader.BlocksModule;
+﻿//using Mooresmaster.Loader.BlocksModule;
+
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -9,9 +10,9 @@ internal static class Program
     private static void Main(string[] args)
     {
         var blockJson = GetJson("blocks");
-        BlocksLoader.Load(blockJson);
+//        BlocksLoader.Load(blockJson);
     }
-
+    
     private static JToken GetJson(string name)
     {
         var blockJsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "TestMod", $"{name}.json");
