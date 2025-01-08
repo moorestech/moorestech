a201452c359c8ea97a68e9063801081d7454892b
Juha <kukiyomenaisapp@gmail.com>
2025-01-08 14:54:48 +0900

    現状のlocalがそもそもglobalになっていたため判定のためにschemaidをdefineinterfaceに含めるように

--- mooresmaster.Generator/DefineInterface.cs
+++ mooresmaster.Generator/DefineInterface.cs
@@ -3,10 +3,11 @@ using mooresmaster.Generator.JsonSchema;
 
 namespace mooresmaster.Generator;
 
-public class DefineInterface(string interfaceName, Dictionary<string, IDefineInterfacePropertySchema> properties, string[] implementationInterfaces, bool isGlobal)
+public class DefineInterface(string rootSchemaId, string interfaceName, Dictionary<string, IDefineInterfacePropertySchema> properties, string[] implementationInterfaces, bool isGlobal)
 {
     public string[] ImplementationInterfaces = implementationInterfaces;
     public string InterfaceName = interfaceName;
     public bool IsGlobal = isGlobal;
     public Dictionary<string, IDefineInterfacePropertySchema> Properties = properties;
+    public string RootSchemaId = rootSchemaId;
 }
