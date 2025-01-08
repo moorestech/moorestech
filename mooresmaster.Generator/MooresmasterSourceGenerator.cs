d23a436aec214422e54a9520bd0a496704f37056
Juha <kukiyomenaisapp@gmail.com>
2025-01-08 14:34:48 +0900

    defineinterfaceテスト

--- mooresmaster.Generator/MooresmasterSourceGenerator.cs
+++ mooresmaster.Generator/MooresmasterSourceGenerator.cs
@@ -22,41 +22,43 @@ public class MooresmasterSourceGenerator : IIncrementalGenerator
         var provider = context.CompilationProvider.Combine(additionalTextsProvider);
         context.RegisterSourceOutput(provider, Emit);
     }
-
+    
     private void Emit(SourceProductionContext context, (Compilation compilation, ImmutableArray<AdditionalText> additionalTexts) input)
     {
         var (schemas, schemaTable) = ParseAdditionalText(input.additionalTexts);
         var semantics = SemanticsGenerator.Generate(schemas.Select(schema => schema.Schema).ToImmutableArray(), schemaTable);
         var nameTable = NameResolver.Resolve(semantics, schemaTable);
         var definitions = DefinitionGenerator.Generate(semantics, nameTable, schemaTable);
-
+        
         var codeFiles = CodeGenerator.Generate(definitions);
         var loaderFiles = LoaderGenerator.Generate(definitions, semantics, nameTable);
-
+        
         // 生成するファイルがある場合のみ固定生成コードを生成する
         if (codeFiles.Length == 0 && loaderFiles.Length == 0) return;
-
+        
         foreach (var codeFile in codeFiles) context.AddSource(codeFile.FileName, codeFile.Code);
         foreach (var loaderFile in loaderFiles) context.AddSource(loaderFile.FileName, loaderFile.Code);
-
+        
         context.AddSource("mooresmaster.loader.BuiltinLoader.g.cs", LoaderGenerator.GenerateBuiltinLoaderCode());
         context.AddSource("mooresmaster.loader.exception.g.cs", LoaderGenerator.GenerateLoaderExceptionTypeCode());
     }
-
+    
     private (ImmutableArray<SchemaFile> files, SchemaTable schemaTable) ParseAdditionalText(ImmutableArray<AdditionalText> additionalTexts)
     {
         var schemas = new List<SchemaFile>();
         var schemaTable = new SchemaTable();
-
-        foreach (var additionalText in additionalTexts.Where(a => Path.GetExtension(a.Path) == ".yml"))
+        var parsedFiles = new HashSet<string>();
+        
+        foreach (var additionalText in additionalTexts.Where(a => Path.GetExtension(a.Path) == ".yml").Where(a => !parsedFiles.Contains(a.Path)))
         {
             var yamlText = additionalText.GetText()!.ToString();
             var jsonText = Yaml.ToJson(yamlText);
             var json = JsonParser.Parse(JsonTokenizer.GetTokens(jsonText));
             var schema = JsonSchemaParser.ParseSchema((json as JsonObject)!, schemaTable);
             schemas.Add(new SchemaFile(additionalText.Path, schema));
+            parsedFiles.Add(additionalText.Path);
         }
-
+        
         return (schemas.ToImmutableArray(), schemaTable);
     }
 }
---
+++ mooresmaster.Tests/DefineInterfaceTests/DefineInterfaceTest.cs
@@ -0,0 +1,35 @@
+using System.Linq;
+using Xunit;
+
+namespace mooresmaster.Tests.DefineInterfaceTests;
+
+public class DefineInterfaceTest
+{
+    [Fact]
+    public void LocalDefineInterfaceTest()
+    {
+        var (schemaTable, nameTable, semantics, definition) = Test.Generate(Test.GetSchema(@"DefineInterfaceTests/DefineInterfaceTestSchema.yml"));
+        
+        var interfaceTable = semantics.InterfaceSemanticsTable;
+        
+        var firstLocalInterface = interfaceTable.Select(i => i.Value.Interface).First(i => i.InterfaceName == "ILocalInterface");
+        
+        Assert.False(firstLocalInterface.IsGlobal);
+        Assert.True(firstLocalInterface.Properties.ContainsKey("test0"));
+        Assert.True(firstLocalInterface.Properties.ContainsKey("test1"));
+    }
+    
+    [Fact]
+    public void GlobalDefineInterfaceTest()
+    {
+        var (schemaTable, nameTable, semantics, definition) = Test.Generate(Test.GetSchema(@"DefineInterfaceTests/DefineInterfaceTestSchema.yml"));
+        
+        var interfaceTable = semantics.InterfaceSemanticsTable;
+        
+        var firstGlobalInterface = interfaceTable.Select(i => i.Value.Interface).First(i => i.InterfaceName == "IGlobalInterface");
+        
+        Assert.True(firstGlobalInterface.IsGlobal);
+        Assert.True(firstGlobalInterface.Properties.ContainsKey("test2"));
+        Assert.True(firstGlobalInterface.Properties.ContainsKey("test3"));
+    }
+}
