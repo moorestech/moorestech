5967eb1f6419c13cf8f220ecdf4978da2c4f4e1a
Juha <kukiyomenaisapp@gmail.com>
2025-02-04 18:58:49 +0900

    FluidPipeの設置テストを追加

---
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/FluidTest.cs
@@ -0,0 +1,49 @@
+﻿using Game.Block.Interface;
+using Game.Block.Interface.Extension;
+using Game.Context;
+using Game.Fluid;
+using NUnit.Framework;
+using Server.Boot;
+using Tests.Module.TestMod;
+using UnityEngine;
+
+namespace Tests.CombinedTest.Core
+{
+    public class FluidTest
+    {
+        /// <summary>
+        ///     1単位の流体が正しく搬入、搬出されるかのテスト
+        ///     Test to 1unit of fluid is carried in and out correctly.
+        /// </summary>
+        [Test]
+        public void FluidTransportTest()
+        {
+        }
+        
+        /// <summary>
+        ///     FluidPipeを設置できるかテスト
+        ///     Test to set FluidPipe
+        /// </summary>
+        [Test]
+        public void SetFluidPipeTest()
+        {
+            var (_, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+            
+            var blockFactory = ServerContext.BlockFactory;
+            
+            var fluidPipeBlock = blockFactory.Create(
+                ForUnitTestModBlockId.FluidPipe,
+                new BlockInstanceId(int.MaxValue),
+                new BlockPositionInfo(
+                    Vector3Int.zero,
+                    BlockDirection.North,
+                    Vector3Int.one
+                )
+            );
+            
+            // FluidPipeComponentの存在を確認
+            // Check the existence of FluidPipeComponent
+            Assert.True(fluidPipeBlock.ExistsComponent<FluidPipeComponent>());
+        }
+    }
+}
