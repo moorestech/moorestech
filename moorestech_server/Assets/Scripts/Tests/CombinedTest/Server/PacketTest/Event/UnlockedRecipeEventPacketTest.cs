4d90112deec3b4872cadf87b56449c5db9f3222b
sakastudio <katsumi.sato@aktsk.jp>
2025-02-20 23:52:45 +0900

    unlockedのイベントのテスト

---
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/Event/UnlockedRecipeEventPacketTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using Server.Boot;
+using Tests.Module.TestMod;
+
+namespace Tests.CombinedTest.Server.PacketTest.Event
+{
+    public class UnlockedRecipeEventPacketTest
+    {
+        [Test]
+        public void UnlockedEventTest()
+        {
+            var (packetResponse, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+            
+            Assert.Fail();
+        }
+    }
+}
