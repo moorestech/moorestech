39ab49e57bd4ba67a0fffb8133e309db5dc38ad4
sakastudio <katsumi.sato@aktsk.jp>
2025-02-22 19:07:35 +0900

    ClientGameUnlockStateDatastoreの実装

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UnlockState/ClientGameUnlockStateDatastore.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UnlockState/ClientGameUnlockStateDatastore.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
+using Client.Game.InGame.Context;
+using Client.Network.API;
 using Game.UnlockState;
+using MessagePack;
+using Server.Event.EventReceive;
 
 namespace Client.Game.InGame.UnlockState
 {
@@ -9,9 +13,27 @@ namespace Client.Game.InGame.UnlockState
         public IReadOnlyDictionary<Guid, CraftRecipeUnlockStateInfo> CraftRecipeUnlockStateInfos => _recipeUnlockStateInfos;
         private readonly Dictionary<Guid, CraftRecipeUnlockStateInfo> _recipeUnlockStateInfos = new();
         
-        public void SetUnlockState(Guid recipeGuid, bool isUnlocked)
+        public ClientGameUnlockStateDatastore(InitialHandshakeResponse initialHandshakeResponse)
         {
-            _recipeUnlockStateInfos[recipeGuid] = new CraftRecipeUnlockStateInfo(recipeGuid, isUnlocked);
+            var unlockCraftRecipeState = initialHandshakeResponse.UnlockCraftRecipeState;
+            foreach (var lockedGuid in unlockCraftRecipeState.LockedCraftRecipeGuids)
+            {
+                _recipeUnlockStateInfos[lockedGuid] = new CraftRecipeUnlockStateInfo(lockedGuid, false);
+            }
+            foreach (var unlockedGuid in unlockCraftRecipeState.UnlockedCraftRecipeGuids)
+            {
+                _recipeUnlockStateInfos[unlockedGuid] = new CraftRecipeUnlockStateInfo(unlockedGuid, true);
+            }
+            
+            ClientContext.VanillaApi.Event.SubscribeEventResponse(UnlockedCraftRecipeEventPacket.EventTag, OnUpdateUnlockCraftRecipe);
+        }
+        
+        public void OnUpdateUnlockCraftRecipe(byte[] payload)
+        {
+             var message = MessagePackSerializer.Deserialize<UnlockCraftRecipeEventMessagePack>(payload);
+             
+             var recipeGuid = message.UnlockedCraftRecipeGuid;
+            _recipeUnlockStateInfos[recipeGuid] = new CraftRecipeUnlockStateInfo(recipeGuid, true);
         }
     }
 }
