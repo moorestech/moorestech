be036db1e918ff6475463b8f37c44f82de36d7fc
Juha <kukiyomenaisapp@gmail.com>
2025-02-24 15:39:33 +0900

    消した

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/Fluid/FluidPipeComponent.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/Fluid/FluidPipeComponent.cs
@@ -1,14 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using Core.Update;
 using Game.Block.Component;
 using Game.Block.Interface;
 using Game.Block.Interface.Component;
-using Game.Block.Interface.Extension;
 using Game.Fluid;
-using Mooresmaster.Model.BlockConnectInfoModule;
-using UnityEngine;
 
 namespace Game.Block.Blocks.Fluid
 {
@@ -34,76 +28,6 @@ namespace Game.Block.Blocks.Fluid
         
         public void Update()
         {
-            // TODO: targetFluidContainerが存在するかどうかのチェックを行う
-            
-            for (var i = FluidContainer.PendingFluidStacks.Count - 1; i >= 0; i--)
-            {
-                var pendingFluidStack = FluidContainer.PendingFluidStacks[i];
-                
-                //TODO: キャッシュする
-                var targetContainers = new List<FluidContainer>();
-                
-                // 移動先の候補をリストアップ
-                foreach (KeyValuePair<IFluidInventory, ConnectedInfo> kvp in _connectorComponent.ConnectedTargets)
-                {
-                    // 元のコンテナは除く
-                    if (kvp.Key.FluidContainer == pendingFluidStack.PreviousContainer) continue;
-                    
-                    var selfOption = kvp.Value.SelfOption as FluidConnectOption;
-                    var targetOption = kvp.Value.TargetOption as FluidConnectOption;
-                    var target = kvp.Value.TargetBlock.GetComponent<IFluidInventory>();
-                    if (selfOption == null || targetOption == null || target == null) throw new Exception();
-                    
-                    // どちらかのflowCapacityが0の場合は除く
-                    if (selfOption.FlowCapacity == 0 || targetOption.FlowCapacity == 0) continue;
-                    
-                    targetContainers.Add(kvp.Key.FluidContainer);
-                }
-                
-                if (targetContainers.Count == 0) continue;
-                
-                var totalAmount = pendingFluidStack.Amount;
-                // 移動先一つに割り当てられるamount
-                var amount = totalAmount / targetContainers.Count;
-                
-                Debug.Log($"amount: {amount}, containers: {targetContainers.Count}");
-                // 移動先予定ごとに割り当て
-                foreach (var targetContainer in targetContainers)
-                {
-                    Debug.Log(FluidContainer.FluidStacks.ContainsKey(targetContainer));
-                    var newStack = new FluidStack(pendingFluidStack.FluidId, amount, pendingFluidStack.PreviousContainer, targetContainer);
-                    FluidContainer.FluidStacks[targetContainer] = newStack;
-                }
-                
-                // 移動先があった場合はpendingListから削除
-                FluidContainer.PendingFluidStacks[i] = FluidContainer.PendingFluidStacks[^1];
-                FluidContainer.PendingFluidStacks.RemoveAt(FluidContainer.PendingFluidStacks.Count - 1);
-            }
-            
-            // ターゲットに移動する
-            foreach (var (targetFluidContainer, fluidStack) in FluidContainer.FluidStacks.Select(kvp => (kvp.Key, kvp.Value)))
-            {
-                //TODO: 圧倒的に効率が悪いためキャッシュする、もしくはつながりやネットワーク自体をCoreに作る
-                var connectInfo = _connectorComponent.ConnectedTargets.First(kvp => kvp.Key.FluidContainer == targetFluidContainer).Value;
-                var selfOption = connectInfo.SelfOption as FluidConnectOption;
-                var targetOption = connectInfo.TargetOption as FluidConnectOption;
-                var target = connectInfo.TargetBlock.GetComponent<IFluidInventory>();
-                if (selfOption == null || targetOption == null || target == null) throw new Exception();
-                
-                var minimumFlowCapacity = Mathf.Min(selfOption.FlowCapacity, targetOption.FlowCapacity) * (float)GameUpdater.UpdateSecondTime;
-                
-                (var transportingStack, FluidStack? remainStack1) = FluidStack.Split(fluidStack, minimumFlowCapacity);
-                
-                // ターゲットに移動
-                targetFluidContainer.AddToPendingList(transportingStack, FluidContainer, out FluidStack? remainStack2);
-                
-                // 残ったfluidStackは元のコンテナにもどす
-                Debug.Log($"{fluidStack.Amount} {remainStack1?.Amount} {minimumFlowCapacity}");
-                if (remainStack1.HasValue) FluidContainer.AddToPendingList(remainStack1.Value, fluidStack.PreviousContainer, out _);
-                if (remainStack2.HasValue) FluidContainer.AddToPendingList(remainStack2.Value, fluidStack.PreviousContainer, out _);
-            }
-            // 余ったstackは全てpendingListに戻っているためFluidStacksを削除
-            FluidContainer.FluidStacks.Clear();
         }
     }
 }
--- moorestech_server/Assets/Scripts/Game.Fluid/FluidContainer.cs
+++ moorestech_server/Assets/Scripts/Game.Fluid/FluidContainer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace Game.Fluid
 {
@@ -11,17 +10,11 @@ namespace Game.Fluid
     {
         public static readonly FluidContainer Empty = new();
         public readonly double Capacity;
-        /// <summary>
-        ///     key: stackのtargetFluidContainer
-        /// </summary>
-        public readonly Dictionary<FluidContainer, FluidStack> FluidStacks = new();
         
         public readonly bool IsEmpty;
-        /// <summary>
-        ///     まだ次の移動先が決まっていないfluidStackのリスト
-        /// </summary>
-        public readonly List<FluidStack> PendingFluidStacks = new();
+        public double Amount;
         public Guid FluidId;
+        public HashSet<FluidContainer> PreviousSourceFluidContainers = new();
         
         /// <param name="capacity">液体の許容量</param>
         /// <param name="fluidId">内部の液体のID</param>
@@ -39,93 +32,6 @@ namespace Game.Fluid
             FluidId = Guid.Empty;
             Capacity = 0;
             IsEmpty = true;
-        }
-        
-        //TODO: CurrentCapacityをキャッシュする。現時点では実装の簡単のため毎回計算する
-        public double TotalAmount => FluidStacks.Sum(kvp => kvp.Value.Amount) + PendingFluidStacks.Sum(s => s.Amount);
-        
-        /// <summary>
-        ///     PendingListに追加する
-        /// </summary>
-        /// <param name="fluidStack">使用するfluidStack</param>
-        /// <param name="previousContainer">移動前のコンテナ</param>
-        /// <param name="remainFluidStack">残ったfluidStack</param>
-        public void AddToPendingList(FluidStack fluidStack, FluidContainer previousContainer, out FluidStack? remainFluidStack)
-        {
-            (var addingStack, FluidStack? remainStack) = Split(fluidStack);
-            remainFluidStack = remainStack;
-            
-            addingStack.TargetContainer = Empty;
-            addingStack.PreviousContainer = previousContainer;
-            
-            PendingFluidStacks.Add(addingStack);
-        }
-        
-        /// <summary>
-        ///     可能な限り液体を入れる
-        /// </summary>
-        /// <param name="fluidStack">使用するfluidStack</param>
-        /// <param name="previousContainer">移動前のコンテナ</param>
-        /// <param name="targetFluidContainer">入れた液体の次の移動先</param>
-        /// <param name="remainFluidStack">残ったfluidStack</param>
-        public void Fill(FluidStack fluidStack, FluidContainer previousContainer, FluidContainer targetFluidContainer, out FluidStack? remainFluidStack)
-        {
-            (var addingStack, FluidStack? remainStack) = Split(fluidStack);
-            addingStack.PreviousContainer = previousContainer;
-            addingStack.TargetContainer = targetFluidContainer;
-            
-            if (!FluidStacks.ContainsKey(addingStack.TargetContainer))
-            {
-                FluidStacks[addingStack.TargetContainer] = new FluidStack(
-                    FluidId,
-                    0,
-                    addingStack.PreviousContainer,
-                    addingStack.TargetContainer
-                );
-            }
-            var currentStack = FluidStacks[addingStack.TargetContainer];
-            currentStack.Amount += addingStack.Amount;
-            FluidStacks[addingStack.TargetContainer] = currentStack;
-            
-            remainFluidStack = remainStack;
-        }
-        
-        /// <summary>
-        ///     FluidStackから可能な限り指定された排出量を排出する
-        /// </summary>
-        /// <param name="maxDrain">最大排出量</param>
-        /// <param name="targetFluidContainer">次のターゲットの液体コンテナ</param>
-        /// <returns>排出したfluidStack</returns>
-        public FluidStack Drain(double maxDrain, FluidContainer targetFluidContainer)
-        {
-            var amount = 0d;
-            
-            
-            var currentStack = FluidStacks[targetFluidContainer];
-            
-            if (currentStack.Amount < maxDrain - amount)
-            {
-                // 余らない場合
-                amount += currentStack.Amount;
-                currentStack.Amount = 0;
-                FluidStacks.Remove(targetFluidContainer);
-            }
-            else
-            {
-                // 余る場合
-                var addingAmount = maxDrain - amount;
-                amount += addingAmount;
-                currentStack.Amount -= addingAmount;
-            }
-            
-            FluidStacks[targetFluidContainer] = currentStack;
-            
-            return new FluidStack(FluidId, amount, this, Empty);
-        }
-        
-        private (FluidStack stack, FluidStack? remain) Split(FluidStack fluidStack)
-        {
-            return FluidStack.Split(fluidStack, Capacity - TotalAmount);
         }
     }
 }
