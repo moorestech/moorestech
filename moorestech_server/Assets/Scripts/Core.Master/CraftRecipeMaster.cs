0361ccdfaa92cc2204a2812b95db8d364dfd0853
sakastudio <katsumi.sato@aktsk.jp>
2025-04-09 15:28:00 +0900

    クラフトレシピ等の調整

--- moorestech_server/Assets/Scripts/Core.Master/CraftRecipeMaster.cs
+++ moorestech_server/Assets/Scripts/Core.Master/CraftRecipeMaster.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Mooresmaster.Loader.CraftRecipesModule;
 using Mooresmaster.Model.CraftRecipesModule;
 using Newtonsoft.Json.Linq;
@@ -12,6 +13,50 @@ namespace Core.Master
         public CraftRecipeMaster(JToken craftRecipeJToken)
         {
             CraftRecipes = CraftRecipesLoader.Load(craftRecipeJToken);
+            ValidateCraftRecipe();
+            
+            #region Internal
+            
+            void ValidateCraftRecipe()
+            {
+                // チェックするアイテムのGUIDを取得
+                var checkTargets = new List<Guid>();
+                foreach (var craftRecipeMasterElement in CraftRecipes.Data)
+                {
+                    foreach (var requiredItem in craftRecipeMasterElement.RequiredItems)
+                    {
+                        checkTargets.Add(requiredItem.ItemGuid);
+                    }
+                    checkTargets.Add(craftRecipeMasterElement.CraftResultItemGuid);
+                }
+                
+                // アイテムのGUIDが存在するかチェック
+                var notExistItemGuids = new List<Guid>();
+                foreach (var checkItem in checkTargets)
+                {
+                    try
+                    {
+                        MasterHolder.ItemMaster.GetItemId(checkItem);
+                    }
+                    catch (InvalidOperationException e)
+                    {
+                        notExistItemGuids.Add(checkItem);
+                    }
+                }
+                
+                // アイテムのGUIDが存在しない場合はエラーを出力
+                if (notExistItemGuids.Count > 0)
+                {
+                    var errorMessage = "クラフトレシピに存在しないアイテムのGUIDが含まれています。\n";
+                    foreach (var notExistItemGuid in notExistItemGuids)
+                    {
+                        errorMessage += $"{notExistItemGuid}\n";
+                    }
+                    throw new InvalidOperationException(errorMessage);
+                }
+            }
+            
+  #endregion
         }
         
         public CraftRecipeMasterElement GetCraftRecipe(Guid guid)
