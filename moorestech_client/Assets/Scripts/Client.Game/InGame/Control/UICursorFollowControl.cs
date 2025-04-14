5f5054ea600e554853fcd143f1a870ebcbcfb6f3
sakastudio <katsumi.sato@aktsk.jp>
2025-04-14 20:03:21 +0900

    マウス説明修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Control/UICursorFollowControl.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Control/UICursorFollowControl.cs
@@ -5,21 +5,16 @@ namespace Client.Game.InGame.Control
     public class UICursorFollowControl : MonoBehaviour
     {
         [SerializeField] private Vector3 offSet = Vector3.zero;
-        private RectTransform _canvasRect;
-        
-        private void Start()
-        {
-            _canvasRect = transform.root.GetComponentsInChildren<RectTransform>()[0];
-        }
+        [SerializeField] private RectTransform canvasRect;
         
         private void Update()
         {
-            var magnification = _canvasRect.sizeDelta.x / Screen.width;
+            var magnification = canvasRect.sizeDelta.x / Screen.width;
             
             var itemPos = new Vector3();
             
-            itemPos.x = UnityEngine.Input.mousePosition.x * magnification - _canvasRect.sizeDelta.x / 2;
-            itemPos.y = UnityEngine.Input.mousePosition.y * magnification - _canvasRect.sizeDelta.y / 2;
+            itemPos.x = UnityEngine.Input.mousePosition.x * magnification - canvasRect.sizeDelta.x / 2;
+            itemPos.y = UnityEngine.Input.mousePosition.y * magnification - canvasRect.sizeDelta.y / 2;
             itemPos.z = transform.localPosition.z;
             
             transform.localPosition = itemPos + offSet;
