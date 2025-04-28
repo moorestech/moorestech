e0f59db4055ff1be8cc8edbebe961e8dffffb83e
sakastudio <katsumi.sato@aktsk.jp>
2025-04-28 12:09:16 +0900

    ビルドできるように色々修正

---
+++ moorestech_server/Assets/Scripts/Server.Boot/ServerDirectory.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using Common.Debug;
+using UnityEngine;
+
+namespace Server.Boot
+{
+    public class ServerDirectory
+    {
+        public const string DebugServerDirectorySettingKey = "DebugServerDirectory";
+        public static string GetDirectory()
+        {
+#if DEBUG
+            var debugServerDirectory = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "../moorestech_client/Server"));
+            var serverDirectory = DebugParameters.GetValueOrDefaultString(DebugServerDirectorySettingKey ,debugServerDirectory);
+#else
+            var serverDirectory = Path.Combine(UnityEngine.Application.dataPath, "../", "game");
+#endif
+            
+            return serverDirectory;
+        }
+    }
+}
