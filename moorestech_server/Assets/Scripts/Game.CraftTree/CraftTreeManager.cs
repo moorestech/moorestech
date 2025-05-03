b03140289a2f4d7d672512c72e501fcdb28c350a
sakastudio <katsumi.sato@aktsk.jp>
2025-05-03 18:04:58 +0900

    サーバー側のモデル

---
+++ moorestech_server/Assets/Scripts/Game.CraftTree/CraftTreeManager.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Game.CraftTree
+{
+    public class CraftTreeManager
+    {
+        private readonly Dictionary<int,List<CraftTreeNode>> _craftTree = new();
+        private readonly Dictionary<int, CraftTreeNode> _usingCraftTree = new();
+        
+        public void ApplyCraftTree(int playerId, CraftTreeNode craftTree)
+        {
+            if (!_craftTree.TryGetValue(playerId, out List<CraftTreeNode> currentCraftTrees))
+            {
+                _craftTree.Add(playerId, new List<CraftTreeNode> { craftTree });
+                _usingCraftTree[playerId] = craftTree;
+                return;
+            }
+            
+            var isReplaced = false;
+            foreach (var tree in currentCraftTrees.Where(tree => tree.NodeId == craftTree.NodeId))
+            {
+                // 既存のクラフトツリーを削除して新しいものを追加
+                isReplaced = true;
+                currentCraftTrees.Remove(tree);
+                currentCraftTrees.Add(craftTree);
+                break;
+            }
+            
+            if (!isReplaced)
+            {
+                currentCraftTrees.Add(craftTree);
+            }
+        }
+        
+    }
+}
