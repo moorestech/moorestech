aea18975532f2cb4ed95ec3f217ec93da3a4a116
sakastudio <katsumi.sato@aktsk.jp>
2025-05-03 18:04:58 +0900

    サーバー側のモデル

---
+++ moorestech_server/Assets/Scripts/Game.CraftTree/CraftTreeNode.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using Core.Master;
+
+namespace Game.CraftTree
+{
+    public class CraftTreeNode
+    {
+        public Guid NodeId { get; } = Guid.NewGuid();
+        
+        public IReadOnlyList<CraftTreeNode> Children => _children;
+        private readonly List<CraftTreeNode> _children = new();
+        
+        public ItemId TargetItemId { get; }
+        public int RequiredCount { get; }
+        
+        public int CurrentCount { get; } = 0;
+        
+        
+        public CraftTreeNode(CraftTreeNode parent, ItemId targetItemId, int requiredCount)
+        {
+            TargetItemId = targetItemId;
+            RequiredCount = requiredCount;
+        }
+        
+        public void AddChildren()
+        {
+            
+        }
+    }
+}
