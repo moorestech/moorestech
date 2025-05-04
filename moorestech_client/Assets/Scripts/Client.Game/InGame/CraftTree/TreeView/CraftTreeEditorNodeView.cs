f883136404a0056cc0589ea8642d5376442a66c0
sakastudio <katsumi.sato@aktsk.jp>
2025-05-04 10:10:02 +0900

    ノードの表示

--- moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
@@ -1,9 +1,25 @@
+using System.Collections.Generic;
+using Game.CraftTree;
 using UnityEngine;
 
 namespace Client.Game.InGame.CraftTree.TreeView
 {
     public class CraftTreeEditorNodeView : MonoBehaviour
     {
+        [SerializeField] private RectTransform uiTransform;
+        [SerializeField] private float depthWidth = 50f;
         
+        public CraftTreeNode Node { get; private set; }
+        private List<CraftTreeEditorNodeView> _children;
+        
+        public void Initialize(List<CraftTreeEditorNodeView> children, CraftTreeNode node, int depth)
+        {
+            var position = uiTransform.anchoredPosition;
+            position.x = depth * depthWidth;
+            uiTransform.anchoredPosition = position;
+            
+            Node = node;
+            _children = children;
+        }
     }
 }
--- moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorView.cs
@@ -1,9 +1,36 @@
+using System.Collections.Generic;
+using Game.CraftTree;
 using UnityEngine;
 
 namespace Client.Game.InGame.CraftTree.TreeView
 {
     public class CraftTreeEditorView : MonoBehaviour
     {
+        [SerializeField] private CraftTreeEditorNodeView nodePrefab;
+        [SerializeField] private RectTransform content;
         
+        public void Show(CraftTreeNode craftTreeNode)
+        {
+            CreateNode(craftTreeNode, 0);
+            
+            #region Internal
+            
+            CraftTreeEditorNodeView CreateNode(CraftTreeNode node, int depth)
+            {
+                var nodeView = Instantiate(nodePrefab, content);
+                var children = new List<CraftTreeEditorNodeView>();
+                
+                foreach (var child in node.Children)
+                {
+                    children.Add(CreateNode(child, depth + 1));
+                }
+                
+                nodeView.Initialize(children, node, depth);
+                return nodeView;
+            }
+            
+            #endregion
+            
+        }
     }
 }
