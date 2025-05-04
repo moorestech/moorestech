103186a1cb03360602f4c87c754a0cb0a5a79ddf
sakastudio <katsumi.sato@aktsk.jp>
2025-05-04 16:12:40 +0900

    コンテンツの操作

--- moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using Client.Game.InGame.UI.ContextMenu;
 using Game.CraftTree;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,9 +13,12 @@ namespace Client.Game.InGame.CraftTree.TreeView
         [SerializeField] private RectTransform uiTransform;
         [SerializeField] private float depthWidth = 50f;
         
-        [SerializeField] private Button contextMenuButton;
-        
         [SerializeField] private GameObject contextMenu;
+        
+        [SerializeField] private UGuiContextMenuTarget contextMenuTarget;
+        
+        public IObservable<Unit> OnUpdateNode => _onUpdateNode;
+        private readonly Subject<Unit> _onUpdateNode = new();
         
         public CraftTreeNode Node { get; private set; }
         private List<CraftTreeEditorNodeView> _children;
@@ -26,11 +32,22 @@ namespace Client.Game.InGame.CraftTree.TreeView
             Node = node;
             _children = children;
             
-            contextMenuButton.onClick.AddListener(OnContextMenuButtonClicked);
+            var contextMenus = new List<ContextMenuBarInfo>
+            {
+                new("レシピを展開", ExpandNode),
+                new("レシピを非表示", HideChildrenNode),
+            };
+            contextMenuTarget.SetContextMenuBars(contextMenus);
         }
         
-        private void OnContextMenuButtonClicked()
+        private void ExpandNode()
         {
+            
+        }
+        
+        private void HideChildrenNode()
+        {
+            
         }
     }
 }
