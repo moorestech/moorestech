3b6855b63f390a3e625be73ec68773196a418fc4
sakastudio <katsumi.sato@aktsk.jp>
2025-05-04 16:56:56 +0900

    クラフトツリーのオブジェクト

--- moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeEditorNodeView.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Collections.Generic;
+using Client.Game.InGame.Context;
 using Client.Game.InGame.UI.ContextMenu;
+using Client.Game.InGame.UI.Inventory.Common;
 using Game.CraftTree;
+using TMPro;
 using UniRx;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Client.Game.InGame.CraftTree.TreeView
 {
     public class CraftTreeEditorNodeView : MonoBehaviour
     {
-        [SerializeField] private RectTransform uiTransform;
-        [SerializeField] private float depthWidth = 50f;
+        [SerializeField] private TMP_Text itemNameText;
+        [SerializeField] private ItemSlotObject itemSlotObject;
         
-        [SerializeField] private GameObject contextMenu;
+        [SerializeField] private RectTransform offsetUiTransform;
+        [SerializeField] private float depthWidth = 50f;
         
         [SerializeField] private UGuiContextMenuTarget contextMenuTarget;
         
@@ -25,19 +28,39 @@ namespace Client.Game.InGame.CraftTree.TreeView
         
         public void Initialize(List<CraftTreeEditorNodeView> children, CraftTreeNode node, int depth)
         {
-            var position = uiTransform.anchoredPosition;
-            position.x = depth * depthWidth;
-            uiTransform.anchoredPosition = position;
-            
             Node = node;
             _children = children;
             
-            var contextMenus = new List<ContextMenuBarInfo>
+            SetItem();
+            SetPosition();
+            SetContextMenu();
+            
+            #region Internal
+            
+            void SetItem()
             {
-                new("レシピを展開", ExpandNode),
-                new("レシピを非表示", HideChildrenNode),
-            };
-            contextMenuTarget.SetContextMenuBars(contextMenus);
+                var itemView = ClientContext.ItemImageContainer.GetItemView(node.TargetItemId);
+                itemNameText.text = $"{itemView.ItemName}  {node.CurrentCount} / {node.RequiredCount}";
+            }
+            
+            void SetPosition()
+            {
+                var position = offsetUiTransform.anchoredPosition;
+                position.x = depth * depthWidth;
+                offsetUiTransform.anchoredPosition = position;
+            }
+            
+            void SetContextMenu()
+            {
+                var contextMenus = new List<ContextMenuBarInfo>
+                {
+                    new("レシピを展開", ExpandNode),
+                    new("レシピを非表示", HideChildrenNode),
+                };
+                contextMenuTarget.SetContextMenuBars(contextMenus);
+            }
+            
+            #endregion
         }
         
         private void ExpandNode()
