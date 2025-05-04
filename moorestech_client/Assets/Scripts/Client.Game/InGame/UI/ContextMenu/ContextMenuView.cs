97145121defccb7ff4dd556af5d1e961d7a15a8a
sakastudio <katsumi.sato@aktsk.jp>
2025-05-04 15:36:12 +0900

    コンテキストメニューのオブジェクトを作成

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/ContextMenu/ContextMenuView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/ContextMenu/ContextMenuView.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Client.Game.InGame.Control;
 using UnityEngine;
 
 namespace Client.Game.InGame.UI.ContextMenu
@@ -6,10 +7,12 @@ namespace Client.Game.InGame.UI.ContextMenu
     public class ContextMenuView : MonoBehaviour
     {
         [SerializeField] private ContextMenuViewItem contextMenuViewItemPrefab;
-        [SerializeField] private Transform menuBarParent;
+        [SerializeField] private Transform menuParent;
+        [SerializeField] private Transform menuBarContent;
         
         public static ContextMenuView Instance { get; private set; }
         
+        private UICursorFollowControlRootCanvasRect _canvasRectRoot;
         private List<ContextMenuBarInfo> _currentBars = new();
         
         private void Awake()
@@ -20,13 +23,23 @@ namespace Client.Game.InGame.UI.ContextMenu
         public void Show(List<ContextMenuBarInfo> contextMenuBars)
         {
             gameObject.SetActive(true);
+            
             _currentBars = contextMenuBars;
             foreach (var contextMenuBar in contextMenuBars)
             {
-                var item = Instantiate(contextMenuViewItemPrefab, menuBarParent);
+                var item = Instantiate(contextMenuViewItemPrefab, menuBarContent);
                 item.Initialize(contextMenuBar);
             }
+            
+            if (_canvasRectRoot == null)
+            {
+                _canvasRectRoot = FindObjectOfType<UICursorFollowControlRootCanvasRect>();
+                if (_canvasRectRoot == null) return;
+            }
+            
+            menuParent.transform.localPosition = UICursorFollowControl.GetLocalPosition(_canvasRectRoot, transform.localPosition, Vector3.zero);
         }
+        
         public void Hide()
         {
             gameObject.SetActive(false);
