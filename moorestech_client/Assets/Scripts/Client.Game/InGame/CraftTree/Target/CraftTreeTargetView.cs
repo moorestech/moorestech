3518604ad7bf3083909377ee0be381237dc635b3
sakastudio <katsumi.sato@aktsk.jp>
2025-05-05 16:18:50 +0900

    ターゲットの設定

--- moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/Target/CraftTreeTargetView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/Target/CraftTreeTargetView.cs
@@ -11,22 +11,44 @@ namespace Client.Game.InGame.CraftTree.Target
         
         private readonly List<CraftTreeTargetViewItems> _targetViewItems = new();
         
-        public void SetTarget(CraftTreeNode node)
+        public void SetTarget(List<CraftTreeNode> nodes)
         {
             ClearTarget();
             
-            // 新しいターゲットをセット
-            var targetItem = Instantiate(targetViewItemsPrefab, itemParent);
-            targetItem.Initialize(node, 0);
-            _targetViewItems.Add(targetItem);
-
-            // 子ノードもセット
-            foreach (var child in node.Children)
+            var parent = nodes[0].Parent;
+            if (parent != null)
             {
-                var childItem = Instantiate(targetViewItemsPrefab, itemParent);
-                childItem.Initialize(child, 1);
-                _targetViewItems.Add(childItem);
+                // 親ノードがあるときはそれが目標になるようにセット
+                SetTopTarget(parent);
+                
+                // 目標となるノードをセット
+                foreach (var node in nodes)
+                {
+                    var childItem = Instantiate(targetViewItemsPrefab, itemParent);
+                    childItem.Initialize(node, 1);
+                    _targetViewItems.Add(childItem);
+                }
+                
+                return;
             }
+            
+            // 親ノードがないときはそれがそのまま最後の目標となる
+            SetTopTarget(nodes[0]);
+            
+            
+            #region Internal
+            
+            
+            // 現状の目標となるノードをセット
+            void SetTopTarget(CraftTreeNode topTarget)
+            {
+                var targetItem = Instantiate(targetViewItemsPrefab, itemParent);
+                targetItem.Initialize(topTarget, 0);
+                _targetViewItems.Add(targetItem);
+            }
+            
+            #endregion
+
         }
         
         public void SetFinalTarget(CraftTreeNode node)
