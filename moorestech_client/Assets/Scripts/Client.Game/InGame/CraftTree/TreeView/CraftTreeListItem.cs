64d3260d150b66b96a927053b6e113523ec9dcec
sakastudio <katsumi.sato@aktsk.jp>
2025-05-05 09:32:01 +0900

    クラフトツリーリスト

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/CraftTree/TreeView/CraftTreeListItem.cs
@@ -0,0 +1,15 @@
+using Client.Game.InGame.UI.Inventory.Common;
+using Game.CraftTree;
+using UnityEngine;
+
+namespace Client.Game.InGame.CraftTree.TreeView
+{
+    public class CraftTreeListItem : MonoBehaviour
+    {
+        [SerializeField] private ItemSlotObject itemSlotObject;
+        public void Initialize(CraftTreeNode craftTreeNode)
+        {
+            
+        }
+    }
+}
