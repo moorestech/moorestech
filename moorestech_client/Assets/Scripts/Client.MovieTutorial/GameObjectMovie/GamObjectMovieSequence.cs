1b27e1b34127252ae599d282c1fb4f1e8f9fd931
sakastudio <katsumi.sato@aktsk.jp>
2025-05-09 13:10:44 +0900

    ゲームオブジェクト修正

--- moorestech_client/Assets/Scripts/Client.MovieTutorial/GameObjectMovie/GamObjectMovieSequence.cs
+++ moorestech_client/Assets/Scripts/Client.MovieTutorial/GameObjectMovie/GamObjectMovieSequence.cs
@@ -8,10 +8,7 @@ namespace Client.MovieTutorial.GameObjectMovie
     [CreateAssetMenu(fileName = "GamObjectMovieSequence", menuName = "moorestech/GamObjectMovieSequence", order = 0)]
     public class GamObjectMovieSequence : ScriptableObject
     {
-        public GameObjectMovieText TextPrefab; 
-        
         public List<MovieSequenceInfo> SequenceInfos;
-        public List<MovieSequenceTextInfo> SequenceTextInfos;
     }
     
     [Serializable]
@@ -23,16 +20,8 @@ namespace Client.MovieTutorial.GameObjectMovie
         public Vector3 Scale = Vector3.one;
         
         public float SpawnTime;
-    }
-    
-    [Serializable]
-    public class MovieSequenceTextInfo
-    {
-        public Vector3 Position;
-        public Vector3 Rotation;
-        public Vector3 Scale = Vector3.one;
         
-        public float SpawnTime;
-        public string Text;
+        // TODo このvaluesは変えたい
+        public List<string> Parameters;
     }
 }
---
+++ moorestech_client/Assets/Scripts/Client.MovieTutorial/GameObjectMovie/Objects/IGameObjectMovieObject.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Client.MovieTutorial.GameObjectMovie.Objects
+{
+    public interface IGameObjectMovieObject
+    {
+        public void SetParameters(List<string> parameters);
+    }
+}
