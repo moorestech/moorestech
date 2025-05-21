443c2d188d8aeaa957c7bb4849a35271b6f31828
sakastudio / Katsumi Sato <55620461+sakastudio@users.noreply.github.com>
2025-05-21 12:21:23 +0900

    Add fluid machine recipe test

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/MachineFluidIOTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/MachineFluidIOTest.cs
@@ -1,8 +1,16 @@
-﻿using Core.Master;
+using System;
+using System.Collections.Generic;
+using Core.Master;
+using Core.Update;
+using Game.Block.Blocks.Machine;
 using Game.Block.Blocks.Machine.Inventory;
+using Game.Block.Component;
+using Game.Block.Event;
 using Game.Block.Interface;
 using Game.Block.Interface.Extension;
 using Game.Context;
+using Game.EnergySystem;
+using Game.Fluid;
 using NUnit.Framework;
 using Server.Boot;
 using Tests.Module.TestMod;
@@ -15,22 +23,58 @@ namespace Tests.CombinedTest.Core
         [Test]
         public void FluidProcessingOutputTest()
         {
-            //TODO ここのテストを書く
-            Assert.Pass();
-            return;
-            
-            var (_, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
-            
-            var blockFactory = ServerContext.BlockFactory;
-            
-            var recipe = MasterHolder.MachineRecipesMaster.MachineRecipes.Data[9]; // L:229
-            
-            var blockId = MasterHolder.BlockMaster.GetBlockId(recipe.BlockGuid);
-            var block = blockFactory.Create(blockId, new BlockInstanceId(1), new BlockPositionInfo(Vector3Int.zero, BlockDirection.North, Vector3Int.one));
-            var blockInventory = block.GetComponent<VanillaMachineBlockInventoryComponent>();
-            foreach (var inputFluid in recipe.InputFluids)
+            var (_, _) = new MoorestechServerDIContainerGenerator().Create(TestModDirectory.ForUnitTestModDirectory);
+
+            var recipe = MasterHolder.MachineRecipesMaster.MachineRecipes.Data[9];
+
+            var blockPosition = new BlockPositionInfo(Vector3Int.zero, BlockDirection.North, Vector3Int.one);
+            var blockEvent = new BlockOpenableInventoryUpdateEvent();
+            var connector = new BlockConnectorComponent<IBlockInventory>(null, null, blockPosition);
+
+            var inputInventory = new VanillaMachineInputInventory(
+                MasterHolder.BlockMaster.GetBlockId(recipe.BlockGuid),
+                0,
+                recipe.InputFluids.Length,
+                10f,
+                blockEvent,
+                new BlockInstanceId(1)
+            );
+
+            var outputInventory = new VanillaMachineOutputInventory(
+                0,
+                ServerContext.ItemStackFactory,
+                blockEvent,
+                new BlockInstanceId(1),
+                0,
+                connector
+            );
+
+            var processor = new VanillaMachineProcessorComponent(inputInventory, outputInventory, recipe, new ElectricPower(100));
+
+            var inventoryComponent = new VanillaMachineBlockInventoryComponent(inputInventory, outputInventory);
+            var machineComponent = new VanillaElectricMachineComponent(new BlockInstanceId(1), processor);
+
+            var components = new List<IBlockComponent> { inventoryComponent, processor, machineComponent, connector };
+            var block = new BlockSystem(new BlockInstanceId(1), recipe.BlockGuid, components, blockPosition);
+
+            for (var i = 0; i < recipe.InputFluids.Length; i++)
             {
-                //blockInventory.InsertItem()
+                var fluid = recipe.InputFluids[i];
+                var fluidId = MasterHolder.FluidMaster.GetFluidId(fluid.FluidGuid);
+                inputInventory.FluidInputSlot[i].AddLiquid(new FluidStack(fluid.Amount, fluidId), FluidContainer.Empty, out var remain);
+                Assert.IsNull(remain);
+            }
+
+            var craftTime = DateTime.Now.AddSeconds(recipe.Time);
+            while (craftTime.AddSeconds(0.2).CompareTo(DateTime.Now) == 1)
+            {
+                processor.SupplyPower(new ElectricPower(10000));
+                GameUpdater.UpdateWithWait();
+            }
+
+            foreach (var container in inputInventory.FluidInputSlot)
+            {
+                Assert.AreEqual(0f, container.Amount, 0.01f);
             }
         }
     }
