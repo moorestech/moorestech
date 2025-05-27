6cef0e60fa046e8131e0524396a4154de928650e
sakastudio <katsumi.sato@aktsk.jp>
2025-05-27 22:21:14 +0900

    いったんジェネレートされたコードを削除

--- moorestech_client/Assets/Scripts/Client.Skit/Commands/CommandId.cs
+++
@@ -1,6 +0,0 @@
-﻿#if ENABLE_COMMAND_FORGE_GENERATOR
-namespace CommandForgeGenerator.Command
-{
-    public enum CommandId { }
-}
-#endif
--- moorestech_client/Assets/Scripts/Client.Skit/Commands/ICommandForgeCommand.cs
+++
@@ -1,8 +0,0 @@
-﻿#if ENABLE_COMMAND_FORGE_GENERATOR
-namespace CommandForgeGenerator.Command
-{
-    public partial interface ICommandForgeCommand
-    {
-    }
-}
-#endif
