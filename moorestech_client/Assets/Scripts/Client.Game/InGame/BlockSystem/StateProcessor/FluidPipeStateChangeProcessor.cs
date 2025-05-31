f7d0da71ad5f582e9f9faf90580e34ccfc674b56
Juha <kukiyomenaisapp@gmail.com>
2025-06-01 02:34:39 +0900

    stopsimulation時にリセットするように

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/StateProcessor/FluidPipeStateChangeProcessor.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/StateProcessor/FluidPipeStateChangeProcessor.cs
@@ -13,7 +13,7 @@ namespace Client.Game.InGame.BlockSystem.StateProcessor
         {
             if (CurrentFluidPipeState == null) return;
             
-            UpdateWaterLevel(CurrentFluidPipeState);
+            UpdateWaterLevel(CurrentFluidPipeState.Amount / CurrentFluidPipeState.Capacity);
         }
         
         public void OnChangeState(BlockStateMessagePack blockState)
@@ -21,9 +21,8 @@ namespace Client.Game.InGame.BlockSystem.StateProcessor
             CurrentFluidPipeState = blockState.GetStateDetail<FluidPipeStateDetail>(FluidPipeStateDetail.FluidPipeStateDetailKey);
         }
         
-        public void UpdateWaterLevel(FluidPipeStateDetail detail)
+        public void UpdateWaterLevel(float waterLevel)
         {
-            var waterLevel = detail.Amount / detail.Capacity;
             waterLevelTransform.position = new Vector3(0.5f, (waterLevel + 0.5f) * 0.5f, 0.5f);
             waterLevelTransform.localScale = new Vector3(Mathf.Sqrt(1 - Mathf.Pow(waterLevel * 2 - 1, 2)) * 0.5f, 1, 1);
         }
