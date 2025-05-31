1febf3471f088795c78a446e5aded9355dcc3a67
Juha <kukiyomenaisapp@gmail.com>
2025-06-01 01:45:27 +0900

    pipeのprefabにfluidpipestatechangeprocessorを追加

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/StateProcessor/FluidPipeStateChangeProcessor.cs
@@ -0,0 +1,16 @@
+﻿using Game.Fluid;
+using Server.Event.EventReceive;
+using UnityEngine;
+
+namespace Client.Game.InGame.BlockSystem.StateProcessor
+{
+    public class FluidPipeStateChangeProcessor : MonoBehaviour, IBlockStateChangeProcessor
+    {
+        public FluidPipeStateDetail FluidPipeState { get; private set; }
+        
+        public void OnChangeState(BlockStateMessagePack blockState)
+        {
+            FluidPipeState = blockState.GetStateDetail<FluidPipeStateDetail>(FluidPipeStateDetail.FluidPipeStateDetailKey);
+        }
+    }
+}
