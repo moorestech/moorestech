4bf79ea8e946e9e2d71af161a44f768b35f23ef9
Juha <kukiyomenaisapp@gmail.com>
2025-06-01 02:25:42 +0900

    FluidPipeStateChangeProcessorにwaterlevelを実装

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/StateProcessor/FluidPipeStateChangeProcessor.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/StateProcessor/FluidPipeStateChangeProcessor.cs
@@ -6,11 +6,26 @@ namespace Client.Game.InGame.BlockSystem.StateProcessor
 {
     public class FluidPipeStateChangeProcessor : MonoBehaviour, IBlockStateChangeProcessor
     {
-        public FluidPipeStateDetail FluidPipeState { get; private set; }
+        [SerializeField] private Transform waterLevelTransform;
+        public FluidPipeStateDetail CurrentFluidPipeState { get; private set; }
+        
+        private void Update()
+        {
+            if (CurrentFluidPipeState == null) return;
+            
+            UpdateWaterLevel(CurrentFluidPipeState);
+        }
         
         public void OnChangeState(BlockStateMessagePack blockState)
         {
-            FluidPipeState = blockState.GetStateDetail<FluidPipeStateDetail>(FluidPipeStateDetail.FluidPipeStateDetailKey);
+            CurrentFluidPipeState = blockState.GetStateDetail<FluidPipeStateDetail>(FluidPipeStateDetail.FluidPipeStateDetailKey);
+        }
+        
+        public void UpdateWaterLevel(FluidPipeStateDetail detail)
+        {
+            var waterLevel = detail.Amount / detail.Capacity;
+            waterLevelTransform.position = new Vector3(0.5f, (waterLevel + 0.5f) * 0.5f, 0.5f);
+            waterLevelTransform.localScale = new Vector3(Mathf.Sqrt(1 - Mathf.Pow(waterLevel * 2 - 1, 2)) * 0.5f, 1, 1);
         }
     }
 }
