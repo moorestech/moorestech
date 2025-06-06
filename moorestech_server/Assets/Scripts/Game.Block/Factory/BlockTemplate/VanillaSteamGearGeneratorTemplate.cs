a1681f977ca692c7318013915ed13faa1b26b681
sakastudio <katsumi.sato@aktsk.jp>
2025-06-06 10:04:44 +0900

    テンプレート修正

--- moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaSteamGearGeneratorTemplate.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaSteamGearGeneratorTemplate.cs
@@ -2,12 +2,11 @@ using System.Collections.Generic;
 using Game.Block.Blocks;
 using Game.Block.Blocks.Fluid;
 using Game.Block.Blocks.Gear;
-using Game.Block.Blocks.Machine;
-using Game.Block.Blocks.Machine.Inventory;
 using Game.Block.Component;
-using Game.Block.Event;
 using Game.Block.Interface;
 using Game.Block.Interface.Component;
+using Game.Block.Interface.Extension;
+using Game.Fluid;
 using Game.Gear.Common;
 using Mooresmaster.Model.BlocksModule;
 
@@ -15,11 +14,8 @@ namespace Game.Block.Factory.BlockTemplate
 {
     public class VanillaSteamGearGeneratorTemplate : IBlockTemplate
     {
-        private readonly BlockOpenableInventoryUpdateEvent _blockInventoryUpdateEvent;
-        
-        public VanillaSteamGearGeneratorTemplate(BlockOpenableInventoryUpdateEvent blockInventoryUpdateEvent)
+        public VanillaSteamGearGeneratorTemplate()
         {
-            _blockInventoryUpdateEvent = blockInventoryUpdateEvent;
         }
         
         public IBlock Load(Dictionary<string, string> componentStates, BlockMasterElement blockMasterElement, BlockInstanceId blockInstanceId, BlockPositionInfo blockPositionInfo)
@@ -43,35 +39,9 @@ namespace Game.Block.Factory.BlockTemplate
             // 流体接続の設定
             var fluidConnector = IFluidInventory.CreateFluidInventoryConnector(configParam.FluidInventoryConnectors, blockPositionInfo);
             
-            // ブロックIDを取得
-            var blockId = Core.Master.MasterHolder.BlockMaster.GetBlockId(blockMasterElement.BlockGuid);
-            
-            // 入力インベントリの作成（流体タンクのみ、アイテムスロットなし）
-            var inputInventory = new VanillaMachineInputInventory(
-                blockId,
-                0, // アイテムスロットなし
-                1, // 蒸気用タンク1つ
-                configParam.FluidCapacity, 
-                _blockInventoryUpdateEvent, 
-                blockInstanceId
-            );
-            
-            // 出力インベントリの作成（出力なし）
-            var outputInventory = new VanillaMachineOutputInventory(
-                0, // アイテムスロットなし
-                0, // 出力タンクなし
-                0, // タンク容量（使用しない）
-                Game.Context.ServerContext.ItemStackFactory,
-                _blockInventoryUpdateEvent,
-                blockInstanceId,
-                0, // 入力スロットサイズ
-                new BlockConnectorComponent<IBlockInventory>(null, null, blockPositionInfo) // ダミーのコネクタ
-            );
-            
-            // 流体インベントリコンポーネント
-            var fluidInventory = new VanillaMachineFluidInventoryComponent(
-                inputInventory,
-                outputInventory,
+            // SteamGearGeneratorFluidComponentの作成
+            var fluidComponent = new SteamGearGeneratorFluidComponent(
+                configParam.FluidCapacity,
                 fluidConnector
             );
             
@@ -80,7 +50,8 @@ namespace Game.Block.Factory.BlockTemplate
                 configParam, 
                 blockInstanceId, 
                 gearConnectorComponent,
-                inputInventory
+                fluidComponent,
+                fluidConnector
             );
             
             var components = new List<IBlockComponent>
@@ -88,7 +59,7 @@ namespace Game.Block.Factory.BlockTemplate
                 steamGearGeneratorComponent,
                 gearConnectorComponent,
                 fluidConnector,
-                fluidInventory,
+                fluidComponent,
             };
             
             return new BlockSystem(blockInstanceId, blockMasterElement.BlockGuid, components, blockPositionInfo);
