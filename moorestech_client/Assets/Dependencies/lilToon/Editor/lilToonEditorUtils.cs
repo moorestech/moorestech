1511fd233792494cad50fccda93a503e2e87a4fb
sakastudio <katsumi.sato@aktsk.jp>
2025-06-11 13:55:40 +0900

    clientを6.1にアップデート

--- moorestech_client/Assets/Dependencies/lilToon/Editor/lilToonEditorUtils.cs
+++ moorestech_client/Assets/Dependencies/lilToon/Editor/lilToonEditorUtils.cs
@@ -597,9 +597,9 @@ namespace lilToon
             sb.AppendLine();
 
             sb.AppendLine("# SRP Information");
-            if(GraphicsSettings.renderPipelineAsset != null)
+            if(GraphicsSettings.defaultRenderPipeline != null)
             {
-                sb.AppendLine("Current RP: " + GraphicsSettings.renderPipelineAsset.ToString());
+                sb.AppendLine("Current RP: " + GraphicsSettings.defaultRenderPipeline.ToString());
             }
             else
             {
