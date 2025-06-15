35699e46313a65f364d2db0465e3ffcf2294f167
sakastudio <katsumi.sato@aktsk.jp>
2025-06-15 15:21:42 +0900

    参考資料を追加

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/GetFluidInventoryProtocol.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/GetFluidInventoryProtocol.cs
@@ -7,22 +7,23 @@ namespace Tests.CombinedTest.Server.PacketTest
         [Test]
         public void GetFluidMachineTest()
         {
-            // 機械を設置
+            // 機械を設置 参考: moorestech/moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/MachineFluidIOTest.cs
             // 機械に液体を挿入
-            // プロトコル経由で液体を取得
+            // プロトコル経由で液体を取得 参考：このディレクトリの他のテスト
             // 機械の液体とプロトコルで取得した液体を比較
         }
         
         [Test]
         public void GetSteamEngineTest()
         {
-            // 上の機械がSteamEngine担ったバージョン
+            // 上の機械がSteamEngineになったバージョン 参考： moorestech/moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/SteamGearGeneratorTest.cs
         }
         
         [Test]
         public void GetFluidPipeTest()
         {
-            // 上の機械がパイプになったバージョン
+            // 上の機械がパイプになったバージョン 参考: moorestech/moorestech_server/Assets/Scripts/Tests/CombinedTest/Core/FluidTest.cs
+            
         }
         
     }
