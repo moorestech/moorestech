d8c866e904c2c9f8cf8e9934dc5bb4c92e7691c7
sakastudio <katsumi.sato@aktsk.jp>
2025-06-15 21:14:40 +0900

    CommonSlotViewExtension.csを修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Common/CommonSlotViewExtension.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Common/CommonSlotViewExtension.cs
@@ -4,54 +4,29 @@ namespace Client.Game.InGame.UI.Inventory.Common
 {
     public static class CommonSlotViewExtension
     {
-        public static void SetGrayOut(this CommonSlotView commonSlotView, bool active)
-        {
-            commonSlotView.SetSlotViewOption(GetGrayOutOption(active));
-        }
-        public static void SetGrayOut(this ItemSlotObject itemSlotObject, bool active)
-        {
-            itemSlotObject.SetSlotViewOption(GetGrayOutOption(active));
-        }
-        private static CommonSlotViewOption GetGrayOutOption(bool active)
-        {
-            return new CommonSlotViewOption { GrayOut = active };
-        }
-        
-        // 上記をさんこうに
-        // 下記を全て修正してください
-        
-        public static void SetFrameType(this CommonSlotView commonSlotView, ItemSlotFrameType frameType)
-        {
-            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
-            {
-                ItemSlotFrameType = frameType
-            });
-        }
+        // ----- GrayOut -----
+        public static void SetGrayOut(this CommonSlotView view, bool active) => view.SetSlotViewOption(GetGrayOutOption(active));
+        public static void SetGrayOut(this ItemSlotObject obj, bool active)   => obj.SetSlotViewOption(GetGrayOutOption(active));
+        private  static CommonSlotViewOption GetGrayOutOption(bool active)    => new() { GrayOut = active };
 
-        
-        public static void SetSlotType(this CommonSlotView commonSlotView, ItemSlotType slotType)
-        {
-            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
-            {
-                ItemSlotType = slotType
-            });
-        }
-        
+        // ----- FrameType -----
+        public static void SetFrameType(this CommonSlotView view, ItemSlotFrameType frameType) => view.SetSlotViewOption(GetFrameTypeOption(frameType));
+        public static void SetFrameType(this ItemSlotObject obj, ItemSlotFrameType frameType)  => obj.SetSlotViewOption(GetFrameTypeOption(frameType));
+        private  static CommonSlotViewOption GetFrameTypeOption(ItemSlotFrameType frameType)   => new() { ItemSlotFrameType = frameType };
 
-        public static void SetHotBarSelected(this CommonSlotView commonSlotView, bool isSelected)
-        {
-            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
-            {
-                HotBarSelected = isSelected
-            });
-        }
-        
-        public static void SetShowToolTip(this CommonSlotView commonSlotView, bool isShow)
-        {
-            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
-            {
-                IsShowToolTip = isShow
-            });
-        }
+        // ----- SlotType -----
+        public static void SetSlotType(this CommonSlotView view, ItemSlotType slotType) => view.SetSlotViewOption(GetSlotTypeOption(slotType));
+        public static void SetSlotType(this ItemSlotObject obj, ItemSlotType slotType)  => obj.SetSlotViewOption(GetSlotTypeOption(slotType));
+        private  static CommonSlotViewOption GetSlotTypeOption(ItemSlotType slotType)   => new() { ItemSlotType = slotType };
+
+        // ----- HotBarSelected -----
+        public static void SetHotBarSelected(this CommonSlotView view, bool isSelected) => view.SetSlotViewOption(GetHotBarSelectedOption(isSelected));
+        public static void SetHotBarSelected(this ItemSlotObject obj, bool isSelected)  => obj.SetSlotViewOption(GetHotBarSelectedOption(isSelected));
+        private  static CommonSlotViewOption GetHotBarSelectedOption(bool isSelected)   => new() { HotBarSelected = isSelected };
+
+        // ----- ShowToolTip -----
+        public static void SetShowToolTip(this CommonSlotView view, bool isShow) => view.SetSlotViewOption(GetShowToolTipOption(isShow));
+        public static void SetShowToolTip(this ItemSlotObject obj, bool isShow)  => obj.SetSlotViewOption(GetShowToolTipOption(isShow));
+        private  static CommonSlotViewOption GetShowToolTipOption(bool isShow)  => new() { IsShowToolTip = isShow };
     }
 }
