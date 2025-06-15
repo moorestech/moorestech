42fa731ce1a255b0fe7df07c299b9baf32b4cbf8
sakastudio <katsumi.sato@aktsk.jp>
2025-06-15 20:48:30 +0900

    CommonSlotViewExtension修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Common/CommonSlotViewExtension.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Common/CommonSlotViewExtension.cs
@@ -9,5 +9,37 @@ namespace Client.Game.InGame.UI.Inventory.Common
                 GrayOut = active
             });
         }
+        
+        public static void SetFrameType(this CommonSlotView commonSlotView, ItemSlotFrameType frameType)
+        {
+            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
+            {
+                ItemSlotFrameType = frameType
+            });
+        }
+        
+        public static void SetSlotType(this CommonSlotView commonSlotView, ItemSlotType slotType)
+        {
+            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
+            {
+                ItemSlotType = slotType
+            });
+        }
+        
+        public static void SetHotBarSelected(this CommonSlotView commonSlotView, bool isSelected)
+        {
+            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
+            {
+                HotBarSelected = isSelected
+            });
+        }
+        
+        public static void SetShowToolTip(this CommonSlotView commonSlotView, bool isShow)
+        {
+            commonSlotView.SetSlotViewOption(new CommonSlotViewOption
+            {
+                IsShowToolTip = isShow
+            });
+        }
     }
 }
