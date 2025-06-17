822db20931ba1a499a8636245e6ddffafd31fee1
Juha <kukiyomenaisapp@gmail.com>
2025-06-17 16:24:56 +0900

    codeとprefabの作成

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Tutorial/HudArrowManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Client.Game.InGame.Tutorial
+{
+    public class HudArrowManager : MonoBehaviour
+    {
+        [SerializeField] private RectTransform hudArrowImagePrefab;
+        private readonly Dictionary<GameObject, RectTransform> _hudArrowImages;
+        public HudArrowManager Instance { get; private set; }
+        
+        private void Awake()
+        {
+            Instance = this;
+        }
+        
+        private void Update()
+        {
+            foreach (var (target, imageTransform) in _hudArrowImages)
+            {
+            }
+        }
+        
+        public void RegisterHudArrowTarget(GameObject target)
+        {
+            _hudArrowImages[target] = Instantiate(hudArrowImagePrefab, transform);
+        }
+        
+        public void UnregisterHudArrowTarget(GameObject target)
+        {
+            _hudArrowImages.Remove(target);
+        }
+    }
+}
