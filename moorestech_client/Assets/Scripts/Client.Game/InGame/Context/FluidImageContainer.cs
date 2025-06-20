f1db7f644f4564dcf94f718ba025b15e077b3d6f
sakastudio <katsumi.sato@aktsk.jp>
2025-06-20 15:13:03 +0900

    液体スロットの作成

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Context/FluidImageContainer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Client.Mod.Texture;
+using Core.Master;
+using UnityEngine;
+
+namespace Client.Game.InGame.Context
+{
+    /// <summary>
+    ///     液体の画像を管理するクラス
+    /// </summary>
+    public class FluidImageContainer
+    {
+        private readonly Dictionary<FluidId, FluidViewData> _fluidImageList = new();
+        
+        private FluidImageContainer(Dictionary<FluidId, FluidViewData> fluidImageList)
+        {
+            _fluidImageList = fluidImageList;
+        }
+        
+        public static FluidImageContainer CreateAndLoadFluidImageContainer(string modsDirectory)
+        {
+            var fluidImageList = FluidTextureLoader.GetItemTexture(modsDirectory);
+            
+            return new FluidImageContainer(fluidImageList);
+        }
+        
+        public FluidViewData GetItemView(Guid fluidGuid)
+        {
+            var fluidId = MasterHolder.FluidMaster.GetFluidId(fluidGuid);
+            return GetItemView(fluidId);
+        }
+        
+        public FluidViewData GetItemView(FluidId fluidId)
+        {
+            if (fluidId == FluidMaster.EmptyFluidId)
+            {
+                return null;
+            }
+            
+            if (_fluidImageList.TryGetValue(fluidId, out var view)) return view;
+            
+            Debug.LogError($"ItemViewData not found. itemId:{fluidId}");
+            return null;
+        }
+        
+        public void AddItemView(FluidId fluidId, FluidViewData fluidViewData)
+        {
+            _fluidImageList[fluidId] = fluidViewData;
+        }
+    }
+}
---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Common/FluidSlotView.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Client.Common.Asset;
+using Client.Mod.Texture;
+using Core.Master;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UnityEngine;
+
+namespace Client.Game.InGame.UI.Inventory.Common
+{
+    public class FluidSlotView : MonoBehaviour
+    {
+        public static FluidSlotView Prefab;
+        
+        public IObservable<(FluidSlotView, ItemUIEventType)> OnPointerEvent => commonSlotView.OnPointerEvent.Select(e => (this, e.Item2));
+        
+        [SerializeField] private CommonSlotView commonSlotView;
+        
+        
+        public void SetItem(FluidViewData fluidView, float amount, string toolTipText = null)
+        {
+            if (fluidView == null || fluidView.FluidId == FluidMaster.EmptyFluidId)
+            {
+                commonSlotView.SetViewClear();
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(toolTipText))
+                {
+                    toolTipText = GetToolTipText(fluidView);
+                }
+                
+                var countText = amount != 0 ? amount.ToString("F2") : string.Empty;
+                commonSlotView.SetView(fluidView.FluidImage, countText, toolTipText);
+            }
+        }
+        
+        public void SetSlotViewOption(CommonSlotViewOption slotOption)
+        {
+            commonSlotView.SetSlotViewOption(slotOption);
+        }
+        
+        public void SetActive(bool active)
+        {
+            commonSlotView.SetActive(active);
+        }
+        
+        
+        public static string GetToolTipText(FluidViewData fluidView)
+        {
+            return $"{fluidView.FluidName}";
+        }
+        
+        public static async UniTask LoadItemSlotViewPrefab()
+        {
+            const string itemSlotViewPath = "Vanilla/UI/FluidSlotView";
+            var prefab = await AddressableLoader.LoadAsyncDefault<GameObject>(itemSlotViewPath);
+            Prefab = prefab.GetComponent<FluidSlotView>();
+        }
+    }
+}
