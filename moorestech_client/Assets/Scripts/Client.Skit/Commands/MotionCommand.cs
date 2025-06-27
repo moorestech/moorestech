9449a812ed3b92d13f37a8f663c5e0e3d4cf7725
sakastudio <katsumi.sato@aktsk.jp>
2025-06-27 15:01:53 +0900

    アニメーションの設定

--- moorestech_client/Assets/Scripts/Client.Skit/Commands/MotionCommand.cs
+++ moorestech_client/Assets/Scripts/Client.Skit/Commands/MotionCommand.cs
@@ -7,7 +7,7 @@ namespace CommandForgeGenerator.Command
         public async UniTask<CommandResultContext> ExecuteAsync(StoryContext storyContext)
         {
             var character = storyContext.GetCharacter(Character);
-            character.PlayAnimation(MotionName);
+            character.PlayAnimation(MotionName, 0);
             return null;
         }
     }
