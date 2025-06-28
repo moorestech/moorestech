e1f85453f5c5087c6ad3c1637c3397188569f39d
Juha <kukiyomenaisapp@gmail.com>
2025-06-29 00:17:52 +0900

    environmentvariablesの取得方法を変更

--- mooresmaster.Generator/MooresmasterSourceGenerator.cs
+++ mooresmaster.Generator/MooresmasterSourceGenerator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -34,14 +35,16 @@ public class MooresmasterSourceGenerator : IIncrementalGenerator
             {
                 GenerateErrorFile(sourceProductionContext, e);
 #pragma warning disable RS1035
-                var environmentVariables = Environment.GetEnvironmentVariables() as Dictionary<string, string> ?? new Dictionary<string, string>();
+                var environmentVariablesRaw = Environment.GetEnvironmentVariables();
+                var environmentVariables = new Dictionary<string, string>();
+                foreach (DictionaryEntry entry in environmentVariablesRaw) environmentVariables.Add(entry.Key.ToString()!, entry.Value.ToString()!);
                 var isSourceGeneratorDebug = environmentVariables.TryGetValue(Tokens.IsSourceGeneratorDebug, out var value) && value == "true";
 #pragma warning restore RS1035
                 if (isSourceGeneratorDebug) throw e;
             }
         });
     }
-    
+
     private void GenerateErrorFile(SourceProductionContext context, Exception exception)
     {
         context.AddSource(
@@ -49,13 +52,13 @@ public class MooresmasterSourceGenerator : IIncrementalGenerator
             $$$"""
                // ErrorType:
                // {{{exception.GetType().Name}}}
-               
+
                // Message: 
                // {{{
                    exception.Message
                        .Replace("\n", "\n// ")
                }}}
-               
+
                // StackTrace:
                // {{{
                    exception.StackTrace
@@ -64,48 +67,44 @@ public class MooresmasterSourceGenerator : IIncrementalGenerator
                """
         );
     }
-    
+
     private void Emit(SourceProductionContext context, (Compilation compilation, ImmutableArray<AdditionalText> additionalTexts) input)
     {
         var analyzer = new Analyzer()
             .AddAnalyzer(new DefineInterfaceScopeAnalyzer());
-        
         var analysis = new Analysis();
-        
         analyzer.PreJsonSchemaLayerAnalyze(analysis, input.additionalTexts.ToAnalyzerTextFiles());
         var (schemas, schemaTable) = ParseAdditionalText(input.additionalTexts);
         analyzer.PostJsonSchemaLayerAnalyze(analysis, schemas, schemaTable);
-        
         analyzer.PreSemanticsLayerAnalyze(analysis, schemas, schemaTable);
         var semantics = SemanticsGenerator.Generate(ImmutableArrayExtensions.Select(schemas, schema => schema.Schema).ToImmutableArray(), schemaTable);
         analyzer.PostSemanticsLayerAnalyze(analysis, semantics, schemas, schemaTable);
-        
         var nameTable = NameResolver.Resolve(semantics, schemaTable);
         analyzer.PreDefinitionLayerAnalyze(analysis, semantics, schemas, schemaTable);
         var definition = DefinitionGenerator.Generate(semantics, nameTable, schemaTable);
         analyzer.PostDefinitionLayerAnalyze(analysis, semantics, schemas, schemaTable, definition);
-        
+
         var codeFiles = CodeGenerator.Generate(definition);
         var loaderFiles = LoaderGenerator.Generate(definition, semantics, nameTable);
-        
+
         analysis.ThrowDiagnostics();
-        
+
         // 生成するファイルがある場合のみ固定生成コードを生成する
         if (codeFiles.Length == 0 && loaderFiles.Length == 0) return;
-        
+
         foreach (var codeFile in codeFiles) context.AddSource(codeFile.FileName, codeFile.Code);
         foreach (var loaderFile in loaderFiles) context.AddSource(loaderFile.FileName, loaderFile.Code);
-        
+
         context.AddSource(Tokens.BuiltinLoaderFileName, LoaderGenerator.GenerateBuiltinLoaderCode());
         context.AddSource(Tokens.ExceptionFileName, LoaderGenerator.GenerateLoaderExceptionTypeCode());
     }
-    
+
     private (ImmutableArray<SchemaFile> files, SchemaTable schemaTable) ParseAdditionalText(ImmutableArray<AdditionalText> additionalTexts)
     {
         var schemas = new List<SchemaFile>();
         var schemaTable = new SchemaTable();
         var parsedFiles = new HashSet<string>();
-        
+
         foreach (var additionalText in Enumerable.Where(ImmutableArrayExtensions.Where(additionalTexts, a => Path.GetExtension(a.Path) == ".yml"), a => !parsedFiles.Contains(a.Path)))
         {
             var yamlText = additionalText.GetText()!.ToString();
@@ -114,7 +113,7 @@ public class MooresmasterSourceGenerator : IIncrementalGenerator
             schemas.Add(new SchemaFile(additionalText.Path, schema));
             parsedFiles.Add(additionalText.Path);
         }
-        
+
         return (schemas.ToImmutableArray(), schemaTable);
     }
 }
