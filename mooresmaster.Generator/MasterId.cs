1eafb35398a19730afe0d5f520183029d579d8d3
Juha <kukiyomenaisapp@gmail.com>
2025-06-30 01:08:02 +0900

    statictypecasheingできないかも

--- mooresmaster.Generator/MasterId.cs
+++ mooresmaster.Generator/MasterId.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace mooresmaster.Generator;
 
-public readonly struct MasterId<T> : IEquatable<MasterId<T>>, IComparable<MasterId<T>>
+public readonly struct MasterId : IEquatable<MasterId>, IComparable<MasterId>
 {
     private static ulong _globalIndex;
     public readonly ulong Index;
@@ -12,14 +12,14 @@ public readonly struct MasterId<T> : IEquatable<MasterId<T>>, IComparable<Master
         Index = _globalIndex++;
     }
 
-    public bool Equals(MasterId<T> other)
+    public bool Equals(MasterId other)
     {
         return Index == other.Index;
     }
 
     public override bool Equals(object? obj)
     {
-        return obj is MasterId<T> other && Equals(other);
+        return obj is MasterId other && Equals(other);
     }
 
     public override int GetHashCode()
@@ -27,13 +27,13 @@ public readonly struct MasterId<T> : IEquatable<MasterId<T>>, IComparable<Master
         return Index.GetHashCode();
     }
 
-    public int CompareTo(MasterId<T> other)
+    public int CompareTo(MasterId other)
     {
         return Index.CompareTo(other.Index);
     }
 
     public override string ToString()
     {
-        return $"{{{typeof(T).Name}: {Index}}}";
+        return Index.ToString();
     }
 }
