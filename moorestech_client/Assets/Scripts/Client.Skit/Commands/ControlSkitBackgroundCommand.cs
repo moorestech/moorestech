88778a914fec32c3ebd9fd517d7be7dbc3967491
sakastudio <katsumi.sato@aktsk.jp>
2025-07-15 13:41:48 +0900

    ControlSkitBackgroundCommand.csの作成

---
+++ moorestech_client/Assets/Scripts/Client.Skit/Commands/ControlSkitBackgroundCommand.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+using Client.Common.Asset;
+using Client.Skit.Context;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace CommandForgeGenerator.Command
+{
+    public interface ISkitEnvironmentManager
+    {
+        UniTask AddEnvironmentAsync(string addressablePath);
+        void RemoveEnvironment(string addressablePath);
+    }
+    
+    public partial class ControlSkitBackgroundCommand
+    {
+        public async UniTask<CommandResultContext> ExecuteAsync(StoryContext storyContext)
+        {
+            var environmentManager = storyContext.GetService<ISkitEnvironmentManager>();
+            
+            if (Action == "Add")
+            {
+                await environmentManager.AddEnvironmentAsync(SkitEnvironmentAddressablePath);
+            }
+            else if (Action == "Remove")
+            {
+                environmentManager.RemoveEnvironment(SkitEnvironmentAddressablePath);
+            }
+            
+            return null;
+        }
+    }
+    
+    public class SkitEnvironmentManager : ISkitEnvironmentManager
+    {
+        private readonly Dictionary<string, GameObject> _loadedEnvironments = new();
+        private readonly Transform _environmentParent;
+        
+        public SkitEnvironmentManager(Transform environmentParent)
+        {
+            _environmentParent = environmentParent;
+        }
+        
+        public async UniTask AddEnvironmentAsync(string addressablePath)
+        {
+            if (string.IsNullOrEmpty(addressablePath))
+                return;
+            
+            if (_loadedEnvironments.ContainsKey(addressablePath))
+                return;
+            
+            var loadedAsset = await AddressableLoader.LoadAsync<GameObject>(addressablePath);
+            if (loadedAsset?.Asset == null)
+                return;
+            
+            var instance = Object.Instantiate(loadedAsset.Asset, _environmentParent);
+            _loadedEnvironments[addressablePath] = instance;
+        }
+        
+        public void RemoveEnvironment(string addressablePath)
+        {
+            if (string.IsNullOrEmpty(addressablePath))
+                return;
+            
+            if (!_loadedEnvironments.TryGetValue(addressablePath, out var environment))
+                return;
+            
+            if (environment != null)
+                Object.Destroy(environment);
+            
+            _loadedEnvironments.Remove(addressablePath);
+        }
+    }
+}
