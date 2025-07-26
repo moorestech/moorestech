1639f29f77c7519a06d93ac217d149dbe4448456
sakastudio <katsumi.sato@aktsk.jp>
2025-07-26 18:26:47 +0900

    BaseCampのUI作成

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/BaseCampBlockInventoryView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/BaseCampBlockInventoryView.cs
@@ -4,6 +4,8 @@ using Client.Game.InGame.Block;
 using Client.Game.InGame.Context;
 using Client.Game.InGame.UI.Inventory.Common;
 using Core.Item.Interface;
+using Core.Master;
+using Game.Block.Blocks.BaseCamp;
 using Game.Context;
 using Mooresmaster.Model.BlocksModule;
 using UnityEngine;
@@ -14,9 +16,14 @@ namespace Client.Game.InGame.UI.Inventory.Block
     public class BaseCampBlockInventoryView : CommonBlockInventoryViewBase
     {
         [SerializeField] private RectTransform slotsParent;
+        [SerializeField] private RectTransform requiredItemsParent;
+        
+        [SerializeField] private Slider progressSlider;
+        
         [SerializeField] private Button completeButton;
         
         private BlockGameObject _blockGameObject;
+        private Dictionary<ItemId, int> _requiredItems;
         
         private void Awake()
         {
@@ -45,6 +52,20 @@ namespace Client.Game.InGame.UI.Inventory.Block
             }
             
             var param = (BaseCampBlockParam)blockGameObject.BlockMasterElement.BlockParam;
+            
+            // 必要アイテムをDictionaryに変換
+            // Convert required items to Dictionary
+            _requiredItems = new Dictionary<ItemId, int>();
+            foreach (var item in param.RequiredItems)
+            {
+                var itemId = MasterHolder.ItemMaster.GetItemId(item.ItemGuid);
+                var itemView = ClientContext.ItemImageContainer.GetItemView(itemId);
+                
+                var slotObject = Instantiate(ItemSlotView.Prefab, requiredItemsParent);
+                slotObject.SetItem(itemView, item.Amount);
+                _requiredItems[itemId] = item.Amount;
+            }
+            
             for (var i = 0; i < param.InventorySlot; i++)
             {
                 var slotObject = Instantiate(ItemSlotView.Prefab, slotsParent);
@@ -53,6 +74,19 @@ namespace Client.Game.InGame.UI.Inventory.Block
             }
             
             UpdateItemList(itemList);
+        }
+        
+        private void Update()
+        {
+            if (_requiredItems == null || SubInventory == null) return;
+            
+            // SubInventoryのアイテムリストを使って進捗を計算
+            // Calculate progress using SubInventory item list
+            var isComplete = BaseCampCalculator.CalculateIsCompleted(SubInventory, _requiredItems);
+            completeButton.interactable = isComplete;
+            
+            var progress = BaseCampCalculator.CalculateProgress(SubInventory, _requiredItems);
+            progressSlider.value = progress;
         }
     }
 }
