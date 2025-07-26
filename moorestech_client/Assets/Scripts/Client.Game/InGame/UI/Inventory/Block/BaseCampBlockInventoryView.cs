f808f606e0e7f366bc7c469f9e8d6643e7ee3ed1
sakastudio <katsumi.sato@aktsk.jp>
2025-07-26 15:58:46 +0900

    とりあえず2つのベースキャンプブロックを追加

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/BaseCampBlockInventoryView.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using Client.Game.InGame.Block;
+using Client.Game.InGame.UI.Inventory.Common;
+using Core.Item.Interface;
+using Game.Context;
+using Mooresmaster.Model.BlocksModule;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Client.Game.InGame.UI.Inventory.Block
+{
+    public class BaseCampBlockInventoryView : CommonBlockInventoryViewBase
+    {
+        [SerializeField] private RectTransform slotsParent;
+        [SerializeField] private Button completeButton;
+        
+        private void Awake()
+        {
+            completeButton.onClick.AddListener(() =>
+            {
+                
+            });
+        }
+        
+        public override void Initialize(BlockGameObject blockGameObject)
+        {
+            base.Initialize(blockGameObject);
+            
+            // アイテムリストを初期化
+            // Initialize item list
+            var itemList = new List<IItemStack>();
+            
+            if (blockGameObject.BlockMasterElement.BlockParam is not BaseCampBlockParam)
+            {
+                var blockName = blockGameObject.BlockMasterElement.Name;
+                var guid = blockGameObject.BlockMasterElement.BlockGuid;
+                // TODO ログ基盤にいれる
+                Debug.LogError($"ブロック名:{blockName} guid:{guid} はBaseCampBlockParamを持っていません。指定しているUIを見直すか、スキーマを見直してください。");
+                return;
+            }
+            
+            var param = (BaseCampBlockParam)blockGameObject.BlockMasterElement.BlockParam;
+            for (var i = 0; i < param.InventorySlot; i++)
+            {
+                var slotObject = Instantiate(ItemSlotView.Prefab, slotsParent);
+                SubInventorySlotObjectsInternal.Add(slotObject);
+                itemList.Add(ServerContext.ItemStackFactory.CreatEmpty());
+            }
+            
+            UpdateItemList(itemList);
+        }
+    }
+}
