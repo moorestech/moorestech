222d52e9f03b9b60cb809eb249663296acec7bfb
sakastudio <katsumi.sato@aktsk.jp>
2025-07-31 15:29:18 +0900

    ライン描画システムの実装

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Challenge/ChallengeTreeView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Challenge/ChallengeTreeView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Mooresmaster.Model.ChallengesModule;
 using UnityEngine;
@@ -11,17 +12,39 @@ namespace Client.Game.InGame.UI.Challenge
         [SerializeField] private Transform challengeListParent;
         [SerializeField] private Transform connectLineParent; // 線は一番下に表示される必要があるため専用の親に格納する
         
-        private readonly List<ChallengeTreeViewElement> _challengeElements = new();
+        private readonly Dictionary<Guid, ChallengeTreeViewElement> _challengeElementsDictionary = new();
         
         public void SetChallengeCategory(ChallengeCategoryMasterElement category)
         {
+            // 既存の要素をクリア
+            ClearChallengeElements();
+            
+            // 新しいチャレンジ要素を作成
             foreach (var challenge in category.Challenges)
             {
                 var challengeElement = Instantiate(categoryElement, challengeListParent);
                 challengeElement.SetChallenge(challenge);
                 
-                _challengeElements.Add(challengeElement);
+                _challengeElementsDictionary.Add(challenge.ChallengeGuid, challengeElement);
             }
+            
+            // 接続線を作成
+            foreach (var challengeElement in _challengeElementsDictionary.Values)
+            {
+                challengeElement.CreateConnect(connectLineParent, _challengeElementsDictionary);
+            }
+        }
+        
+        private void ClearChallengeElements()
+        {
+            foreach (var element in _challengeElementsDictionary.Values)
+            {
+                if (element != null)
+                {
+                    Destroy(element.gameObject);
+                }
+            }
+            _challengeElementsDictionary.Clear();
         }
     }
 }
--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Challenge/ChallengeTreeViewElement.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Challenge/ChallengeTreeViewElement.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Mooresmaster.Model.ChallengesModule;
 using UnityEngine;
 
@@ -5,12 +7,88 @@ namespace Client.Game.InGame.UI.Challenge
 {
     public class ChallengeTreeViewElement : MonoBehaviour
     {
+        public Vector2 AnchoredPosition => rectTransform.anchoredPosition;
+        public ChallengeMasterElement ChallengeMasterElement { get; private set; }
+        
         [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private RectTransform connectLinePrefab;
+        
+        // 生成された接続線のリスト
+        private readonly List<RectTransform> _connectLines = new();
         
         public void SetChallenge(ChallengeMasterElement challengeMasterElement)
         {
+            ChallengeMasterElement = challengeMasterElement;
             rectTransform.anchoredPosition = challengeMasterElement.DisplayListParam.UIPosition;
             rectTransform.localScale = challengeMasterElement.DisplayListParam.UIScale;
+        }
+        
+        public void CreateConnect(Transform lineParent, Dictionary<Guid, ChallengeTreeViewElement> challengeElements)
+        {
+            // 既存の接続線をクリア
+            ClearConnectLines();
+            
+            // 前のチャレンジがある場合、線を引く
+            var prevGuids = ChallengeMasterElement.PrevChallengeGuids;
+            if (prevGuids == null) return;
+            
+            foreach (var prev in prevGuids)
+            {
+                if (!challengeElements.TryGetValue(prev, out var prevChallengeElement)) continue;
+                
+                // 線を引く
+                CreateLine(prevChallengeElement, lineParent);
+            }
+            
+            #region Internal
+            
+            void CreateLine(ChallengeTreeViewElement prevChallengeElement, Transform parent)
+            {
+                // 線の長さと角度を計算して適用
+                var currentPosition = AnchoredPosition;
+                var targetPosition = prevChallengeElement.AnchoredPosition;
+                
+                // 接続線を取得または作成
+                var connectLine = Instantiate(connectLinePrefab, transform);
+                connectLine.gameObject.SetActive(true);
+                
+                var distance = Vector2.Distance(currentPosition, targetPosition);
+                connectLine.sizeDelta = new Vector2(distance, connectLine.sizeDelta.y);
+                
+                var angle = Mathf.Atan2(targetPosition.y - currentPosition.y, targetPosition.x - currentPosition.x) * Mathf.Rad2Deg;
+                connectLine.localEulerAngles = new Vector3(0, 0, angle);
+                
+                // 親の位置を変更
+                connectLine.SetParent(parent);
+                
+                // 親によってスケールが変わっている可能性があるので戻す
+                connectLine.localScale = Vector3.one;
+                
+                // 接続線リストに追加
+                _connectLines.Add(connectLine);
+            }
+            
+            #endregion
+        }
+        
+        private void OnDestroy()
+        {
+            // 生成された接続線を削除
+            ClearConnectLines();
+        }
+        
+        private void ClearConnectLines()
+        {
+            // 生成された接続線を削除
+            foreach (var line in _connectLines)
+            {
+                if (line != null)
+                {
+                    Destroy(line.gameObject);
+                }
+            }
+            
+            _connectLines.Clear();
         }
     }
 }
