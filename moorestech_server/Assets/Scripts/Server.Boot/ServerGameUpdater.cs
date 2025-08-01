f4b54f07e842c3b3db1c314468a95408d6af894c
sakastudio <sakastudio100@gmail.com>
2025-08-01 19:24:04 +0900

    アップデートのtaskの作成

---
+++ moorestech_server/Assets/Scripts/Server.Boot/ServerGameUpdater.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Core.Update;
+
+namespace Server.Boot
+{
+    public class ServerGameUpdater
+    {
+        public static Task CreateUpdateThread()
+        {
+            var  startTask = Task.Run(StartUpdate);;
+            return startTask;
+        }
+        
+        public const int FrameIntervalMs = 100;
+        private static readonly TimeSpan FrameInterval = TimeSpan.FromMilliseconds(FrameIntervalMs);
+        
+        
+        private static async Task StartUpdate()
+        {
+            var stopwatch = new Stopwatch();
+            
+            while (true)
+            {
+                stopwatch.Restart();
+                
+                try
+                {
+                    GameUpdater.Update();
+                }
+                catch (Exception ex)
+                {
+                    UnityEngine.Debug.LogException(ex);
+                }
+                
+                // 経過時間を測定
+                var remaining = FrameInterval - stopwatch.Elapsed;
+                
+                // まだフレーム時間が余っていれば、その分だけ待機
+                if (remaining > TimeSpan.Zero)
+                    await Task.Delay(remaining);
+            }
+        }
+    }
+}
--- moorestech_server/Assets/ServerStarter.cs
+++ moorestech_server/Assets/ServerStarter.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Core.Update;
 using Server.Boot;
 using UnityEngine;
@@ -7,17 +8,13 @@ using UnityEngine;
 public class ServerStarter : MonoBehaviour
 {
     private CancellationTokenSource _autoSaveToken;
-    private Thread _serverUpdateThread;
+    private Thread _connectionUpdateThread;
+    private Task _gameUpdaterThread;
 
     private void Start()
     {
-        (_serverUpdateThread, _autoSaveToken) = StartServer.Start(new string[] { });
-        _serverUpdateThread.Start();
-    }
-
-    private void FixedUpdate()
-    {
-        GameUpdater.Update();
+        (_connectionUpdateThread, _gameUpdaterThread, _autoSaveToken) = StartServer.Start(new string[] { });
+        _connectionUpdateThread.Start();
     }
 
     private void OnDestroy()
@@ -33,7 +30,8 @@ public class ServerStarter : MonoBehaviour
     private void FinishServer()
     {
         Debug.Log("サーバーを終了します");
-        _serverUpdateThread?.Abort();
+        _gameUpdaterThread?.Dispose();
+        _connectionUpdateThread?.Abort();
         _autoSaveToken?.Cancel();
         GameUpdater.Dispose();
         Debug.Log("サーバーを終了しました");
