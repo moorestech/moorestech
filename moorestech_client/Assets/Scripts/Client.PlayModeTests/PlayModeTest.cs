bdf854455c14f3bc3fd8b8f0a48a04219233d5fd
sakastudio <sakastudio100@gmail.com>
2025-08-07 16:55:13 +0900

    プレイモードテストの実装

--- moorestech_client/Assets/Scripts/Client.PlayModeTests/PlayModeTest.cs
+++ moorestech_client/Assets/Scripts/Client.PlayModeTests/PlayModeTest.cs
@@ -1,4 +1,7 @@
 using System.Threading.Tasks;
+using Client.Common;
+using Client.Starter;
+using Cysharp.Threading.Tasks;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -11,9 +14,16 @@ namespace Client.PlayModeTests
         [Test]
         public async Task  NewTestScriptSimplePasses()
         {
-            // 最初のSceneをロード（"Scenes in Build" に含まれている必要があります）
-            await SceneManager.LoadSceneAsync("MainGame");
+            SceneManager.LoadScene(SceneConstant.GameInitializerSceneName);
             
+            // GameInitializerSceneLoaderが表示されるか15秒のタイムアウトを待つ
+            // Wait for GameInitializerSceneLoader to appear or 15 seconds timeout
+            var timeout = UniTask.Delay(15000);
+            var waitForLoader = UniTask.WaitUntil(() => Object.FindObjectOfType<GameInitializerSceneLoader>() != null);
+            await UniTask.WhenAny(waitForLoader, timeout);
+            
+            var loader = Object.FindObjectOfType<GameInitializerSceneLoader>();
+            Assert.IsNotNull(loader, "GameInitializerSceneLoader was not found within 15 seconds");
         }
     }
 }
