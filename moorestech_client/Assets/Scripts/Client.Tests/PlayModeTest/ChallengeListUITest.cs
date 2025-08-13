1b8b77f9145273022c6213be81f04b9ed9288c0d
sakastudio <sakastudio100@gmail.com>
2025-08-13 19:05:32 +0900

    不要なもう一個のテストを削除

--- moorestech_client/Assets/Scripts/Client.Tests/PlayModeTest/ChallengeListUITest.cs
+++ moorestech_client/Assets/Scripts/Client.Tests/PlayModeTest/ChallengeListUITest.cs
@@ -55,52 +55,5 @@ namespace Client.Tests.PlayModeTest
             
             #endregion
         }
-        
-        
-        
-        [UnityTest]
-        public IEnumerator CategoryElementTest2()
-        {
-            yield return new EnterPlayMode(expectDomainReload: true);
-            
-            yield return Test().ToCoroutine();
-            
-            yield return new ExitPlayMode();
-            
-            #region Internal
-            
-            async UniTask Test()
-            {
-                await PlayModeTestUtil.LoadMainGame();
-                
-                var challengeListView = Object.FindFirstObjectByType<ChallengeListView>(FindObjectsInactive.Include);
-                var categoryParent = challengeListView.DebugCategoryListParent;
-                
-                // 子要素（カテゴリの要素）が1個であることを確認
-                // Confirm that there is one child element (category element)
-                Assert.AreEqual(1, GetCategoryCount(categoryParent));
-                
-                // アイテムを付与してチャレンジ1を完了
-                // Grant an item and complete Challenge 1
-                await PlayModeTestUtil.GiveItem("小石", 1);
-                
-                // カテゴリが増えていないことを確認
-                // Confirm that the category has not increased
-                Assert.AreEqual(1, GetCategoryCount(categoryParent));
-                
-                await PlayModeTestUtil.GiveItem("石器", 1);
-                
-                // 上記のチャレンジクリアによってカテゴリが増えることを確認
-                // Confirm that the category increases due to the completion of the above challenge
-                Assert.AreEqual(2, GetCategoryCount(categoryParent));
-            }
-            
-            int GetCategoryCount(Transform categoryParent)
-            {
-                return categoryParent.GetComponentsInChildren<ChallengeListViewCategoryElement>().Length;
-            }
-            
-            #endregion
-        }
     }
 }
