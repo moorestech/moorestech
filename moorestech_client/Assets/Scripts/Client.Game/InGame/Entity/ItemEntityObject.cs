8af25427788b3881b348194167dd078a410c2875
sakastudio <sakastudio100@gmail.com>
2025-08-19 17:08:10 +0900

    一旦補完を0にする

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Entity/ItemEntityObject.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Entity/ItemEntityObject.cs
@@ -44,6 +44,8 @@ namespace Client.Game.InGame.Entity
         
         public void SetInterpolationPosition(Vector3 position)
         {
+            SetDirectPosition(position);
+            return;
             _previousPosition = transform.position;
             _targetPosition = position;
             _linerTime = 0;
