43b9933cf4c2a6bda6d84920ba76f4c406a1e7e2
sakastudio <sakastudio100@gmail.com>
2025-09-14 19:24:56 +0900

    GameActionを作成

---
+++ moorestech_server/Assets/Scripts/Game.Action/GameActionExecutor.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using Core.Master;
+using Game.UnlockState;
+using Mooresmaster.Model.ChallengeActionModule;
+
+namespace Game.Action
+{
+    public class GameActionExecutor : IGameActionExecutor
+    {
+        private readonly IGameUnlockStateDataController _gameUnlockStateDataController;
+
+        public GameActionExecutor(IGameUnlockStateDataController gameUnlockStateDataController)
+        {
+            _gameUnlockStateDataController = gameUnlockStateDataController;
+        }
+        public void ExecuteUnlockActions(ChallengeActionElement[] actions)
+        {
+            foreach (var action in actions)
+            {
+                switch (action.ChallengeActionType)
+                {
+                    case ChallengeActionElement.ChallengeActionTypeConst.unlockCraftRecipe:
+                    case ChallengeActionElement.ChallengeActionTypeConst.unlockItemRecipeView:
+                    case ChallengeActionElement.ChallengeActionTypeConst.unlockChallengeCategory:
+                        ExecuteAction(action);
+                        break;
+                }
+            }
+        }
+        public void ExecuteActions(ChallengeActionElement[] actions)
+        {
+            foreach (var action in actions)
+            {
+                ExecuteAction(action);
+            }
+        }
+        
+        private void ExecuteAction(ChallengeActionElement action)
+        {
+            if (action == null) return;
+            
+            switch (action.ChallengeActionType)
+            {
+                case ChallengeActionElement.ChallengeActionTypeConst.unlockCraftRecipe:
+                    UnlockCraftRecipe();
+                    break;
+                
+                case ChallengeActionElement.ChallengeActionTypeConst.unlockItemRecipeView:
+                    UnlockItemRecipeView();
+                    break;
+                
+                case ChallengeActionElement.ChallengeActionTypeConst.unlockChallengeCategory:
+                    UnlockChallengeCategory();
+                    break;
+            }
+            
+            #region Internal
+            
+            void UnlockCraftRecipe()
+            {
+                var unlockRecipeGuids = ((UnlockCraftRecipeChallengeActionParam)action.ChallengeActionParam).UnlockRecipeGuids;
+                foreach (var guid in unlockRecipeGuids)
+                {
+                    _gameUnlockStateDataController.UnlockCraftRecipe(guid);
+                }
+            }
+            
+            void UnlockItemRecipeView()
+            {
+                var itemGuids = ((UnlockItemRecipeViewChallengeActionParam)action.ChallengeActionParam).UnlockItemGuids;
+                foreach (var itemGuid in itemGuids)
+                {
+                    var itemId = MasterHolder.ItemMaster.GetItemId(itemGuid);
+                    _gameUnlockStateDataController.UnlockItem(itemId);
+                }
+            }
+            
+            void UnlockChallengeCategory()
+            {
+                var challenges = ((UnlockChallengeCategoryChallengeActionParam)action.ChallengeActionParam).UnlockChallengeCategoryGuids;
+                foreach (var guid in challenges)
+                {
+                    _gameUnlockStateDataController.UnlockChallenge(guid);
+                }
+            }
+            
+            #endregion
+        }
+    }
+}
