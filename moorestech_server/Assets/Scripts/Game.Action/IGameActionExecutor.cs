9d408d65a5ad45ce53a18f63b52d957e5b2e451a
sakastudio <sakastudio100@gmail.com>
2025-09-14 17:09:36 +0900

    Revert "claude codeの実装"
    This reverts commit 397c2dafc6a5eb5398c1cd8453d9586fa8e4a1a8.

--- moorestech_server/Assets/Scripts/Game.Action/IGameActionExecutor.cs
+++
@@ -1,9 +0,0 @@
-using Mooresmaster.Model.ChallengeActionModule;
-
-namespace Game.Action
-{
-    public interface IGameActionExecutor
-    {
-        void ExecuteAction(ChallengeActionElement action);
-    }
-}
