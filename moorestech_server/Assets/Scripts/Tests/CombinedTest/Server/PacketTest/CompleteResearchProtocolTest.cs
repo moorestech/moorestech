9bdb0de9090d03b4d1009be546443681ea4686f0
sakastudio <sakastudio100@gmail.com>
2025-09-16 01:13:35 +0900

    テストコード修正

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/CompleteResearchProtocolTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/CompleteResearchProtocolTest.cs
@@ -7,8 +7,11 @@ using MessagePack;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Server.Boot;
+using Server.Protocol;
 using Server.Protocol.PacketResponse;
 using Tests.Module.TestMod;
+using static Tests.CombinedTest.Game.ResearchDataStoreTest;
+
 
 namespace Tests.CombinedTest.Server.PacketTest
 {
@@ -19,71 +22,86 @@ namespace Tests.CombinedTest.Server.PacketTest
         {
             var (packet, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(new MoorestechServerDIContainerOptions(TestModDirectory.ForUnitTestModDirectory));
             
-            const int playerId = 0;
-            var researchGuid1 = Guid.Parse("cd05e30d-d599-46d3-a079-769113cbbf17"); // Research 1 - no prerequisites
-            var researchGuid2 = Guid.Parse("7f1464a7-ba55-4b96-9257-cfdeddf5bbdd"); // Research 2 - requires Research 1
-            var researchGuid4 = Guid.Parse("bf9bda9e-dace-43c4-9a33-75f248fd17f6"); // Research 4 - no prerequisites
-            
-            // プレイヤーインベントリにアイテムを追加
-            var playerInventoryData = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(playerId);
-            var itemId1 = MasterHolder.ItemMaster.GetItemId(Guid.Parse("00000000-0000-0000-1234-000000000001"));
-            
-            // Research 1の完了テスト（アイテムあり）
-            // 必要なアイテムを配置（1個必要）
-            var item = ServerContext.ItemStackFactory.Create(itemId1, 1);
-            playerInventoryData.MainOpenableInventory.SetItem(0, item);
-            
-            // 研究を完了
-            var requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, researchGuid1)).ToList();
-            var response = packet.GetPacketResponse(requestData);
-            var responseData = MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
-            
+            // 研究1の完了テスト
+            // Study 1 Completion Test
+            AddRequiredItemsForResearch(serviceProvider, Research1Guid);
+            var responseData = SendCompleteResearchRequest(packet, Research1Guid);
+
             Assert.IsTrue(responseData.Success);
-            Assert.AreEqual(researchGuid1.ToString(), responseData.CompletedResearchGuidStr);
-            
+            Assert.AreEqual(Research1Guid.ToString(), responseData.CompletedResearchGuidStr);
+
             // アイテムが消費されたことを確認
-            Assert.AreEqual(0, playerInventoryData.MainOpenableInventory.GetItem(0).Count);
+            // Confirm that the item has been consumed
+            CheckInventoryEmpty(serviceProvider);
             
             // 同じ研究をもう一度完了しようとすると失敗する
-            requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, researchGuid1)).ToList();
-            response = packet.GetPacketResponse(requestData);
-            responseData = MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
-            
+            // Fails when trying to complete the same study again
+            responseData = SendCompleteResearchRequest(packet, Research1Guid);
+
             Assert.IsFalse(responseData.Success);
             
-            // Research 2の完了テスト（前提条件あり）
-            // 必要なアイテムを配置（1個必要）
-            item = ServerContext.ItemStackFactory.Create(itemId1, 1);
-            playerInventoryData.MainOpenableInventory.SetItem(1, item);
-            
-            requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, researchGuid2)).ToList();
-            response = packet.GetPacketResponse(requestData);
-            responseData = MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
-            
+            // Research 2の完了テスト（テスト1の完了条件あり）
+            // Research 2 Completion Test (with Test 1 completion conditions)
+            AddRequiredItemsForResearch(serviceProvider, Research2Guid);
+            responseData = SendCompleteResearchRequest(packet, Research2Guid);
+
             Assert.IsTrue(responseData.Success);
-            Assert.AreEqual(researchGuid2.ToString(), responseData.CompletedResearchGuidStr);
+            Assert.AreEqual(Research2Guid.ToString(), responseData.CompletedResearchGuidStr);
             
             // Research 4の完了テスト（アイテム不足）
-            // アイテムが1個だけの状態で2個必要な研究を試みる
-            item = ServerContext.ItemStackFactory.Create(itemId1, 1);
-            playerInventoryData.MainOpenableInventory.SetItem(2, item);
-            
-            requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, researchGuid4)).ToList();
-            response = packet.GetPacketResponse(requestData);
-            responseData = MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
-            
+            // Research 4 Completion Test (Insufficient Items)
+            AddItem1(serviceProvider, 1);
+
+            responseData = SendCompleteResearchRequest(packet, Research4Guid);
+
             Assert.IsFalse(responseData.Success);
             
             // Research 4の完了テスト（アイテム十分）
-            item = ServerContext.ItemStackFactory.Create(itemId1, 2);
-            playerInventoryData.MainOpenableInventory.SetItem(3, item);
-            
-            requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, researchGuid4)).ToList();
-            response = packet.GetPacketResponse(requestData);
-            responseData = MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
-            
+            // Research 4 Completion Test (Sufficient Items)
+            AddItem1(serviceProvider, 1);
+
+            responseData = SendCompleteResearchRequest(packet, Research4Guid);
+
             Assert.IsTrue(responseData.Success);
-            Assert.AreEqual(researchGuid4.ToString(), responseData.CompletedResearchGuidStr);
+            Assert.AreEqual(Research4Guid.ToString(), responseData.CompletedResearchGuidStr);
+        }
+
+        private void AddRequiredItemsForResearch(ServiceProvider serviceProvider, Guid researchGuid)
+        {
+            var playerInventoryData = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId);
+            var researchMaster = MasterHolder.ResearchMaster.GetResearch(researchGuid);
+
+            foreach (var consumeItem in researchMaster.ConsumeItems)
+            {
+                var item = ServerContext.ItemStackFactory.Create(consumeItem.ItemGuid, consumeItem.ItemCount);
+                playerInventoryData.MainOpenableInventory.InsertItem(item);
+            }
+        }
+
+        private void AddItem1(ServiceProvider serviceProvider, int itemCount)
+        {
+            var playerInventoryData = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId);
+            var itemId1 = MasterHolder.ItemMaster.GetItemId(Guid.Parse("00000000-0000-0000-1234-000000000001"));
+            var item = ServerContext.ItemStackFactory.Create(itemId1, itemCount);
+            playerInventoryData.MainOpenableInventory.InsertItem(item);
+        }
+        
+        private void CheckInventoryEmpty(ServiceProvider serviceProvider)
+        {
+            var playerInventoryData = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(PlayerId);
+            for (int i = 0; i < playerInventoryData.MainOpenableInventory.GetSlotSize(); i++)
+            {
+                var item = playerInventoryData.MainOpenableInventory.GetItem(i);
+                Assert.IsTrue(item.Id == ItemMaster.EmptyItemId);
+            }
+        }
+
+        private CompleteResearchProtocol.ResponseCompleteResearchMessagePack SendCompleteResearchRequest(PacketResponseCreator packet, Guid researchGuid)
+        {
+            var requestData = MessagePackSerializer.Serialize(new CompleteResearchProtocol.RequestCompleteResearchMessagePack(PlayerId, researchGuid)).ToList();
+            var response = packet.GetPacketResponse(requestData);
+            
+            return MessagePackSerializer.Deserialize<CompleteResearchProtocol.ResponseCompleteResearchMessagePack>(response[0].ToArray());
         }
     }
 }
--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/Event/ResearchCompleteEventPacketTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/Event/ResearchCompleteEventPacketTest.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Linq;
-using Core.Master;
-using Game.Context;
-using Game.PlayerInventory.Interface;
-using Game.Research;
 using MessagePack;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Server.Boot;
+using Server.Event;
 using Server.Event.EventReceive;
-using Server.Protocol.PacketResponse;
 using Tests.Module.TestMod;
+using static Server.Protocol.PacketResponse.EventProtocol;
+using static Tests.CombinedTest.Game.ResearchDataStoreTest;
 
 namespace Tests.CombinedTest.Server.PacketTest.Event
 {
@@ -21,65 +17,45 @@ namespace Tests.CombinedTest.Server.PacketTest.Event
         {
             var (packetResponse, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(new MoorestechServerDIContainerOptions(TestModDirectory.ForUnitTestModDirectory));
 
-            const int playerId = 0;
-            var researchGuid1 = Guid.Parse("cd05e30d-d599-46d3-a079-769113cbbf17"); // Research 1 - no prerequisites
-            var researchGuid2 = Guid.Parse("7f1464a7-ba55-4b96-9257-cfdeddf5bbdd"); // Research 2 - requires Research 1
-
             // イベントがないことを確認する
-            var response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(playerId));
-            var eventMessagePack = MessagePackSerializer.Deserialize<EventProtocol.ResponseEventProtocolMessagePack>(response[0].ToArray());
+            var response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(PlayerId));
+            var eventMessagePack = MessagePackSerializer.Deserialize<ResponseEventProtocolMessagePack>(response[0].ToArray());
             Assert.AreEqual(0, eventMessagePack.Events.Count);
 
-            // プレイヤーインベントリにアイテムを追加
-            var playerInventoryData = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(playerId);
-            var itemId1 = MasterHolder.ItemMaster.GetItemId(Guid.Parse("00000000-0000-0000-1234-000000000001"));
-
             // Research 1を完了させる
-            // 必要なアイテムを配置（1個必要）
-            var item = ServerContext.ItemStackFactory.Create(itemId1, 1);
-            playerInventoryData.MainOpenableInventory.SetItem(0, item);
-
-            // 研究を完了
-            var researchDataStore = serviceProvider.GetService<IResearchDataStore>();
-            var result = researchDataStore.CompleteResearch(researchGuid1, playerId);
-            Assert.IsTrue(result);
+            CompleteResearchForTest(serviceProvider, Research1Guid);
 
             // イベントを受け取り、テストする
-            response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(playerId));
-            eventMessagePack = MessagePackSerializer.Deserialize<EventProtocol.ResponseEventProtocolMessagePack>(response[0].ToArray());
+            response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(PlayerId));
+            eventMessagePack = MessagePackSerializer.Deserialize<ResponseEventProtocolMessagePack>(response[0].ToArray());
 
             // イベントがあることを確認する
             Assert.AreEqual(1, eventMessagePack.Events.Count);
 
             // 研究完了イベントを確認
-            var researchCompleteEvent = eventMessagePack.Events[0];
-            Assert.AreEqual(ResearchCompleteEventPacket.EventTag, researchCompleteEvent.Tag);
-
-            var researchCompleteData = MessagePackSerializer.Deserialize<ResearchCompleteEventPacket.ResearchCompleteEventMessagePack>(researchCompleteEvent.Payload);
-            Assert.AreEqual(playerId, researchCompleteData.PlayerId);
-            Assert.AreEqual(researchGuid1.ToString(), researchCompleteData.ResearchGuidStr);
+            VerifyResearchCompleteEvent(eventMessagePack.Events[0], Research1Guid);
 
             // Research 2を完了させる（前提条件付き）
-            item = ServerContext.ItemStackFactory.Create(itemId1, 1);
-            playerInventoryData.MainOpenableInventory.SetItem(1, item);
-
-            result = researchDataStore.CompleteResearch(researchGuid2, playerId);
-            Assert.IsTrue(result);
+            CompleteResearchForTest(serviceProvider, Research2Guid);
 
             // イベントを受け取り、テストする
-            response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(playerId));
-            eventMessagePack = MessagePackSerializer.Deserialize<EventProtocol.ResponseEventProtocolMessagePack>(response[0].ToArray());
+            response = packetResponse.GetPacketResponse(EventTestUtil.EventRequestData(PlayerId));
+            eventMessagePack = MessagePackSerializer.Deserialize<ResponseEventProtocolMessagePack>(response[0].ToArray());
 
             // 新しいイベントがあることを確認する
             Assert.AreEqual(1, eventMessagePack.Events.Count);
 
             // Research 2の完了イベントを確認
-            researchCompleteEvent = eventMessagePack.Events[0];
-            Assert.AreEqual(ResearchCompleteEventPacket.EventTag, researchCompleteEvent.Tag);
+            VerifyResearchCompleteEvent(eventMessagePack.Events[0], Research2Guid);
+        }
 
-            researchCompleteData = MessagePackSerializer.Deserialize<ResearchCompleteEventPacket.ResearchCompleteEventMessagePack>(researchCompleteEvent.Payload);
-            Assert.AreEqual(playerId, researchCompleteData.PlayerId);
-            Assert.AreEqual(researchGuid2.ToString(), researchCompleteData.ResearchGuidStr);
+        private void VerifyResearchCompleteEvent(EventMessagePack eventData, Guid expectedResearchGuid)
+        {
+            Assert.AreEqual(ResearchCompleteEventPacket.EventTag, eventData.Tag);
+
+            var researchCompleteData = MessagePackSerializer.Deserialize<ResearchCompleteEventPacket.ResearchCompleteEventMessagePack>(eventData.Payload);
+            Assert.AreEqual(PlayerId, researchCompleteData.PlayerId);
+            Assert.AreEqual(expectedResearchGuid.ToString(), researchCompleteData.ResearchGuidStr);
         }
     }
 }
