eeb1afa2269613aab47a6e7d8c549c7bca2a2c70
sakastudio <sakastudio100@gmail.com>
2025-09-15 16:34:51 +0900

    Revert "codex"
    This reverts commit ca326154bffcec78b7dfdb2b41a6c0c255bc6017.

--- moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/Event/ResearchCompleteEventPacketTest.cs
+++ moorestech_server/Assets/Scripts/Tests/CombinedTest/Server/PacketTest/Event/ResearchCompleteEventPacketTest.cs
@@ -1,13 +1,5 @@
-using System.Linq;
-using Core.Master;
-using Game.Context;
-using Game.PlayerInventory.Interface;
-using MessagePack;
-using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Server.Boot;
-using Server.Event.EventReceive;
-using Server.Protocol.PacketResponse;
 using Tests.Module.TestMod;
 
 namespace Tests.CombinedTest.Server.PacketTest.Event
@@ -17,43 +9,8 @@ namespace Tests.CombinedTest.Server.PacketTest.Event
         [Test]
         public void ResearchCompleteToEventPacketTest()
         {
-            var (packet, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(new MoorestechServerDIContainerOptions(TestModDirectory.ForUnitTestModDirectory));
-            const int playerId = 1;
+            var (packetResponse, serviceProvider) = new MoorestechServerDIContainerGenerator().Create(new MoorestechServerDIContainerOptions(TestModDirectory.ForUnitTestModDirectory));
 
-            // まずイベントキューを初期化（プレイヤー登録）
-            packet.GetPacketResponse(EventTestUtil.EventRequestData(playerId));
-
-            // 対象の研究を選定（消費無し優先）
-            var all = MasterHolder.ResearchMaster.GetAllResearches();
-            var target = all.FirstOrDefault(e => e.ConsumeItems == null || e.ConsumeItems.Length == 0) ?? all.First();
-
-            // 必要であれば要求アイテムを投入
-            if (target.ConsumeItems != null)
-            {
-                var inv = serviceProvider.GetService<IPlayerInventoryDataStore>().GetInventoryData(playerId);
-                for (var i = 0; i < target.ConsumeItems.Length; i++)
-                {
-                    var req = target.ConsumeItems[i];
-                    var stack = ServerContext.ItemStackFactory.Create(req.ItemGuid, req.ItemCount);
-                    inv.MainOpenableInventory.SetItem(i, stack);
-                }
-            }
-
-            // 研究完了をプロトコル経由で実行
-            var req = new CompleteResearchProtocol.RequestCompleteResearchMessagePack(playerId, target.ResearchNodeGuid);
-            packet.GetPacketResponse(MessagePackSerializer.Serialize(req).ToList());
-
-            // イベントを取得
-            var evBytes = packet.GetPacketResponse(EventTestUtil.EventRequestData(playerId));
-            var ev = MessagePackSerializer.Deserialize<EventProtocol.ResponseEventProtocolMessagePack>(evBytes[0].ToArray());
-
-            // 研究完了イベントを抽出
-            var researchEvent = ev.Events.FirstOrDefault(e => e.Tag == ResearchCompleteEventPacket.EventTag);
-            Assert.IsNotNull(researchEvent, "ResearchComplete event not found");
-
-            var payload = MessagePackSerializer.Deserialize<ResearchCompleteEventPacket.ResearchCompleteEventMessagePack>(researchEvent.Payload);
-            Assert.AreEqual(playerId, payload.PlayerId);
-            Assert.AreEqual(target.ResearchNodeGuid.ToString(), payload.ResearchGuidStr);
         }
     }
 }
