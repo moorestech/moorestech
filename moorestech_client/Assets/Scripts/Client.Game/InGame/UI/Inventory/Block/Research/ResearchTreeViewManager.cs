14c641c5919d11196d38ef92ae7536a82185afd4
sakastudio <sakastudio100@gmail.com>
2025-09-16 11:58:07 +0900

    リサーチシステムの基盤

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/Research/ResearchTreeViewManager.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/Research/ResearchTreeViewManager.cs
@@ -1,7 +1,11 @@
 using System.Collections.Generic;
+using System.Threading;
 using Client.Game.InGame.Block;
+using Client.Game.InGame.Context;
 using Client.Game.InGame.UI.Inventory.Common;
+using Client.Network.API;
 using Core.Item.Interface;
+using Cysharp.Threading.Tasks;
 using Server.Protocol.PacketResponse.Util.InventoryMoveUtil;
 using UnityEngine;
 
@@ -9,15 +13,30 @@ namespace Client.Game.InGame.UI.Inventory.Block.Research
 {
     public class ResearchTreeViewManager : MonoBehaviour, IBlockInventoryView
     {
+        [SerializeField] private ResearchTreeView researchTreeView;
+        
         public List<IItemStack> SubInventory { get; } = new();
         public ItemMoveInventoryInfo ItemMoveInventoryInfo { get; private set; }
         public IReadOnlyList<ItemSlotView> SubInventorySlotObjects { get; } = new List<ItemSlotView>();
         public int Count => 0;
         
+        private CancellationTokenSource _cancellationTokenSource;
+        
         public void Initialize(BlockGameObject blockGameObject)
         {
             ItemMoveInventoryInfo = new ItemMoveInventoryInfo(ItemMoveInventoryType.BlockInventory, blockGameObject.BlockPosInfo.OriginalPos);
+            _cancellationTokenSource = new CancellationTokenSource();
+            
+            #region Internal
+            
+            async UniTask GetResearchData()
+            {
+                //var blockStates = await ClientContext.VanillaApi.Response.GetBlockState(pos, _gameObjectCancellationToken);
+            }
+            
+  #endregion
         }
+        
         public void UpdateItemList(List<IItemStack> response) { }
         public void UpdateInventorySlot(int slot, IItemStack item) { }
         public void DestroyUI() { }
