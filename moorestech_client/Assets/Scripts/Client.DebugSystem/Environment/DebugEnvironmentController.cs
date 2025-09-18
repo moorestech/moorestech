e3b7e6bb87a6b3dde3cd55d2c9f9599c48935f67
sakastudio <sakastudio100@gmail.com>
2025-09-18 16:24:33 +0900

    無限落下防止措置を実装

---
+++ moorestech_client/Assets/Scripts/Client.DebugSystem/Environment/DebugEnvironmentController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+namespace Client.DebugSystem.Environment
+{
+    public class DebugEnvironmentController : MonoBehaviour
+    {
+        [SerializeField] private GameObject gameEnvironment;
+        [SerializeField] private GameObject debugEnvironment;
+        [SerializeField] private GameObject pureNatureEnvironment;
+        
+        public static void SetEnvironment(DebugEnvironmentType environmentType)
+        {
+            var debugEnvironmentController = FindObjectOfType<DebugEnvironmentController>();
+            if (debugEnvironmentController == null)
+            {
+                return;
+            }
+            
+            var debugEnvironment = false;
+            var pureNatureEnvironment = false;
+            var gameEnvironment = false;
+            switch (environmentType)
+            {
+                case DebugEnvironmentType.Debug:
+                    debugEnvironment = true;
+                    break;
+                case DebugEnvironmentType.PureNature:
+                    pureNatureEnvironment = true;
+                    break;
+                case DebugEnvironmentType.Game:
+                    gameEnvironment = true;
+                    break;
+            }
+            
+            debugEnvironmentController.debugEnvironment.SetActive(debugEnvironment);
+            debugEnvironmentController.pureNatureEnvironment.SetActive(pureNatureEnvironment);
+            debugEnvironmentController.gameEnvironment.SetActive(gameEnvironment);
+        }
+    }
+    
+    public enum DebugEnvironmentType
+    {
+        Debug,
+        PureNature,
+        Game,
+    }
+}
