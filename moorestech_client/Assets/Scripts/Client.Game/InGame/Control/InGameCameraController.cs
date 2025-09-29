3a9c3713df34883238de1c4946fb607659903a26
Juha <juhasapps@gmail.com>
2025-09-30 03:05:15 +0900

    camera

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Control/InGameCameraController.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Control/InGameCameraController.cs
@@ -41,7 +41,9 @@ namespace Client.Game.InGame.Control
         
         private void Update()
         {
-            var distance = _cinemachineFraming.m_CameraDistance + InputManager.UI.SwitchHotBar.ReadValue<float>() / -200f;
+            var distance = _cinemachineFraming.m_CameraDistance;
+            if (UnityEngine.Input.GetKey(KeyCode.F1)) distance -= Time.deltaTime * 1f;
+            if (UnityEngine.Input.GetKey(KeyCode.F2)) distance += Time.deltaTime * 1f;
             _cinemachineFraming.m_CameraDistance = Mathf.Clamp(distance, 0.6f, 10);
             
             if (!_isControllable && _currentSequence == null) return;
