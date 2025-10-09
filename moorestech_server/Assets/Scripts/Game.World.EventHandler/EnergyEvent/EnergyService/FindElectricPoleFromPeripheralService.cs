9d99ab31cf6e4feeb699bb2120a78f571020cf49
sakastudio <sakastudio100@gmail.com>
2025-10-09 14:36:39 +0900

    Findの自身のブロックが追加されないように

--- moorestech_server/Assets/Scripts/Game.World.EventHandler/EnergyEvent/EnergyService/FindElectricPoleFromPeripheralService.cs
+++ moorestech_server/Assets/Scripts/Game.World.EventHandler/EnergyEvent/EnergyService/FindElectricPoleFromPeripheralService.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Linq;
+using Game.Block.Interface;
 using Game.Context;
 using Game.EnergySystem;
 using Game.World.Interface.DataStore;
@@ -15,45 +17,45 @@ namespace Game.World.EventHandler.EnergyEvent.EnergyService
         /// </summary>
         /// <param name="x"></param>
         /// <param name="y"></param>
-        /// <param name="electricPoleConfigParam"></param>
+        /// <param name="param"></param>
         /// <returns></returns>
-        public static List<IElectricTransformer> Find(Vector3Int pos, ElectricPoleBlockParam electricPoleConfigParam)
+        public static List<IElectricTransformer> Find(Vector3Int pos, ElectricPoleBlockParam param)
         {
-            var electricPoles = new List<IElectricTransformer>();
+            var electricPoles = new Dictionary<BlockInstanceId,IElectricTransformer>();
             var blockDatastore = ServerContext.WorldBlockDatastore;
+            var selfInstanceId = blockDatastore.GetBlock<IElectricTransformer>(pos).BlockInstanceId;
 
-            foreach (var targetPos in EnumerateRange(pos, electricPoleConfigParam.PoleConnectionRange,
-                         electricPoleConfigParam.PoleConnectionHeightRange))
+            foreach (var targetPos in EnumerateRange(pos, param))
             {
-                if (!blockDatastore.ExistsComponent<IElectricTransformer>(targetPos) || targetPos == pos) continue;
-
-                electricPoles.Add(blockDatastore.GetBlock<IElectricTransformer>(targetPos));
+                if(!blockDatastore.TryGetBlock<IElectricTransformer>(targetPos,out var poleBlock)) continue;
+                if (poleBlock.BlockInstanceId == selfInstanceId) continue;
+                
+                electricPoles.TryAdd(poleBlock.BlockInstanceId, poleBlock);
             }
             
-            return electricPoles;
+            return electricPoles.Values.ToList();
 
-            #region Internal
-
-            static IEnumerable<Vector3Int> EnumerateRange(Vector3Int center, int horizontalRange, int heightRange)
-            {
-                horizontalRange = Mathf.Max(horizontalRange, 1);
-                heightRange = Mathf.Max(heightRange, 1);
-
-                var startX = center.x - horizontalRange / 2;
-                var startZ = center.z - horizontalRange / 2;
-                var startY = center.y - heightRange / 2;
-
-                var endX = startX + horizontalRange;
-                var endZ = startZ + horizontalRange;
-                var endY = startY + heightRange;
-
-                for (var x = startX; x < endX; x++)
-                for (var y = startY; y < endY; y++)
-                for (var z = startZ; z < endZ; z++)
-                    yield return new Vector3Int(x, y, z);
-            }
-
-            #endregion
+        }
+        public static IEnumerable<Vector3Int> EnumerateRange(Vector3Int center, ElectricPoleBlockParam param)
+        {
+            var horizontalRange = param.PoleConnectionRange;
+            var heightRange = param.PoleConnectionHeightRange;
+            
+            horizontalRange = Mathf.Max(horizontalRange, 1);
+            heightRange = Mathf.Max(heightRange, 1);
+            
+            var startX = center.x - horizontalRange / 2;
+            var startZ = center.z - horizontalRange / 2;
+            var startY = center.y - heightRange / 2;
+            
+            var endX = startX + horizontalRange;
+            var endZ = startZ + horizontalRange;
+            var endY = startY + heightRange;
+            
+            for (var x = startX; x < endX; x++)
+            for (var y = startY; y < endY; y++)
+            for (var z = startZ; z < endZ; z++)
+                yield return new Vector3Int(x, y, z);
         }
     }
 }
