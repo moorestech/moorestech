2f50bd747177205d829c82243a8f959e7ad050fa
sakastudio <sakastudio100@gmail.com>
2025-10-24 14:00:44 +0900

    ネットワークの稼働率をもとに燃料消費速度を変更

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/PowerGenerator/PowerGeneratorStateDetail.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/PowerGenerator/PowerGeneratorStateDetail.cs
@@ -12,15 +12,16 @@ namespace Game.Block.Blocks.PowerGenerator
         [Key(0)] public double RemainingFuelTime;
         [Key(1)] public double CurrentFuelTime;
         
-        public PowerGeneratorStateDetail(SteamGearGeneratorFuelService fuelService)
+        [Key(2)] public float OperatingRate;
+        
+        public PowerGeneratorStateDetail(SteamGearGeneratorFuelService fuelService, float operatingRate)
         {
+            OperatingRate = operatingRate;
             RemainingFuelTime = fuelService.RemainingFuelTime;
             CurrentFuelTime = fuelService.CurrentFuelTime;
         }
         
         [Obsolete("デシリアライズ用のコンストラクタです。基本的に使用しないでください。")]
-        public PowerGeneratorStateDetail()
-        {
-        }
+        public PowerGeneratorStateDetail() { }
     }
 }
