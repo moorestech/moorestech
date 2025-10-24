e07d72e3fe39cd864f69e5fde309609902d22349
sakastudio <sakastudio100@gmail.com>
2025-10-24 10:10:21 +0900

    ジェネレーターUIの修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/GeneratorBlockInventoryView.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/Block/GeneratorBlockInventoryView.cs
@@ -4,6 +4,7 @@ using Client.Game.InGame.UI.Inventory.Common;
 using Core.Item.Interface;
 using Game.Context;
 using Mooresmaster.Model.BlocksModule;
+using TMPro;
 using UnityEngine;
 
 namespace Client.Game.InGame.UI.Inventory.Block
@@ -11,15 +12,17 @@ namespace Client.Game.InGame.UI.Inventory.Block
     public class GeneratorBlockInventoryView : CommonBlockInventoryViewBase
     {
         [SerializeField] private RectTransform powerGeneratorFuelItemParent;
+        [SerializeField] private TMP_Text blockName;
         
         public override void Initialize(BlockGameObject blockGameObject)
         {
             base.Initialize(blockGameObject);
             
+            blockName.text = blockGameObject.BlockMasterElement.Name;
             
             var itemList = new List<IItemStack>();
             var param = blockGameObject.BlockMasterElement.BlockParam;
-            var generatorParam = (ElectricGeneratorBlockParam)param;
+            var generatorParam = (IFuelItemSlotParam)param;
             for (var i = 0; i < generatorParam.FuelItemSlotCount; i++)
             {
                 var slotObject = Instantiate(ItemSlotView.Prefab, powerGeneratorFuelItemParent);
