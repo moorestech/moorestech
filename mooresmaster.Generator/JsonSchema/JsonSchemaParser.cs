5a2cba2170d3c36e9473839d8e2c8d72c4f6686f
Juha <juhasapps@gmail.com>
2025-10-25 23:53:06 +0900

    add test

--- mooresmaster.Generator/JsonSchema/JsonSchemaParser.cs
+++ mooresmaster.Generator/JsonSchema/JsonSchemaParser.cs
@@ -181,8 +181,10 @@ public static class JsonSchemaParser
 
             ifThenList.Add(new SwitchCaseSchema(switchPath, whenJson.Literal, Parse(thenJson, schemaId, table)));
         }
+        
+        var isNullable = IsNullable(json) || ifThenList.Any(c => table.Table[c.Schema].IsNullable);
 
-        table.Add(schemaId, new SwitchSchema((json[Tokens.PropertyNameKey] as JsonString)?.Literal, parent, ifThenList.ToArray(), IsNullable(json)));
+        table.Add(schemaId, new SwitchSchema((json[Tokens.PropertyNameKey] as JsonString)?.Literal, parent, ifThenList.ToArray(), isNullable));
         return schemaId;
     }
 
