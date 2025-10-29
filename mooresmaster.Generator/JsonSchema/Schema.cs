3420a1d16b4db3fae4d9ea1c7c94fa6a21af9c70
Juha <juhasapps@gmail.com>
2025-10-29 21:57:16 +0900

    i

--- mooresmaster.Generator/JsonSchema/Schema.cs
+++ mooresmaster.Generator/JsonSchema/Schema.cs
@@ -8,14 +8,14 @@ namespace mooresmaster.Generator.JsonSchema;
 public class SchemaTable
 {
     public readonly Dictionary<SchemaId, ISchema> Table = new();
-
+    
     public SchemaId Add(ISchema schema)
     {
         var id = SchemaId.New();
         Table.Add(id, schema);
         return id;
     }
-
+    
     public void Add(SchemaId id, ISchema schema)
     {
         Table.Add(id, schema);
@@ -29,9 +29,7 @@ public interface ISchema
     SchemaId? Parent { get; }
 }
 
-public interface IDefineInterfacePropertySchema : ISchema
-{
-}
+public interface IDefineInterfacePropertySchema : ISchema;
 
 public record Schema(string SchemaId, SchemaId InnerSchema, DefineInterface[] Interfaces)
 {
@@ -40,7 +38,7 @@ public record Schema(string SchemaId, SchemaId InnerSchema, DefineInterface[] In
     public string SchemaId = SchemaId;
 }
 
-public record ObjectSchema(string? PropertyName, SchemaId? Parent, Dictionary<string, SchemaId> Properties, string[] Required, bool IsNullable, string[] InterfaceImplementations) : ISchema
+public record ObjectSchema(string? PropertyName, SchemaId? Parent, Dictionary<string, SchemaId> Properties, string[] Required, bool IsNullable, string[] InterfaceImplementations) : ISchema, IDefineInterfacePropertySchema
 {
     public string[] InterfaceImplementations = InterfaceImplementations;
     public Dictionary<string, SchemaId> Properties = Properties;
@@ -50,7 +48,7 @@ public record ObjectSchema(string? PropertyName, SchemaId? Parent, Dictionary<st
     public SchemaId? Parent { get; } = Parent;
 }
 
-public record ArraySchema(string? PropertyName, SchemaId? Parent, SchemaId Items, JsonString? OverrideCodeGeneratePropertyName, bool IsNullable) : ISchema
+public record ArraySchema(string? PropertyName, SchemaId? Parent, SchemaId Items, JsonString? OverrideCodeGeneratePropertyName, bool IsNullable) : ISchema, IDefineInterfacePropertySchema
 {
     public SchemaId Items = Items;
     public JsonString? OverrideCodeGeneratePropertyName = OverrideCodeGeneratePropertyName;
@@ -64,24 +62,24 @@ public static class ArraySchemaExtension
     public static string GetPropertyName(this ArraySchema arraySchema)
     {
         if (arraySchema.OverrideCodeGeneratePropertyName != null) return arraySchema.OverrideCodeGeneratePropertyName.Literal;
-
+        
         return $"{arraySchema.PropertyName!}Element";
     }
 }
 
 public record SwitchSchema(string? PropertyName, SchemaId? Parent, SwitchCaseSchema[] IfThenArray, bool IsNullable, bool HasOptionalCase) : ISchema
 {
+    public bool HasOptionalCase = HasOptionalCase;
     public SwitchCaseSchema[] IfThenArray = IfThenArray;
     public string? PropertyName { get; } = PropertyName;
     public bool IsNullable { get; } = IsNullable;
-    public bool HasOptionalCase = HasOptionalCase;
     public SchemaId? Parent { get; } = Parent;
 }
 
 public record SwitchCaseSchema(SwitchPath SwitchReferencePath, string When, SchemaId Schema)
 {
-    public SwitchPath SwitchReferencePath = SwitchReferencePath;
     public SchemaId Schema = Schema;
+    public SwitchPath SwitchReferencePath = SwitchReferencePath;
     public string When = When;
 }
 
@@ -120,7 +118,7 @@ public record RefSchema(string? PropertyName, SchemaId? Parent, string Ref, bool
     public string? PropertyName { get; } = PropertyName;
     public bool IsNullable { get; } = IsNullable;
     public SchemaId? Parent { get; } = Parent;
-
+    
     public TypeName GetRefName()
     {
         return new TypeName(Ref, $"{Ref}Module");
@@ -173,67 +171,67 @@ public readonly struct SchemaId : IEquatable<SchemaId>, IComparable<SchemaId>
 {
     private static ulong _globalIndex;
     private readonly ulong _value;
-
+    
     private SchemaId(ulong value)
     {
         _value = value;
     }
-
+    
     public static SchemaId New()
     {
         return new SchemaId(_globalIndex++);
     }
-
+    
     public bool Equals(SchemaId other)
     {
         return _value.Equals(other._value);
     }
-
+    
     public override bool Equals(object? obj)
     {
         return obj is SchemaId other && Equals(other);
     }
-
+    
     public override int GetHashCode()
     {
         return _value.GetHashCode();
     }
-
+    
     public int CompareTo(SchemaId other)
     {
         return _value.CompareTo(other._value);
     }
-
+    
     public override string ToString()
     {
         return _value.ToString();
     }
-
+    
     public static bool operator ==(SchemaId left, SchemaId right)
     {
         return left.Equals(right);
     }
-
+    
     public static bool operator !=(SchemaId left, SchemaId right)
     {
         return !left.Equals(right);
     }
-
+    
     public static bool operator <(SchemaId left, SchemaId right)
     {
         return left.CompareTo(right) < 0;
     }
-
+    
     public static bool operator <=(SchemaId left, SchemaId right)
     {
         return left.CompareTo(right) <= 0;
     }
-
+    
     public static bool operator >(SchemaId left, SchemaId right)
     {
         return left.CompareTo(right) > 0;
     }
-
+    
     public static bool operator >=(SchemaId left, SchemaId right)
     {
         return left.CompareTo(right) >= 0;
