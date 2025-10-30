c4b0cfb24700c030a483aa091474f146bdde2c3a
sakastudio <sakastudio100@gmail.com>
2025-10-30 20:51:01 +0900

    IBlockPreviewStateProcessorの定義

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/Common/PreviewController/IBlockPreviewStateProcessor.cs
@@ -0,0 +1,7 @@
+namespace Client.Game.InGame.BlockSystem.PlaceSystem.Common.PreviewController
+{
+    public interface IBlockPreviewStateProcessor
+    {
+        public void Process(PreviewPlaceInfo previewPlaceInfo);
+    }
+}
