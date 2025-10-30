3623f5d1b9b0d444b45aca57e55e75c637b30891
sakastudio <sakastudio100@gmail.com>
2025-10-30 15:03:07 +0900

    レール橋脚の設置システムとCommonBlockPlaceSystemを必要に応じた共通化実装

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRail/TrainRailPlaceSystem.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRail/TrainRailPlaceSystem.cs
@@ -1,18 +1,54 @@
+using System.Collections.Generic;
+using Client.Game.InGame.BlockSystem.PlaceSystem.Common.PreviewController;
+using Game.Block.Interface;
+using Server.Protocol.PacketResponse;
+using UnityEngine;
+using static Client.Game.InGame.BlockSystem.PlaceSystem.Util.PlaceSystemUtil;
+
 namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRail
 {
     public class TrainRailPlaceSystem : IPlaceSystem
     {
+        private const int HeightOffset = 0;
+        private const BlockDirection DefaultBlockDirection = BlockDirection.North;
+        
+        private readonly Camera _mainCamera;
+        private readonly IPlacementPreviewBlockGameObjectController _previewBlockController;
+        
+        public TrainRailPlaceSystem(Camera mainCamera, IPlacementPreviewBlockGameObjectController previewBlockController)
+        {
+            _mainCamera = mainCamera;
+            _previewBlockController = previewBlockController;
+        }
+        
         public void Enable()
         {
-            throw new System.NotImplementedException();
         }
+        
         public void ManualUpdate(PlaceSystemUpdateContext context)
         {
-            throw new System.NotImplementedException();
+            _previewBlockController.SetActive(false);
+            
+            if (!TryGetRayHitBlockPosition(_mainCamera,HeightOffset, BlockDirection.North, out var placePoint, out var boundingBoxSurface)) return;
+            
+            _previewBlockController.SetActive(true);
+            
+            var placeInfo = new List<PlaceInfo>
+            {
+                new()
+                {
+                    Position = placePoint,
+                    Direction = DefaultBlockDirection,
+                    VerticalDirection = BlockVerticalDirection.Horizontal,
+                    Placeable = true
+                },
+            };
+            _previewBlockController.SetPreviewAndGroundDetect(placeInfo, boundingBoxSurface.BlockGameObject.BlockMasterElement);
+            
         }
+        
         public void Disable()
         {
-            throw new System.NotImplementedException();
         }
     }
 }
