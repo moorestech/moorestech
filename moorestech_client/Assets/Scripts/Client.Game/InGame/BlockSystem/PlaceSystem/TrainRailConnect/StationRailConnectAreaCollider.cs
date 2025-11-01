f56994ab42358c4b2b191e716349045e409abf5c
sakastudio <sakastudio100@gmail.com>
2025-11-01 18:35:55 +0900

    front、backが逆だったので修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
@@ -8,9 +8,9 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
     {
         public RailComponentSpecifierMode RailComponentSpecifierMode => RailComponentSpecifierMode.Station;
         
-        // 0番のBackと1番のFrontは駅の内部で繋がっているためこうなる
-        // 0 is Back and 1 is Front, as they are connected inside the station.
-        public bool IsFront => railNodeIndex == 0;
+        // 1番のBackと0番のFrontは駅の内部で繋がっているためこうなる
+        // 1 is Back and 0 is Front, as they are connected inside the station.
+        public bool IsFront => railNodeIndex == 1;
         
         /// <summary>
         /// 駅には2つのレールノードがあるため、そのインデックスを指定する。詳しくはドキュメント参照
