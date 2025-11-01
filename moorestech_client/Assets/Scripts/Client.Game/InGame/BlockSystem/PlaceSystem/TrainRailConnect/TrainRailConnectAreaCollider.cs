8fd82a68e494b807ddecef5168b656eccfc6829c
sakastudio <sakastudio100@gmail.com>
2025-11-01 17:47:45 +0900

    駅とrailを接続するためのコンポーネント拡張

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectAreaCollider.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectAreaCollider.cs
@@ -1,20 +1,28 @@
 using Client.Game.InGame.Block;
 using Client.Game.InGame.BlockSystem.StateProcessor;
 using UnityEngine;
+using static Server.Protocol.PacketResponse.RailConnectionEditProtocol;
 
 namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
 {
     [RequireComponent(typeof(Collider))]
-    public class TrainRailConnectAreaCollider : MonoBehaviour, IBlockGameObjectInnerComponent
+    public class TrainRailConnectAreaCollider : MonoBehaviour, IRailComponentConnectAreaCollider
     {
-        public BlockGameObject BlockGameObject { get; private set; }
-        
+        public RailComponentSpecifierMode RailComponentSpecifierMode => RailComponentSpecifierMode.Rail;
         public bool IsFront => isFront;
         [SerializeField] public bool isFront;
+        
+        public BlockGameObject BlockGameObject { get; private set; }
         
         public void Initialize(BlockGameObject blockGameObject)
         {
             BlockGameObject = blockGameObject;
+        }
+        
+        
+        public RailComponentSpecifier CreateRailComponentSpecifier()
+        {
+            return RailComponentSpecifier.CreateRailSpecifier(BlockGameObject.BlockPosInfo.OriginalPos);
         }
     }
 }
