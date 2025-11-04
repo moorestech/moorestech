2c8790b17d9f899118a164114e51c0d15fc222bb
sakastudio <sakastudio100@gmail.com>
2025-11-04 10:45:15 +0900

    駅のエリアコライダー修正

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
@@ -10,13 +10,13 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
         
         // 1番のBackと0番のFrontは駅の内部で繋がっているためこうなる
         // 1 is Back and 0 is Front, as they are connected inside the station.
-        public bool IsFront => railNodeIndex == 1;
+        public bool IsFront => railNodeIndex == StationRailNodeIndex.Index1;
         
         /// <summary>
         /// 駅には2つのレールノードがあるため、そのインデックスを指定する。詳しくはドキュメント参照
         /// The station has two rail nodes, so specify the index. See the documentation for details.
         /// </summary>
-        [SerializeField] private int railNodeIndex;
+        [SerializeField] private StationRailNodeIndex railNodeIndex;
         
         
         public BlockGameObject BlockGameObject { get; private set; }
@@ -29,7 +29,13 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
         
         public RailComponentSpecifier CreateRailComponentSpecifier()
         {
-            return RailComponentSpecifier.CreateStationSpecifier(BlockGameObject.BlockPosInfo.OriginalPos, railNodeIndex);
+            return RailComponentSpecifier.CreateStationSpecifier(BlockGameObject.BlockPosInfo.OriginalPos, (int)railNodeIndex);
         }
+    }
+    
+    public enum StationRailNodeIndex
+    {
+        Index0 = 0,
+        Index1 = 1,
     }
 }
