c714a474b614f017c189f97da9b6c72f0a2600fc
sakastudio <sakastudio100@gmail.com>
2025-11-04 15:50:41 +0900

    電車のマスター名変更

--- moorestech_server/Assets/Scripts/Core.Master/TrainUnitMaster.cs
+++ moorestech_server/Assets/Scripts/Core.Master/TrainUnitMaster.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Mooresmaster.Loader.TrainModule;
 using Mooresmaster.Model.TrainModule;
 using Newtonsoft.Json.Linq;
@@ -9,17 +10,25 @@ namespace Core.Master
     public class TrainUnitMaster
     {
         public readonly Train Train;
-        private readonly Dictionary<ItemId, TrainUnitMasterElement> _unitsByItemId;
+        
+        private readonly Dictionary<ItemId, TrainCarMasterElement> _trainCraMastersByItemId;
+        private readonly Dictionary<Guid, TrainCarMasterElement> _trainCarMastersByGuid;
 
         public TrainUnitMaster(JToken jToken, ItemMaster itemMaster)
         {
             Train = TrainLoader.Load(jToken);
-            _unitsByItemId = new Dictionary<ItemId, TrainUnitMasterElement>();
+            _trainCraMastersByItemId = Train.TrainCars.ToDictionary(car => MasterHolder.ItemMaster.GetItemId(car.ItemGuid), car => car);
+            _trainCarMastersByGuid = Train.TrainCars.ToDictionary(car => car.TrainCarGuid, car => car);
         }
 
-        public bool TryGetTrainUnit(ItemId itemId, out TrainUnitMasterElement element)
+        public bool TryGetTrainUnit(ItemId itemId, out TrainCarMasterElement element)
         {
-            return _unitsByItemId.TryGetValue(itemId, out element);
+            return _trainCraMastersByItemId.TryGetValue(itemId, out element);
+        }
+        
+        public bool TryGetTrainUnit(Guid guid, out TrainCarMasterElement element)
+        {
+            return _trainCarMastersByGuid.TryGetValue(guid, out element);
         }
     }
 }
