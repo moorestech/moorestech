f4077601fe837e400d17b779402686b27cb44bd0
sakastudio <sakastudio100@gmail.com>
2025-11-06 14:43:21 +0900

    add comment

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/ISubInventory.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/Inventory/ISubInventory.cs
@@ -5,12 +5,33 @@ using Game.PlayerInventory.Interface.Subscription;
 
 namespace Client.Game.InGame.UI.Inventory
 {
+    /// <summary>
+    /// プレイヤーのインベントリとは別に、ブロックや列車など「他のインベントリ」を表すインターフェース
+    ///
+    /// TODO この辺はリファクタリングとかして整理して、ドキュメント描きたいなぁ、、
+    /// </summary>
     public interface ISubInventory
     {
+        /// <summary>
+        /// データ上での本当のサブインベントリのデータ。
+        /// このように書くのは、アイテムを持った後の右クリック長押しでアイテムを分割できる機能において、実際のデータとUI上のデータが異なる場合があるため、真のデータとしてこれを保持しておく必要がある。
+        /// </summary>
         public List<IItemStack> SubInventory { get; }
-        public int Count { get; }
-        public ISubInventoryIdentifier ISubInventoryIdentifier { get; }
+        
+        /// <summary>
+        /// サブインベントリのUIオブジェクト。上記の理由から、UIでの見た目と実データが異なることがある。そのため、UI側だけ表示を書き換えられるようにするために、このプロパティを用意している。
+        /// </summary>
         public IReadOnlyList<ItemSlotView> SubInventorySlotObjects { get; }
+        
+        /// <summary>
+        /// SubInventoryのスロットの個数
+        /// </summary>
+        public int Count { get; }
+        
+        /// <summary>
+        /// サブインベントリの識別子
+        /// </summary>
+        public ISubInventoryIdentifier ISubInventoryIdentifier { get; }
     }
     
     public static class ISubInventoryExtension
