87fe3e2fa2155101c9264656efc8228e3a30230c
sakastudio <sakastudio100@gmail.com>
2025-11-06 14:06:45 +0900

    ISubInventoryIdentifier周りのコードを整理

---
+++ moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/Subscription/ISubInventoryIdentifierExtension.cs
@@ -0,0 +1,19 @@
+using System;
+using Server.Util.MessagePack;
+using static Server.Util.MessagePack.InventoryIdentifierMessagePack;
+
+namespace Game.PlayerInventory.Interface.Subscription
+{
+    public static class ISubInventoryIdentifierExtension
+    {
+        public static InventoryIdentifierMessagePack ToMessagePack(this ISubInventoryIdentifier identifier)
+        {
+            return identifier switch
+            {
+                BlockInventorySubInventoryIdentifier blockId => CreateBlockMessage(blockId.Position),
+                TrainInventorySubInventoryIdentifier trainId => CreateTrainMessage(trainId.TrainCarId),
+                _ => throw new ArgumentException($"Unknown ISubInventoryIdentifier type: {identifier.GetType()}")
+            };
+        }
+    }
+}
