76707705718f4779e1083d560a0352ecc1b050f3
sakastudio <sakastudio100@gmail.com>
2025-11-06 14:06:45 +0900

    ISubInventoryIdentifier周りのコードを整理

---
+++ moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/Subscription/BlockInventorySubInventoryIdentifier.cs
@@ -0,0 +1,32 @@
+using Server.Util.MessagePack;
+using UnityEngine;
+
+namespace Game.PlayerInventory.Interface.Subscription
+{
+    // ブロックインベントリを識別する実装
+    // Identifier implementation for block inventories
+    public class BlockInventorySubInventoryIdentifier : ISubInventoryIdentifier
+    {
+        public InventoryType Type => InventoryType.Block;
+        public Vector3Int Position { get; }
+
+        public BlockInventorySubInventoryIdentifier(Vector3Int position)
+        {
+            Position = position;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is BlockInventorySubInventoryIdentifier other)
+            {
+                return Position.Equals(other.Position);
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Position.GetHashCode();
+        }
+    }
+}
--- moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/Subscription/ISubInventoryIdentifier.cs
+++ moorestech_server/Assets/Scripts/Game.PlayerInventory.Interface/Subscription/ISubInventoryIdentifier.cs
@@ -1,6 +1,4 @@
-using System;
 using Server.Util.MessagePack;
-using UnityEngine;
 
 namespace Game.PlayerInventory.Interface.Subscription
 {
@@ -9,65 +7,11 @@ namespace Game.PlayerInventory.Interface.Subscription
     public interface ISubInventoryIdentifier
     {
         InventoryType Type { get; }
-        
+
         // HashSetで使用するので、EqualsとGetHashCodeをオーバーライドする必要がある
         // Since it is used in HashSet, Equals and GetHashCode need to be overridden
         bool Equals(object obj);
         int GetHashCode();
-    }
-
-    // ブロックインベントリを識別する実装
-    // Identifier implementation for block inventories
-    public class BlockInventorySubInventoryIdentifier : ISubInventoryIdentifier
-    {
-        public InventoryType Type => InventoryType.Block;
-        public Vector3Int Position { get; }
-
-        public BlockInventorySubInventoryIdentifier(Vector3Int position)
-        {
-            Position = position;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is BlockInventorySubInventoryIdentifier other)
-            {
-                return Position.Equals(other.Position);
-            }
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            return Position.GetHashCode();
-        }
-    }
-
-    // 列車インベントリを識別する実装
-    // Identifier implementation for train inventories
-    public class TrainInventorySubInventoryIdentifier : ISubInventoryIdentifier
-    {
-        public InventoryType Type => InventoryType.Train;
-        public Guid TrainCarId { get; }
-
-        public TrainInventorySubInventoryIdentifier(Guid trainCarId)
-        {
-            TrainCarId = trainCarId;
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is TrainInventorySubInventoryIdentifier other)
-            {
-                return TrainCarId.Equals(other.TrainCarId);
-            }
-            return false;
-        }
-
-        public override int GetHashCode()
-        {
-            return TrainCarId.GetHashCode();
-        }
     }
 }
 
