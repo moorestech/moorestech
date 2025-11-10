85e67673d0df0f0014b67686fc23bce6d9ccd85d
sakastudio <sakastudio100@gmail.com>
2025-11-10 15:46:36 +0900

    位置調整をするエディタ拡張

---
+++ moorestech_client/Assets/Scripts/Editor/TransformRounder.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TransformRounder : EditorWindow
+{
+    // GameObjectのリスト
+    // List of GameObjects
+    [SerializeField] private List<GameObject> gameObjects = new();
+
+    private SerializedObject _serializedObject;
+    private SerializedProperty _gameObjectsProperty;
+
+    [MenuItem("moorestech/Util/Transform Rounder")]
+    private static void ShowWindow()
+    {
+        var window = GetWindow<TransformRounder>();
+        window.titleContent = new GUIContent("Transform Rounder");
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        _serializedObject = new SerializedObject(this);
+        _gameObjectsProperty = _serializedObject.FindProperty(nameof(gameObjects));
+    }
+
+    private void OnGUI()
+    {
+        _serializedObject.Update();
+
+        // GameObjectリストを表示
+        // Display GameObject list
+        EditorGUILayout.PropertyField(_gameObjectsProperty, true);
+
+        EditorGUILayout.Space();
+
+        // 座標と角度を四捨五入するボタン
+        // Button to round positions and rotations
+        if (GUILayout.Button("Round Transforms"))
+        {
+            RoundTransforms();
+        }
+
+        _serializedObject.ApplyModifiedProperties();
+    }
+
+    #region Internal
+
+    // 各GameObjectの座標と角度を四捨五入
+    // Round positions and rotations for each GameObject
+    private void RoundTransforms()
+    {
+        foreach (var go in gameObjects)
+        {
+            if (go == null) continue;
+
+            // Undo対応
+            // Support Undo
+            Undo.RecordObject(go.transform, "Round Transform");
+
+            // ローカル座標を四捨五入
+            // Round local position
+            var pos = go.transform.localPosition;
+            pos.x = Mathf.Round(pos.x);
+            pos.y = Mathf.Round(pos.y);
+            pos.z = Mathf.Round(pos.z);
+            go.transform.localPosition = pos;
+
+            // ローカル角度を四捨五入
+            // Round local rotation
+            var rot = go.transform.localEulerAngles;
+            rot.x = Mathf.Round(rot.x);
+            rot.y = Mathf.Round(rot.y);
+            rot.z = Mathf.Round(rot.z);
+            go.transform.localEulerAngles = rot;
+        }
+    }
+
+    #endregion
+}
