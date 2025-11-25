3c6533d74806e5bdfc39d8ce867c939fbe40af0f
sakastudio <sakastudio100@gmail.com>
2025-11-25 21:06:52 +0900

    compolerequesterの作成

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -1,9 +1,9 @@
 
 // このコードはCore.Masterアセンブリを再コンパイルするためのスクリプトです。SchemaWatcherによって更新されます。
-// スキーマを更新したら、こちらの更新もコミットしてください。
 // This code is a script to recompile the Core.Master assembly. It is updated by SchemaWatcher.
-// If you update the schema, please also commit this update.
 public class CompileRequester
 {
-    private const string dummyText = "2025/11/25 20:53:06";
+// スキーマを更新したら、こちらの更新もコミットしてください。
+// If you update the schema, please also commit this update.
+    private const string dummyText = "2025/11/25 21:05:02";
 }
