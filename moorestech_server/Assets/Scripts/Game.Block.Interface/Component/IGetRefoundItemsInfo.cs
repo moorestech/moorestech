d087038ae8ca4998a129cd23072e56870684f162
sakastudio / Katsumi Sato <55620461+sakastudio@users.noreply.github.com>
2025-11-25 17:39:16 +0900

    Update moorestech_server/Assets/Scripts/Game.Block.Interface/Component/IGetRefoundItemsInfo.cs
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- moorestech_server/Assets/Scripts/Game.Block.Interface/Component/IGetRefoundItemsInfo.cs
+++ moorestech_server/Assets/Scripts/Game.Block.Interface/Component/IGetRefoundItemsInfo.cs
@@ -5,7 +5,7 @@ namespace Game.Block.Interface.Component
 {
     // ブロック削除時に返却すべきアイテム情報を取得するインターフェース
     // Interface for retrieving refundable item information when a block is removed
-    public interface IGetRefoundItemsInfo : IBlockComponent
+    public interface IGetRefundItemsInfo : IBlockComponent
     {
         // 返却すべきアイテムのリストを取得する
         // Get list of items that should be refunded
