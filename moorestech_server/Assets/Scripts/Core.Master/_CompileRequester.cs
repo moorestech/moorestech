c5bff25d7dbf20adbfc2fbf3fd4af7da97c3286e
sakastudio <sakastudio100@gmail.com>
2025-11-25 21:00:29 +0900

    replace schema watcher

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -1,6 +1,8 @@
 
 // このコードはCore.Masterアセンブリを再コンパイルするためのスクリプトです。SchemaWatcherによって更新されます。
+// スキーマを更新したら、こちらの更新もコミットしてください。
 // This code is a script to recompile the Core.Master assembly. It is updated by SchemaWatcher.
+// If you update the schema, please also commit this update.
 public class CompileRequester
 {
     private const string dummyText = "2025/11/25 20:53:06";
