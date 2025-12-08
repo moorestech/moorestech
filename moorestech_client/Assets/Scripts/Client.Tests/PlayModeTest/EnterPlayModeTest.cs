c79181aaa096a8814618e96e4a859381e307c257
sakastudio <sakastudio100@gmail.com>
2025-12-08 17:28:02 +0900

    プレイモードに入れることをチェックするテストを追加

---
+++ moorestech_client/Assets/Scripts/Client.Tests/PlayModeTest/EnterPlayModeTest.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using Client.Tests.PlayModeTest.Util;
+using Cysharp.Threading.Tasks;
+using UnityEngine.TestTools;
+
+namespace Client.Tests.PlayModeTest
+{
+    /// <summary>
+    /// テスト自体はEditModeで実行されるが、実行中にプレイモードに変更する
+    /// This test is executed in EditMode, but it switches to PlayMode during execution.
+    /// </summary>
+    public class EnterPlayModeTest
+    {
+        [UnityTest]
+        public IEnumerator CategoryElementTest()
+        {
+            yield return new EnterPlayMode(expectDomainReload: true);
+            
+            yield return LoadMainGame().ToCoroutine();
+            
+            yield return new ExitPlayMode();
+            
+            #region Internal
+            
+            async UniTask LoadMainGame()
+            {
+                await PlayModeTestUtil.LoadMainGame();
+            }
+            
+            #endregion
+        }
+        
+    }
+}
