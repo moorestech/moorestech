7cd405a7e8e727ae2cc153846858c4e89a4571ea
Juha <juhasapps@gmail.com>
2025-12-14 16:34:24 +0900

    protocolの定義

---
+++ moorestech_server/Assets/Scripts/Server.Protocol/PacketResponse/RemoveTrainCarProtocol.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using MessagePack;
+using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
+
+namespace Server.Protocol.PacketResponse
+{
+    public class RemoveTrainCarProtocol : IPacketResponse
+    {
+        public const string ProtocolTag = "va:removeTrainCar";
+        
+        public RemoveTrainCarProtocol(ServiceProvider serviceProvider)
+        {
+        }
+        
+        public ProtocolMessagePackBase GetResponse(List<byte> payload)
+        {
+            var request = MessagePackSerializer.Deserialize<RemoveTrainCarRequestMessagePack>(payload.ToArray());
+            
+            Debug.Log("Request remove train car.");
+            
+            return null;
+        }
+        
+        [MessagePackObject]
+        public class RemoveTrainCarRequestMessagePack : ProtocolMessagePackBase
+        {
+            [Key(2)] public long EntityId { get; set; }
+            
+            [Obsolete("デシリアライズ用のコンストラクタです。基本的に使用しないでください。")]
+            public RemoveTrainCarRequestMessagePack()
+            {
+                Tag = ProtocolTag;
+            }
+            
+            public RemoveTrainCarRequestMessagePack(long trainCarEntityId)
+            {
+                Tag = ProtocolTag;
+                EntityId = trainCarEntityId;
+            }
+        }
+    }
+}
