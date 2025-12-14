72a823ea7d54b1cb9d9eac3d2971465ae817f309
toropippi <efghipippi@gmail.com>
2025-12-14 19:02:02 +0900

    rename

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/StationRailConnectAreaCollider.cs
@@ -8,13 +8,13 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
     {
         // 1番のBackと0番のFrontは駅の内部で繋がっているためこうなる
         // 1 is Back and 0 is Front, as they are connected inside the station.
-        public bool IsFront => railNodeIndex == StationRailNodeIndex.Index1;
+        public bool IsFront => railComponentIndex == StationrailComponentIndex.Index1;
         
         /// <summary>
-        /// 駅には2つのレールノードがあるため、そのインデックスを指定する。詳しくはドキュメント参照
+        /// 駅には4つのrailcomponentがあるため、そのインデックスを指定する。詳しくはドキュメント参照
         /// The station has two rail nodes, so specify the index. See the documentation for details.
         /// </summary>
-        [SerializeField] private StationRailNodeIndex railNodeIndex;
+        [SerializeField] private StationrailComponentIndex railComponentIndex;
         
         
         public BlockGameObject BlockGameObject { get; private set; }
@@ -28,11 +28,11 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
         public ConnectionDestination CreateConnectionDestination()
         {
             var origin = BlockGameObject.BlockPosInfo.OriginalPos;
-            return new ConnectionDestination(origin, (int)railNodeIndex, IsFront);
+            return new ConnectionDestination(origin, (int)railComponentIndex, IsFront);
         }
     }
     
-    public enum StationRailNodeIndex
+    public enum StationrailComponentIndex
     {
         Index0 = 0,
         Index1 = 1,
