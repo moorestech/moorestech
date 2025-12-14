360ae6ca5282cfbb3d526ae0669bb05b0adcba79
toropippi <efghipippi@gmail.com>
2025-12-14 18:18:41 +0900

    過去コード削除

--- moorestech_server/Assets/Scripts/Game.Train/RailGraph/RailNodeInitializationNotifier.cs
+++ moorestech_server/Assets/Scripts/Game.Train/RailGraph/RailNodeInitializationNotifier.cs
@@ -1,5 +1,6 @@
 using System;
 using UniRx;
+using UnityEngine;
 
 namespace Game.Train.RailGraph
 {
@@ -7,7 +8,7 @@ namespace Game.Train.RailGraph
     ///     RailNode初期化イベントを発火するためのラッパー
     ///     Wrapper that exposes a subject for rail node initialization events
     /// </summary>
-    internal sealed class RailNodeInitializationNotifier : IDisposable
+    public class RailNodeInitializationNotifier : IDisposable
     {
         public Subject<RailNodeInitializationData> RailNodeInitializedEvent { get; private set; }
 
@@ -16,9 +17,20 @@ namespace Game.Train.RailGraph
             RailNodeInitializedEvent = new Subject<RailNodeInitializationData>();
         }
 
-        public void Notify(RailNodeInitializationData data)
+        public void Notify(int NodeId)
         {
-            RailNodeInitializedEvent?.OnNext(data);
+            RailGraphDatastore.TryGetRailNode(NodeId, out var node);
+            if (node == null)
+                return;
+            RailNodeInitializedEvent?.OnNext(
+                new RailNodeInitializationData(
+                    NodeId,
+                    node.Guid,
+                    node.ConnectionDestination,
+                    node.FrontControlPoint.OriginalPosition,
+                    node.FrontControlPoint.ControlPointPosition,
+                    node.BackControlPoint.ControlPointPosition)
+                );
         }
 
         public void Reset()
@@ -30,6 +42,26 @@ namespace Game.Train.RailGraph
         public void Dispose()
         {
             RailNodeInitializedEvent?.Dispose();
+        }
+
+        public readonly struct RailNodeInitializationData
+        {
+            public RailNodeInitializationData(int nodeId, Guid nodeGuid, ConnectionDestination connectionDestination, Vector3 originPoint, Vector3 frontControlPoint, Vector3 backControlPoint)
+            {
+                NodeId = nodeId;
+                NodeGuid = nodeGuid;
+                ConnectionDestination = connectionDestination;
+                OriginPoint = originPoint;
+                FrontControlPoint = frontControlPoint;
+                BackControlPoint = backControlPoint;
+            }
+
+            public int NodeId { get; }
+            public Guid NodeGuid { get; }
+            public ConnectionDestination ConnectionDestination { get; }
+            public Vector3 OriginPoint { get; }
+            public Vector3 FrontControlPoint { get; }
+            public Vector3 BackControlPoint { get; }
         }
     }
 }
