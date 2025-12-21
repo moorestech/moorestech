fef7c552ee9a493e0ecc6bfe4d9f4da2c758acf0
sakastudio <sakastudio100@gmail.com>
2025-12-21 15:14:38 +0900

    インサートコンテキスト実装：第一弾、とりあえず検品してOKだったやつ

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/BeltConveyor/VanillaBeltConveyorBlockInventoryInserter.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/BeltConveyor/VanillaBeltConveyorBlockInventoryInserter.cs
@@ -9,20 +9,24 @@ namespace Game.Block.Blocks.BeltConveyor
     public class VanillaBeltConveyorBlockInventoryInserter : IBlockInventoryInserter
     {
         private readonly BlockConnectorComponent<IBlockInventory> _blockConnectorComponent;
-        
+
         public VanillaBeltConveyorBlockInventoryInserter(BlockConnectorComponent<IBlockInventory> blockConnectorComponent)
         {
             _blockConnectorComponent = blockConnectorComponent;
         }
 
-        public IItemStack InsertItem(IItemStack itemStack)
+        public IItemStack InsertItem(IItemStack itemStack, InsertItemContext context)
         {
             var targets = _blockConnectorComponent.ConnectedTargets;
             if (targets.Count == 0) return itemStack;
-            
+
             var connector = targets.First();
-            var output = connector.Key.InsertItem(itemStack);
-            
+
+            // ConnectedInfoからBlockConnectInfoElementを取得
+            // Get BlockConnectInfoElement from ConnectedInfo
+            var newContext = new InsertItemContext(connector.Value.SelfConnector, connector.Value.TargetConnector);
+            var output = connector.Key.InsertItem(itemStack, newContext);
+
             return output;
         }
     }
