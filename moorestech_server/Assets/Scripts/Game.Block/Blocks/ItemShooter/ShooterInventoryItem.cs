d4e4e88816408988dfc310e3100b468106d3349d
sakastudio <sakastudio100@gmail.com>
2025-12-21 15:37:39 +0900

    シューター関連の変更を追加

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/ItemShooter/ShooterInventoryItem.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/ItemShooter/ShooterInventoryItem.cs
@@ -7,18 +7,21 @@ namespace Game.Block.Blocks.ItemShooter
     public class ShooterInventoryItem : IOnBeltConveyorItem
     {
         public ItemId ItemId { get; }
-        
+
         public ItemInstanceId ItemInstanceId { get; }
-        
+
         public double RemainingPercent { get; set; }
-        
+
+        public string SourcePathId { get; }
+
         public float CurrentSpeed { get; set; }
-        
-        public ShooterInventoryItem(ItemId itemId, ItemInstanceId itemInstanceId, float currentSpeed)
+
+        public ShooterInventoryItem(ItemId itemId, ItemInstanceId itemInstanceId, float currentSpeed, string sourcePathId)
         {
             ItemId = itemId;
             ItemInstanceId = itemInstanceId;
             CurrentSpeed = currentSpeed;
+            SourcePathId = sourcePathId;
             RemainingPercent = 1;
         }
     }
