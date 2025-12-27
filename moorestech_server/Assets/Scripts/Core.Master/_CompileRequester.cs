229238ebaa6468415386a82e5707ef896b0cecdd
sakastudio <sakastudio100@gmail.com>
2025-12-27 11:16:02 +0900

    path idを削除

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2025/12/24 00:16:05";
+    private const string dummyText = "2025/12/27 11:12:09";
 }
