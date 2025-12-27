98c33419b248c12f404b0e61a1a91b11f8593771
sakastudio <sakastudio100@gmail.com>
2025-12-27 11:09:29 +0900

    とりあえず不要なものを削除

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/BeltConveyor/VanillaBeltConveyorInventoryItem.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/BeltConveyor/VanillaBeltConveyorInventoryItem.cs
@@ -22,9 +22,7 @@ namespace Game.Block.Blocks.BeltConveyor
         public ItemId ItemId { get; }
         public ItemInstanceId ItemInstanceId { get; }
         public BlockConnectInfoElement StartConnector { get; }
-        public BlockConnectInfoElement GoalConnector { get; set; }
-        private Guid? _startConnectorGuid;
-        private Guid? _goalConnectorGuid;
+        public BlockConnectInfoElement GoalConnector { get; private set; }
 
         public VanillaBeltConveyorInventoryItem(ItemId itemId, ItemInstanceId itemInstanceId, BlockConnectInfoElement startConnector, BlockConnectInfoElement goalConnector)
         {
@@ -33,26 +31,6 @@ namespace Game.Block.Blocks.BeltConveyor
             StartConnector = startConnector;
             GoalConnector = goalConnector;
             RemainingPercent = 1;
-            _startConnectorGuid = startConnector?.ConnectorGuid;
-            _goalConnectorGuid = goalConnector?.ConnectorGuid;
-        }
-
-        /// <summary>
-        /// セーブ済みのConnectorGuidを取得
-        /// Get saved ConnectorGuid
-        /// </summary>
-        public Guid? GetStartConnectorGuid()
-        {
-            return StartConnector?.ConnectorGuid ?? _startConnectorGuid;
-        }
-
-        /// <summary>
-        /// セーブ済みのGoalConnectorGuidを取得
-        /// Get saved GoalConnectorGuid
-        /// </summary>
-        public Guid? GetGoalConnectorGuid()
-        {
-            return GoalConnector?.ConnectorGuid ?? _goalConnectorGuid;
         }
 
         /// <summary>
@@ -62,17 +40,6 @@ namespace Game.Block.Blocks.BeltConveyor
         public void SetGoalConnector(BlockConnectInfoElement goalConnector)
         {
             GoalConnector = goalConnector;
-            _goalConnectorGuid = goalConnector?.ConnectorGuid ?? _goalConnectorGuid;
-        }
-
-        /// <summary>
-        /// ロード時にConnectorGuidを記録
-        /// Store ConnectorGuid on load
-        /// </summary>
-        public void SetSavedConnectorGuids(Guid? startConnectorGuid, Guid? goalConnectorGuid)
-        {
-            _startConnectorGuid = startConnectorGuid;
-            _goalConnectorGuid = goalConnectorGuid;
         }
 
         public string GetSaveJsonString()
@@ -90,14 +57,11 @@ namespace Game.Block.Blocks.BeltConveyor
             var itemId = MasterHolder.ItemMaster.GetItemId(jsonData.ItemStack.ItemGuid);
             var remainingPercent = jsonData.RemainingPercent;
             var itemInstanceId = ItemInstanceId.Create();
-
-            // 参照は復元できないため、Guidのみ保持して解決する
-            // Resolve by keeping only Guid because references cannot be restored
+            
             var item = new VanillaBeltConveyorInventoryItem(itemId, itemInstanceId, null, null)
             {
                 RemainingPercent = remainingPercent
             };
-            item.SetSavedConnectorGuids(jsonData.SourceConnectorGuid, jsonData.GoalConnectorGuid);
             return item;
         }
     }
