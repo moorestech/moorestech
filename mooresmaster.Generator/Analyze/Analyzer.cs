5521525dee34c57e25c9855a349610699683697a
Juha <juhasapps@gmail.com>
2025-12-31 17:18:27 +0900

    ifからswitchに変更しておく

--- mooresmaster.Generator/Analyze/Analyzer.cs
+++ mooresmaster.Generator/Analyze/Analyzer.cs
@@ -17,19 +17,34 @@ public class Analyzer
     private readonly List<IPreDefinitionLayerAnalyzer> _preDefinitionLayerAnalyzers = new();
     private readonly List<IPreJsonSchemaLayerAnalyzer> _preJsonSchemaLayerAnalyzers = new();
     private readonly List<IPreSemanticsLayerAnalyzer> _preSemanticsLayerAnalyzers = new();
-
+    
     public Analyzer AddAnalyzer(IAnalyzer analyzer)
     {
-        if (analyzer is IPostSemanticsLayerAnalyzer postSemanticsLayerAnalyzer) _postSemanticsLayerAnalyzers.Add(postSemanticsLayerAnalyzer);
-        if (analyzer is IPreSemanticsLayerAnalyzer preSemanticsLayerAnalyzer) _preSemanticsLayerAnalyzers.Add(preSemanticsLayerAnalyzer);
-        if (analyzer is IPostDefinitionLayerAnalyzer postDefinitionLayerAnalyzer) _postDefinitionLayerAnalyzers.Add(postDefinitionLayerAnalyzer);
-        if (analyzer is IPreDefinitionLayerAnalyzer preDefinitionLayerAnalyzer) _preDefinitionLayerAnalyzers.Add(preDefinitionLayerAnalyzer);
-        if (analyzer is IPostJsonSchemaLayerAnalyzer postJsonSchemaLayerAnalyzer) _postJsonSchemaLayerAnalyzers.Add(postJsonSchemaLayerAnalyzer);
-        if (analyzer is IPreJsonSchemaLayerAnalyzer preJsonSchemaLayerAnalyzer) _preJsonSchemaLayerAnalyzers.Add(preJsonSchemaLayerAnalyzer);
-
+        switch (analyzer)
+        {
+            case IPostSemanticsLayerAnalyzer postSemanticsLayerAnalyzer:
+                _postSemanticsLayerAnalyzers.Add(postSemanticsLayerAnalyzer);
+                break;
+            case IPreSemanticsLayerAnalyzer preSemanticsLayerAnalyzer:
+                _preSemanticsLayerAnalyzers.Add(preSemanticsLayerAnalyzer);
+                break;
+            case IPostDefinitionLayerAnalyzer postDefinitionLayerAnalyzer:
+                _postDefinitionLayerAnalyzers.Add(postDefinitionLayerAnalyzer);
+                break;
+            case IPreDefinitionLayerAnalyzer preDefinitionLayerAnalyzer:
+                _preDefinitionLayerAnalyzers.Add(preDefinitionLayerAnalyzer);
+                break;
+            case IPostJsonSchemaLayerAnalyzer postJsonSchemaLayerAnalyzer:
+                _postJsonSchemaLayerAnalyzers.Add(postJsonSchemaLayerAnalyzer);
+                break;
+            case IPreJsonSchemaLayerAnalyzer preJsonSchemaLayerAnalyzer:
+                _preJsonSchemaLayerAnalyzers.Add(preJsonSchemaLayerAnalyzer);
+                break;
+        }
+        
         return this;
     }
-
+    
     public Analyzer AddAllAnalyzer()
     {
         IAnalyzer[] analyzers =
@@ -37,37 +52,37 @@ public class Analyzer
             new DefineInterfaceScopeAnalyzer(),
             new NamingAnalyzer()
         ];
-
+        
         foreach (var analyzer in analyzers) AddAnalyzer(analyzer);
-
+        
         return this;
     }
-
+    
     public void PostJsonSchemaLayerAnalyze(Analysis analysis, ImmutableArray<SchemaFile> schemaFiles, SchemaTable schemaTable)
     {
         foreach (var postJsonSchemaLayerAnalyzer in _postJsonSchemaLayerAnalyzers) postJsonSchemaLayerAnalyzer.PostJsonSchemaLayerAnalyze(analysis, schemaFiles, schemaTable);
     }
-
+    
     public void PreJsonSchemaLayerAnalyze(Analysis analysis, AnalyzerTextFile[] texts)
     {
         foreach (var preJsonSchemaLayerAnalyzer in _preJsonSchemaLayerAnalyzers) preJsonSchemaLayerAnalyzer.PreJsonSchemaLayerAnalyze(analysis, texts);
     }
-
+    
     public void PostSemanticsLayerAnalyze(Analysis analysis, Semantics semantics, ImmutableArray<SchemaFile> schemaFiles, SchemaTable schemaTable)
     {
         foreach (var postSemanticsLayerAnalyzer in _postSemanticsLayerAnalyzers) postSemanticsLayerAnalyzer.PostSemanticsLayerAnalyze(analysis, semantics, schemaFiles, schemaTable);
     }
-
+    
     public void PreSemanticsLayerAnalyze(Analysis analysis, ImmutableArray<SchemaFile> schemaFiles, SchemaTable schemaTable)
     {
         foreach (var preSemanticsLayerAnalyzer in _preSemanticsLayerAnalyzers) preSemanticsLayerAnalyzer.PreSemanticsLayerAnalyze(analysis, schemaFiles, schemaTable);
     }
-
+    
     public void PostDefinitionLayerAnalyze(Analysis analysis, Semantics semantics, ImmutableArray<SchemaFile> schemaFiles, SchemaTable schemaTable, Definition definition)
     {
         foreach (var postDefinitionLayerAnalyzer in _postDefinitionLayerAnalyzers) postDefinitionLayerAnalyzer.PostDefinitionLayerAnalyze(analysis, semantics, schemaFiles, schemaTable, definition);
     }
-
+    
     public void PreDefinitionLayerAnalyze(Analysis analysis, Semantics semantics, ImmutableArray<SchemaFile> schemaFiles, SchemaTable schemaTable)
     {
         foreach (var preDefinitionLayerAnalyzer in _preDefinitionLayerAnalyzers) preDefinitionLayerAnalyzer.PreDefinitionLayerAnalyze(analysis, semantics, schemaFiles, schemaTable);
@@ -118,7 +133,7 @@ public static class TextFileExtension
     {
         return additionalTexts.Select(t => new AnalyzerTextFile(t.Path, t.GetText()?.ToString() ?? "")).ToArray();
     }
-
+    
     public static AnalyzerTextFile[] ToAnalyzerTextFiles(this string[] texts)
     {
         return texts.Select(t => new AnalyzerTextFile("", t)).ToArray();
