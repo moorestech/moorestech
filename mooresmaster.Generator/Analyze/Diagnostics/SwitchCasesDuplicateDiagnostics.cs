17a8c2b09be380492c6f57fdf4b761c83cc9da69
Juha <juhasapps@gmail.com>
2026-01-05 19:33:42 +0900

    Switchのcaseが重複している場合にdiagnosticsを生成するように変更

---
+++ mooresmaster.Generator/Analyze/Diagnostics/SwitchCasesDuplicateDiagnostics.cs
@@ -0,0 +1,25 @@
+using mooresmaster.Generator.Json;
+using mooresmaster.Generator.JsonSchema;
+
+namespace mooresmaster.Generator.Analyze.Diagnostics;
+
+/// <summary>
+/// Switchケースに重複したwhen値がある場合のDiagnostics
+/// </summary>
+public class SwitchCasesDuplicateDiagnostics : IDiagnostics
+{
+    public SwitchCasesDuplicateDiagnostics(SwitchSchema switchSchema, string duplicateCase, int occurrenceCount, Location location)
+    {
+        SwitchSchema = switchSchema;
+        DuplicateCase = duplicateCase;
+        OccurrenceCount = occurrenceCount;
+        Locations = new[] { location };
+    }
+
+    public SwitchSchema SwitchSchema { get; }
+    public string DuplicateCase { get; }
+    public int OccurrenceCount { get; }
+    public Location[] Locations { get; }
+
+    public string Message => $"Duplicate switch case '{DuplicateCase}' found {OccurrenceCount} times. Each case must be unique.";
+}
