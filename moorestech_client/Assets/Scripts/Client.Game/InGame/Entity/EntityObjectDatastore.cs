1dd02c1fe4c08bae9f9df402ae178b3489157fd9
toropippi <efghipippi@gmail.com>
2026-01-07 18:32:50 +0900

    クライアントで1秒noUpdateで自動削除されるのにフラグをつけた

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Entity/EntityObjectDatastore.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Entity/EntityObjectDatastore.cs
@@ -14,7 +14,6 @@ namespace Client.Game.InGame.Entity
     {
         private EntityObjectFactory _entityObjectFactory;
         private readonly Dictionary<long, (DateTime lastUpdate, IEntityObject objectEntity)> _entities = new();
-        private readonly Dictionary<Guid, TrainCarEntityObject> _trainCars = new();
         
         private void Awake()
         {
@@ -34,12 +33,7 @@ namespace Client.Game.InGame.Entity
 
             foreach (var removeEntity in removeEntities)
             {
-                // 削除対象エンティティが列車の場合は登録も解除する
                 var removeTarget = _entities[removeEntity].objectEntity;
-                if (removeTarget is TrainCarEntityObject trainCarEntity)
-                {
-                    _trainCars.Remove(trainCarEntity.TrainCarId);
-                }
                 removeTarget.Destroy();
                 _entities.Remove(removeEntity);
             }
@@ -56,12 +50,7 @@ namespace Client.Game.InGame.Entity
                 if (_entities.ContainsKey(entity.InstanceId))
                 {
                     var cachedEntity = _entities[entity.InstanceId].objectEntity;
-                    if (cachedEntity is not TrainCarEntityObject)
-                    {
-                        cachedEntity.SetPositionWithLerp(entity.Position);
-                    }
-                    // 列車はローカル側の姿勢更新処理に任せる
-                    _entities[entity.InstanceId] = (DateTime.Now, cachedEntity);
+                    cachedEntity.SetPositionWithLerp(entity.Position);
                     continue;
                 }
                 
@@ -70,24 +59,8 @@ namespace Client.Game.InGame.Entity
                 {
                     entityObject.Initialize(entity.InstanceId);
                     _entities.Add(entity.InstanceId, (DateTime.Now, entityObject));
-
-                    // 列車エンティティの場合は辞書へ登録する
-                    if (entityObject is TrainCarEntityObject trainCarEntity)
-                    {
-                        _trainCars[trainCarEntity.TrainCarId] = trainCarEntity;
-                    }
                     return entityObject;
                 });
-            }
-        }
-
-        // 現在存在する列車エンティティをまとめて取得する
-        public void CopyTrainCarEntitiesTo(List<TrainCarEntityObject> buffer)
-        {
-            buffer.Clear();
-            foreach (var trainCar in _trainCars.Values)
-            {
-                buffer.Add(trainCar);
             }
         }
     }
