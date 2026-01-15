aecedbad9515ddd4a6e722b9551d1f556dc810b9
Juha <juhasapps@gmail.com>
2026-01-15 16:12:46 +0900

    終点が駅の場合のpreviewを表示できるように変更

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectPreviewCalculator.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectPreviewCalculator.cs
@@ -1,4 +1,6 @@
 using System;
+using Client.Game.InGame.Train;
+using Game.Train.RailGraph;
 using UnityEngine;
 
 namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
@@ -8,27 +10,49 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
     /// </summary>
     public class TrainRailConnectPreviewCalculator
     {
-        public static TrainRailConnectPreviewData CalculatePreviewData(IRailComponentConnectAreaCollider fromArea, IRailComponentConnectAreaCollider toArea)
+        public static TrainRailConnectPreviewData CalculatePreviewData(ConnectionDestination from, ConnectionDestination to, RailGraphClientCache cache)
         {
-            // TODO
+            // 起点ノードを取得
+            if (!cache.TryGetNodeId(from, out var fromNodeId) || !cache.TryGetNode(fromNodeId, out var fromNode))
+            {
+                return TrainRailConnectPreviewData.Invalid;
+            }
+            
+            // 終点ノードを取得
+            if (!cache.TryGetNodeId(to, out var toNodeId) || !cache.TryGetNode(toNodeId, out var toNode))
+            {
+                return TrainRailConnectPreviewData.Invalid;
+            }
+            
+            // 起点の制御点
+            var fromControlPoint = fromNode.FrontControlPoint;
+            var p0 = fromControlPoint.OriginalPosition;
+            var p1 = fromControlPoint.OriginalPosition + fromControlPoint.ControlPointPosition;
+            var toControlPoint = toNode.BackControlPoint;
+            var p2 = toControlPoint.OriginalPosition + toControlPoint.ControlPointPosition;
+            var p3 = toControlPoint.OriginalPosition;
             
             // 仮実装: 常に前面同士を接続する
-            return new TrainRailConnectPreviewData(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
+            return new TrainRailConnectPreviewData(p0, p1, p2, p3);
         }
     }
     
     public struct TrainRailConnectPreviewData : IEquatable<TrainRailConnectPreviewData>
     {
+        public static TrainRailConnectPreviewData Invalid => new(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero, false); 
         public Vector3 StartPoint;
         public Vector3 StartControlPoint;
         public Vector3 EndControlPoint;
         public Vector3 EndPoint;
-        public TrainRailConnectPreviewData(Vector3 startPoint, Vector3 startControlPoint, Vector3 endControlPoint, Vector3 endPoint)
+        public bool IsValid;
+        
+        public TrainRailConnectPreviewData(Vector3 startPoint, Vector3 startControlPoint, Vector3 endControlPoint, Vector3 endPoint, bool isValid = true)
         {
             StartPoint = startPoint;
             StartControlPoint = startControlPoint;
             EndControlPoint = endControlPoint;
             EndPoint = endPoint;
+            IsValid = isValid;
         }
         public bool Equals(TrainRailConnectPreviewData other)
         {
