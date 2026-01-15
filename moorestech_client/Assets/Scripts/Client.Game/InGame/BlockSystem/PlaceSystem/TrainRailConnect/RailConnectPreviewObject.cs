df7be6b4da54bb36c19e427bcfe028b395a2c466
Juha <juhasapps@gmail.com>
2026-01-15 15:54:06 +0900

    previewの定義と表示

--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/RailConnectPreviewObject.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/RailConnectPreviewObject.cs
@@ -1,9 +1,19 @@
+using Client.Game.InGame.Train;
 using UnityEngine;
 
 namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
 {
     public class RailConnectPreviewObject : MonoBehaviour
     {
+        [SerializeField] private BezierRailChain _railChainPrefab;
+        private TrainRailConnectPreviewData _previewDataCache;
+        private BezierRailChain _railChain;
+        
+        private void Start()
+        {
+            _railChain = Instantiate(_railChainPrefab);
+        }
+        
         public void SetActive(bool active)
         {
             gameObject.SetActive(active);
@@ -11,7 +21,18 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
         
         public void ShowPreview(TrainRailConnectPreviewData data)
         {
-            // TODO
+            Debug.Log($"ShowPreview {data.StartPoint} {data.StartControlPoint} {data.EndControlPoint} {data.EndPoint}");
+            Debug.DrawLine(data.StartPoint, data.StartControlPoint, Color.blue);
+            Debug.DrawLine(data.StartControlPoint, data.EndControlPoint, Color.purple);
+            Debug.DrawLine(data.EndControlPoint, data.EndPoint, Color.red);
+            Debug.DrawLine(data.StartPoint - Vector3.down * 0.1f, data.EndPoint - Vector3.down * 0.1f, Color.white);
+            
+            if (!_previewDataCache.Equals(data))
+            {
+                _railChain.SetControlPoints(data.StartPoint, data.StartControlPoint, data.EndControlPoint, data.EndPoint);
+                _railChain.Rebuild();
+                _previewDataCache = data;
+            }
         }
     }
 }
--- moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectPreviewCalculator.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/BlockSystem/PlaceSystem/TrainRailConnect/TrainRailConnectPreviewCalculator.cs
@@ -1,3 +1,5 @@
+using System;
+using UnityEngine;
 
 namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
 {
@@ -11,17 +13,34 @@ namespace Client.Game.InGame.BlockSystem.PlaceSystem.TrainRailConnect
             // TODO
             
             // 仮実装: 常に前面同士を接続する
-            return new TrainRailConnectPreviewData
-            {
-                IsFromFront = fromArea.IsFront,
-                IsToFront = toArea.IsFront,
-            };
+            return new TrainRailConnectPreviewData(Vector3.zero, Vector3.zero, Vector3.zero, Vector3.zero);
         }
     }
     
-    public struct TrainRailConnectPreviewData
+    public struct TrainRailConnectPreviewData : IEquatable<TrainRailConnectPreviewData>
     {
-        public bool IsFromFront;
-        public bool IsToFront;
+        public Vector3 StartPoint;
+        public Vector3 StartControlPoint;
+        public Vector3 EndControlPoint;
+        public Vector3 EndPoint;
+        public TrainRailConnectPreviewData(Vector3 startPoint, Vector3 startControlPoint, Vector3 endControlPoint, Vector3 endPoint)
+        {
+            StartPoint = startPoint;
+            StartControlPoint = startControlPoint;
+            EndControlPoint = endControlPoint;
+            EndPoint = endPoint;
+        }
+        public bool Equals(TrainRailConnectPreviewData other)
+        {
+            return StartPoint.Equals(other.StartPoint) && StartControlPoint.Equals(other.StartControlPoint) && EndControlPoint.Equals(other.EndControlPoint) && EndPoint.Equals(other.EndPoint);
+        }
+        public override bool Equals(object obj)
+        {
+            return obj is TrainRailConnectPreviewData other && Equals(other);
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StartPoint, StartControlPoint, EndControlPoint, EndPoint);
+        }
     }
 }
