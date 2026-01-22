3c9d97064d295b5e85248a6446526331d8a4e048
sakastudio <sakastudio100@gmail.com>
2026-01-22 09:15:24 +0900

    不要なコードを削除

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/01/02 blockConnectInfo refactoring v2";
+    private const string dummyText = "2026/01/22 09:14:07";
 }
