aa142e1d8f9bd7c9356635e9dd1b26571bd5038b
Juha <juhasapps@gmail.com>
2026-02-05 12:22:52 +0900

    pierの設置を行うように変更

--- moorestech_server/Assets/Scripts/Server.Protocol/PacketResponse/RailConnectWithPlacePierProtocol.cs
+++ moorestech_server/Assets/Scripts/Server.Protocol/PacketResponse/RailConnectWithPlacePierProtocol.cs
@@ -1,16 +1,53 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Core.Master;
+using Game.Block.Interface;
+using Game.Block.Interface.Extension;
+using Game.Context;
+using Game.PlayerInventory.Interface;
+using Game.World.Interface.DataStore;
 using MessagePack;
+using Microsoft.Extensions.DependencyInjection;
+using UnityEngine;
 
 namespace Server.Protocol.PacketResponse
 {
     public class RailConnectWithPlacePierProtocol : IPacketResponse
     {
         public const string Tag = "va:railConnectWithPlacePier";
+        private readonly IPlayerInventoryDataStore _playerInventoryDataStore;
+        
+        public RailConnectWithPlacePierProtocol(ServiceProvider serviceProvider)
+        {
+            _playerInventoryDataStore = serviceProvider.GetService<IPlayerInventoryDataStore>();
+        }
         
         public ProtocolMessagePackBase GetResponse(List<byte> payload)
         {
             var request = MessagePackSerializer.Deserialize<RailConnectWithPlacePierRequest>(payload.ToArray());
+            var inventoryData = _playerInventoryDataStore.GetInventoryData(request.PlayerId);
+            
+            // すでにブロックがある場合はそのまま処理を終了
+            if (ServerContext.WorldBlockDatastore.Exists(request.PierPlaceInfo.Position)) return null;
+            
+            // アイテムIDがブロックIDに変換できない場合はそのまま処理を終了
+            var item = inventoryData.MainOpenableInventory.GetItem(request.PierInventorySlot);
+            if (!MasterHolder.BlockMaster.IsBlock(item.Id)) return null;
+            
+            // ブロックIDの設定
+            var blockId = MasterHolder.BlockMaster.GetBlockId(item.Id);
+            blockId = blockId.GetVerticalOverrideBlockId(request.PierPlaceInfo.VerticalDirection);
+            
+            // paramsの設定
+            BlockCreateParam[] createParams = request.PierPlaceInfo.BlockCreateParams.Select(v => new BlockCreateParam(v.Key, v.Value)).ToArray();
+            
+            // ブロックの設置
+            ServerContext.WorldBlockDatastore.TryAddBlock(blockId, request.PierPlaceInfo.Position, request.PierPlaceInfo.Direction, createParams, out var block);
+            
+            // アイテムを減らし、セットする
+            item = item.SubItem(1);
+            inventoryData.MainOpenableInventory.SetItem(request.PierInventorySlot, item);
             
             return null;
         }
