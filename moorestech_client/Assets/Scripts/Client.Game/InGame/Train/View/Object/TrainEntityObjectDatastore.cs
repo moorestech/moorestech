f8bd206ab414e581d4a386d30f12fa325e491dee
sakastudio <sakastudio100@gmail.com>
2026-02-09 20:43:05 +0900

    TrainEntityObjectの移動

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Train/View/Object/TrainEntityObjectDatastore.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Train/View/Object/TrainEntityObjectDatastore.cs
@@ -2,42 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Client.Game.InGame.Entity.Factory;
-using Client.Game.InGame.Entity.Object;
 using Client.Game.InGame.Train.Unit;
 using Client.Network.API;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using VContainer;
 
-namespace Client.Game.InGame.Entity
+namespace Client.Game.InGame.Train.View.Object
 {
     public class TrainEntityObjectDatastore : MonoBehaviour
     {
-        private TrainEntityObjectFactory _entityObjectFactory;
-        private readonly Dictionary<long, IEntityObject> _entities = new();
+        private TrainCarObjectFactory carObjectFactory;
+        private readonly Dictionary<long, TrainCarEntityObject> _entities = new();
 
         [Inject]
         public void Construct(TrainUnitClientCache trainUnitClientCache)
         {
-            _entityObjectFactory = new TrainEntityObjectFactory(trainUnitClientCache);
+            carObjectFactory = new TrainCarObjectFactory(trainUnitClientCache);
         }
 
-        public void OnEntitiesUpdate(List<EntityResponse> entities)
+        public void OnTrainObjectUpdate(List<EntityResponse> entities)
         {
             // 列車エンティティの生成・更新を反映する。列車以外のチェックは行わない
             // Apply create/update for train entities
             for (var i = 0; i < entities.Count; i++)
             {
                 var entity = entities[i];
-                if (_entities.TryGetValue(entity.InstanceId, out var existing))
-                {
-                    //existing.SetPositionWithLerp(entity.Position);//実装なしなので
-                    //existing.SetEntityData(entity.EntityData);//実装なしなので
-                    continue;
-                }
                 // EntityObjectDatastoreにならう
                 // Following EntityObjectDatastore
-                _entityObjectFactory.CreateEntity(transform, entity).ContinueWith(entityObject =>
+                carObjectFactory.CreateTrainCarObject(transform, entity).ContinueWith(entityObject =>
                 {
                     entityObject.Initialize(entity.InstanceId);
                     _entities.Add(entity.InstanceId, entityObject);
