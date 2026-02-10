64dab92ec341b356a6733e5686f6c7eb1e2890be
toropippi <efghipippi@gmail.com>
2026-02-10 22:54:35 +0900

    Fix rail direction persistence using existing state detail component

--- moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaTrainRailTemplate.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaTrainRailTemplate.cs
@@ -1,17 +1,18 @@
 ﻿using System.Collections.Generic;
 using Game.Block.Blocks;
 using Game.Block.Blocks.TrainRail;
+using Game.Block.Factory.BlockTemplate.Utility;
 using Game.Block.Interface;
 using Game.Block.Interface.Component;
-using Mooresmaster.Model.BlocksModule;
 using Game.Train.RailGraph;
+using Mooresmaster.Model.BlocksModule;
 using UnityEngine;
-using Game.Block.Factory.BlockTemplate.Utility;
 
 namespace Game.Block.Factory.BlockTemplate
 {
     /// <summary>
     /// バニラ版のTrainRailブロックを生成・復元するテンプレート
+    /// Template that creates and restores vanilla TrainRail blocks
     /// </summary>
     public class VanillaTrainRailTemplate : IBlockTemplate
     {
@@ -21,59 +22,55 @@ namespace Game.Block.Factory.BlockTemplate
         {
             _railGraphDatastore = railGraphDatastore;
         }
-        /// <summary>
-        /// 新規にブロック（と対応するRailComponent等）を生成
-        /// </summary>
-        public IBlock New(BlockMasterElement blockMasterElement,
+
+        public IBlock New(
+            BlockMasterElement blockMasterElement,
             BlockInstanceId blockInstanceId,
-            BlockPositionInfo blockPositionInfo, BlockCreateParam[] createParams)
+            BlockPositionInfo blockPositionInfo,
+            BlockCreateParam[] createParams)
         {
             var trainRailParam = blockMasterElement.BlockParam as TrainRailBlockParam;
-            // railブロックは常にRailComponentが1つだけ
             var railComponents = new RailComponent[1];
-            
-            // ブロック生成パラメータからRailBridgePierComponentStateDetailを取得して方向ベクトルを取得
+
+            // 生成時に渡された向き情報を読み出す
+            // Read rail direction provided at placement time
             var state = createParams.GetStateDetail<RailBridgePierComponentStateDetail>(RailBridgePierComponentStateDetail.StateDetailKey);
             if (state == null)
             {
-                // 状態情報が存在しないため詳細ログを出力
                 Debug.LogError($"[VanillaTrainRailTemplate] Missing create param: {RailBridgePierComponentStateDetail.StateDetailKey} for block {blockMasterElement.Name}");
             }
-            var railBlockDirection = state?.RailBlockDirection;
 
-            // RailComponentを生成
-            var railComponentPosition = RailComponentUtility.CalculateRailComponentPosition(blockPositionInfo,trainRailParam.RailPosition);
-            railComponents[0] = new RailComponent(_railGraphDatastore, railComponentPosition, railBlockDirection, blockPositionInfo.OriginalPos, 0);
-            // StateDetailコンポーネントを生成
+            var railDirection = state?.RailBlockDirection;
+            var railComponentPosition = RailComponentUtility.CalculateRailComponentPosition(blockPositionInfo, trainRailParam.RailPosition);
+            railComponents[0] = new RailComponent(_railGraphDatastore, railComponentPosition, railDirection, blockPositionInfo.OriginalPos, 0);
+
             var stateDetailComponent = new RailComponentStateDetailComponent(railComponents[0]);
-            // コンポーネントをまとめてブロックに登録
             var components = new List<IBlockComponent>();
             components.AddRange(railComponents);
             components.Add(stateDetailComponent);
             return new BlockSystem(blockInstanceId, blockMasterElement.BlockGuid, components, blockPositionInfo);
         }
 
-        /// <summary>
-        /// セーブデータ（componentStates）からRailComponent等を復元してブロックを生成
-        /// </summary>
         public IBlock Load(
-                Dictionary<string, string> componentStates,
-                BlockMasterElement masterElement,
-                BlockInstanceId instanceId,
-                BlockPositionInfo positionInfo)
+            Dictionary<string, string> componentStates,
+            BlockMasterElement masterElement,
+            BlockInstanceId instanceId,
+            BlockPositionInfo positionInfo)
         {
             var trainRailParam = masterElement.BlockParam as TrainRailBlockParam;
-            var railComponents = RailComponentUtility.Restore1RailComponents(positionInfo, trainRailParam.RailPosition, _railGraphDatastore);
-            // StateDetailコンポーネントを生成
-            var stateDetailComponent = new RailComponentStateDetailComponent(railComponents[0]);
 
+            // ブロック保存データから向きを復元する
+            // Restore rail direction from block save data
+            var railDirection = RailComponentStateDetailComponent.LoadRailDirection(componentStates);
+            var railComponents = new RailComponent[1];
+            var railComponentPosition = RailComponentUtility.CalculateRailComponentPosition(positionInfo, trainRailParam.RailPosition);
+            railComponents[0] = new RailComponent(_railGraphDatastore, railComponentPosition, railDirection, positionInfo.OriginalPos, 0);
+
+            var stateDetailComponent = new RailComponentStateDetailComponent(railComponents[0]);
             var components = new List<IBlockComponent>();
             components.AddRange(railComponents);
             components.Add(stateDetailComponent);
-
             return new BlockSystem(instanceId, masterElement.BlockGuid, components, positionInfo);
         }
     }
 }
-
-
