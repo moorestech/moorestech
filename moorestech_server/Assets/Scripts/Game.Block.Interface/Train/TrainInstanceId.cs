055a43067f2a061623dba0a8a5366894137f8e88
toropippi <efghipippi@gmail.com>
2026-02-19 23:39:00 +0900

    Update TrainInstanceId.cs

--- moorestech_server/Assets/Scripts/Game.Block.Interface/Train/TrainInstanceId.cs
+++ moorestech_server/Assets/Scripts/Game.Block.Interface/Train/TrainInstanceId.cs
@@ -6,8 +6,6 @@ namespace Game.Train.Unit
     [UnitOf(typeof(Guid), UnitGenerateOptions.MessagePackFormatter | UnitGenerateOptions.Comparable)]
     public readonly partial struct TrainInstanceId
     {
-        public static TrainInstanceId Empty => new(Guid.Empty);
-
         public static TrainInstanceId Create()
         {
             return new TrainInstanceId(Guid.NewGuid());
