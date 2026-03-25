bf866259e38c578dcd8ef5a9777f921da377c3d0
Juha <juhasapps@gmail.com>
2026-03-25 23:58:15 +0900

    内部も実装

--- moorestech_server/Assets/Scripts/Game.Block/Blocks/TrainRail/ContainerComponents/TrainPlatformFluidContainerComponent.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Blocks/TrainRail/ContainerComponents/TrainPlatformFluidContainerComponent.cs
@@ -1,21 +1,26 @@
+using System;
 using System.Collections.Generic;
+using Core.Master;
 using Game.Block.Blocks.Fluid;
 using Game.Block.Component;
 using Game.Block.Interface.Component;
 using Game.Fluid;
+using Game.Train.Unit;
 using Game.Train.Unit.Containers;
 using JetBrains.Annotations;
+using MessagePack;
 
 namespace Game.Block.Blocks.TrainRail.ContainerComponents
 {
-    public class TrainPlatformFluidContainerComponent : IBlockComponent, IFluidInventory
+    public class TrainPlatformFluidContainerComponent : IUpdatableBlockComponent, IFluidInventory, IBlockSaveState
     {
         public bool IsDestroy { get; private set; }
-        [CanBeNull] public FluidTrainCarContainer Container;
+        [CanBeNull] public FluidTrainCarContainer Container { get; private set; }
+
         private readonly TrainPlatformDockingComponent _dockingComponent;
         private readonly TrainPlatformTransferComponent _transferComponent;
         private readonly BlockConnectorComponent<IFluidInventory> _fluidConnector;
-        private readonly double _capacity;
+        private readonly FluidContainer _fluidContainer;
 
         public TrainPlatformFluidContainerComponent(
             TrainPlatformDockingComponent dockingComponent,
@@ -25,8 +30,8 @@ namespace Game.Block.Blocks.TrainRail.ContainerComponents
         {
             _dockingComponent = dockingComponent;
             _transferComponent = transferComponent;
-            _capacity = capacity;
             _fluidConnector = fluidConnector;
+            _fluidContainer = new FluidContainer(capacity);
         }
 
         public TrainPlatformFluidContainerComponent(
@@ -38,29 +43,191 @@ namespace Game.Block.Blocks.TrainRail.ContainerComponents
         {
             _dockingComponent = dockingComponent;
             _transferComponent = transferComponent;
-            _capacity = capacity;
             _fluidConnector = fluidConnector;
-            // TODO セーブデータからの復元処理
-            // TODO Restore from save data
+            _fluidContainer = new FluidContainer(capacity);
+
+            if (componentStates.TryGetValue(SaveKey, out var serialized))
+            {
+                var serializedBytes = MessagePackSerializer.ConvertFromJson(serialized);
+                var saveData = MessagePackSerializer.Deserialize<TrainPlatformFluidContainerSaveData>(serializedBytes);
+                _fluidContainer.FluidId = saveData.FluidId;
+                _fluidContainer.Amount = saveData.Amount;
+                Container = saveData.Container;
+            }
+        }
+
+        public void Update()
+        {
+            PushFluidToAdjacentBlocks();
+
+            var dockedCar = _dockingComponent.DockedTrainCar;
+            if (dockedCar == null) return;
+
+            if (!IsTargetContainer(out var targetContainer)) return;
+
+            switch (_transferComponent.Mode)
+            {
+                case TrainPlatformTransferComponent.TransferMode.LoadToTrain:
+                    LoadFluidToTrain(dockedCar, targetContainer);
+                    break;
+                case TrainPlatformTransferComponent.TransferMode.UnloadToPlatform:
+                    UnloadFluidFromTrain(dockedCar, targetContainer);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
         }
 
         public List<FluidStack> GetFluidInventory()
         {
-            // TODO 液体インベントリの取得処理
-            // TODO Return fluid inventory
-            return new List<FluidStack>();
+            var result = new List<FluidStack>();
+            if (_fluidContainer.Amount > 0)
+            {
+                result.Add(new FluidStack(_fluidContainer.Amount, _fluidContainer.FluidId));
+            }
+            return result;
         }
 
         public FluidStack AddLiquid(FluidStack fluidStack, FluidContainer source)
         {
-            // TODO 液体追加処理
-            // TODO Add liquid to container
-            return fluidStack;
+            return _fluidContainer.AddLiquid(fluidStack, source);
         }
 
         public void Destroy()
         {
             IsDestroy = true;
+        }
+
+        public string SaveKey { get; } = typeof(TrainPlatformFluidContainerComponent).FullName;
+
+        public string GetSaveState()
+        {
+            var saveData = new TrainPlatformFluidContainerSaveData(_fluidContainer.FluidId, _fluidContainer.Amount, Container);
+            return MessagePackSerializer.ConvertToJson(MessagePackSerializer.Serialize(saveData));
+        }
+
+        private void LoadFluidToTrain(TrainCar dockedCar, FluidTrainCarContainer trainContainer)
+        {
+            if (_fluidContainer.Amount < double.Epsilon)
+            {
+                _dockingComponent.StartRetracting();
+                return;
+            }
+
+            _dockingComponent.StartExtending();
+            if (_dockingComponent.ArmState != ArmState.Extended) return;
+
+            if (trainContainer == null)
+            {
+                var newContainer = new FluidTrainCarContainer();
+                newContainer.Container = new FluidContainer(_fluidContainer.Capacity);
+                TransferFluid(_fluidContainer, newContainer.Container);
+                dockedCar.SetContainer(newContainer);
+                Container = newContainer;
+                _dockingComponent.StartRetracting();
+                return;
+            }
+
+            TransferFluid(_fluidContainer, trainContainer.Container);
+            _dockingComponent.StartRetracting();
+        }
+
+        private void UnloadFluidFromTrain(TrainCar dockedCar, FluidTrainCarContainer trainContainer)
+        {
+            if (trainContainer == null || trainContainer.Container.Amount < double.Epsilon)
+            {
+                _dockingComponent.StartRetracting();
+                return;
+            }
+
+            _dockingComponent.StartExtending();
+            if (_dockingComponent.ArmState != ArmState.Extended) return;
+
+            TransferFluid(trainContainer.Container, _fluidContainer);
+            _dockingComponent.StartRetracting();
+        }
+
+        private static void TransferFluid(FluidContainer from, FluidContainer to)
+        {
+            if (from.Amount < double.Epsilon) return;
+            if (to.FluidId != FluidMaster.EmptyFluidId && to.FluidId != from.FluidId) return;
+
+            var transferAmount = Math.Min(from.Amount, to.Capacity - to.Amount);
+            if (transferAmount < double.Epsilon) return;
+
+            if (to.FluidId == FluidMaster.EmptyFluidId)
+            {
+                to.FluidId = from.FluidId;
+            }
+
+            to.Amount += transferAmount;
+            from.Amount -= transferAmount;
+
+            if (from.Amount < double.Epsilon)
+            {
+                from.FluidId = FluidMaster.EmptyFluidId;
+            }
+        }
+
+        private bool IsTargetContainer([CanBeNull] out FluidTrainCarContainer fluidContainer)
+        {
+            var trainContainer = _dockingComponent.DockedTrainCar?.Container;
+            if (trainContainer is FluidTrainCarContainer f)
+            {
+                fluidContainer = f;
+                Container = f;
+                return true;
+            }
+
+            if (trainContainer is null)
+            {
+                fluidContainer = null;
+                Container = null;
+                return true;
+            }
+
+            fluidContainer = null;
+            return false;
+        }
+
+        private void PushFluidToAdjacentBlocks()
+        {
+            if (_fluidContainer.Amount < double.Epsilon) return;
+
+            foreach (var kvp in _fluidConnector.ConnectedTargets)
+            {
+                if (_fluidContainer.Amount < double.Epsilon) break;
+
+                var fluidStack = new FluidStack(_fluidContainer.Amount, _fluidContainer.FluidId);
+                var remain = kvp.Key.AddLiquid(fluidStack, _fluidContainer);
+                _fluidContainer.Amount -= (fluidStack.Amount - remain.Amount);
+            }
+
+            if (_fluidContainer.Amount < double.Epsilon)
+            {
+                _fluidContainer.FluidId = FluidMaster.EmptyFluidId;
+            }
+
+            _fluidContainer.PreviousSourceFluidContainers.Clear();
+        }
+
+        [MessagePackObject]
+        public class TrainPlatformFluidContainerSaveData
+        {
+            [Key(0)] public FluidId FluidId;
+            [Key(1)] public double Amount;
+            [Key(2)] public FluidTrainCarContainer Container;
+
+            [Obsolete]
+            public TrainPlatformFluidContainerSaveData() { }
+
+            public TrainPlatformFluidContainerSaveData(FluidId fluidId, double amount, FluidTrainCarContainer container)
+            {
+                FluidId = fluidId;
+                Amount = amount;
+                Container = container;
+            }
         }
     }
 }
--- moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaTrainFluidPlatformTemplate.cs
+++ moorestech_server/Assets/Scripts/Game.Block/Factory/BlockTemplate/VanillaTrainFluidPlatformTemplate.cs
@@ -20,33 +20,21 @@ namespace Game.Block.Factory.BlockTemplate
             _railGraphDatastore = railGraphDatastore;
         }
 
-        /// <summary>
-        /// 新規にブロック（および対応するRailComponent等）を生成する
-        /// Create a new block with RailComponents
-        /// </summary>
         public IBlock New(BlockMasterElement masterElement,
             BlockInstanceId instanceId,
             BlockPositionInfo positionInfo, BlockCreateParam[] createParams)
         {
             var param = masterElement.BlockParam as TrainFluidPlatformBlockParam;
 
-            // レールコンポーネントの生成と接続
-            // Create and connect rail components
             var railComponents = RailComponentUtility.Create2RailComponents(positionInfo, param.EntryRailPosition, param.ExitRailPosition, _railGraphDatastore);
             RailComponentUtility.RegisterAndConnetStationBlocks(railComponents, _railGraphDatastore);
 
-            // ドッキング・転送コンポーネントの生成
-            // Create docking and transfer components
             var trainPlatformDockingComponent = new TrainPlatformDockingComponent(param.LoadingAnimeSpeed);
             var trainPlatformTransferComponent = new TrainPlatformTransferComponent(TrainPlatformTransferComponent.TransferMode.LoadToTrain);
 
-            // 液体コネクタとコンテナの生成
-            // Create fluid connector and container
             var fluidConnector = IFluidInventory.CreateFluidInventoryConnector(param.FluidInventoryConnectors, positionInfo);
             var trainPlatformFluidContainer = new TrainPlatformFluidContainerComponent(trainPlatformDockingComponent, trainPlatformTransferComponent, param.Capacity, fluidConnector);
 
-            // 生成したコンポーネントをブロックに登録する
-            // Register created components to the block
             var blockComponents = new List<IBlockComponent>();
             blockComponents.AddRange(railComponents);
             blockComponents.Add(trainPlatformDockingComponent);
@@ -54,8 +42,6 @@ namespace Game.Block.Factory.BlockTemplate
             blockComponents.Add(trainPlatformFluidContainer);
             blockComponents.Add(fluidConnector);
 
-            // 各RailNodeにStationReferenceを設定
-            // Set StationReference on each RailNode
             var createdBlock = new BlockSystem(instanceId, masterElement.BlockGuid, blockComponents, positionInfo);
             railComponents[0].FrontNode.StationRef.SetStationReference(createdBlock, StationNodeRole.Entry, StationNodeSide.Front);
             railComponents[1].FrontNode.StationRef.SetStationReference(createdBlock, StationNodeRole.Exit, StationNodeSide.Front);
@@ -72,23 +58,15 @@ namespace Game.Block.Factory.BlockTemplate
         {
             var param = masterElement.BlockParam as TrainFluidPlatformBlockParam;
 
-            // レールコンポーネントの復元と登録
-            // Restore and register rail components
             var railComponents = RailComponentUtility.Restore2RailComponents(positionInfo, param.EntryRailPosition, param.ExitRailPosition, _railGraphDatastore);
             RailComponentUtility.RegisterStationBlocks(railComponents, _railGraphDatastore);
 
-            // ドッキング・転送コンポーネントの復元
-            // Restore docking and transfer components
             var trainPlatformDockingComponent = new TrainPlatformDockingComponent(componentStates, param.LoadingAnimeSpeed);
             var trainPlatformTransferComponent = new TrainPlatformTransferComponent(componentStates);
 
-            // 液体コネクタとコンテナの復元
-            // Restore fluid connector and container
             var fluidConnector = IFluidInventory.CreateFluidInventoryConnector(param.FluidInventoryConnectors, positionInfo);
             var trainPlatformFluidContainer = new TrainPlatformFluidContainerComponent(trainPlatformDockingComponent, trainPlatformTransferComponent, param.Capacity, fluidConnector, componentStates);
 
-            // 復元したコンポーネントをブロックに登録する
-            // Register restored components to the block
             var blockComponents = new List<IBlockComponent>();
             blockComponents.AddRange(railComponents);
             blockComponents.Add(trainPlatformDockingComponent);
@@ -96,8 +74,6 @@ namespace Game.Block.Factory.BlockTemplate
             blockComponents.Add(trainPlatformFluidContainer);
             blockComponents.Add(fluidConnector);
 
-            // 各RailNodeにStationReferenceを設定
-            // Set StationReference on each RailNode
             var createdBlock = new BlockSystem(instanceId, masterElement.BlockGuid, blockComponents, positionInfo);
             railComponents[0].FrontNode.StationRef.SetStationReference(createdBlock, StationNodeRole.Entry, StationNodeSide.Front);
             railComponents[1].FrontNode.StationRef.SetStationReference(createdBlock, StationNodeRole.Exit, StationNodeSide.Front);
