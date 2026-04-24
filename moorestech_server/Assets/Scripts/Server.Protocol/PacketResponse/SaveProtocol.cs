84b9a73025f6f19b0844b906070f63a82e5b1b56
sakastudio <sakastudio100@gmail.com>
2026-04-24 11:18:48 +0900

    feat(server): SaveProtocol に完了 ACK レスポンスを追加

--- moorestech_server/Assets/Scripts/Server.Protocol/PacketResponse/SaveProtocol.cs
+++ moorestech_server/Assets/Scripts/Server.Protocol/PacketResponse/SaveProtocol.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using Game.SaveLoad.Interface;
+﻿using Game.SaveLoad.Interface;
 using MessagePack;
 using Microsoft.Extensions.DependencyInjection;
 using UnityEngine;
@@ -9,27 +8,37 @@ namespace Server.Protocol.PacketResponse
     public class SaveProtocol : IPacketResponse
     {
         public const string ProtocolTag = "va:save";
-        
+
         private readonly IWorldSaveDataSaver _worldSaveDataSaver;
-        
+
         public SaveProtocol(ServiceProvider serviceProvider)
         {
             _worldSaveDataSaver = serviceProvider.GetService<IWorldSaveDataSaver>();
         }
-        
+
         public ProtocolMessagePackBase GetResponse(byte[] payload)
         {
+            // セーブ完了をクライアントに ACK で返す
+            // Return ACK to the client once save completes
             Debug.Log("セーブ開始");
             _worldSaveDataSaver.Save();
             Debug.Log("セーブ完了");
-            return null;
+            return new SaveResponseMessagePack();
         }
-        
-        
+
         [MessagePackObject]
         public class SaveProtocolMessagePack : ProtocolMessagePackBase
         {
             public SaveProtocolMessagePack()
+            {
+                Tag = ProtocolTag;
+            }
+        }
+
+        [MessagePackObject]
+        public class SaveResponseMessagePack : ProtocolMessagePackBase
+        {
+            public SaveResponseMessagePack()
             {
                 Tag = ProtocolTag;
             }
