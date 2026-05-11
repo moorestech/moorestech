4a6ddc108ff0abcd3dddf79886d5698563ac873c
sakastudio <sakastudio100@gmail.com>
2026-05-11 17:33:17 +0900

    AIが書いたコードの修正その1 Vein関連の共通化

--- moorestech_client/Assets/Scripts/Client.Game/InGame/Map/MapVein/FluidMapVeinGameObject.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Map/MapVein/FluidMapVeinGameObject.cs
@@ -6,61 +6,30 @@ namespace Client.Game.InGame.Map.MapVein
     [ExecuteAlways]
     public class FluidMapVeinGameObject : MonoBehaviour
     {
-        public Vector3Int MinPosition => new(
-            Mathf.RoundToInt(transform.position.x - bounds.size.x / 2f + bounds.center.x),
-            Mathf.RoundToInt(transform.position.y - bounds.size.y / 2f + bounds.center.y),
-            Mathf.RoundToInt(transform.position.z - bounds.size.z / 2f + bounds.center.z));
-
-        public Vector3Int MaxPosition => new(
-            Mathf.RoundToInt(transform.position.x + bounds.size.x / 2f + bounds.center.x),
-            Mathf.RoundToInt(transform.position.y + bounds.size.y / 2f + bounds.center.y),
-            Mathf.RoundToInt(transform.position.z + bounds.size.z / 2f + bounds.center.z));
+        public Vector3Int MinPosition => Service.MinPosition(bounds);
+        public Vector3Int MaxPosition => Service.MaxPosition(bounds);
 
         public Guid VeinFluidGuid => Guid.Parse(veinFluidGuid);
         [SerializeField] private string veinFluidGuid;
 
         public Bounds Bounds => bounds;
         [SerializeField] private Bounds bounds = new(Vector3.zero, Vector3.one);
+        
+        private MapVeinGameObjectService _service;
+        public MapVeinGameObjectService Service => _service ??= new MapVeinGameObjectService(transform);
 
-        public void SetBounds(Bounds setBounds)
-        {
-            bounds = setBounds;
-
-            var size = bounds.size;
-            var sizeX = size.x < 1 ? 1 : Mathf.RoundToInt(size.x);
-            var sizeY = size.y < 1 ? 1 : Mathf.RoundToInt(size.y);
-            var sizeZ = size.z < 1 ? 1 : Mathf.RoundToInt(size.z);
-            bounds.size = new Vector3(sizeX, sizeY, sizeZ);
-
-            var centerX = sizeX % 2f == 0 ? 0 : 0.5f;
-            var centerY = sizeY % 2f == 0 ? 0 : 0.5f;
-            var centerZ = sizeZ % 2f == 0 ? 0 : 0.5f;
-            bounds.center = new Vector3(centerX, centerY, centerZ);
-        }
+        public void SetBounds(Bounds setBounds) => bounds = MapVeinGameObjectService.NormalizeBounds(setBounds);
 
         private void Update()
         {
 #if UNITY_EDITOR
-            OnEditorUpdate();
+            bounds = MapVeinGameObjectService.NormalizeBounds(bounds);
 #endif
-        }
-
-        private void OnEditorUpdate()
-        {
-            SetBounds(bounds);
         }
 
         private void OnDrawGizmosSelected()
         {
-            var gizmoBounds = new Bounds();
-            gizmoBounds.SetMinMax(MinPosition, MaxPosition);
-
-            // 液体Veinは青系で表示してアイテムVein(赤)と区別
-            // Render fluid vein in blue to distinguish from item vein (red)
-            var color = Color.blue;
-            color.a = 0.5f;
-            Gizmos.color = color;
-            Gizmos.DrawCube(gizmoBounds.center, gizmoBounds.size);
+            Service.DrowGizmo(bounds, Color.blue);
         }
     }
 }
--- moorestech_client/Assets/Scripts/Editor/Inspector/MapVeinGameObjectInspector.cs
+++ moorestech_client/Assets/Scripts/Editor/Inspector/MapVeinGameObjectInspector.cs
@@ -1,37 +1,17 @@
-using System;
 using Client.Game.InGame.Map.MapVein;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 [CustomEditor(typeof(MapVeinGameObject))]
 public class MapVeinGameObjectInspector : Editor
 {
-    private readonly BoxBoundsHandle _boxBoundsHandle = new();
-    
+    private readonly MapVeinGameObjectEditorService _editorService = new();
+
     private void OnSceneGUI()
     {
         var mapVein = target as MapVeinGameObject;
-        if (mapVein == null)
-        {
-            return;
-        }
-        
-        EditorGUI.BeginChangeCheck();
-        
-        _boxBoundsHandle.center = mapVein.Bounds.center + mapVein.transform.position;
-        _boxBoundsHandle.size = mapVein.Bounds.size;
-        
-        _boxBoundsHandle.SetColor(Color.red);
-        _boxBoundsHandle.DrawHandle();
-        
-        if (EditorGUI.EndChangeCheck())
-        {
-            
-            var bounds = new Bounds(_boxBoundsHandle.center, _boxBoundsHandle.size);
-            mapVein.SetBounds(bounds);
-            Undo.RecordObject(mapVein, "Change Bounds");
-            EditorUtility.SetDirty(mapVein);
-        }
+        if (mapVein == null) return;
+
+        _editorService.DrawSceneGUI(mapVein.Service, mapVein.Bounds, mapVein.SetBounds, mapVein, Color.red);
     }
 }
