2e2506616174e805382444e4acded1cff8fbd2b2
Juha <kukiyomenaisapp@gmail.com>
2026-05-13 23:13:10 +0900

    0.0.6

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/05/02 19:14:19";
+    private const string dummyText = "2026/05/13 22:22:41";
 }
