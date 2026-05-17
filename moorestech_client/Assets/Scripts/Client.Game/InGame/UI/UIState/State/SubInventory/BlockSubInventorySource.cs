de5de80933f550a9998808badc0fc1a900248e5b
sakastudio <sakastudio100@gmail.com>
2026-05-17 15:57:09 +0900

    とりあえずインベントリ開いてエラーが出ないように

--- moorestech_client/Assets/Scripts/Client.Game/InGame/UI/UIState/State/SubInventory/BlockSubInventorySource.cs
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/UI/UIState/State/SubInventory/BlockSubInventorySource.cs
@@ -1,7 +1,9 @@
+using System.Collections.Generic;
 using Client.Game.InGame.Block;
 using Client.Game.InGame.UI.Inventory;
 using Client.Game.InGame.UI.Inventory.Block;
 using Client.Network.API;
+using Core.Item.Interface;
 using Server.Protocol.PacketResponse;
 using Server.Util.MessagePack;
 using UnityEngine;
@@ -23,13 +25,15 @@ namespace Client.Game.InGame.UI.UIState.State.SubInventory
         
         public void ExecuteInitialize(ISubInventoryView subInventoryView, InventoryResponse inventoryResponse)
         {
+            ((IBlockInventoryView)subInventoryView).Initialize(_blockGameObject);
+            
             if (inventoryResponse.Result != InventoryRequestResult.Success)
             {
-                Debug.LogError($"BlockSubInventorySource: inventory request failed. Result:{inventoryResponse.Result} Position:{InventoryIdentifier.BlockPosition.Vector3Int}");
+                subInventoryView.UpdateItemList(new List<IItemStack>());
+                Debug.Log($"ブロックインベントリの取得に失敗しました。結果:{inventoryResponse.Result} 位置:{InventoryIdentifier.BlockPosition.Vector3Int}");
                 return;
             }
 
-            ((IBlockInventoryView)subInventoryView).Initialize(_blockGameObject);
             subInventoryView.UpdateItemList(inventoryResponse.Items);
         }
     }
