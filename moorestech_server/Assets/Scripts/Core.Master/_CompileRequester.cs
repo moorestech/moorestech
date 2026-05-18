68ed21540833432bc4d3656dc33aaf8b3ce914fe
sakastudio <sakastudio100@gmail.com>
2026-05-18 19:01:31 +0900

    レール消費プロパティを railLengthPerItem に改名
    consumptionPerUnitLength（消費/単位長）から railLengthPerItem（1アイテムあたりの敷設長）に意味と計算を反転。CalculateRailItemRequiredCount は railLength を railLengthPerItem で除算する形に修正。

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/05/15 14:36:42";
+    private const string dummyText = "2026/05/18 18:59:27";
 }
