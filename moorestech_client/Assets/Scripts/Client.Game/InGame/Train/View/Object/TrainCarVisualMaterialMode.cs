1464d5b7eae47025d54b790014a13e25cf25106a
toropippi <efghipippi@gmail.com>
2026-06-08 03:44:35 +0900

    mode

---
+++ moorestech_client/Assets/Scripts/Client.Game/InGame/Train/View/Object/TrainCarVisualMaterialMode.cs
@@ -0,0 +1,11 @@
+namespace Client.Game.InGame.Train.View.Object
+{
+    public enum TrainCarVisualMaterialMode
+    {
+        Normal,
+        PlacementPreviewPlaceable,
+        PlacementPreviewNotPlaceable,
+        PlacementOverlapHighlight,
+        RemovePreview,
+    }
+}
