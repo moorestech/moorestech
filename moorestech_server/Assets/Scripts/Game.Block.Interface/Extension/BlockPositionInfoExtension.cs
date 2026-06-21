90ee785da958d00bb4f7395749ddf7729457ef45
toropippi <efghipippi@gmail.com>
2026-06-21 20:13:51 +0900

    座標購読イベントを辞書dispatchに変更

--- moorestech_server/Assets/Scripts/Game.Block.Interface/Extension/BlockPositionInfoExtension.cs
+++ moorestech_server/Assets/Scripts/Game.Block.Interface/Extension/BlockPositionInfoExtension.cs
@@ -5,13 +5,6 @@ namespace Game.Block.Interface.Extension
 {
     public static class BlockPositionInfoExtension
     {
-        public static bool IsContainPos(this BlockPositionInfo self, Vector3Int pos)
-        {
-            return self.MinPos.x <= pos.x && pos.x <= self.MaxPos.x &&
-                   self.MinPos.y <= pos.y && pos.y <= self.MaxPos.y &&
-                   self.MinPos.z <= pos.z && pos.z <= self.MaxPos.z;
-        }
-        
         public static bool IsOverlap(this BlockPositionInfo self, BlockPositionInfo other)
         {
             return self.MinPos.x <= other.MaxPos.x && other.MinPos.x <= self.MaxPos.x &&
