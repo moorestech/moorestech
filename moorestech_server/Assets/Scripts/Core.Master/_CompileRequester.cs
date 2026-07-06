54016d022b3d270ce88f973c1f19266f48bf7341
sakastudio <sakastudio100@gmail.com>
2026-07-06 16:01:00 +0900

    chore: マージ後のSourceGenerator再生成に伴うdummyText更新

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/07/06 11:29:18";
+    private const string dummyText = "2026/07/06 15:58:56";
 }
