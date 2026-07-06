28762860515a715de786f08d4b18254a421c4851
sakastudio <sakastudio100@gmail.com>
2026-07-06 11:30:16 +0900

    chore: SchemaWatcher自動更新ファイルをコミット

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/07/06 00:00:01";
+    private const string dummyText = "2026/07/06 11:29:18";
 }
