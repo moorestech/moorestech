fb6517f7df388e120191c68f0c5531873ae199e7
sakastudio <sakastudio100@gmail.com>
2026-07-22 00:54:05 +0900

    feat(master): blockCategoriesマスタスキーマを新設

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマを更新したら、こちらの更新もコミットしてください。
 // If you update the schema, please also commit this update.
-    private const string dummyText = "2026/07/19 23:21:42";
+    private const string dummyText = "2026/07/22 00:52:18";
 }
