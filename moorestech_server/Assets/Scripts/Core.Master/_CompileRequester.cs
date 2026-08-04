89c1b153e37a4887cc039feee1a52522a45d2c19
sakastudio <sakastudio100@gmail.com>
2026-08-05 02:23:06 +0900

    chore: 外部リポジトリ参照ピンの更新と孤立metaファイルの削除
    - .moorestech-external-revisions.json をmoorestech_master/client_privateの最新コミットへ更新
    - 既にconfig本体が削除済みで孤立していたconfig.metaを削除
    - SchemaWatcher再コンパイル印を更新

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -1,9 +1,9 @@
 
-// SchemaWatcher再コンパイル印
-// SchemaWatcher recompile marker
+// SchemaWatcher更新用の再compile印
+// Recompile marker updated by SchemaWatcher
 public class CompileRequester
 {
-// スキーマ更新時もコミット
-// Commit with schema updates
-    private const string dummyText = "2026/08/03 22:35:00";
+// スキーマ更新時はこの印もcommit
+// Commit this marker with schema changes
+    private const string dummyText = "1F-3C-40-88-36-FD-DD-DE-C3-E2-0C-65-74-2A-77-49";
 }
