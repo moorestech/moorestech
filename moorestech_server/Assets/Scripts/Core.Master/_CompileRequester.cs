7dce723b906607fd32ce5f082f653c03f49b442a
sakastudio <55620461+sakastudio@users.noreply.github.com>
2026-08-18 14:11:12 +0900

    feat: チャレンジスキーマにcompleteResearchタスクとuiDragGuideチュートリアルを追加する

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマ更新時はこの印もcommit
 // Commit this marker with schema changes
-    private const string dummyText = "EB-CD-5F-AA-CB-EB-03-7B-67-6B-FE-B5-5B-20-28-81";
+    private const string dummyText = "23-DE-01-35-21-49-C8-42-A1-CA-45-02-09-7B-91-E0";
 }
