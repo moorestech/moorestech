f8fcec298fcaa16b738a22d77e8a9480e11d5795
sakastudio <sakastudio100@gmail.com>
2026-08-20 20:39:21 +0900

    chore: スキーマ再生成マーカー更新

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマ更新時はこの印もcommit
 // Commit this marker with schema changes
-    private const string dummyText = "7E-31-4C-9A-21-B0-4D-8E-A3-55-6F-1C-2D-90-EE-42";
+    private const string dummyText = "7A-22-8D-94-DE-0C-EA-E2-BF-39-18-D6-6D-7B-58-35";
 }
