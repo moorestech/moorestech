ff43b3986667ec355fb9f1deb4db433057f5945b
sakastudio <55620461+sakastudio@users.noreply.github.com>
2026-08-30 05:30:50 +0900

    feat: mapObjectのminingTypeにNone（装飾物）を追加する

--- moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
+++ moorestech_server/Assets/Scripts/Core.Master/_CompileRequester.cs
@@ -5,5 +5,5 @@ public class CompileRequester
 {
 // スキーマ更新時はこの印もcommit
 // Commit this marker with schema changes
-    private const string dummyText = "77-1A-A2-E0-21-A3-7F-E6-3B-F7-F9-52-07-04-FF-32";
+    private const string dummyText = "2026-08-30-map-mining-type-none";
 }
